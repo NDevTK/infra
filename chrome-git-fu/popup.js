bd1fee39c7012fa37a8a22aa4aa1c6123d2eda68
Stefan Zager <szager@chromium.org>
2014-04-24 15:57:27 -0700

    Display icon in URL bar for affected pages.
    Switched to a page action; also, immediately updated pages when
    the tracking branch is changed.
    
    Change-Id: I5b5bb01e6928a72a0a9f6e55f25c9fcbe55db4c3

--- chrome-git-fu/popup.js
+++ chrome-git-fu/popup.js
@@ -1,21 +1,34 @@
+var tracking_branch = null;
+
 function load_prefs () {
   console.log("load_prefs");
   chrome.storage.local.get("tracking_branch", function(tb) {
+    tracking_branch = tb["tracking_branch"];
     var tb_input = document.getElementById("tracking_branch");
+    if (tb && tb_input && tracking_branch) {
+      tb_input.value = tracking_branch;
+    }
     tb_input.focus();
     tb_input.addEventListener("change", set_tracking_branch);
-    if (tb && tb_input && tb["tracking_branch"]) {
-      tb_input.value = tb["tracking_branch"];
-    }
   });
 }
 
 function set_tracking_branch () {
+  function notify_tabs () {
+    query_params = {'active': true, 'currentWindow': true};
+    chrome.tabs.query(query_params, function (tab_array) {
+      for (var i = 0; i < tab_array.length; i++) {
+	chrome.tabs.sendMessage(tab_array[i].id, 'tracking_branch_changed');
+      }
+    });
+  }
   console.log("set_tracking_branch");
   var tb_input = document.getElementById("tracking_branch");
-  var tb = tb_input.value;
-  console.log("tb=" + tb);
-  chrome.storage.local.set({"tracking_branch": tb});
+  if (tracking_branch != tb_input.value) {
+    tracking_branch = tb_input.value;
+    chrome.storage.local.set(
+	{"tracking_branch": tracking_branch}, notify_tabs);
+  }
 }
 
 document.addEventListener("DOMContentLoaded", load_prefs);
