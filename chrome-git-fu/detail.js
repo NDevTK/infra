0b53a93390406a52613cf02567a4b045b6ad9577
Stefan Zager <szager@chromium.org>
2014-04-24 15:57:27 -0700

    Display icon in URL bar for affected pages.
    Switched to a page action; also, immediately updated pages when
    the tracking branch is changed.
    
    Change-Id: I5b5bb01e6928a72a0a9f6e55f25c9fcbe55db4c3

--- chrome-git-fu/detail.js
+++ chrome-git-fu/detail.js
@@ -8,12 +8,13 @@
   var sha1_re = /[a-f0-9]{40}/i;
   var commit_url_re = new RegExp(
       "^(https://.*\.googlesource\.com/[^+]*)/[+]/[a-f0-9]{40}$", "i");
+  var json_prefix_re = new RegExp("^\)\]}'");
 
   function gitiles_log_load () {
     if (this.status != 200) {
       return;
     }
-    var json_str = this.responseText.replace(")]}'", "");
+    var json_str = this.responseText.replace(json_prefix_re, "");
     var j = JSON.parse(json_str);
     var cls = "contains_loading";
     if (j && j["log"].length > 0) {
@@ -35,7 +36,7 @@
     var new_text = document.createTextNode(" (" + branch + ")");
     new_span.appendChild(new_text);
     var log_url = gitiles_url + "/+log/" + branch + ".." + sha1
-	+ "?format=JSON&n=1";
+        + "?format=JSON&n=1";
     var req = new XMLHttpRequest();
     req.target_span = new_span;
     req.onload = gitiles_log_load;
@@ -56,8 +57,8 @@
       if (n.nodeType == Node.ELEMENT_NODE && n.tagName == "A") {
         var match = (n.getAttribute("href") || "").match(commit_url_re);
         if (match) {
-  	  gitiles_url = match[1];
-  	  break;
+          gitiles_url = match[1];
+          break;
         }
       }
     }
@@ -65,6 +66,11 @@
     // Second pass: Find/annotate sha1's.
     for (var j = 0; j < node.childNodes.length; j++) {
       var n = node.childNodes[j];
+      // Discard previous annotations.
+      if (n.nodeType == Node.ELEMENT_NODE && n.tagName == "SPAN" &&
+          (n.getAttribute("class") || "").substr(0, 9) == "contains_") {
+        continue;
+      }
       if (n.nodeType != Node.TEXT_NODE) {
         new_nodes.push(n);
         continue;
@@ -75,20 +81,25 @@
         var sha1_str = n.wholeText.substr(match_pos, 40);
         modified = true;
         var new_text = document.createTextNode(
-  	  n.wholeText.substr(start_pos, match_pos - start_pos));
+            n.wholeText.substr(start_pos, match_pos - start_pos + 40));
         new_nodes.push(new_text);
-        var new_span = document.createElement('span');
-        new_span.setAttribute('class', 'sha1');
-        new_text = document.createTextNode(sha1_str);
-        new_span.appendChild(new_text);
-	new_nodes.push(new_span);
-	if (gitiles_url) {
-	  new_nodes.push(branch_contains(gitiles_url, sha1_str, "master"));
-	  if (tb) {
-	    new_nodes.push(
-		branch_contains(gitiles_url, sha1_str, "branch-heads/" + tb));
-	  }
-	}
+        if (gitiles_url) {
+          // If this is a re-annotation after tracking_branch changed, don't
+          // recompute information for "master".
+          if (match_pos + 40 == n.wholeText.length &&
+              n.nextSibling && n.nextSibling.nodeType == Node.ELEMENT_NODE &&
+              n.nextSibling.tagName == "SPAN" &&
+              (n.nextSibling.getAttribute("class") || "").substr(0, 9) == "contains_" &&
+              n.nextSibling.firstChild.wholeText == " (master)") {
+            new_nodes.push(n.nextSibling);
+          } else {
+            new_nodes.push(branch_contains(gitiles_url, sha1_str, "master"));
+          }
+          if (tb) {
+            new_nodes.push(
+                branch_contains(gitiles_url, sha1_str, "branch-heads/" + tb));
+          }
+        }
         start_pos = match_pos + 40;
         match_pos = n.wholeText.substr(start_pos).search(sha1_re);
       }
@@ -97,7 +108,7 @@
         new_nodes.push(n);
       } else if (start_pos < n.wholeText.length) {
         new_text = document.createTextNode(
-  	  n.wholeText.substr(start_pos));
+            n.wholeText.substr(start_pos));
         new_nodes.push(new_text);
       }
     }
@@ -112,15 +123,27 @@
     }
   }
 
-  chrome.storage.local.get("tracking_branch", function(tb) {
-    var tracking_branch = tb["tracking_branch"];
-    var comments = document.getElementsByClassName('cursor_off vt issuecomment');
-    for (var i = 0; i < comments.length; i++) {
-      var el = comments.item(i).getElementsByTagName('pre');
-      if (el.length != 1) {
-        continue;
+  chrome.runtime.sendMessage(null, "show");
+
+  function annotate_page () {
+    chrome.storage.local.get("tracking_branch", function(tb) {
+      var tracking_branch = tb["tracking_branch"];
+      var comments = document.getElementsByClassName('cursor_off vt issuecomment');
+      for (var i = 0; i < comments.length; i++) {
+        var el = comments.item(i).getElementsByTagName('pre');
+        if (el.length != 1) {
+          continue;
+        }
+        annotate_shas(el[0], tracking_branch);
       }
-      annotate_shas(el[0], tracking_branch);
+    });
+  }
+
+  chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message == 'tracking_branch_changed') {
+      setTimeout(function() { annotate_page() }, 0);
     }
   });
+
+  annotate_page();
 })();
