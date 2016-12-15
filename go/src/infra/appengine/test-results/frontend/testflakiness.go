c5cbe9fa17557c8b6f6120e8dbd25d1cbacd8764
Sergiy Byelozyorov <sergiyb@chromium.org>
2016-12-15 11:09:27 +0100

    Merge AppEngine context with LUCI context instead of creating a new one
    This allows to use BigQuery from native AE package and logging from LUCI wrapper
    package at the same time.
    
    Change-Id: Iec34bf3494a3f89108f9a486a8023f8268ae26af

--- go/src/infra/appengine/test-results/frontend/testflakiness.go
+++ go/src/infra/appengine/test-results/frontend/testflakiness.go
@@ -418,7 +418,7 @@ func testFlakinessGroupsHandler(ctx *router.Context) {
 		}
 	}
 
-	aeCtx := appengine.NewContext(ctx.Request)
+	aeCtx := appengine.WithContext(ctx.Context, ctx.Request)
 	bq, err := createBQService(aeCtx)
 	if err != nil {
 		writeError(ctx, err, "testFlakinessGroupsHandler", "failed create BigQuery client")
