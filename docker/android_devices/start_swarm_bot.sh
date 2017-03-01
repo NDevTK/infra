7718c80851edcefe9137587bba48ce29d9b5a270
Benjamin Pastene <bpastene@google.com>
2017-02-28 17:21:29 -0800

    android-docker: Fetch depot_tools on container start-up.
    BUG=670879
    
    Change-Id: Ia198036d46aceff9d0ce432678838744aa5683fb

--- docker/android_devices/start_swarm_bot.sh
+++ docker/android_devices/start_swarm_bot.sh
@@ -4,12 +4,25 @@ SWARM_DIR=/b/swarming
 SWARM_URL="https://chromium-swarm.appspot.com/bot_code"
 SWARM_ZIP=swarming_bot.zip
 
+DEPOT_TOOLS_DIR=/b/depot_tools
+DEPOT_TOOLS_URL="https://chromium.googlesource.com/chromium/tools/depot_tools.git"
+DEPOT_TOOLS_REV="da3a29e13e816459234b0b08ed1059300bae46dd"
+
 # Wait until this container has access to a device before starting the bot.
 while [ ! -d /dev/bus/usb ]
 do
   echo "Waiting for an available usb device..."
   sleep 10
 done
+
+# Some chromium tests need depot tools.
+mkdir -p $DEPOT_TOOLS_DIR
+/bin/chown chrome-bot:chrome-bot $DEPOT_TOOLS_DIR
+/bin/su -c "cd $DEPOT_TOOLS_DIR && \
+            /usr/bin/git init && \
+            /usr/bin/git remote add origin $DEPOT_TOOLS_URL && \
+            /usr/bin/git fetch origin $DEPOT_TOOLS_REV && \
+            /usr/bin/git reset --hard FETCH_HEAD"
 
 mkdir -p $SWARM_DIR
 /bin/chown chrome-bot:chrome-bot $SWARM_DIR
