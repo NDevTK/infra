9e4a3a23d31e59b94466a065a28b6efeae6b3caa
Sharu Jiang <katesonia@google.com>
2017-03-24 16:43:41 -0700

    [Predator] Add script to crawl data from datastore and dump it locally.
    Change-Id: Ie00f94a5c92e9d6923973bedc269a18e17cf9fbf

--- appengine/findit/util_scripts/script_util.py
+++ appengine/findit/util_scripts/script_util.py
@@ -5,6 +5,7 @@
 """This module contains util functions that local scripts can use."""
 
 import atexit
+from datetime import datetime
 import functools
 import os
 import Queue
@@ -154,3 +155,22 @@ def GetLockedMethod(cls, method_name, lock):  # pragma: no cover
       return method(*args, **kwargs)
 
   return functools.partial(LockedMethod, cls)
+
+
+# TODO(katesonia): Move this to gae_libs.
+# TODO(crbug.com/662540): Add unittests.
+def GetFilterQuery(query, time_property, start_date, end_date,
+                   property_values=None,
+                   datetime_pattern='%Y-%m-%d'):  # pragma: no cover.
+  start_date = datetime.strptime(start_date, datetime_pattern)
+  end_date = datetime.strptime(end_date, datetime_pattern)
+
+  if property_values:
+    for cls_property, value in property_values.iteritems():
+      if isinstance(value, list):
+        query = query.filter(cls_property.IN(value))
+      else:
+        query = query.filter(cls_property == value)
+
+  return query.filter(time_property >= start_date).filter(
+      time_property < end_date)
