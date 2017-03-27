45d2f2a80b6ed4fe3cefcf013c00e1bacba6bd9a
Roberto Carrillo <robertocn@google.com>
2017-03-27 16:02:33 -0700

    [Findit] Implement PostMessage for Gerrit codereview.
    R=stgao,chanli
    BUG=703327
    
    Change-Id: Id005a76b52f10dc1af2a97de87f78a28b8b614fb

--- appengine/findit/infra_api_clients/codereview/cl_info.py
+++ appengine/findit/infra_api_clients/codereview/cl_info.py
@@ -70,8 +70,12 @@ class Revert(object):  # pragma: no cover
 
 class ClInfo(object):  # pragma: no cover
 
-  def __init__(self, review_url):
-    self.url = review_url
+  def __init__(self, server_hostname, change_id):
+    # The host name for the code review site this cl is on.
+    self.server_hostname = server_hostname
+    # The number or string that uniquely identifies this change in the given
+    # code review site.
+    self.change_id = change_id
     # A map of patchset_id to CommitAttempt objects.
     self.commit_attempts = {}
     # A list of Commit objects.
@@ -97,7 +101,8 @@ class ClInfo(object):  # pragma: no cover
 
   def serialize(self):
     return {
-        'url': self.url,
+        'server_hostname': self.server_hostname,
+        'change_id': self.change_id,
         'commits': [x.serialize() for x in self.commits],
         'commit_attempts': [x.serialize() for x in
                             self.commit_attempts.values()],
