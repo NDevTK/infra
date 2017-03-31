ea14de10fcbec0ec6bff4e7495a60e8da8dce83d
Sharu Jiang <katesonia@google.com>
2017-03-30 18:57:31 -0700

    [Culprit-Finder] Move pubsub_util.py and pipeline_wrapper.py to gae_libs/
    Bug: 705860
    Change-Id: I66d3ac0112c25afe019cc1599d5245aed030035b

--- appengine/findit/waterfall/test/build_failure_analysis_pipelines_test.py
+++ appengine/findit/waterfall/test/build_failure_analysis_pipelines_test.py
@@ -7,9 +7,9 @@ import mock
 from testing_utils import testing
 
 from common import constants
-from common.pipeline_wrapper import pipeline_handlers
 from model import analysis_status
 from model.wf_analysis import WfAnalysis
+from gae_libs.pipeline_wrapper import pipeline_handlers
 from waterfall import build_failure_analysis_pipelines
 
 
