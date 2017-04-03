8e7e737ae103e9b6d2e043544f853a00ae346429
Sharu Jiang <katesonia@google.com>
2017-04-03 14:06:23 -0700

    [Predator] Refactor delta test script.
    Make ``run-predator.py`` a stand-alone script that can run on a single crash through its key.
    
    Use LocalCache cache decorator in several place to make code cleaner.
    
    Change-Id: I2922d2bda89e46cfd10c427d3c5738e898dd6262

--- appengine/findit/util_scripts/crash_queries/delta_test/delta_util.py
+++ appengine/findit/util_scripts/crash_queries/delta_test/delta_util.py
@@ -6,38 +6,40 @@
 
 import base64
 import hashlib
-import json
 import logging
 import os
 import pickle
 import re
 import subprocess
+import sys
 import traceback
 import urllib2
 
-import dev_appserver
-dev_appserver.fix_sys_path()
+_SCRIPT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                           os.path.pardir)
+sys.path.insert(1, _SCRIPT_DIR)
+import script_util
+script_util.SetUpSystemPaths()
 
 from gae_libs import appengine_util
 
+# TODO(crbug.com/662540): Add unittests.
+
 # TODO(katesonia): move host to predator host after migration.
-_FRACAS_FEEDBACK_URL_TEMPLATE = (
-    'https://%s.appspot.com/crash/fracas-result-feedback?key=%s')
+_FEEDBACK_URL_TEMPLATE = (
+    'https://%s.appspot.com/crash/%s-result-feedback?key=%s')
 GIT_HASH_PATTERN = re.compile(r'^[0-9a-fA-F]{40}$')
 
 
-# TODO(crbug.com/662540): Add unittests.
 def GenerateFileName(*args):  # pragma: no cover
   """Encodes args and returns the generated result file."""
   return hashlib.md5(pickle.dumps(args)).hexdigest()
 
 
-# TODO(crbug.com/662540): Add unittests.
 def IsGitHash(revision):  # pragma: no cover
   return GIT_HASH_PATTERN.match(str(revision)) or revision.lower() == 'master'
 
 
-# TODO(crbug.com/662540): Add unittests.
 def ParseGitHash(revision):  # pragma: no cover
   """Gets git hash of a revision."""
   if IsGitHash(revision):
@@ -53,27 +55,8 @@ def ParseGitHash(revision):  # pragma: no cover
     return None
 
 
-# TODO(crbug.com/662540): Add unittests.
-def EnsureDirExists(path):  # pragma: no cover
-  directory = os.path.dirname(path)
-  # TODO: this has a race condition. Should ``try: os.makedirs`` instead,
-  # discarding the error and returning if the directory already exists.
-  if os.path.exists(directory):
-    return
-
-  os.makedirs(directory)
-
-
-# TODO(crbug.com/662540): Add unittests.
-def FlushResult(result, result_path, serializer=pickle):  # pragma: no cover
-  print '\nFlushing results to', result_path
-  EnsureDirExists(result_path)
-  with open(result_path, 'wb') as f:
-    serializer.dump(result, f)
-
-
-# TODO(crbug.com/662540): Add unittests.
-def PrintDelta(deltas, crash_num, app_id):  # pragma: no cover
+def PrintDelta(deltas, crash_num, client_id, app_id):  # pragma: no cover
+  """Prints delta in a pretty format for a list of deltas."""
   print ('\n+++++++++++++++++++++'
          '\nDelta on %d crashes '
          '\n+++++++++++++++++++++') % crash_num
@@ -84,28 +67,103 @@ def PrintDelta(deltas, crash_num, app_id):  # pragma: no cover
 
   for crash_id, delta in deltas.iteritems():
     print '\nCrash: %s\n%s\n' % (
-        _FRACAS_FEEDBACK_URL_TEMPLATE % (app_id, crash_id), str(delta))
+        _FEEDBACK_URL_TEMPLATE % (app_id, client_id, crash_id), str(delta))
 
 
-# TODO(crbug.com/662540): Add unittests.
-def WriteDeltaToCSV(deltas, crash_num, app_id,
-                    git_hash1, git_hash2, file_path):  # pragma: no cover
-  EnsureDirExists(file_path)
-  def _EncodeStr(string):
-    return string.replace('\"', '\'') if string else ''
+def _EncodeStrForCSV(string):  # pragma: no cover.
+  """Encodes strings for printing in csv.
 
+  In Excel/Google Sheet, to print newlines in CSV, '\n' has to be wrapped in
+  double quotes.
+  """
+  if not string:
+    return ''
+
+  string = str(string) if not isinstance(string, basestring) else string
+  # Because the encoded string will be wrapped in double quotes, replace all
+  # double quotes in string with single quotes.
+  return string.replace('\"', '\'') if string else ''
+
+
+def _PrettifySuspectClsForPrintInCSV(suspect_cls):  # pragma: no cover.
+  suspect_cls_strs = []
+  for index, suspect_cl in enumerate(suspect_cls):
+    suspect_cls_strs.append(
+        '(%d)\n%s\nScore: %.2f\nReason:\n%s\n' % (
+            index + 1, suspect_cl.get('url'), suspect_cl.get('confidence'),
+            suspect_cl.get('reasons')))
+
+  return '\n'.join(suspect_cls_strs)
+
+
+def _GetDeltaStringsForCSV(delta_dict):  # pragma: no cover.
+  # The project delta.
+  if 'suspected_project' in delta_dict:
+    project1, project2 = delta_dict['suspected_project']
+    projects = 'Project 1:\n%s\nProject 2:\n%s' % (project1 or '',
+                                                   project2 or '')
+  else:
+    projects = ''
+
+  # Supected cls delta.
+  if 'suspected_cls' in delta_dict:
+    suspect_cl_1, suspect_cl_2 = delta_dict['suspected_cls']
+    suspect_cl_1 = _PrettifySuspectClsForPrintInCSV(suspect_cl_1 or [])
+    suspect_cl_2 = _PrettifySuspectClsForPrintInCSV(suspect_cl_2 or [])
+  else:
+    suspect_cl_1 = ''
+    suspect_cl_2 = ''
+
+  # The components delta.
+  if 'suspected_components' in delta_dict:
+    components1, components2 = delta_dict['suspected_components']
+    components = 'Components 1:\n%s\nComponents 2:\n%s' % (components1 or '',
+                                                           components2 or '')
+  else:
+    components = ''
+
+  # The regression range delta.
+  if 'regression_range' in delta_dict:
+    regression_range1, regression_range2 = delta_dict['regression_range']
+    regression_ranges = (
+        'Regression_range 1:\n%s\nRegression range 2:\n%s' % (
+            regression_range1 or '', regression_range2 or ''))
+  else:
+    regression_ranges = ''
+
+  return {'projects': _EncodeStrForCSV(projects),
+          'suspect_cl_1': _EncodeStrForCSV(suspect_cl_1),
+          'suspect_cl_2': _EncodeStrForCSV(suspect_cl_2),
+          'components': _EncodeStrForCSV(components),
+          'regression_ranges': _EncodeStrForCSV(regression_ranges)}
+
+
+def WriteDeltaToCSV(deltas, crash_num, client_id, app_id,
+                    git_hash1, git_hash2, file_path,
+                    triage_results=None):  # pragma: no cover
+  script_util.EnsureDirExists(file_path)
+  triage_results = triage_results or {}
   with open(file_path, 'wb') as f:
     f.write('Delta between githash1 %s and githash2 %s on %d crashes\n\n' % (
         git_hash1, git_hash2, crash_num))
-    f.write('crash url, project, components, cl 1, cl 2, regression_range\n')
+    f.write('crash url, project, cl 1, cl 2, culprit_cl, components, '
+            'culprit_components, regression_range, culprit_regression_range\n')
     for crash_id, delta in deltas.iteritems():
-      delta_dict_str = delta.delta_dict_str
-      feedback_url = _FRACAS_FEEDBACK_URL_TEMPLATE % (app_id, crash_id)
-      f.write('%s, "%s", "%s", "%s", "%s", "%s"\n' % (
-          feedback_url,
-          _EncodeStr(delta_dict_str.get('suspected_project', '')),
-          _EncodeStr(delta_dict_str.get('suspected_components', '')),
-          _EncodeStr(delta_dict_str.get('suspected_cl_1', '')),
-          _EncodeStr(delta_dict_str.get('suspected_cl_2', '')),
-          _EncodeStr(delta_dict_str.get('regression_range', ''))
-      ))
+      # The crash_url.
+      feedback_url = _FEEDBACK_URL_TEMPLATE % (app_id, client_id, crash_id)
+      deltas = _GetDeltaStringsForCSV(delta.delta_dict)
+
+      triage_result = triage_results.get(crash_id)
+      culprit_cl = '\n'.join(triage_result.get(
+          'culprit_cls', [])) if triage_result else ''
+      culprit_components = triage_result.get(
+          'culprit_components', '') if triage_result else ''
+      culprit_regression_range = triage_result.get(
+          'culprit_regression_range', '') if triage_result else '',
+
+      f.write('%s, "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s"\n' % (
+          feedback_url, deltas['projects'], deltas['suspect_cl_1'],
+          deltas['suspect_cl_2'], _EncodeStrForCSV(culprit_cl),
+          deltas['components'], _EncodeStrForCSV(culprit_components),
+          deltas['regression_ranges'],
+          _EncodeStrForCSV(culprit_regression_range)))
