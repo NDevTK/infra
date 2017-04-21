fcbd2dfe0125994494df0792d6575dcbaca45c3b
Dan Jacques <dnj@chromium.org>
2017-04-21 07:35:33 -0700

    vpython: CIPD homedir, PEP425 update.
    Use a default CIPD cache directory if one is not provided.
    
    Update PEP425 logic a bit, and add some documentation. Add support for
    OS-specific tag preference, and implement a Mac tag preference
    function.
    
    BUG=None
    TEST=local
    
    Change-Id: Ib7e507338be0b03526eaa742e41938d8445b7ac0

--- go/src/infra/tools/vpython/pep425.go
+++ go/src/infra/tools/vpython/pep425.go
@@ -5,18 +5,117 @@
 package main
 
 import (
+	"strconv"
 	"strings"
-
-	"golang.org/x/net/context"
 
 	"github.com/luci/luci-go/vpython/api/vpython"
 )
 
+type pep425MacArch struct {
+	major int
+	minor int
+	arch  string
+}
+
+func parsePEP425MacArch(v string) (a pep425MacArch, is bool) {
+	parts := strings.SplitN(v, "_", 4)
+	if len(parts) != 4 {
+		return
+	}
+	if parts[0] != "macosx" {
+		return
+	}
+
+	var err error
+	if a.major, err = strconv.Atoi(parts[1]); err != nil {
+		return
+	}
+	if a.minor, err = strconv.Atoi(parts[2]); err != nil {
+		return
+	}
+
+	a.arch = parts[3]
+	is = true
+	return
+}
+
+func (a *pep425MacArch) less(other pep425MacArch) bool {
+	switch {
+	case a.major < other.major:
+		return true
+	case a.minor < other.minor:
+		return true
+	default:
+		return false
+	}
+}
+
+func pep425IsBetterMacArch(cur, other string) bool {
+	// Parse a Mac architecture string
+	curArch, curIs := parsePEP425MacArch(cur)
+	otherArch, otherIs := parsePEP425MacArch(other)
+	switch {
+	case !curIs:
+		return otherIs
+	case !otherIs:
+		return false
+	case curArch.arch != "intel" && otherArch.arch == "intel":
+		// Prefer "intel" architecture over others, since it's more modern and
+		// generic.
+		return true
+	case curArch.arch == "intel" && otherArch.arch != "intel":
+		return false
+	case otherArch.less(curArch):
+		// We prefer the lowest Mac architecture available.
+		return true
+	default:
+		return false
+	}
+}
+
+func pep425IsBetterLinuxArch(cur, other string) bool {
+	// Determies if the specified architecture is a Linux architecture and, if so,
+	// is it a "manylinux1_" Linux architecture.
+	isLinuxArch := func(arch string) (is bool, many bool) {
+		switch {
+		case strings.HasPrefix(arch, "linux_"):
+			is = true
+		case strings.HasPrefix(arch, "manylinux1_"):
+			is, many = true, true
+		}
+		return
+	}
+
+	// We prefer "manylinux1_" architectures over "linux_" architectures.
+	curIs, curMany := isLinuxArch(cur)
+	otherIs, otherMany := isLinuxArch(other)
+	switch {
+	case !curIs:
+		return otherIs
+	case !otherIs:
+		return false
+	case curMany:
+		return false
+	default:
+		return otherMany
+	}
+}
+
 // pep425TagSelector chooses the "best" PEP425 tag from a set of potential tags.
 // This "best" tag will be used to resolve our CIPD templates and allow for
 // Python implementation-specific CIPD template parameters.
-func pep425TagSelector(tags []*vpython.Environment_Pep425Tag) *vpython.Environment_Pep425Tag {
+func pep425TagSelector(goOS string, tags []*vpython.Environment_Pep425Tag) *vpython.Environment_Pep425Tag {
 	var best *vpython.Environment_Pep425Tag
+
+	// isPreferredOSArch is an OS-specific architecture preference function.
+	isPreferredOSArch := func(best, candidate string) bool { return false }
+	switch goOS {
+	case "linux":
+		isPreferredOSArch = pep425IsBetterLinuxArch
+	case "darwin":
+		isPreferredOSArch = pep425IsBetterMacArch
+	}
+
 	isBetter := func(t *vpython.Environment_Pep425Tag) bool {
 		switch {
 		case best == nil:
@@ -30,8 +129,7 @@ func pep425TagSelector(tags []*vpython.Environment_Pep425Tag) *vpython.Environme
 		case !best.HasABI() && t.HasABI():
 			// More specific ABI is preferred.
 			return true
-		case strings.HasPrefix(best.Arch, "linux_") && strings.HasPrefix(t.Arch, "manylinux1_"):
-			// Linux: Prefer "manylinux_" to "linux_".
+		case isPreferredOSArch(best.Arch, t.Arch):
 			return true
 		case strings.HasPrefix(best.Version, "py") && !strings.HasPrefix(t.Version, "py"):
 			// Prefer specific Python (e.g., cp27) version over generic (e.g., py27).
@@ -50,7 +148,7 @@ func pep425TagSelector(tags []*vpython.Environment_Pep425Tag) *vpython.Environme
 	return best
 }
 
-// getCIPDTemplatesForEnvironment returns the set of CIPD template strings for a
+// getPEP425CIPDTemplates returns the set of CIPD template strings for a
 // given PEP425 tag.
 //
 // Template parameters are derived from the most representative PEP425 tag.
@@ -62,10 +160,13 @@ func pep425TagSelector(tags []*vpython.Environment_Pep425Tag) *vpython.Environme
 // - py_abi: The PEP425 Python ABI (e.g., "cp27mu").
 // - py_arch: The PEP425 Python architecture (e.g., "manylinux1_x86_64").
 // - py_tag: The full PEP425 tag (e.g., "cp27-cp27mu-manylinux1_x86_64").
-func getCIPDTemplatesForEnvironment(c context.Context, e *vpython.Environment) (map[string]string, error) {
-	tag := pep425TagSelector(e.Pep425Tag)
+//
+// Infra CIPD packages tend to use "${platform}" (generic) combined with
+// "${py_abi}" and "${py_arch}" to identify its packages.
+func getPEP425CIPDTemplates(goOS string, tags []*vpython.Environment_Pep425Tag) map[string]string {
+	tag := pep425TagSelector(goOS, tags)
 	if tag == nil {
-		return nil, nil
+		return nil
 	}
 
 	template := make(map[string]string, 4)
@@ -81,5 +182,5 @@ func getCIPDTemplatesForEnvironment(c context.Context, e *vpython.Environment) (
 	if tag.Version != "" && tag.Abi != "" && tag.Arch != "" {
 		template["py_tag"] = tag.TagString()
 	}
-	return template, nil
+	return template
 }
--- go/src/infra/tools/vpython/pep425_test.go
+++ go/src/infra/tools/vpython/pep425_test.go
@@ -9,8 +9,6 @@ import (
 	"strings"
 	"testing"
 
-	"golang.org/x/net/context"
-
 	"github.com/luci/luci-go/vpython/api/vpython"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -28,20 +26,21 @@ func TestPEP425TagSelector(t *testing.T) {
 	t.Parallel()
 
 	Convey(`Testing PEP425 tag selection`, t, func() {
-		c := context.Background()
-
 		for _, tc := range []struct {
+			goOS     string
 			tags     []*vpython.Environment_Pep425Tag
 			template map[string]string
 		}{
-			{nil, nil},
+			{"linux", nil, nil},
 
 			{
+				"linux",
 				[]*vpython.Environment_Pep425Tag{
 					mkTag("py2", "none", "any"),
 					mkTag("py27", "none", "any"),
 					mkTag("cp27", "cp27mu", "linux_x86_64"),
 					mkTag("cp27", "cp27mu", "manylinux1_x86_64"),
+					mkTag("cp27", "none", "manylinux1_x86_64"),
 				},
 				map[string]string{
 					"py_tag":     "cp27-cp27mu-manylinux1_x86_64",
@@ -52,6 +51,56 @@ func TestPEP425TagSelector(t *testing.T) {
 			},
 
 			{
+				"darwin",
+				[]*vpython.Environment_Pep425Tag{
+					mkTag("cp27", "cp27m", "macosx_10_12_x86_64"),
+					mkTag("cp27", "cp27m", "macosx_10_12_fat64"),
+					mkTag("cp27", "cp27m", "macosx_10_12_fat32"),
+					mkTag("cp27", "cp27m", "macosx_10_12_intel"),
+					mkTag("cp27", "cp27m", "macosx_10_10_intel"),
+					mkTag("cp27", "cp27m", "macosx_10_9_fat64"),
+					mkTag("cp27", "cp27m", "macosx_10_9_fat32"),
+					mkTag("cp27", "cp27m", "macosx_10_9_universal"),
+					mkTag("cp27", "cp27m", "macosx_10_8_fat32"),
+					mkTag("cp27", "cp27m", "macosx_10_8_universal"),
+					mkTag("cp27", "cp27m", "macosx_10_6_intel"),
+					mkTag("cp27", "cp27m", "macosx_10_6_fat64"),
+					mkTag("cp27", "cp27m", "macosx_10_6_fat32"),
+					mkTag("cp27", "cp27m", "macosx_10_6_universal"),
+					mkTag("cp27", "cp27m", "macosx_10_5_universal"),
+					mkTag("cp27", "cp27m", "macosx_10_4_intel"),
+					mkTag("cp27", "cp27m", "macosx_10_4_fat32"),
+					mkTag("cp27", "cp27m", "macosx_10_1_universal"),
+					mkTag("cp27", "cp27m", "macosx_10_0_fat32"),
+					mkTag("cp27", "cp27m", "macosx_10_0_universal"),
+					mkTag("cp27", "none", "macosx_10_12_x86_64"),
+					mkTag("cp27", "none", "macosx_10_12_intel"),
+					mkTag("cp27", "none", "macosx_10_12_fat64"),
+					mkTag("cp27", "none", "macosx_10_9_universal"),
+					mkTag("cp27", "none", "macosx_10_8_x86_64"),
+					mkTag("cp27", "none", "macosx_10_8_intel"),
+					mkTag("cp27", "none", "macosx_10_7_intel"),
+					mkTag("cp27", "none", "macosx_10_7_fat64"),
+					mkTag("cp27", "none", "macosx_10_7_fat32"),
+					mkTag("cp27", "none", "macosx_10_6_universal"),
+					mkTag("cp27", "none", "macosx_10_5_x86_64"),
+					mkTag("cp27", "none", "macosx_10_5_intel"),
+					mkTag("cp27", "none", "macosx_10_3_fat32"),
+					mkTag("cp27", "none", "macosx_10_3_universal"),
+					mkTag("cp27", "none", "macosx_10_2_fat32"),
+					mkTag("py2", "none", "macosx_10_4_intel"),
+					mkTag("cp27", "none", "any"),
+				},
+				map[string]string{
+					"py_tag":     "cp27-cp27m-macosx_10_4_intel",
+					"py_version": "cp27",
+					"py_abi":     "cp27m",
+					"py_arch":    "macosx_10_4_intel",
+				},
+			},
+
+			{
+				"exampleOS",
 				[]*vpython.Environment_Pep425Tag{
 					mkTag("py27", "none", "any"),
 					mkTag("py27", "foo", "bar"),
@@ -65,6 +114,7 @@ func TestPEP425TagSelector(t *testing.T) {
 			},
 
 			{
+				"exampleOS",
 				[]*vpython.Environment_Pep425Tag{
 					mkTag("py27", "none", "any"),
 					mkTag("py27", "none", "linux_386"),
@@ -78,6 +128,7 @@ func TestPEP425TagSelector(t *testing.T) {
 			},
 
 			{
+				"exampleOS",
 				[]*vpython.Environment_Pep425Tag{
 					mkTag("py27", "none", "any"),
 					mkTag("py27", "cp27mu", "any"),
@@ -96,13 +147,7 @@ func TestPEP425TagSelector(t *testing.T) {
 			}
 
 			Convey(fmt.Sprintf(`Generates template for [%s]`, strings.Join(tagsStr, ", ")), func() {
-				env := vpython.Environment{
-					Pep425Tag: tc.tags,
-				}
-
-				template, err := getCIPDTemplatesForEnvironment(c, &env)
-				So(err, ShouldBeNil)
-				So(template, ShouldResemble, tc.template)
+				So(getPEP425CIPDTemplates(tc.goOS, tc.tags), ShouldResemble, tc.template)
 			})
 		}
 	})
