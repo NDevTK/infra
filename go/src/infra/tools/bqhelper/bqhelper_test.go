86b076f73fba971c1eb2090f5d3d7ff0a3e4c5ed
Katie Thomas <katthomas@google.com>
2017-04-27 13:59:29 -0700

    Add bq helper
    BUG=615218
    
    Change-Id: I1d6d76f66d7746a57b86373d38c1b7683c6b736a

---
+++ go/src/infra/tools/bqhelper/bqhelper_test.go
@@ -0,0 +1,25 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package bqhelper
+
+import (
+	"cloud.google.com/go/bigquery"
+	"golang.org/x/net/context"
+)
+
+type tableKey struct {
+	datasetID, tableID string
+}
+
+type localTableStore map[tableKey]*bigquery.TableMetadata
+
+func (ts localTableStore) getTableMetadata(ctx context.Context, datasetID, tableID string) (*bigquery.TableMetadata, error) {
+	key := tableKey{datasetID: datasetID, tableID: tableID}
+	md, ok := bq.tableMetadatas[key]
+	if ok {
+		return md, nil
+	}
+	return nil, &googleapi.Error{Code: http.StatusNotFound}
+}
