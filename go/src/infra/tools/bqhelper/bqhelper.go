f764fc450c7f3abe606fc6d66a454e6b8ffdef4a
Katie Thomas <katthomas@google.com>
2017-04-27 14:03:37 -0700

    Add bq helper
    BUG=615218
    
    Change-Id: I1d6d76f66d7746a57b86373d38c1b7683c6b736a

---
+++ go/src/infra/tools/bqhelper/bqhelper.go
@@ -0,0 +1,40 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package bqhelper is a tool for creating and updating BigQuery tables.
+package bqhelper
+
+import (
+	"net/http"
+
+	"cloud.google.com/go/bigquery"
+	"golang.org/x/net/context"
+	"google.golang.org/api/googleapi"
+)
+
+type tableDef struct {
+	datasetID string
+	tableID   string
+	toUpdate  bigquery.TableMetadataToUpdate
+}
+
+type tableStore interface {
+	getTableMetadata(ctx context.Context, datasetID, tableID string) (*bigquery.TableMetadata, error)
+}
+
+type bqTableStore *bigquery.Client
+
+func errNotFound(e error) bool {
+	err, ok := e.(*googleapi.Error)
+	return ok && err.Code == http.StatusNotFound
+}
+
+func (bq bqTableStore) getTableMetadata(ctx context.Context, datasetID, tableID string) (*bigquery.TableMetadata, error) {
+	t := bq.Dataset(datasetID).Table(tableID)
+	return t.Metadata(ctx)
+}
+
+func updateFromTableDef(ctx context.Context, td tableDef, ts tableStore) {
+	md, err := ts.getTableMetadata(ctx, td.datasetID, td.tableID)
+}
