b2226549fe84180b115f41e0fb8bed90926db71b
Nodir Turakulov <nodir@google.com>
2017-05-20 00:27:43 -0700

    luci-migration: fix UNSET
    https://chromium-review.googlesource.com/c/510086/
    was bad because
    1) it does not include changes to config.pb.go
    2) apparently, enum member names must be unique within proto package
    
    Bug: 719734
    Change-Id: Id06af6f38652676bdf78b320e31ea9390b3d43f6

--- go/src/infra/appengine/luci-migration/config/config.go
+++ go/src/infra/appengine/luci-migration/config/config.go
@@ -7,6 +7,9 @@ import (
 	"github.com/luci/luci-go/luci_config/server/cfgclient/textproto"
 )
 
+// UNSET is a value of an enum field that was not set.
+const UNSET = 0
+
 // Get returns currently imported config.
 func Get(c context.Context) (*Config, error) {
 	var cfg Config
