80145516d1417845bc89791901c73e048714121b
Sharu Jiang <katesonia@google.com>
2017-06-06 13:31:58 -0700

    [Predator] Smooth rerun pipeline.
    Bug:
    Change-Id: I337baa47ee3806f62a72ea7dde9661dbc334898a

--- appengine/predator/backend/handlers/rerun_analysis.py
+++ appengine/predator/backend/handlers/rerun_analysis.py
@@ -6,12 +6,22 @@ from datetime import datetime
 from datetime import timedelta
 import logging
 
+from google.appengine.ext import ndb
+
 from analysis.type_enums import CrashClient
-from common.crash_pipeline import CrashWrapperPipeline
 from common.crash_pipeline import RerunPipeline
+from common.crash_pipeline import FinditForClientID
+from common.model.clusterfuzz_analysis import ClusterfuzzAnalysis
+from common.model.cracas_crash_analysis import CracasCrashAnalysis
+from common.model.crash_analysis import CrashAnalysis
+from common.model.crash_config import CrashConfig
+from common.model.fracas_crash_analysis import FracasCrashAnalysis
 from gae_libs import appengine_util
+from gae_libs.gitiles.cached_gitiles_repository import CachedGitilesRepository
 from gae_libs.handlers.base_handler import BaseHandler
 from gae_libs.handlers.base_handler import Permission
+from gae_libs.http.http_client_appengine import HttpClientAppengine
+from gae_libs.iterator import Iterate
 from libs import time_util
 
 RERUN_SERVICE = 'backend-process'
@@ -19,12 +29,54 @@ RERUN_QUEUE = 'rerun-queue'
 
 DATETIME_FORMAT = '%Y-%m-%d'
 
+CLIENT_ID_TO_CRASH_ANALYSIS = {
+    CrashClient.FRACAS: FracasCrashAnalysis,
+    CrashClient.CRACAS: CracasCrashAnalysis,
+    CrashClient.CLUSTERFUZZ: ClusterfuzzAnalysis
+}
+
+_BATCH_SIZE = 500
+
+
+def IterateCrashBatches(client_id, start_date, end_date,
+                        batch_size=_BATCH_SIZE):
+  """Iterates and re-initializes crash analyses in [start_date, end_date).
+
+  Args:
+    client_id (CrashClient): The client whose crash we should iterate.
+    start_date (datetime): Start date of the time range we iterate.
+    end_date (datetime): End date of the time range we iterate
+    batch_size (int): Size of the batch we iterate.
+  """
+  analysis = CLIENT_ID_TO_CRASH_ANALYSIS.get(client_id)
+
+  query = analysis.query()
+  query = query.filter(
+      analysis.requested_time >= start_date).filter(
+          analysis.requested_time < end_date)
+
+  # The client is used to do pre-processings when initializing a crash.
+  client = FinditForClientID(
+      client_id,
+      CachedGitilesRepository.Factory(HttpClientAppengine()),
+      CrashConfig.Get())
+
+  for crash_batch in Iterate(query, batch_size=batch_size, batch_run=True):
+    updated_crashes = []
+    for crash in crash_batch:
+      crash.ReInitialize(client)
+      updated_crashes.append(crash)
+
+    keys = ndb.put_multi(updated_crashes)
+    yield [key.urlsafe() for key in keys]
+
 
 class RerunAnalysis(BaseHandler):
+  """Rerun analysis on all crashes in a time range."""
+
   PERMISSION_LEVEL = Permission.ADMIN
 
   def HandleGet(self):
-    """Update crash analysis models."""
     client_id = self.request.get('client_id') or CrashClient.CRACAS
 
     now = time_util.GetUTCNow()
@@ -38,7 +90,19 @@ class RerunAnalysis(BaseHandler):
     end_date = datetime.strptime(
         end_date, DATETIME_FORMAT) if end_date else now
 
-    pipeline = RerunPipeline(client_id, start_date, end_date)
-    # Attribute defined outside __init__ - pylint: disable=W0201
-    pipeline.target = appengine_util.GetTargetNameForModule(RERUN_SERVICE)
-    pipeline.start(queue_name=RERUN_QUEUE)
+    count = 0
+    for crash_keys in IterateCrashBatches(client_id, start_date, end_date):
+      pipeline = RerunPipeline(client_id, crash_keys)
+      # Attribute defined outside __init__ - pylint: disable=W0201
+      pipeline.target = appengine_util.GetTargetNameForModule(RERUN_SERVICE)
+      pipeline.start(queue_name=RERUN_QUEUE)
+      count += 1
+
+    if count == 0:
+      message = 'No rerun pipeline started.'
+    elif count == 1:
+      message = 'A rerun pipeline kicked off.'
+    else:
+      message = '%d rerun pipelines kicked off.' % count
+
+    return {'data': {'message': message}}
