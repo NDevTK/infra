0e7d0013b5923a00601e602da8e3885799a00f1b
Emma <emso@chromium.org>
2017-06-06 15:19:01 +0200

    Refactors the tracking data stored in Tricium
    This is to separate stored data into mutable/immutable entries.
    
    Bug:
    Change-Id: Ide53cc7288601f5cffe0e65f826a5c0885b59ef3

--- go/src/infra/tricium/appengine/frontend/rpc_analyze.go
+++ go/src/infra/tricium/appengine/frontend/rpc_analyze.go
@@ -34,9 +34,9 @@ var server = &TriciumServer{}
 
 const repo = "https://chromium-review.googlesource.com/playground/gerrit-tricium"
 
-// Analyze processes one analysis request to Tricium.
+// Analyze processes one Analyze request to Tricium.
 //
-// Launched a workflow customized to the project and listed paths.  The run ID
+// Launched a workflow customized to the project and listed paths. The run ID
 // in the response can be used to track the progress and results of the request
 // via the Tricium UI.
 func (r *TriciumServer) Analyze(c context.Context, req *tricium.AnalyzeRequest) (*tricium.AnalyzeResponse, error) {
@@ -73,56 +73,53 @@ func analyze(c context.Context, req *tricium.AnalyzeRequest, cp config.ProviderA
 	// TODO(emso): Verify that there is no current run for this request (map hashed requests to run IDs).
 	// TODO(emso): Read Git repo info from the configuration projects/ endpoint.
 	// TODO(emso): Verify that a project has Gerrit details if a Gerrit reporter has been selected.
-	run := &track.Run{
+	run := &track.WorkflowRun{
 		Received: clock.Now(c).UTC(),
-		State:    tricium.State_PENDING,
 		Project:  req.Project,
 		Reporter: req.Reporter,
+		Paths:    req.Paths,
+		GitRepo:  repo,
+		GitRef:   req.GitRef,
 	}
-	sr := &track.ServiceRequest{
+	rr := &track.WorkflowRunResult{
+		ID:    "1",
+		State: tricium.State_PENDING,
+	}
+	lr := &admin.LaunchRequest{
 		Project: req.Project,
 		Paths:   req.Paths,
 		GitRepo: repo,
 		GitRef:  req.GitRef,
 	}
-	lr := &admin.LaunchRequest{
-		Project: sr.Project,
-		Paths:   sr.Paths,
-		GitRepo: repo,
-		GitRef:  sr.GitRef,
-	}
-	// This is a cross-group transaction because first Run is stored to get the ID,
-	// and then ServiceRequest is stored, with Run key as parent.
+	// This is a cross-group transaction because first AnalyzeRequest is stored to get the ID,
+	// and then AnalyzeRequestResult is stored, with the added AnalyzeRequest entity as parent.
 	err = ds.RunInTransaction(c, func(c context.Context) (err error) {
-		// Add tracking entries for run.
+		// Add tracking entries for request.
 		if err := ds.Put(c, run); err != nil {
-			return fmt.Errorf("failed to store run entry: %v", err)
+			return fmt.Errorf("failed to store AnalyzeRequest entity: %v", err)
 		}
-		// Run the below operations in parallel.
-		done := make(chan error)
-		defer func() {
-			if err2 := <-done; err == nil {
-				err = err2
-			}
-		}()
-		go func() {
-			// Add tracking entry for service request.
-			sr.Parent = ds.KeyForObj(c, run)
-			if err := ds.Put(c, sr); err != nil {
-				done <- fmt.Errorf("failed to store service request: %v", err)
-				return
-			}
-			done <- nil
-		}()
-		// Launch workflow, enqueue launch request.
-		lr.RunId = run.ID
-		t := tq.NewPOSTTask("/launcher/internal/launch", nil)
-		b, err := proto.Marshal(lr)
-		if err != nil {
-			return fmt.Errorf("failed to enqueue launch request: %v", err)
+		ops := []func() error{
+			// Add AnalyzeRequestResult entity for requst status tracking.
+			func() error {
+				rr.Parent = ds.KeyForObj(c, run)
+				if err := ds.Put(c, rr); err != nil {
+					return fmt.Errorf("failed to store AnalyzeRequestResult entry: %v", err)
+				}
+				return nil
+			},
+			// Launch workflow, enqueue launch request.
+			func() error {
+				lr.RunId = run.ID
+				t := tq.NewPOSTTask("/launcher/internal/launch", nil)
+				b, err := proto.Marshal(lr)
+				if err != nil {
+					return fmt.Errorf("failed to enqueue launch request: %v", err)
+				}
+				t.Payload = b
+				return tq.Add(c, common.LauncherQueue, t)
+			},
 		}
-		t.Payload = b
-		return tq.Add(c, common.LauncherQueue, t)
+		return common.RunInParallel(ops)
 	}, &ds.TransactionOptions{XG: true})
 	if err != nil {
 		return "", codes.Internal, fmt.Errorf("failed to track and launch request: %v", err)
