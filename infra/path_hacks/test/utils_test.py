1ef76585c2575f23b4b1506e0de068c6688a9be9
Michael Achenbach <machenbach@chromium.org>
2017-06-22 15:35:55 +0200

    [Not for commit] Path test stuff
    Bug:
    Change-Id: I489f24066cd3f2c3ea98766ecb4fef64d43b0e9b

--- infra/path_hacks/test/utils_test.py
+++ infra/path_hacks/test/utils_test.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import glob
 import os
 import unittest
 
@@ -10,3 +11,8 @@ import infra.path_hacks
 class FullInfraPathTest(unittest.TestCase):
   def test_check_valid_full_infra_path(self):
     self.assertTrue(os.path.isdir(infra.path_hacks.full_infra_path))
+
+  def test_path_stuff_on_windows(self):
+    infra_abs_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    gl = os.path.join(infra_abs_dir, 'tes*', '*_test.py')
+    self.assertEqual(5, len(glob.glob(gl)))
