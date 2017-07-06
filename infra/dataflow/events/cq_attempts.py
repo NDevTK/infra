e1101faf618e2a6bf5fd79f65edd1f429293ed5b
Katie Thomas <katthomas@google.com>
2017-07-05 18:49:27 -0700

    Add beginning of dataflow pipeline for cq_attempts
    I'll still need to manage the result table: adding insertIDs,
    configuring the range of events the job looks at, and truncating
    rows that will be replaced.
    
    Bug:615218
    Change-Id: I60a1f9fb669aa7dd6581211fe2beda49d9a8d497

---
+++ infra/dataflow/events/cq_attempts.py
@@ -0,0 +1,67 @@
+import logging
+import time
+
+import aggregate_objects
+import chops_beam
+
+import apache_beam as beam
+
+ACTION_PATCH_START = 'PATCH_START'
+
+class CombineEventsToAttempt(beam.CombineFn):
+  def create_accumulator(self):
+    return aggregate_objects.CQAttempt()
+
+  def add_input(self, accumulator, input_rows):
+    for row in input_rows:
+      if row.get('attempt_start_usec') is None:
+        logging.warn('recieved row with null attempt_start_usec: %s', row)
+        continue
+
+      timestamp = row.get('timestamp_millis')
+      if timestamp is None:
+        logging.warn('recieved raw with null timestamp: %s', row)
+        continue
+
+      attempt_start_msec = row.get('attempt_start_usec') / 1000
+      assert (accumulator.attempt_start_msec is None or
+              accumulator.attempt_start_msec == attempt_start_msec)
+      accumulator.attempt_start_msec = attempt_start_msec
+      action = row.get('action')
+
+      if action == ACTION_PATCH_START:
+        accumulator.update_first_start(timestamp)
+        accumulator.update_last_start(timestamp)
+
+    return accumulator
+
+  def merge_accumulators(self, accumulators):
+    if len(accumulators) == 1:
+      return accumulators[0]
+    merged = accumulators[0]
+    for a in accumulators[1:]:
+      merged.update_first_start(a.first_start_msec)
+      merged.update_last_start(a.last_start_msec)
+    return merged
+
+  def extract_output(self, a):
+    return a.__dict__
+
+
+def main():
+  one_day_ago_usec = time.time() * 1000000 - 24 * 60 * 60 * 1000000
+  q = "SELECT timestamp_millis, action, attempt_start_usec \
+       FROM `chrome-infra-events.raw_events.cq` \
+       WHERE attempt_start_usec > %d" % one_day_ago_usec
+  p = chops_beam.EventsPipeline()
+  _ = (p
+   | chops_beam.BQRead(q)
+   | beam.Map(lambda e: (e['attempt_start_usec'], e))
+   | beam.GroupByKey()
+   | beam.CombinePerKey(CombineEventsToAttempt())
+   | beam.Map(lambda (k, v): v)
+   | chops_beam.BQWrite('cq_attempts'))
+  p.run()
+
+if __name__ == '__main__':
+  main()
