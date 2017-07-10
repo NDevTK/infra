ef919b63918201310544314169460aa8634495c4
Joanna Wang <jojwang@google.com>
2017-07-10 16:10:43 -0700

    dumb bugs
    Bug:
    Change-Id: Id6487e2fee87f18667790d638472a0e48fee8376

--- appengine/monorail/static/js/tracker/tracker-nav.js
+++ appengine/monorail/static/js/tracker/tracker-nav.js
@@ -164,6 +164,9 @@ function TKR_addSort(colname, descending) {
   }
 
   var isHotlist = window.location.href.includes('/hotlists/');
+  console.log(window.location.href);
+  console.log($('can'));
+  console.log($('hotlist_name'));
   var url = isHotlist ? ($('hotlist_name').value + '?') : ('list?');
   url  += ('can='+ $('can').value + '&q=' +
       TKR_getArtifactSearchField().value);
