df4a473408fe9cb28677b1391292381a5bbd5458
Sharu Jiang <katesonia@google.com>
2017-08-16 20:27:17 -0700

    [Predator] Add clusterfuzz dashboard and feedback page.
    testing-url: https://katesonia-test-test-dot-findit-for-me.googleplex.com/clusterfuzz/dashboard
    
    Bug: 754510
    Change-Id: Idf9df5becd2f5474c90015c2abae38926fe3b6cf

---
+++ appengine/predator/app/frontend/handlers/clusterfuzz_dashboard.py
@@ -0,0 +1,56 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from analysis.type_enums import CrashClient
+from common.model.clusterfuzz_analysis import ClusterfuzzAnalysis
+from frontend.handlers.dashboard import DashBoard
+from gae_libs.handlers.base_handler import Permission
+
+
+class ClusterfuzzDashBoard(DashBoard):
+  PERMISSION_LEVEL = Permission.ADMIN
+
+  @property
+  def crash_analysis_cls(self):
+    return ClusterfuzzAnalysis
+
+  @property
+  def client(self):
+    return CrashClient.CLUSTERFUZZ
+
+  @property
+  def template(self):
+    return 'clusterfuzz_dashboard.html'
+
+  def Order(self):
+    return -self.crash_analysis_cls.signature
+
+  def CrashDataToDisplay(self, crash_analyses):
+    """Gets the crash data to display."""
+    if not crash_analyses:
+      return []
+
+    crashes = []
+    for crash in crash_analyses:
+      display_data = {
+          'signature': crash.signature,
+          'testcase_id': crash.testcase_id,
+          'version': crash.crashed_version,
+          'job_type': crash.job_type,
+          'platform': crash.platform,
+          'regression_range': crash.regression_range,
+          'commits': crash.commits_in_regression_range,
+          'error_name': crash.error_name or '',
+          'suspected_cls': (crash.result.get('suspected_cls', [])
+                            if crash.result else []),
+          'suspected_project': (crash.result.get('suspected_project', '')
+                                if crash.result else ''),
+          'suspected_components': (crash.result.get('suspected_components', [])
+                                   if crash.result else []),
+          'key': crash.key.urlsafe()
+      }
+      crashes.append(display_data)
+
+    crashes.sort(key=lambda crash: crash['signature'])
+    return crashes
