65cff80fca5cc180e02cd283887c071fc5d6f509
Sean McCullough <seanmccullough@chromium.org>
2017-10-17 17:42:41 -0700

    [bqschema] add support for protos-as-tabledefs using Options
    Bug: 775262
    Change-Id: Ibac0f42aa239981601fb8fbf828d77bf42b2faf1

--- go/src/infra/tools/bqschemaupdater/bqschemaupdater.go
+++ go/src/infra/tools/bqschemaupdater/bqschemaupdater.go
@@ -15,6 +15,8 @@ import (
 	"time"
 
 	"cloud.google.com/go/bigquery"
+	protobuf "github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/protoc-gen-go/descriptor"
 	"golang.org/x/net/context"
 	"google.golang.org/api/option"
 
@@ -24,6 +26,79 @@ import (
 	pb "infra/libs/bqschema/tabledef"
 	"infra/libs/infraenv"
 )
+
+func loadProtoDescriptor(path string) (*descriptor.FileDescriptorSet, error) {
+	descFileBytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var desc descriptor.FileDescriptorSet
+	err = protobuf.Unmarshal(descFileBytes, &desc)
+	if err != nil {
+		return nil, err
+	}
+
+	return &desc, nil
+}
+
+func protoDescToTableDef(fs *descriptor.FileDescriptorSet) (*pb.TableDef, error) {
+	ret := &pb.TableDef{}
+	for _, f := range fs.File {
+		for _, mt := range f.MessageType {
+			if mt.Options == nil {
+				continue
+			}
+			log.Printf("checking MessageType: %q", *mt.Name)
+			var datasetID, tableID, tableName, tableDescription *string
+			var ok bool
+
+			val, err := protobuf.GetExtension(mt.Options, pb.E_DatasetId)
+			if err == nil {
+				datasetID, ok = val.(*string)
+				if !ok {
+					log.Fatalf("Couldn't cast dataset_id to string.")
+				}
+			}
+
+			val, err = protobuf.GetExtension(mt.Options, pb.E_TableId)
+			if err == nil {
+				tableID, ok = val.(*string)
+				if !ok {
+					log.Fatalf("Couldn't cast table_id to string.")
+				}
+			}
+
+			val, err = protobuf.GetExtension(mt.Options, pb.E_TableName)
+			if err == nil {
+				tableName, ok = val.(*string)
+				if !ok {
+					log.Fatalf("Couldn't cast table_name to string.")
+				}
+			}
+
+			val, err = protobuf.GetExtension(mt.Options, pb.E_TableDescription)
+			if err == nil {
+				tableDescription, ok = val.(*string)
+				if !ok {
+					log.Fatalf("Couldn't cast table_description to string.")
+				}
+			}
+
+			if datasetID == nil && tableID != nil || datasetID != nil && tableID == nil {
+				log.Fatalf("Must declare both dataset_id and table_id if either is declared.")
+			}
+
+			if datasetID != nil { // we have everything we need.
+				log.Printf("Should create table: %q, %q, %q, %q", *datasetID, *tableID, *tableName, *tableDescription)
+			} else {
+				log.Printf("%q has options but not for bq schema.")
+			}
+
+		}
+	}
+	return ret, nil
+}
 
 func loadTableDef(path string) (*pb.TableDef, error) {
 	content, err := ioutil.ReadFile(path)
@@ -62,16 +137,25 @@ func updateFromTableDef(ctx context.Context, ts tableStore, td *pb.TableDef) err
 }
 
 func main() {
+	log.SetOutput(os.Stdout)
 	ctx := context.Background()
 
 	dry := flag.Bool("dry-run", false, "Only performs non-mutating operations; logs what would happen otherwise")
 	project := flag.String("project", infraenv.ChromeInfraEventsProject, "Cloud project that the table belongs to.")
+	useDescriptor := flag.Bool("use-descriptor", false, "Read input file as proto descriptor instead of text proto.")
 
 	flag.Parse()
 	file := flag.Arg(0)
 	if file == "" {
 		log.Fatal("Missing arg: file path for schema to add/update")
 	}
+
+	if *useDescriptor {
+		desc, err := loadProtoDescriptor(file)
+		log.Printf("desc, err: %v, %v", desc != nil, err)
+		td, err := protoDescToTableDef(desc)
+		log.Printf("def, err: %v, %v", td != nil, err)
+	} // else {
 	td, err := loadTableDef(file)
 	if err != nil {
 		log.Fatalf("Failed to load TableDef from %q: %s", file, err)
