fcd6f2d76c73fff2d9f8d95a13f81c5368d3e469
Sean McCullough <seanmccullough@chromium.org>
2017-10-17 17:41:54 -0700

    [bqschema] add support for protos-as-tabledefs using Options
    Bug: 775262
    Change-Id: Ibac0f42aa239981601fb8fbf828d77bf42b2faf1

--- go/src/infra/appengine/sheriff-o-matic/som/model/generate.go
+++ go/src/infra/appengine/sheriff-o-matic/som/model/generate.go
@@ -7,3 +7,5 @@ package model
 //go:generate go install infra/cmd/bqexport
 //go:generate bqexport -name SOMAlertsEvent -path ./som_alerts.pb.txt
 //go:generate bqexport -name SOMAnnotationEvent -path ./annotations.pb.txt
+
+//go:generate cproto -desc protos.desc
