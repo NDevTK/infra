281d8fbca02fb4f50a6b9edd4f7f4d48f84578c5
Chan <chanli@chromium.org>
2018-02-14 09:52:21 -0800

    [Findit] Replace mocks assert_called and assert_not_called to assert mock.called
    When I ran unit tests locally, I noticed that I can replace assert_called to assert_not_called or vise versa and the tests still pass. This seems not expected.
    Replace all calls to these 2 methods to assert mock.called.
    
    Change-Id: I62965c2c6c7628f9ed70ae099a6a66643dfed39d

--- appengine/findit/pipelines/test_failure/test/revert_and_notify_test_culprit_pipeline_test.py
+++ appengine/findit/pipelines/test_failure/test/revert_and_notify_test_culprit_pipeline_test.py
@@ -85,4 +85,4 @@ class RevertAndNotifyTestCulpritPipelineTest(wf_testcase.WaterfallTestCase):
             heuristic_cls=heuristic_cls))
     pipeline.start(queue_name=constants.DEFAULT_QUEUE)
     self.execute_queued_tasks()
-    mocked_pipeline.assert_not_called()
+    self.assertFalse(mocked_pipeline.called)
