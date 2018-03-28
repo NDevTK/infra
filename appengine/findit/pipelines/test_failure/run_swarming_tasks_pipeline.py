7cbeb569a4c13880e5ec0ec853639559e7b9007f
Chan <chanli@chromium.org>
2018-03-28 16:33:43 -0700

    [Findit] Wrapper pipeline to run swarming tasks.
    Wrapper pipeline that spawns swarming task pipelines for new failed steps in a build.
    
    Bug: 808695
    Change-Id: I879b8685cad6321b9013fb7b7afba10df967aa4a

---
+++ appengine/findit/pipelines/test_failure/run_swarming_tasks_pipeline.py
@@ -0,0 +1,25 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from dto.run_swarming_tasks_input import RunSwarmingTasksInput
+from dto.run_swarming_task_parameters import RunSwarmingTaskParameters
+from gae_libs.pipelines import GeneratorPipeline
+from pipelines.test_failure.run_test_swarming_task_pipeline import (
+    RunTestSwarmingTaskPipeline)
+from services.test_failure import test_swarming
+
+
+class RunSwarmingTasksPipeline(GeneratorPipeline):
+  """Wrapper Pipeline to run swarming tasks."""
+  input_type = RunSwarmingTasksInput
+
+  def RunImpl(self, run_swarming_tasks_input):
+    steps = test_swarming.GetFirstTimeTestFailuresToRunSwarmingTasks(
+        run_swarming_tasks_input)
+    for step_name, base_tests in steps.iteritems():
+      run_swarming_task_params = RunSwarmingTaskParameters(
+          build_key=run_swarming_tasks_input.build_key,
+          step_name=step_name,
+          tests=base_tests)
+      yield RunTestSwarmingTaskPipeline(run_swarming_task_params)
