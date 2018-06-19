9c0b2c5dd6b012fabaa70b16ac1f0ce266b65e84
Quinten Yearsley <qyearsley@chromium.org>
2018-06-19 15:50:40 -0700

    [tricium] Add an initial UI set-up using Polymer 3
    Some features of the Polymer 3 version:
     - Each element is defined in a JS file
     - JS module imports are used rather than HTML imports
     - All dependencies are defined in package.json
     - Config for polymer CLI tools is in polymer.json
     - Node-style package name import paths are used,
       which means the elements must be compiled with
       `polymer build` for deployment, or served using
       `polymer serve` for local development.
    
    Bug: 674478
    Change-Id: Ibff676e3d2b082483d8a2aed24b179680d4a07db

---
+++ go/src/infra/tricium/appengine/frontend/ui/src/tricium-run.js
@@ -0,0 +1,67 @@
+'use strict';
+
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import '@polymer/polymer/lib/elements/dom-if.js';
+import '@polymer/iron-ajax/iron-ajax.js'
+import '@polymer/iron-form/iron-form.js'
+import '@polymer/paper-button/paper-button.js'
+import '@polymer/paper-input/paper-input.js'
+
+class TriciumRun extends PolymerElement {
+
+  static get properties() {
+    return {
+      runId: String,
+      content: String,
+      error: String,
+    };
+  }
+
+  ready() {
+    super.ready();
+    if (this.runId) {
+      this._refresh();
+    }
+  }
+
+  _refresh() {
+    this.$.ajax.body = JSON.stringify({runId: this.runId});
+    this.$.ajax.generateRequest();
+  }
+
+  _showResponse(event) {
+    this.content = JSON.stringify(event.target.lastResponse, null, 2);
+  }
+
+  _showError(event) {
+    this.error = event.detail.error;
+  }
+
+  static get template() {
+    return html`
+        <iron-form id="form" on-submit="_refresh()">
+          <paper-input
+              label="Run ID"
+              allowed-pattern="[0-9]"
+              style="width:20em"
+              value={{runId}}></paper-input>
+        </iron-form>
+      <iron-ajax
+        id="ajax"
+        url="/prpc/tricium.Tricium/Progress"
+        method="POST"
+        content-type="application/json"
+        accept="json"
+        json-prefix=")]}'"
+        on-response="_showResponse"
+        on-error="_showError"
+        handle-as="json"
+        last-response="{{ajaxResponse}}">
+      </iron-ajax>
+      <pre>[[content]]</pre>
+      <p style="color:red">[[error]]</p>
+    `;
+  }
+}
+
+customElements.define('tricium-run', TriciumRun);
