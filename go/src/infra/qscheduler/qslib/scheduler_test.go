6de35a5f453ee9812027a1e61c276dbcf9b905eb
Aviv Keshet <akeshet@chromium.org>
2018-07-31 13:16:15 -0700

    qscheduler: [wip] scheduler code
    BUG=None
    TEST=None
    
    Change-Id: I218590ed807c62c4b2fe81ef2d45bd5a51ac29a2

---
+++ go/src/infra/qscheduler/qslib/scheduler_test.go
@@ -0,0 +1,14 @@
+package qslib
+
+/*func TestMatchWithIdleWorkers(t *testing.T) {
+	state := State {
+		Balances : map[AccountID]AccountBalance {
+			"a1": {1, 0, 0},
+			"a2": {0, 1, 0},
+		},
+		RequestQueue: map[RequestID]*TaskRequest {
+			"r1": &TaskRequest
+		}
+	}
+}
+*/
