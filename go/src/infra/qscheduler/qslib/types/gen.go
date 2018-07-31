43ee517b4f76badb86513a1002fefc2c667ceb62
Aviv Keshet <akeshet@chromium.org>
2018-07-30 20:46:02 -0700

    qscheduler: add protos for basic state and config model
    BUG=None
    TEST=None
    
    Change-Id: Id436596a676b9e5cb561139302c02593f48fb2d1

---
+++ go/src/infra/qscheduler/qslib/types/gen.go
@@ -0,0 +1,4 @@
+package types
+
+//go:generate go install go.chromium.org/luci/grpc/cmd/cproto
+//go:generate cproto
---
+++ go/src/infra/qscheduler/qslib/types/task/gen.go
@@ -0,0 +1,4 @@
+package task
+
+//go:generate go install go.chromium.org/luci/grpc/cmd/cproto
+//go:generate cproto
