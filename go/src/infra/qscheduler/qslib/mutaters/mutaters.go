a580239c060e1035ae96f9208bfb10ba9d19e476
Aviv Keshet <akeshet@chromium.org>
2018-07-31 13:16:15 -0700

    qscheduler: initial implementation of scheduler logic
    BUG=chromium:870537
    TEST=unit tests
    
    Change-Id: I218590ed807c62c4b2fe81ef2d45bd5a51ac29a2

---
+++ go/src/infra/qscheduler/qslib/mutaters/mutaters.go
@@ -0,0 +1,65 @@
+// Copyright 2018 The LUCI Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mutaters
+
+import (
+	"infra/qscheduler/qslib/types"
+	"infra/qscheduler/qslib/types/task"
+	"infra/qscheduler/qslib/types/vector"
+)
+
+// TODO: Turn mutaters code into an independent package called mutaters.
+
+// Mutater is an interface that describes operations that mutate a types.State
+type Mutater interface {
+	Mutate(state *types.State)
+}
+
+// Mutate implements Mutaters.Mutate().
+func (m *AssignIdleWorker) Mutate(state *types.State) {
+	rt := &task.Run{
+		Priority: m.Priority,
+		Request:  state.RequestQueue[m.RequestId],
+		Cost:     vector.New(),
+	}
+	delete(state.RequestQueue, m.RequestId)
+	state.Running = append(state.Running, rt)
+	state.Workers[m.WorkerId].RunningTask = rt
+}
+
+// Mutate implements Mutaters.Mutate().
+func (m *ChangePriority) Mutate(state *types.State) {
+	state.Workers[m.WorkerId].RunningTask.Priority = m.NewPriority
+}
+
+// Mutate implements Mutaters.Mutate().
+func (m *PreemptTask) Mutate(state *types.State) {
+	w := state.Workers[m.WorkerId]
+	cost := w.RunningTask.Cost
+	oT := w.RunningTask
+	nT := state.RequestQueue[m.RequestId]
+	oAcc := w.RunningTask.Request.AccountId
+	nAcc := nT.AccountId
+
+	oBal := state.Balances[oAcc].Plus(*cost)
+	state.Balances[oAcc] = &oBal
+
+	nBal := state.Balances[nAcc].Minus(*cost)
+	state.Balances[nAcc] = &nBal
+
+	delete(state.RequestQueue, m.RequestId)
+	state.RequestQueue[oT.Request.Id] = oT.Request
+	w.RunningTask = &task.Run{Cost: cost, Priority: m.Priority, Request: nT}
+}
