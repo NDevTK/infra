6e0a93deae6cd4e7a9fef1f893b64161eb6d1a2f
Aviv Keshet <akeshet@chromium.org>
2018-07-31 13:16:23 -0700

    qscheduler: add a simple wrapper for running all qscheduler tests
    BUG=None
    TEST=None
    
    Change-Id: I4e0b5dd0fd4647a408d1657cf7701356b4014c12

---
+++ go/src/infra/qscheduler/qslib/run_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+go test infra/qscheduler/qslib/priority
+go test infra/qscheduler/qslib/types
+go test infra/qscheduler/qslib/types/account
+go test infra/qscheduler/qslib/types/task
+go test infra/qscheduler/qslib
