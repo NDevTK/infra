4025cfbe567c8861092ae11dd0d6bb06ac99407e
Jeffrey Li <lijeffrey@chromium.org>
2018-09-11 15:45:53 -0400

    [Findit] Flake Analyzer - Store build id to MasterFlakeAnalysis
    Captures and stores build_id at creation of MasterFlakeAnalysis. To start,
    build_id and original_build_id will be used for display purposes only,
    especially when surfacing links to the build in the various auto actions.
    
    Bug: 881920
    Change-Id: I07b0ca93d356f0bc487b102b26d84eaa55494b14

--- appengine/findit/model/flake/analysis/master_flake_analysis.py
+++ appengine/findit/model/flake/analysis/master_flake_analysis.py
@@ -381,6 +381,7 @@ class MasterFlakeAnalysis(BaseAnalysis, BaseBuildModel, VersionedModel,
     self.original_step_name = None
     self.original_test_name = None
     self.bug_id = None
+    self.build_id = None
     self.error = None
     self.correct_regression_range = None
     self.correct_culprit = None
@@ -630,7 +631,8 @@ class MasterFlakeAnalysis(BaseAnalysis, BaseBuildModel, VersionedModel,
     if any_changes:
       self.put()
 
-  # TODO(crbug.com/881920): Capture and store build_id to replace build_number.
+  # The id of the build that triggered the analysis.
+  build_id = ndb.StringProperty(indexed=False)
 
   # The original build/step/test in which a flake actually occurred.
   # A CQ trybot step has to be mapped to a Waterfall buildbot step.
