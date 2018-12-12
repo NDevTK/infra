e237d8adf6616f2246243fd402b8d2b67e284adb
Nodir Turakulov <nodir@google.com>
2018-12-12 10:21:01 -0800

    [buildbucket] Remove Build.parameters_actual
    parameters_actual was needed because server might have change user-specified
    parameters. Now server uses Build.input_properties, which is allowed to have
    server-defined properties.
    
    Update all usages to use Build.input_properties instead.
    
    R=vadimsh@chromium.org
    
    Bug: 875056
    Change-Id: I39979d3541c12542bdf572ebafda5126d56d033b

---
+++ appengine/cr-buildbucket/bbutil.py
@@ -0,0 +1,38 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Utility functions.
+
+Has "bb" prefix to avoid confusion with components.utils.
+"""
+
+from google.protobuf import json_format
+from google.protobuf import struct_pb2
+
+
+def message_to_jsonpb(msg):  # pragma: no cover
+  """Converts msg to JSONPB deterministically."""
+  return json_format.MessageToJson(msg, indent=None, sort_keys=True)
+
+
+def dict_to_struct(d):  # pragma: no cover
+  """Converts a dict to google.protobuf.Struct."""
+  s = struct_pb2.Struct()
+  s.update(d)
+  return s
+
+
+def dict_to_jsonpb(d):  # pragma: no cover
+  """Converts a dict to JSONPB."""
+  return message_to_jsonpb(dict_to_struct(d))
+
+
+def update_struct(dest, src):  # pragma: no cover
+  """Updates dest struct with values from src.
+
+  Like dict.update, but for google.protobuf.Struct.
+  """
+  for key, value in src.fields.iteritems():
+    # This will create a new struct_pb2.Value if one does not exist.
+    dest.fields[key].CopyFrom(value)
--- appengine/cr-buildbucket/fix_builds.py
+++ appengine/cr-buildbucket/fix_builds.py
@@ -38,29 +38,9 @@ def _fix_builds(build_keys):  # pragma: no cover
 
 @ndb.transactional_tasklet
 def _fix_build_async(build_key):  # pragma: no cover
-  build, out_props = yield [
-      build_key.get_async(),
-      model.BuildOutputProperties.key_for(build_key).get_async(),
-  ]
+  build = yield build_key.get_async()
   if not build:
     return
 
-  to_put = []
-
-  if not build.input_properties:
-    params = build.parameters_actual or build.parameters or {}
-    build.input_properties = struct_pb2.Struct()
-    build.input_properties.update(params.get('properties') or {})
-    to_put.append(build)
-
-  if not out_props:
-    props = struct_pb2.Struct()
-    props.update((build.result_details or {}).get('properties') or {})
-    to_put.append(
-        model.BuildOutputProperties(
-            key=model.BuildOutputProperties.key_for(build_key),
-            properties=props,
-        )
-    )
-
+  to_put = [build]
   yield ndb.put_multi_async(to_put)
