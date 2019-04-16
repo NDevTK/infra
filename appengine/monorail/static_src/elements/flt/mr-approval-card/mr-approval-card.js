404e1ca01668cf8d5a74cfcda2546691d2c1a8e8
Edward Lemur <ehmaldonado@chromium.org>
2019-04-15 22:02:39 -0400

    monorail: Move focused comment logic out of mr-issue-details.
    Bug: monorail:5319
    Change-Id: I4cd1645fcd46585bdb531bb7527bfdcc182a893c

--- appengine/monorail/static_src/elements/flt/mr-approval-card/mr-approval-card.js
+++ appengine/monorail/static_src/elements/flt/mr-approval-card/mr-approval-card.js
@@ -218,8 +218,6 @@ export class MrApprovalCard extends ReduxMixin(PolymerElement) {
         ></mr-description>
         <mr-comment-list
           heading-level=4
-          focus-id="[[focusId]]"
-          focused-comment="[[_focusedComment]]"
           comments="[[_comments]]"
         >
           <h4 id$="[[_editId]]" class="medium-heading">
@@ -335,11 +333,13 @@ export class MrApprovalCard extends ReduxMixin(PolymerElement) {
         type: String,
         computed: '_computeStatusIcon(class)',
       },
-      _focusedComment: {
-        type: Number,
-        computed: '_computeFocusedComment(_comments, focusId)',
-      },
     };
+  }
+
+  static get observers() {
+    return [
+      '_onFocusId(_comments, focusId)',
+    ];
   }
 
   static mapStateToProps(state, element) {
@@ -509,17 +509,14 @@ export class MrApprovalCard extends ReduxMixin(PolymerElement) {
     }));
   }
 
-  _computeFocusedComment(comments, focusId) {
-    const index = (comments || []).findIndex((comment) => {
+  _onFocusId(comments, focusId) {
+    (comments || []).some((comment) => {
       const commentId = 'c' + comment.sequenceNum;
       if (commentId === focusId) {
+        this.opened = true;
         return true;
       }
     });
-    if (index !== -1) {
-      this.opened = true;
-    }
-    return index;
   }
 }
 
