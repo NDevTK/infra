f75ab4b95675f408c7d447937be60164444f28a9
Aviv Keshet <akeshet@chromium.org>
2019-06-10 17:46:41 -0700

    cros_test_platform: skeletal implementation of skylab executor
    Implement basic structure of skylab executor, with a variety of TODOs
    in place that await resolution.
    
    BUG=971376
    TEST=unit tests
    
    Change-Id: Ia42066174ea8d6d02d6115ae10dce2943ddca761

---
+++ go/src/infra/cmd/cros_test_platform/internal/skylab/skylab.go
@@ -0,0 +1,159 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package skylab implements logic necessary for Skylab execution of an
+// ExecuteRequest.
+package skylab
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	chromite "go.chromium.org/chromiumos/infra/proto/go/chromite/api"
+	"go.chromium.org/chromiumos/infra/proto/go/test_platform/common"
+	"go.chromium.org/chromiumos/infra/proto/go/test_platform/steps"
+	swarming_api "go.chromium.org/luci/common/api/swarming/swarming/v1"
+	"go.chromium.org/luci/common/errors"
+
+	"infra/libs/skylab/request"
+)
+
+// Run encapsulates the running state of an ExecuteRequest.
+type Run struct {
+	testRuns []*testRun
+}
+
+type testRun struct {
+	test     *chromite.AutotestTest
+	attempts []attempt
+}
+
+type attempt struct {
+	taskID    string
+	completed bool
+
+	state common.TaskState
+}
+
+// Swarming defines an interface used to interact with a swarming service.
+// It is implemented by infra/libs/skylab/swarming.Client
+type Swarming interface {
+	CreateTask(context.Context, *swarming_api.SwarmingRpcsNewTaskRequest) (*swarming_api.SwarmingRpcsTaskRequestMetadata, error)
+	GetResults(ctx context.Context, IDs []string) ([]*swarming_api.SwarmingRpcsTaskResult, error)
+}
+
+// NewRun creates a new Run.
+func NewRun(tests []*chromite.AutotestTest) *Run {
+	testRuns := make([]*testRun, len(tests))
+	for i, test := range tests {
+		testRuns[i] = &testRun{test: test}
+	}
+	return &Run{testRuns}
+}
+
+// LaunchAndWait launches a skylab execution and waits for it to complete,
+// polling for new results periodically (TODO(akeshet): and retrying tests that
+// need retry, based on retry policy).
+//
+// If the supplied context is cancelled prior to completion, or some other error
+// is encountered, this method returns whatever partial execution response
+// was visible to it prior to that error.
+func (r *Run) LaunchAndWait(ctx context.Context, swarming Swarming) (*steps.ExecuteResponse, error) {
+	if err := r.launch(ctx, swarming); err != nil {
+		return r.response(), err
+	}
+
+	return r.wait(ctx, swarming)
+}
+
+func (r *Run) launch(ctx context.Context, swarming Swarming) error {
+	for _, testRun := range r.testRuns {
+		// TODO(akeshet): These request args don't include any of the actual
+		// test details yet. Fix this, and use correct args.
+		req, err := request.New(request.Args{})
+		if err != nil {
+			return errors.Annotate(err, "launch test").Err()
+		}
+
+		resp, err := swarming.CreateTask(ctx, req)
+		if err != nil {
+			return errors.Annotate(err, "launch test").Err()
+		}
+
+		testRun.attempts = append(testRun.attempts, attempt{taskID: resp.TaskId})
+	}
+	return nil
+}
+
+func (r *Run) wait(ctx context.Context, swarming Swarming) (*steps.ExecuteResponse, error) {
+	for {
+		complete := true
+		for _, testRun := range r.testRuns {
+			attempt := testRun.attempts[len(testRun.attempts)-1]
+			if attempt.completed {
+				continue
+			}
+
+			results, err := swarming.GetResults(ctx, []string{attempt.taskID})
+			if err != nil {
+				return r.response(), errors.Annotate(err, "wait for tests").Err()
+			}
+
+			result, err := unpackResultForAttempt(results, attempt)
+			if err != nil {
+				return r.response(), errors.Annotate(err, "wait for tests").Err()
+			}
+
+			// TODO(akeshet): Respect actual completed statuses.
+			if result.State == "COMPLETED" {
+				attempt.completed = true
+				continue
+			}
+
+			complete = false
+		}
+
+		if complete {
+			return r.response(), nil
+		}
+
+		select {
+		case <-ctx.Done():
+			return r.response(), errors.Annotate(ctx.Err(), "wait for tests").Err()
+		case <-time.After(15 * time.Second):
+		}
+	}
+}
+
+func unpackResultForAttempt(results []*swarming_api.SwarmingRpcsTaskResult, a attempt) (*swarming_api.SwarmingRpcsTaskResult, error) {
+	if len(results) != 1 {
+		return nil, fmt.Errorf("expected 1 result, got %d", len(results))
+	}
+
+	result := results[0]
+	if result.TaskId != a.taskID {
+		return nil, fmt.Errorf("expected result for task id %s, got %s", a.taskID, result.TaskId)
+	}
+
+	return result, nil
+}
+
+func (r *Run) response() *steps.ExecuteResponse {
+	resp := &steps.ExecuteResponse{}
+	for _, test := range r.testRuns {
+		for _, attempt := range test.attempts {
+			resp.TaskResults = append(resp.TaskResults, &steps.ExecuteResponse_TaskResult{
+				Name: test.test.Name,
+				// TODO(akeshet): Map task status correctly.
+				State:  &common.TaskState{},
+				TaskId: attempt.taskID,
+				// TODO(akeshet): Determine correct taskURL. This requires knowing
+				// which swarming instance was used.
+				TaskUrl: "taskurl_placeholder",
+			})
+		}
+	}
+	return resp
+}
