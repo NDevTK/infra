0e6cb9dda2a7a14b8fe2cdc6e80ac6385520e5db
Anushruth <anushruth@chromium.org>
2019-06-14 16:26:36 -0700

    Add CrOS monitoring to sysmon
    Added CrOS monitoring to sysmon. This implementation includes
    DUT status metric
    
    R=bpastene@google.com
    
    Bug: 937943
    Change-Id: I4830d676f265b374f0a01a5df551ec3481f51979

---
+++ go/src/infra/monitoring/sysmon/cros/devicefile_test.go
@@ -0,0 +1,79 @@
+package cros
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"go.chromium.org/luci/common/clock/testclock"
+	"golang.org/x/net/context"
+)
+
+func TestLoadfile(t *testing.T) {
+	now := time.Date(2000, 1, 2, 3, 4, 5, 0, time.UTC)
+	c := context.Background()
+	c, _ = testclock.UseTime(c, now)
+
+	Convey("Using tmp directory", t, func() {
+		// Use tmp dir to create a mock file
+		path, err := ioutil.TempDir("", "cros-devicefile-test")
+		So(err, ShouldBeNil)
+		defer os.RemoveAll(path)
+
+		fileName := filepath.Join(path, "file.json")
+
+		Convey("loads a valid file", func() {
+			err := ioutil.WriteFile(fileName, []byte(`{
+			"devices": {
+			  "variable_chromeos_device_hostname": {
+			    "CHROMEOS_RELEASE_BOARD": "kevin",
+			    "CHROMEOS_RELEASE_VERSION": "12248.0.0-rc1"
+			  }
+			},
+			"timestamp": 1559855998.093489
+			}`), 0644)
+			So(err, ShouldBeNil)
+			f, err := loadfile(c, fileName)
+			So(err, ShouldBeNil)
+			So(f, ShouldResemble, deviceStatusFile{
+				Devices: map[string]deviceStatus{
+					"variable_chromeos_device_hostname": {
+						Board: "kevin",
+					},
+				},
+				Timestamp: 1559855998.093489,
+			})
+		})
+
+		Convey("file not found", func() {
+			_, err := loadfile(c, "/file/not/found")
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("invalid json", func() {
+			err := ioutil.WriteFile(fileName,
+				[]byte(`not valid json`), 0644)
+			So(err, ShouldBeNil)
+
+			_, err = loadfile(c, fileName)
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("stale json", func() {
+			staleTime := float64(now.Unix()) - 161.0
+			err := ioutil.WriteFile(fileName, []byte(fmt.Sprintf(`
+			{
+			  "timestamp": %9f
+			}
+			`, staleTime)), 0644)
+			So(err, ShouldBeNil)
+
+			_, err = loadfile(c, fileName)
+			So(err, ShouldNotBeNil)
+		})
+	})
+}
