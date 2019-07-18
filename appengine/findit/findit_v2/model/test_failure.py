4f9571fafc8657e4842425e75d70488bb20aabc6
Chan <chanli@chromium.org>
2019-07-17 19:25:13 -0700

    [Findit v2] Define data models for test failure analysis.
    Bug: 978296
    Change-Id: Ibcdea46a07d81bab261f38929901be8e1bdb40b1

--- appengine/findit/findit_v2/model/test_failure.py
+++ appengine/findit/findit_v2/model/test_failure.py
@@ -11,69 +11,66 @@ from findit_v2.model.base_failure_analysis import BaseFailureAnalysis
 from findit_v2.model.failure_group import BaseFailureGroup
 from findit_v2.model.gitiles_commit import GitilesCommit
 from findit_v2.model.luci_build import LuciBuild
-from findit_v2.model.luci_build import LuciFailedBuild
 from gae_libs.model.versioned_model import VersionedModel
 
 
-def GetFailedTargets(compile_failures):
-  """Gets failed compile targets of each compile step."""
-  failed_targets = defaultdict(list)
-  for compile_failure in compile_failures or []:
-    failed_targets[compile_failure.step_ui_name].extend(
-        compile_failure.output_targets)
-  return {
-      step_ui_name: list(set(failed_targets_in_step))
-      for step_ui_name, failed_targets_in_step in failed_targets.iteritems()
-  }
+def GetTestFailures(test_failure_entities):
+  """Gets test failures in dict format."""
+  test_failures = {}
+  for test_failure_entity in test_failure_entities:
+    if not test_failures.get(test_failure_entity.step_ui_name):
+      test_failures[test_failure_entity.step_ui_name] = {
+          'tests': [],
+      }
+    step_failures = test_failures.get(test_failure_entity.step_ui_name)
+
+    if test_failure_entity.test:
+      step_failures['tests'].append({
+          'name': test_failure_entity.test,
+          'properties': test_failure_entity.properties
+      })
+    else:
+      step_failures['properties'] = test_failure_entity.properties
+
+  return test_failures
 
 
-class CompileFailure(AtomicFailure):
-  """Atom failure inside a compile failure - a failed compile target."""
-  # The list of targets the build edge will produce if it succeeds.
-  output_targets = ndb.StringProperty(repeated=True)
+class TestFailure(AtomicFailure):
+  """ Test failure that cannot be further divided.
 
-  # Compile rule, e.g. ACTION, CXX, etc.
-  # For chromium builds, it can be found in json.output[ninja_info] log of
-  # compile step.
-  # For chromeos builds, it can be found in build_compile_failure_output of
-  # build.
-  rule = ndb.StringProperty()
-
-  # The checked-in source files in the code base that the build edge takes as
-  # inputs. Only for CXX or CC failures.
-  dependencies = ndb.StringProperty(repeated=True)
+  Usually each entity should be for a test. But it's possible that Findit cannot
+  get test level information for any reason, if so the entity could be at
+  suite or step level.
+  """
+  # Name of the failed test
+  test = ndb.StringProperty()
 
   # Key to the failure that this failure merges into.
   # No analysis on current failure, instead use the results of merged_failure.
-  merged_failure_key = ndb.KeyProperty(kind='CompileFailure')
+  merged_failure_key = ndb.KeyProperty(kind='TestFailure')
 
   # Arguments number differs from overridden method - pylint: disable=W0221
   @classmethod
   def Create(cls,
              failed_build_key,
              step_ui_name,
-             output_targets,
-             rule=None,
+             test,
              first_failed_build_id=None,
              last_passed_build_id=None,
              failure_group_build_id=None,
              files=None,
-             dependencies=None,
              merged_failure_key=None,
              properties=None):
-    instance = super(CompileFailure, cls).Create(
+    instance = super(TestFailure, cls).Create(
         failed_build_key, step_ui_name, first_failed_build_id,
         last_passed_build_id, failure_group_build_id, files, properties)
-    instance.output_targets = output_targets or []
-    instance.rule = rule
-    instance.dependencies = dependencies or []
+    instance.test = test
     instance.merged_failure_key = merged_failure_key
     return instance
 
   def GetFailureIdentifier(self):
-    """Gets the identifier to differentiate a compile failure in the compile
-      step."""
-    return frozenset(self.output_targets) if self.output_targets else None
+    """Gets the identifier to differentiate a test failure in a step."""
+    return self.test
 
   def GetMergedFailure(self):
     """Gets the most up-to-date merged_failure for the current failure."""
@@ -100,45 +97,44 @@ class CompileFailure(AtomicFailure):
     return None
 
 
-class CompileFailureGroup(BaseFailureGroup):
-  """Class for group of compile failures."""
+class TestFailureGroup(BaseFailureGroup):
+  """Class for group of test failures."""
 
-  # Keys to the failed targets in the first build of the group, remain
+  # Keys to the failures in the first build of the group, remain
   # unchanged after the group is created.
-  # These targets are used to compare to failed targets other builds and decide
-  # if those targets can be added to the group.
-  # If they can, add the targets to the group by setting their
+  # These failures are used to compare to failures in other builds and decide
+  # if those failures can be added to the group.
+  # If they can, add the failures to this group by setting their
   # failure_group_build_id this group's id.
-  compile_failure_keys = ndb.KeyProperty(CompileFailure, repeated=True)
+  test_failure_keys = ndb.KeyProperty(TestFailure, repeated=True)
 
   @property
-  def failed_targets(self):
-    """Gets failed compile targets of each compile step that are included in the
-      group."""
-    failed_target_objects = ndb.get_multi(self.compile_failure_keys)
-    return GetFailedTargets(failed_target_objects)
+  def test_failures(self):
+    """Gets test failures that are included in the group."""
+    failed_target_objects = ndb.get_multi(self.test_failure_keys)
+    return GetTestFailures(failed_target_objects)
 
   # Arguments number differs from overridden method - pylint: disable=W0221
   @classmethod
   def Create(cls, luci_project, luci_bucket, build_id, gitiles_host,
              gitiles_project, gitiles_ref, last_passed_gitiles_id,
              last_passed_commit_position, first_failed_gitiles_id,
-             first_failed_commit_position, compile_failure_keys):
-    assert compile_failure_keys, (
-        'no failed_targets when creating CompileFailureGroup for {}'.format(
+             first_failed_commit_position, test_failure_keys):
+    assert test_failure_keys, (
+        'no failed_targets when creating TestFailureGroup for {}'.format(
             build_id))
 
-    instance = super(CompileFailureGroup, cls).Create(
+    instance = super(TestFailureGroup, cls).Create(
         luci_project, luci_bucket, build_id, gitiles_host, gitiles_project,
         gitiles_ref, last_passed_gitiles_id, last_passed_commit_position,
         first_failed_gitiles_id, first_failed_commit_position)
 
-    instance.compile_failure_keys = compile_failure_keys
+    instance.test_failure_keys = test_failure_keys
     return instance
 
 
-class CompileFailureAnalysis(BaseFailureAnalysis, VersionedModel):
-  """Class for a compile analysis.
+class TestFailureAnalysis(BaseFailureAnalysis, VersionedModel):
+  """Class for a test analysis.
 
   This class stores information that is needed during the analysis, and also
   some metadata for the analysis.
@@ -147,23 +143,15 @@ class CompileFailureAnalysis(BaseFailureAnalysis, VersionedModel):
   newer version, instead of deleting the existing analysis.
   """
   # Key to the failed targets this analysis analyzes.
-  compile_failure_keys = ndb.KeyProperty(CompileFailure, repeated=True)
-
-  @property
-  def failed_targets(self):
-    """Gets a list of failed compile targets of each compile step that are
-      analyzed in the analysis."""
-    failed_target_objects = ndb.get_multi(self.compile_failure_keys)
-    return GetFailedTargets(failed_target_objects)
+  test_failure_keys = ndb.KeyProperty(TestFailure, repeated=True)
 
   # Arguments number differs from overridden method - pylint: disable=W0221
   @classmethod
   def Create(cls, luci_project, luci_bucket, luci_builder, build_id,
              gitiles_host, gitiles_project, gitiles_ref, last_passed_gitiles_id,
              last_passed_commit_position, first_failed_gitiles_id,
-             first_failed_commit_position, rerun_builder_id,
-             compile_failure_keys):
-    instance = super(CompileFailureAnalysis, cls).Create(build_id)
+             first_failed_commit_position, rerun_builder_id, test_failure_keys):
+    instance = super(TestFailureAnalysis, cls).Create(build_id)
 
     last_passed_commit = GitilesCommit(
         gitiles_host=gitiles_host,
@@ -187,7 +175,7 @@ class CompileFailureAnalysis(BaseFailureAnalysis, VersionedModel):
     instance.last_passed_commit = last_passed_commit
     instance.first_failed_commit = first_failed_commit
     instance.rerun_builder_id = rerun_builder_id
-    instance.compile_failure_keys = compile_failure_keys
+    instance.test_failure_keys = test_failure_keys
 
     return instance
 
@@ -199,42 +187,48 @@ class CompileFailureAnalysis(BaseFailureAnalysis, VersionedModel):
     self.put()
 
 
-class CompileFailureInRerunBuild(ndb.Model):
-  """Atomic compile failure in a rerun build.
+class TestFailureInRerunBuild(ndb.Model):
+  """Atomic test failure in a rerun build.
 
   Since we only need to keep a simple record on what's failed in rerun build,
-  it's no need to reuse CompileFailure.
+  it's no need to reuse TestFailure.
   """
   # Full step name.
   step_ui_name = ndb.StringProperty()
 
-  # Output targets of one failed compile edge that the rerun build tested.
-  output_targets = ndb.StringProperty(repeated=True)
+  # Failed test name.
+  test = ndb.StringProperty()
 
 
-class CompileRerunBuild(LuciBuild):
+class TestRerunBuild(LuciBuild):
   """Class for a rerun build for a compile failure analysis."""
 
   # Compile failures in the rerun build.
-  failures = ndb.LocalStructuredProperty(
-      CompileFailureInRerunBuild, repeated=True)
+  failures = ndb.LocalStructuredProperty(TestFailureInRerunBuild, repeated=True)
 
-  def GetFailedTargets(self):
-    """Gets a list of failed compile targets of each compile step that failed
-      in the rerun build."""
-    return GetFailedTargets(self.failures)
+  def GetFailuresInBuild(self):
+    """Gets a list of failed tests of each step that failed in the rerun build.
+    """
+    test_failures = defaultdict(list)
+    for failure in self.failures or []:
+      test_failures[failure.step_ui_name].append(failure.test)
 
-  def SaveRerunBuildResults(self, status, detailed_compile_failures):
+    return {
+        step_ui_name: list(set(failed_targets_in_step) - {None})
+        for step_ui_name, failed_targets_in_step in test_failures.iteritems()
+    }
+
+  def SaveRerunBuildResults(self, status, detailed_test_failures):
     """Saves the results of this rerun build.
 
     Args:
       status (int): status of the build. See common_pb2 for available values.
-      detailed_compile_failures (dict): Compile failures in the rerun build.
+      detailed_test_failures (dict): Compile failures in the rerun build.
       Format is like:
       {
         'step_name': {
           'failures': {
-            frozenset(['target1', 'target2']): {
+            'test1': {
               'first_failed_build': {
                 'id': 8765432109,
                 'number': 123,
@@ -259,10 +253,10 @@ class CompileRerunBuild(LuciBuild):
     """
     self.status = status  # pylint: disable=attribute-defined-outside-init
     self.failures = []
-    for step_ui_name, step_info in detailed_compile_failures.iteritems():
-      for output_targets in step_info['failures']:
-        failure_entity = CompileFailureInRerunBuild(
-            step_ui_name=step_ui_name, output_targets=output_targets)
+    for step_ui_name, step_info in detailed_test_failures.iteritems():
+      for test in step_info['failures']:
+        failure_entity = TestFailureInRerunBuild(
+            step_ui_name=step_ui_name, test=test)
         self.failures.append(failure_entity)
     self.put()
 
