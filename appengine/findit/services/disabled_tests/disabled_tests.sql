777912b2362383bc47dc180f71bfeb943a3916c3
navil <navil@google.com>
2019-07-19 16:12:50 -0700

    Disabled Tests Query V2
    Redesigned disabled tests query that reduces the amount of data
    retrieved from ~277 GB to ~101 MB by elimination repetition and omit
    unnecessary columns.
    
    [Query design](https://docs.google.com/document/d/1rwJ-N3zCpecrF43ms4lf
    T-ACVdx7H23qPU-QqLtTC4U/edit?usp=sharing)
    Usage design info at go/cr-disabled-test-dashboard-doc under datasets.
    
    Bug: 967758, 973623
    Change-Id: Ia37a3a47ce25478bd12e91c5e324b9c515d820c3

--- appengine/findit/services/disabled_tests/disabled_tests.sql
+++ appengine/findit/services/disabled_tests/disabled_tests.sql
@@ -3,14 +3,14 @@
 # Assumptions for the disabled tests in this query are:
 # 1. A disabled test should have 'SKIP' in run.expected
 SELECT
-  path as test_name,
+  DISTINCT path AS test_name,
   step_name,
-  buildbot_info.master_name,
   buildbot_info.builder_name,
-  buildbot_info.build_number,
-  run.bugs
+  bug
 FROM
   `test-results-hrd.events.test_results`
+LEFT JOIN
+    UNNEST(run.bugs) AS bug
 WHERE
   # According to https://cloud.google.com/bigquery/docs/partitioned-tables,
   # _PARTITIONTIME is always the start of each day, so to make sure all data
@@ -18,3 +18,4 @@ WHERE
   # than or equal to the timestamp of 2 days ago.
   _PARTITIONTIME >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 2 day)
   AND 'SKIP' IN UNNEST(run.expected)
+  
