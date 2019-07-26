ce5986a29061b72c25b0dea413aaa00880945783
navil <navil@google.com>
2019-07-25 19:19:33 -0700

    Disabled Tests Query V2
    Redesigned disabled tests query that reduces the amount of data
    retrieved from ~277 GB to ~101 MB by elimination repetition and omit
    unnecessary columns.
    
    [Query design](https://docs.google.com/document/d/1rwJ-N3zCpecrF43ms4lfT-ACVdx7H23qPU-QqLtTC4U/edit?usp=sharing)
    Usage design info at go/cr-disabled-test-dashboard-doc under datasets.
    
    Bug: 967758, 973623
    Change-Id: Ia37a3a47ce25478bd12e91c5e324b9c515d820c3

--- appengine/findit/services/disabled_tests/disabled_tests.sql
+++ appengine/findit/services/disabled_tests/disabled_tests.sql
@@ -2,19 +2,39 @@
 #
 # Assumptions for the disabled tests in this query are:
 # 1. A disabled test should have 'SKIP' in run.expected
-SELECT
-  path as test_name,
-  step_name,
-  buildbot_info.master_name,
-  buildbot_info.builder_name,
-  buildbot_info.build_number,
-  run.bugs
-FROM
-  `test-results-hrd.events.test_results`
-WHERE
+WITH
+  last_runs AS (
+  SELECT
+    path AS test_name,
+    step_name,
+    buildbot_info.builder_name,
+    ARRAY_AGG( STRUCT(build_id,
+        run)
+    ORDER BY
+      start_time DESC
+    LIMIT
+      1)[
+  OFFSET
+    (0)] latest_run
+  FROM
+    `test-results-hrd.events.test_results` AS tr
+  WHERE
   # According to https://cloud.google.com/bigquery/docs/partitioned-tables,
   # _PARTITIONTIME is always the start of each day, so to make sure all data
   # within the past 1 day is included, _PARTITIONTIME needs to be greater
   # than or equal to the timestamp of 2 days ago.
-  _PARTITIONTIME >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 2 day)
-  AND 'SKIP' IN UNNEST(run.expected)
+    _PARTITIONTIME >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 2 day)
+  GROUP BY
+    1,
+    2,
+    3)
+SELECT
+  test_name,
+  step_name,
+  builder_name,
+  latest_run.build_id,
+  latest_run.run.bugs
+FROM
+  last_runs
+WHERE
+  'SKIP' IN UNNEST(latest_run.run.expected)
