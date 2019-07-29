59ae246a7e5de0ac78f9a33882ca974db4273586
Yuke Liao <liaoyuke@chromium.org>
2019-07-29 16:46:08 -0700

    [code coverage] Calculate equivalent patchsets
    This CL adds a function to calculate equivalent patchsets to assist
    sharing coverage data between patchsets that are commit-message or
    trivial-rebase away.
    
    Bug: 910289
    Change-Id: Ie18697037d9cd5a73705d1aa23dfb06796cb6138

--- appengine/findit/services/code_coverage.py
+++ appengine/findit/services/code_coverage.py
@@ -3,6 +3,15 @@
 # found in the LICENSE file.
 """Utility functions for code coverage."""
 
+import json
+import urllib2
+
+from common.findit_http_client import FinditHttpClient
+from gae_libs.caches import PickledMemCache
+from libs.cache_decorator import Cached
+
+# Mapping from metrics names to detailed explainations, and one use case is to
+# use as the tooltips.
 _METRIC_NAME_DETAIL_MAPPING = {
     'line': (
         "Line coverage is the percentage of code lines which have been "
@@ -25,6 +34,31 @@ _METRIC_NAME_DETAIL_MAPPING = {
         "Java instruction coverage is the percentage of the Java byte code "
         "instructions which have been executed at least once."),
 }
+
+# List of patchset kind that are applicable for sharing coverage data between
+# patchsets, and the list of possible kinds is defined in:
+# https://gerrit-review.googlesource.com/Documentation/json.html
+#
+# REWORK
+# Nontrivial content changes.
+#
+# TRIVIAL_REBASE
+# Conflict-free merge between the new parent and the prior patch set.
+#
+# MERGE_FIRST_PARENT_UPDATE
+# Conflict-free change of first (left) parent of a merge commit.
+#
+# NO_CODE_CHANGE
+# No code changed; same tree and same parent tree.
+#
+# NO_CHANGE
+# No changes; same commit message, same tree and same parent tree.
+_NON_CONFLICT_CHANGE_KIND = [
+    'TRIVIAL_REBASE', 'MERGE_FIRST_PARENT_UPDATE', 'NO_CODE_CHANGE', 'NO_CHANGE'
+]
+
+# Duration (in seconds) to cache equivalent patchsets for a given patchset.
+_EQUIVALENT_PATCHSETS_CACHE_EXPIRE_TIME_SECONDS = 7 * 24 * 60 * 60
 
 
 def GetMetricsBasedOnCoverageTool(coverage_tool):
@@ -51,3 +85,58 @@ def GetMetricsBasedOnCoverageTool(coverage_tool):
       'name': m,
       'detail': _METRIC_NAME_DETAIL_MAPPING.get(m, '')
   } for m in metrics]
+
+
+@Cached(
+    PickledMemCache(),
+    namespace='coverage_equivalent_patchsets',
+    expire_time=_EQUIVALENT_PATCHSETS_CACHE_EXPIRE_TIME_SECONDS)
+def GetEquivalentPatchsets(host, project, change, patchset):
+  """Gets equivalent patchsets that are applicable for sharing coverag data.
+
+  The reason why this is not just the current patchset number is because there
+  may have been a succession of "trivial" changes before the current patchset.
+
+  Args:
+    host (str): The host name.
+    project (str): The project name.
+    change (str): The change number.
+    patchset (str): The patchset number.
+
+  Returns:
+    A list of patchset numbers in descending order and the current patchset is
+    included. In case this method fails to communicate with Gerrit, an mepty
+    list is returned.
+  """
+  project_quoted = urllib2.quote(project, safe='')
+
+  # Uses the Get Change API to get and parse the details of this change.
+  # https://gerrit-review.googlesource.com/Documentation/rest-api-changes.html#get-change.
+  template_to_get_change = (
+      'https://%s/changes/%s~%s?o=ALL_REVISIONS&o=SKIP_MERGEABLE')
+  url = template_to_get_change % (host, project_quoted, change)
+  status_code, content, _ = FinditHttpClient().Get(url)
+  if status_code != 200:
+    logging.error('todo')
+    return []
+
+  # Remove XSSI magic prefix
+  if content.startswith(')]}\''):
+    content = content[4:]
+  change_details = json.loads(content)
+
+  revisions = change_details['revisions'].values()
+  revisions.sort(key=lambda r: r['_number'], reverse=True)
+  patchsets = []
+  for r in revisions:
+    current_patchset = r['_number']
+    if current_patchset > patchset:
+      continue
+
+    patchsets.append(current_patchset)
+    if r['kind'] not in _NON_CONFLICT_CHANGE_KIND:
+      # If this revision was a non-trivial change, don't consider patchsets
+      # prior to it.
+      break
+
+  return patchsets
