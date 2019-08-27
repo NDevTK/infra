cc2d17f4848663b2e14598ec82a7f4ffa78aacad
Aviv Keshet <akeshet@chromium.org>
2019-08-27 13:39:52 -0700

    qscheduler:  overhaul batchrunner; run multiple assignments in single pass
    This CL overhauls batchrunner in a few ways:
     - Makes it no longer run generic state-mutating operations, thus
     simplifying its API.
     - Makes types.Operation no longer return an error; in reality none of
     the prior error returning paths were important, and handling errors
     from different requests within a batch differently was a large source
     of complexity.
     - Simplifies the test fixture.
     - Calls all Assign operations within a batch in a single scheduler
     pass.
    
    BUG=977005
    TEST=unittests updated
    
    Change-Id: I68ac514f593389e46f62fe6dbf9160ae84ba04ba

--- go/src/infra/appengine/qscheduler-swarming/app/state/batchrunner_test.go
+++ go/src/infra/appengine/qscheduler-swarming/app/state/batchrunner_test.go
@@ -16,25 +16,27 @@ package state_test
 
 import (
 	"context"
-	"strings"
+	"fmt"
 	"sync"
 	"testing"
 	"time"
 
-	"github.com/pkg/errors"
+	"github.com/google/uuid"
 	. "github.com/smartystreets/goconvey/convey"
 	"go.chromium.org/luci/appengine/gaetesting"
+	"go.chromium.org/luci/common/clock/testclock"
 
 	"infra/appengine/qscheduler-swarming/app/eventlog"
 	"infra/appengine/qscheduler-swarming/app/state"
 	"infra/appengine/qscheduler-swarming/app/state/nodestore"
-	"infra/appengine/qscheduler-swarming/app/state/types"
-	"infra/qscheduler/qslib/scheduler"
+	"infra/qscheduler/qslib/tutils"
+	"infra/swarming"
 )
 
-func TestBatcherErrors(t *testing.T) {
+func TestBatcherCancellations(t *testing.T) {
 	Convey("Given a testing context with a scheduler pool, and a batcher for that pool", t, func() {
 		ctx := gaetesting.TestingContext()
+		ctx, _ = testclock.UseTime(ctx, time.Now())
 		ctx = eventlog.Use(ctx, &eventlog.NullBQInserter{})
 		poolID := "pool 1"
 		store := nodestore.New(poolID)
@@ -44,40 +46,60 @@ func TestBatcherErrors(t *testing.T) {
 		batcher.Start(store)
 		defer batcher.Close()
 
-		Convey("an error in one operation should only affect that operation.", func() {
-			var goodError error
-			goodOperation := func(ctx context.Context, s *types.QScheduler, m scheduler.EventSink) error {
-				return nil
-			}
+		Convey("with a leader request and a bunch of followers", func() {
+			leaderctx, leadercancel := context.WithCancel(ctx)
+			defer leadercancel()
+			nFollowers := 5
+			errs := make([]error, nFollowers+1)
 
-			var badError error
-			badOperation := func(ctx context.Context, s *types.QScheduler, m scheduler.EventSink) error {
-				return errors.New("a bad error occurred")
-			}
-
-			wg := sync.WaitGroup{}
-			wg.Add(2)
+			leaderWait := sync.WaitGroup{}
+			leaderWait.Add(1)
 
 			go func() {
-				done := batcher.EnqueueOperation(ctx, goodOperation, 0)
-				goodError = <-done
-				wg.Done()
+				_, err := batcher.Notify(leaderctx, &swarming.NotifyTasksRequest{})
+				errs[0] = err
+				leaderWait.Done()
 			}()
+			batcher.TBatchWait(1)
 
-			go func() {
-				done := batcher.EnqueueOperation(ctx, badOperation, 0)
-				badError = <-done
-				wg.Done()
-			}()
+			followerWait := sync.WaitGroup{}
+			followerWait.Add(nFollowers)
 
-			batcher.TBatchStart()
-			batcher.TBatchWait(2)
-			wg.Wait()
+			followerctx, followercancel := context.WithCancel(ctx)
+			defer followercancel()
+			addFollowers := func() {
+				for i := 0; i < nFollowers; i++ {
+					go func(i int) {
+						_, err := batcher.Notify(followerctx, &swarming.NotifyTasksRequest{})
+						errs[i+1] = err
+						followerWait.Done()
+					}(i)
+				}
+			}
 
-			So(badError, ShouldNotBeNil)
-			So(badError.Error(), ShouldEqual, "a bad error occurred")
+			Convey("when the leader context is cancelled, the whole batch unwinds.", func() {
+				addFollowers()
+				batcher.TBatchWait(nFollowers)
+				leadercancel()
+				leaderWait.Wait()
+				followerWait.Wait()
+				for _, err := range errs {
+					So(err, ShouldEqual, context.Canceled)
+				}
+			})
 
-			So(goodError, ShouldBeNil)
+			Convey("when the follower contexts are cancelled prior to their being batched, only they are unwound.", func() {
+				followercancel()
+				batcher.TBatchStart()
+				addFollowers()
+				leaderWait.Wait()
+				followerWait.Wait()
+				So(errs[0], ShouldBeNil)
+
+				for _, err := range errs[1:] {
+					So(err, ShouldEqual, context.Canceled)
+				}
+			})
 		})
 	})
 }
@@ -85,6 +107,7 @@ func TestBatcherErrors(t *testing.T) {
 func TestBatcherBehavior(t *testing.T) {
 	Convey("Given a testing context with a scheduler pool, and a batcher for that pool", t, func() {
 		ctx := gaetesting.TestingContext()
+		ctx, _ = testclock.UseTime(ctx, time.Now())
 		ctx = eventlog.Use(ctx, &eventlog.NullBQInserter{})
 		poolID := "pool 1"
 		store := nodestore.New(poolID)
@@ -94,39 +117,74 @@ func TestBatcherBehavior(t *testing.T) {
 		batcher.Start(store)
 		defer batcher.Close()
 
-		Convey("a batch of requests are run in priority order.", func() {
-			s := &[]string{}
-			operationA := func(_ context.Context, _ *types.QScheduler, _ scheduler.EventSink) error {
-				temp := append(*s, "A")
-				s = &temp
-				return nil
+		Convey("a batch of requests can run, with notifications coming before assignments.", func() {
+			nTasks := 5
+			labels := make([]string, nTasks)
+			// Give each bot-task pair a unique dimension.
+			for i := range labels {
+				labels[i] = uuid.New().String()
 			}
-			operationB := func(_ context.Context, _ *types.QScheduler, _ scheduler.EventSink) error {
-				temp := append(*s, "B")
-				s = &temp
-				return nil
-			}
+			assignements := make([]*swarming.AssignTasksResponse, nTasks)
+			now := tutils.TimestampProto(time.Now())
 
 			wg := sync.WaitGroup{}
-			for i := 0; i < 10; i++ {
+			for i := 0; i < nTasks; i++ {
 				wg.Add(2)
-				go func() {
-					done := batcher.EnqueueOperation(ctx, operationA, state.BatchPriorityNotify)
-					<-done
+				// Run nTasks assignment requests concurrently.
+				go func(i int) {
+					req := &swarming.AssignTasksRequest{
+						IdleBots: []*swarming.IdleBot{
+							{
+								BotId:      fmt.Sprintf("%d", i),
+								Dimensions: []string{labels[i]},
+							},
+						},
+						Time: now,
+					}
+					resp, err := batcher.Assign(ctx, req)
+					if err != nil {
+						panic(err)
+					}
+					assignements[i] = resp
 					wg.Done()
-				}()
-				go func() {
-					done := batcher.EnqueueOperation(ctx, operationB, state.BatchPriorityAssign)
-					<-done
+				}(i)
+				// Also run nTasks task notifications concurrently.
+				go func(i int) {
+					req := &swarming.NotifyTasksRequest{
+						Notifications: []*swarming.NotifyTasksItem{
+							{
+								Task: &swarming.TaskSpec{
+									EnqueuedTime: now,
+									Id:           fmt.Sprintf("%d", i),
+									State:        swarming.TaskState_PENDING,
+									Slices: []*swarming.SliceSpec{
+										{
+											Dimensions: []string{labels[i]},
+										},
+									},
+								},
+								Time: now,
+							},
+						},
+					}
+					resp, err := batcher.Notify(ctx, req)
+					if err != nil {
+						panic(err)
+					}
+					if resp == nil {
+						panic("unexpectedly nil response")
+					}
 					wg.Done()
-				}()
+				}(i)
 			}
+			batcher.TBatchWait(2 * nTasks)
 			batcher.TBatchStart()
-			batcher.TBatchWait(20)
 			wg.Wait()
-
-			j := strings.Join(*s, "")
-			So(j, ShouldEqual, "AAAAAAAAAABBBBBBBBBB")
+			// All tasks should be assigned to their corresponding idle bot.
+			for _, a := range assignements {
+				So(a.Assignments, ShouldHaveLength, 1)
+				So(a.Assignments[0].BotId, ShouldEqual, a.Assignments[0].TaskId)
+			}
 		})
 	})
 }
