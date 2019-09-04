5021cdf886ab12e3b709ec1867d4cfdd06cddb78
Allen Li <ayatane@chromium.org>
2019-09-03 17:26:44 -0700

    drone-queen: Fix handling of empty string DUTs
    For some reason, crosskylabadmin is passing DUTs with empty string
    hostnames to drone queen.  These are interpreted as "generate a new
    ID".  Therefore, we are inserting TONs of DUTs with randomly generated
    IDs into drone queen.
    
    This CL fixes it.
    
    Bug: 1000507
    Change-Id: I6a9ba0b159a3edb695698093122b204705dcfeda

--- go/src/infra/appengine/drone-queen/internal/frontend/queen.go
+++ go/src/infra/appengine/drone-queen/internal/frontend/queen.go
@@ -164,7 +164,8 @@ func (q *DroneQueenImpl) DeclareDuts(ctx context.Context, req *api.DeclareDutsRe
 		var modifiedDUTs []*entities.DUT
 		var newDUTs []entities.DUTID
 		declared := make(map[entities.DUTID]bool)
-		for _, dut := range req.GetDuts() {
+		reqDUTs := filterInvalidDUTs(req.GetDuts())
+		for _, dut := range reqDUTs {
 			dutID := entities.DUTID(dut)
 			if dut, ok := existing[dutID]; ok {
 				dut.Draining = false
@@ -198,6 +199,17 @@ func (q *DroneQueenImpl) DeclareDuts(ctx context.Context, req *api.DeclareDutsRe
 		return nil, err
 	}
 	return &api.DeclareDutsResponse{}, nil
+}
+
+func filterInvalidDUTs(s []string) []string {
+	new := make([]string, 0, len(s))
+	for _, s := range s {
+		if s == "" {
+			continue
+		}
+		new = append(new, s)
+	}
+	return new
 }
 
 // ListDrones implements service interfaces.
