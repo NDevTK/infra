3ad8225a03769dc9c57db2727cc834da953b778b
Oleh Prypin <oprypin@google.com>
2019-09-09 09:49:55 +0200

    Add a crawler that makes a static copy of Rietveld, and a server for it
    Change-Id: I69856f0f997f596668f4fa81f0f30c0316c95356

---
+++ appengine/rietveld_crawler/server.py
@@ -0,0 +1,28 @@
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is govered by a BSD-style
+# license that can be found in the LICENSE file or at
+# https://developers.google.com/open-source/licenses/bsd
+
+import datetime
+
+import flask
+
+import common
+
+CACHE_TIMEOUT = datetime.timedelta(days=365).total_seconds()
+
+app = flask.Flask(__name__, static_folder=None)
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve_file(path):
+  query = flask.request.query_string.decode()
+  mimetype = common.content_type(path, query)
+  path = common.make_path(path, query)
+  return flask.send_from_directory('files', path, mimetype=mimetype,
+                                   cache_timeout=CACHE_TIMEOUT)
+
+
+if __name__ == '__main__':
+  app.run('0.0.0.0', 8000)
