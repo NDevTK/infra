9004027aa1d2f92d75c27961de9ed67cfd710628
Alex Zamorzaev <zamorzaev@chromium.org>
2019-12-26 18:49:19 -0800

    cros_test_platform: Pipe through the BB client.
    This CL just modifies the interfaces of all functions that will require
    a BB client without any change in logic.
    
    Bug: 1033291
    
    skylab lib: Add test_runner BB request builder
    
    Bug: 1033291
    Change-Id: I0feedace85d71c8bfcaa39f88de2ef604029ee6f

--- go/src/infra/libs/skylab/request/request.go
+++ go/src/infra/libs/skylab/request/request.go
@@ -11,6 +11,12 @@ import (
 	"strings"
 	"time"
 
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/duration"
+	structpb "github.com/golang/protobuf/ptypes/struct"
+	"go.chromium.org/chromiumos/infra/proto/go/test_platform/skylab_test_runner"
+	buildbucket_pb "go.chromium.org/luci/buildbucket/proto"
 	swarming "go.chromium.org/luci/common/api/swarming/swarming/v1"
 	"go.chromium.org/luci/common/data/strpair"
 	"go.chromium.org/luci/common/errors"
@@ -46,6 +52,183 @@ type Args struct {
 	ParentTaskID      string
 	//Pubsub Topic for status updates on the tests run for the request
 	StatusTopic string
+	// BuilderID identifies the builder that will run the test task.
+	BuilderID *buildbucket_pb.BuilderID
+	// Test describes the test to be run.
+	Test *skylab_test_runner.Request_Test
+}
+
+// BuildbucketNewBuildRequest returns the Buildbucket request to create the
+// test_runner build with these arguments.
+func (a *Args) BuildbucketNewBuildRequest() (*buildbucket_pb.ScheduleBuildRequest, error) {
+	dims, err := a.getBBDimensions()
+	if err != nil {
+		return nil, errors.Annotate(err, "create bb request").Err()
+	}
+
+	provisionableLabels, err := provisionDimensionsToLabelDict(a.ProvisionableDimensions)
+	if err != nil {
+		return nil, errors.Annotate(err, "create bb request").Err()
+	}
+
+	// TODO(crbug.com/1036559#c1): Add timeouts.
+	req, err := requestToStructPB(&skylab_test_runner.Request{
+		Prejob: &skylab_test_runner.Request_Prejob{
+			ProvisionableLabels: provisionableLabels,
+		},
+		Test: a.Test,
+	})
+	if err != nil {
+		return nil, errors.Annotate(err, "create bb request").Err()
+	}
+
+	props := &structpb.Struct{
+		Fields: map[string]*structpb.Value{
+			"request": req,
+		},
+	}
+
+	tags, err := splitTagPairs(a.SwarmingTags)
+	if err != nil {
+		return nil, errors.Annotate(err, "create bb request").Err()
+	}
+
+	return &buildbucket_pb.ScheduleBuildRequest{
+		Builder:    a.BuilderID,
+		Properties: props,
+		Tags:       tags,
+		Dimensions: dims,
+		Priority:   int32(a.Priority),
+		Swarming: &buildbucket_pb.ScheduleBuildRequest_Swarming{
+			ParentRunId: a.ParentTaskID,
+		},
+		Notify: getNotificationConfig(a.StatusTopic),
+	}, nil
+}
+
+// getBBDimensions returns both required and optional dimensions that will be
+// used to match this request with a Swarming bot.
+func (a *Args) getBBDimensions() ([]*buildbucket_pb.RequestedDimension, error) {
+	dims := schedulableLabelsToRequestedDimensions(&a.SchedulableLabels)
+
+	optionalDimExpiration := 30 * time.Second
+	pd, err := stringsToRequestedDimensions(a.ProvisionableDimensions, &optionalDimExpiration)
+	if err != nil {
+		return nil, errors.Annotate(err, "get BB dimensions").Err()
+	}
+	dims = append(dims, pd...)
+
+	// TODO(zamorzaev): move the dut_state dimension to the builder config.
+	dims = append(dims, &buildbucket_pb.RequestedDimension{
+		Key:   "dut_state",
+		Value: "ready",
+	})
+
+	extraDims, err := stringsToRequestedDimensions(a.Dimensions, nil)
+	if err != nil {
+		return nil, errors.Annotate(err, "get BB dimensions").Err()
+	}
+	dims = append(dims, extraDims...)
+	return dims, nil
+}
+
+// schedulableLabelsToRequestedDimensions converts a slice of strings in foo:bar form to a slice of BB
+// rpc requested dimensions.
+func schedulableLabelsToRequestedDimensions(inv *inventory.SchedulableLabels) []*buildbucket_pb.RequestedDimension {
+	var rd []*buildbucket_pb.RequestedDimension
+	id := swarming_inventory.Convert(inv)
+	for key, values := range id {
+		for _, value := range values {
+			rd = append(rd, &buildbucket_pb.RequestedDimension{
+				Key:   key,
+				Value: value,
+			})
+		}
+	}
+	return rd
+}
+
+// stringsToRequestedDimensions converts a slice of strings in foo:bar form to
+// a slice of BB rpc requested dimensions.
+func stringsToRequestedDimensions(dims []string, expiration *time.Duration) ([]*buildbucket_pb.RequestedDimension, error) {
+	var expirationPb *duration.Duration
+	if expiration != nil {
+		expirationPb = ptypes.DurationProto(*expiration)
+	}
+
+	ret := make([]*buildbucket_pb.RequestedDimension, len(dims))
+	for i, d := range dims {
+		k, v := strpair.Parse(d)
+		if v == "" {
+			return nil, fmt.Errorf("malformed dimension with key '%s' has no value", k)
+		}
+		ret[i] = &buildbucket_pb.RequestedDimension{
+			Key:        k,
+			Value:      v,
+			Expiration: expirationPb,
+		}
+	}
+	return ret, nil
+}
+
+// provisionDimensionsToLabelDict converts provisionable dimensions to labels.
+func provisionDimensionsToLabelDict(dims []string) (map[string]string, error) {
+	labels := make(map[string]string)
+	for _, d := range dims {
+		k, v := strpair.Parse(d)
+		if v == "" {
+			return nil, fmt.Errorf("malformed provisionable dimension with key '%s' has no value", k)
+		}
+		k = strings.TrimPrefix(k, "provisionable-")
+		labels[k] = v
+	}
+	return labels, nil
+}
+
+// splitTagPairs converts a slice of strings in foo:bar form to a slice of BB
+// rpc string pairs.
+func splitTagPairs(tags []string) ([]*buildbucket_pb.StringPair, error) {
+	ret := make([]*buildbucket_pb.StringPair, len(tags))
+	for i, t := range tags {
+		k, v := strpair.Parse(t)
+		if v == "" {
+			return nil, fmt.Errorf("malformed tag with key '%s' has no value", k)
+		}
+		ret[i] = &buildbucket_pb.StringPair{
+			Key:   k,
+			Value: v,
+		}
+	}
+	return ret, nil
+}
+
+// requestToStructPB converts a skylab_test_runner.Request into a Struct
+// with the same JSON presentation.
+func requestToStructPB(from *skylab_test_runner.Request) (*structpb.Value, error) {
+	m := jsonpb.Marshaler{}
+	jsonStr, err := m.MarshalToString(from)
+	if err != nil {
+		return nil, err
+	}
+	reqStruct := &structpb.Struct{}
+	if err := jsonpb.UnmarshalString(jsonStr, reqStruct); err != nil {
+		return nil, err
+	}
+	return &structpb.Value{
+		Kind: &structpb.Value_StructValue{StructValue: reqStruct},
+	}, nil
+}
+
+// getNotificationConfig constructs a valid NotificationConfig.
+func getNotificationConfig(topic string) *buildbucket_pb.NotificationConfig {
+	if topic == "" {
+		// BB will crash if it encounters a non-nil NotificationConfig with an
+		// empty PubsubTopic.
+		return nil
+	}
+	return &buildbucket_pb.NotificationConfig{
+		PubsubTopic: topic,
+	}
 }
 
 // SwarmingNewTaskRequest returns the Swarming request to create the Skylab
