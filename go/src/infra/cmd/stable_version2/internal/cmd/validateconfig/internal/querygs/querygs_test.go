351153a6eba1cf3a91f7424975926fae90558176
Gregory Nisbet <gregorynisbet@google.com>
2020-01-21 13:58:06 -0800

    [stableversion] check metadata.json files in chromeos-image-archive
    BUG=chromium:1041548
    TEST=go test querygs_test.go
    Change-Id: I09ba75690835434e9da02e194c7efcc7912b7a81

--- go/src/infra/cmd/stable_version2/internal/cmd/validateconfig/internal/querygs/querygs_test.go
+++ go/src/infra/cmd/stable_version2/internal/cmd/validateconfig/internal/querygs/querygs_test.go
@@ -5,11 +5,15 @@
 package querygs
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	labPlatform "go.chromium.org/chromiumos/infra/proto/go/lab_platform"
 	"go.chromium.org/luci/common/gcloud/gs"
+
+	"infra/libs/cros/stableversion/validateconfig"
 )
 
 const DONTCARE = "f7e8bdf6-f67c-4d63-aea3-46fa5e980403"
@@ -79,8 +83,166 @@ func TestMaybeDownloadFile(t *testing.T) {
 	}
 }
 
+var testFirmwareVersionData = []struct {
+	name        string
+	uuid        string
+	metadata    string
+	bt          string
+	model       string
+	CrOSVersion string
+	out         string
+}{
+	{
+		"extract firmware version \"sona\"",
+		"20db725f-9f0e-457b-8da7-3fb6dfb57b7b",
+		exampleMetadataJSON,
+		"nami",
+		"sona",
+		"xxx-cros-version",
+		"Google_Nami.42.43.44",
+	},
+	{
+		"extract firmware version \"nami\"",
+		"c9b5c1e3-a40f-4d5a-8353-6f890d80d9be",
+		exampleMetadataJSON,
+		"nami",
+		"akali360",
+		"xxx-cros-version",
+		"Google_Nami.52.53.54",
+	},
+}
+
+func TestGetFirmwareVersion(t *testing.T) {
+	t.Parallel()
+	for _, tt := range testFirmwareVersionData {
+		t.Run(tt.name, func(t *testing.T) {
+			var r Reader
+			r.dld = makeConstantDownloader(tt.metadata)
+			version, e := r.getFirmwareVersion(tt.bt, tt.model, tt.CrOSVersion)
+			if e != nil {
+				msg := fmt.Sprintf("name (%s): uuid (%s): unexpected error (%s)", tt.name, tt.uuid, e.Error())
+				t.Errorf(msg)
+			}
+			diff := cmp.Diff(tt.out, version)
+			if diff != "" {
+				msg := fmt.Sprintf("name (%s): uuid (%s): unexpected diff (%s)", tt.name, tt.uuid, diff)
+				t.Errorf(msg)
+			}
+		})
+	}
+}
+
+var testValidateConfigData = []struct {
+	name     string
+	uuid     string
+	metadata string
+	in       string
+	out      string
+	hasErr   bool
+}{
+	{
+		"empty",
+		"b2b7aa51-3c2b-4c3f-93bf-0b26e6483489",
+		`{}`,
+		`{}`,
+		`{
+			"missing_boards": null,
+			"failed_to_lookup": null,
+			"invalid_versions": null
+		}`,
+		false,
+	},
+	{
+		"two present boards",
+		"f63476d1-0382-4098-8a15-18fcb1a2e61a",
+		exampleMetadataJSON,
+		`{
+			"cros": [
+				{
+					"key": {
+						"buildTarget": {"name": "nami"},
+						"modelId": {}
+					},
+					"version": "R81-12835.0.0"
+				}
+			],
+			"firmware": [
+				{
+					"key": {
+						"modelId": {"value": "sona"},
+						"buildTarget": {"name": "nami"}
+					},
+					"version": "Google_Nami.42.43.44"
+				},
+				{
+					"key": {
+						"modelId": {"value": "akali360"},
+						"buildTarget": {"name": "nami"}
+					},
+					"version": "Google_Nami.52.53.54"
+				}
+			]
+		}`,
+		`{
+			"missing_boards": null,
+			"failed_to_lookup": null,
+			"invalid_versions": null
+		}`,
+		false,
+	},
+}
+
+func TestValidateConfig(t *testing.T) {
+	t.Parallel()
+	for _, tt := range testValidateConfigData {
+		t.Run(tt.name, func(t *testing.T) {
+			var r Reader
+			r.dld = makeConstantDownloader(tt.metadata)
+			sv := parseStableVersionsOrPanic(tt.in)
+			expected := parseResultsOrPanic(tt.out)
+			result, e := r.ValidateConfig(sv)
+			if tt.hasErr {
+				if e == nil {
+					msg := fmt.Sprintf("name (%s): uuid (%s): there should have been an error", tt.name, tt.uuid)
+					t.Errorf(msg)
+				}
+			} else {
+				if e != nil {
+					msg := fmt.Sprintf("name (%s): uuid (%s): unexpected error (%s)", tt.name, tt.uuid, e.Error())
+					t.Errorf(msg)
+				}
+			}
+			diff := cmp.Diff(expected, result)
+			if diff != "" {
+				msg := fmt.Sprintf("name (%s): uuid (%s): unexpected diff (%s)", tt.name, tt.uuid, diff)
+				t.Errorf(msg)
+			}
+		})
+	}
+}
+
 func makeConstantDownloader(content string) downloader {
 	return func(gsPath gs.Path) ([]byte, error) {
 		return []byte(content), nil
 	}
+}
+
+// parseStableVersionsOrPanic is a helper function that's used in tests to feed
+// a stable version file contained in a string literal to a test.
+func parseStableVersionsOrPanic(content string) *labPlatform.StableVersions {
+	out, err := validateconfig.ParseStableVersions([]byte(content))
+	if err != nil {
+		panic(err.Error())
+	}
+	return out
+}
+
+// parseStableVersionsOrPanic is a helper function that's used in tests to feed
+// a result file contained in a string literal to a test.
+func parseResultsOrPanic(content string) *ValidationResult {
+	var out ValidationResult
+	if err := json.Unmarshal([]byte(content), &out); err != nil {
+		panic(err.Error())
+	}
+	return &out
 }
