83db9dcd25a52887605578a67c0bcf019f7531e1
Jacob Kopczynski <jkop@google.com>
2020-04-16 17:40:32 -0700

    Fail if upload destination still empty. Add debug logging.
    The phosphorus upload command seems to be failing mysteriously but not
    erroring. This remedies the latter and may help debug the former.
    
    BUG=chromium:1022986
    
    Change-Id: I9ec74b8e915b3af5ea92d27ee5760663e4ee38d4

--- go/src/infra/cros/cmd/phosphorus/internal/gs/gs.go
+++ go/src/infra/cros/cmd/phosphorus/internal/gs/gs.go
@@ -2,6 +2,7 @@ package gs
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -13,11 +14,12 @@ import (
 	"go.chromium.org/luci/auth/client/authcli"
 	"go.chromium.org/luci/common/errors"
 	gcgs "go.chromium.org/luci/common/gcloud/gs"
+	"go.chromium.org/luci/common/logging"
 )
 
 // DirWriter Mockable interface for writing whole directories at once
 type DirWriter interface {
-	WriteDir() error
+	WriteDir(ctx context.Context) error
 }
 
 type prodDirWriter struct {
@@ -62,12 +64,12 @@ func NewDirWriter(localPath string, gsPath Path, client AuthedClient) (DirWriter
 func verifyPaths(localPath string, gsPath string) error {
 	problems := []string{}
 	if _, err := os.Stat(localPath); err != nil {
-		problems = append(problems, "invalid local path")
+		problems = append(problems, fmt.Sprintf("invalid local path (%s)", localPath))
 	} else if _, err := os.Open(localPath); err != nil {
-		problems = append(problems, "unreadable local path")
+		problems = append(problems, fmt.Sprintf("unreadable local path (%s)", localPath))
 	}
 	if _, err := url.Parse(gsPath); err != nil {
-		problems = append(problems, "invalid GS path")
+		problems = append(problems, fmt.Sprintf("invalid GS path (%s)", gsPath))
 	}
 	if len(problems) > 0 {
 		return errors.Reason("path errors: %s", strings.Join(problems, ", ")).Err()
@@ -80,37 +82,64 @@ type Path gcgs.Path
 
 // WriteDir Write all files in the subtree of the local path to the corresponding places
 //  in the subtree of the GS path
-func (w *prodDirWriter) WriteDir() error {
+func (w *prodDirWriter) WriteDir(ctx context.Context) error {
+	logging.Debugf(ctx, "writing %s", w.localRootDir)
 	writeOne := func(src string, info os.FileInfo, err error) error {
 		if info.IsDir() {
+			logging.Debugf(ctx, "file %s is a directory", src)
 			return nil
-		}
-		if err != nil {
-			return err
 		}
 		relPath, err := filepath.Rel(w.localRootDir, src)
 		if err != nil {
-			return err
+			return errors.Annotate(err, "writing from %s to %s/<?>", src, w.gsRootDir).Err()
 		}
-		dest := Path(w.gsRootDir.Concat(relPath))
+		gsDest := w.gsRootDir.Concat(relPath)
+		dest := Path(gsDest)
+		logging.Debugf(ctx, "writing from %s to %s", src, dest)
 		f, err := os.Open(src)
 		if err != nil {
-			return err
+			return errors.Annotate(err, "writing from %s to %s", src, dest).Err()
+		}
+		if info.Size() <= 0 {
+			logging.Warningf(ctx, "file %s empty", src)
+			return nil
 		}
 		writer, err := w.client.NewWriter(dest)
 		if err != nil {
-			return err
+			return errors.Annotate(err, "writing from %s to %s", src, dest).Err()
 		}
 		bs := make([]byte, info.Size())
 		if _, err = f.Read(bs); err != nil {
-			return err
-		}
-		if _, err = writer.Write(bs); err != nil {
 			return errors.Annotate(err, "writing from %s to %s", src, dest).Err()
 		}
+		if n, err := writer.Write(bs); err != nil || n <= 0 {
+			if err != nil {
+				return errors.Annotate(err, "writing from %s to %s", src, dest).Err()
+			}
+			return errors.Reason("nothing written to %s", dest).Err()
+		} else {
+			logging.Debugf(ctx, "wrote %d bytes to %s", n, dest)
+		}
+		cli := (w.client).(*realAuthedClient).client
+		logging.Debugf(ctx, "now checking contents of  %s", dest)
+		if a, err := cli.Attrs(gsDest); err != nil {
+			return errors.Annotate(err, "checking contents of file %s", gsDest).Err()
+		} else {
+			logging.Debugf(ctx, "%s successfully written to\nsize: %d, bucket: %s, name: %s, owner: %s\n",
+				dest, a.Size, a.Bucket, a.Name, a.Owner,
+			)
+		}
+		logging.Debugf(ctx, "checked contents of  %s", dest)
 		return nil
 	}
-	return filepath.Walk(w.localRootDir, writeOne)
+	if err := filepath.Walk(w.localRootDir, writeOne); err != nil {
+		return errors.Annotate(err, "writing dir %s to %s", w.localRootDir, w.gsRootDir).Err()
+	}
+	cli := (w.client).(*realAuthedClient).client
+	if _, err := cli.Attrs(w.gsRootDir); err != nil {
+		return errors.Annotate(err, "checking contents of root dir %s", w.gsRootDir).Err()
+	}
+	return nil
 }
 
 func newAuthenticatedTransport(ctx context.Context, f *authcli.Flags) (http.RoundTripper, error) {
