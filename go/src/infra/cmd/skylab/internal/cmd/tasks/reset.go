d131ba0e4ac4fc500c16e64d8618c194d635da8d
Otabek Kasimov <otabek@google.com>
2020-04-20 16:13:29 -0700

    skylab: Print batch tasks URL
    BUG=chromium:1072131
    TEST=local run
    
    Change-Id: I4cf060b5fe2f9df1cf1e8d9e1fc50b58f35a2963

--- go/src/infra/cmd/skylab/internal/cmd/tasks/reset.go
+++ go/src/infra/cmd/skylab/internal/cmd/tasks/reset.go
@@ -74,5 +74,11 @@ func (c *resetRun) innerRun(a subcommands.Application, args []string, env subcom
 		}
 		fmt.Fprintf(a.GetOut(), "Created Swarming task %s for host %s\n", swarming.TaskURL(creator.Environment.SwarmingService, id), dutName)
 	}
+	if len(args) > 1 {
+		tags := []string{
+			creator.GetSessionTag(),
+		}
+		fmt.Fprintf(a.GetOut(), "\nBatch tasks URL: %s\n\n", swarming.TaskListURLForTags(creator.Environment.SwarmingService, tags))
+	}
 	return nil
 }
