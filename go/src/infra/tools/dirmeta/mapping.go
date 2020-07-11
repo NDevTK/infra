0a52c671f722a1dc65ed422f69d8a2e2c1a9fc41
Nodir Turakulov <nodir@chromium.org>
2020-07-11 11:14:54 -0700

    [dirmeta] Implement Reduce
    Reduce removes all redundant information from a Mapping.
    
    This CL also removes Mapping.Expand() function.
    It turned out that it is not useful in the Mapping struct,
    because we need an entry for each directory existing on the file system.
    For that we need to know the root path, which is not available
    in the Mapping struct.
    
    Bug: 1104246
    Change-Id: I89e58a5caf9fb0f015deac58f724c0aa338ab740

--- go/src/infra/tools/dirmeta/mapping.go
+++ go/src/infra/tools/dirmeta/mapping.go
@@ -5,9 +5,6 @@
 package dirmeta
 
 import (
-	"path"
-	"sort"
-
 	"google.golang.org/protobuf/proto"
 
 	dirmetapb "infra/tools/dirmeta/proto"
@@ -18,68 +15,32 @@ import (
 // It wraps the corresponding protobuf message and adds utility functions.
 type Mapping dirmetapb.Mapping
 
+// NewMapping initializes an empty mapping.
+func NewMapping(size int) *Mapping {
+	return &Mapping{
+		Dirs: make(map[string]*dirmetapb.Metadata, size),
+	}
+}
+
 // Proto converts m back to the protobuf message.
 func (m *Mapping) Proto() *dirmetapb.Mapping {
 	return (*dirmetapb.Mapping)(m)
 }
 
-// Reduce returns a new mapping with all redundancies removed.
-func (m *Mapping) Reduce() *Mapping {
-	panic("not implemented")
+// Merge merges metadata from src to dest, where dst contains inherited metadata
+// and src contains directory-specific metadata.
+//
+// The current implementation is just proto.Merge, but it may change in the
+// future.
+func Merge(dst, src *dirmetapb.Metadata) {
+	proto.Merge(dst, src)
 }
 
-// Expand returns a new mapping where each dir has attributes inherited
-// from the parent dir.
-func (m *Mapping) Expand() *Mapping {
-	ret := &Mapping{
-		Dirs: make(map[string]*dirmetapb.Metadata, len(m.Dirs)),
+// cloneMeta clones meta.
+// If meta is nil, returns a new message.
+func cloneMeta(meta *dirmetapb.Metadata) *dirmetapb.Metadata {
+	if meta == nil {
+		return &dirmetapb.Metadata{}
 	}
-
-	// nearestAncestor returns metadata of the nearest ancestor in ret.
-	nearestAncestor := func(dir string) *dirmetapb.Metadata {
-		for {
-			parent := path.Dir(dir)
-			if parent == dir {
-				// We have reached the root.
-				return nil
-			}
-			dir = parent
-
-			if meta, ok := ret.Dirs[dir]; ok {
-				return meta
-			}
-		}
-	}
-
-	// Process directories in the shorest-path to longest-path order,
-	// such that, when computing the expanded metadata for a given directory,
-	// we only need to check the nearest ancestor.
-	for _, dir := range m.dirsSortedByLength() {
-		if ancestor := nearestAncestor(dir); ancestor == nil {
-			ret.Dirs[dir] = m.Dirs[dir]
-		} else {
-			meta := proto.Clone(ancestor).(*dirmetapb.Metadata)
-			proto.Merge(meta, m.Dirs[dir])
-			ret.Dirs[dir] = meta
-		}
-	}
-
-	return ret
-}
-
-// dirsSortedByLength returns directory names sorted by length.
-// Directory "." is treated as shortest of all.
-func (m *Mapping) dirsSortedByLength() []string {
-	ret := make([]string, 0, len(m.Dirs))
-	for k := range m.Dirs {
-		ret = append(ret, k)
-	}
-	sort.Slice(ret, func(i, j int) bool {
-		// "." is considered shortest of all.
-		if ret[i] == "." {
-			return true
-		}
-		return len(ret[i]) < len(ret[j])
-	})
-	return ret
+	return proto.Clone(meta).(*dirmetapb.Metadata)
 }
