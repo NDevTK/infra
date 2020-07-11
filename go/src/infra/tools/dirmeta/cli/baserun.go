39822e5b21ba2e4482821cbeb229aaa5a0d3a7ae
Nodir Turakulov <nodir@chromium.org>
2020-07-10 22:38:47 -0700

    [dirmeta] Add export subcommand
    Subcommand export reads metadata files from a given tree and exports
    it all to a single file.
    Also it accepts -expand and -reduce flags.
    
    Bug: 1104246
    Change-Id: I70b05b609ad24c48633cabf0bc17f357bc52cd78

--- go/src/infra/tools/dirmeta/cli/baserun.go
+++ go/src/infra/tools/dirmeta/cli/baserun.go
@@ -6,6 +6,7 @@ package cli
 
 import (
 	"context"
+	"os"
 
 	"github.com/maruel/subcommands"
 
@@ -16,6 +17,11 @@ import (
 // All dirmeta subcommands must embed it directly or indirectly.
 type baseCommandRun struct {
 	subcommands.CommandRunBase
+	output string
+}
+
+func (r *baseCommandRun) RegisterOutputFlag() {
+	r.Flags.StringVar(&r.output, "output", "-", `Path to the output file. If "-", then print the output to stdout`)
 }
 
 func (r *baseCommandRun) done(ctx context.Context, err error) int {
@@ -24,4 +30,23 @@ func (r *baseCommandRun) done(ctx context.Context, err error) int {
 		return 1
 	}
 	return 0
+}
+
+func (r *baseCommandRun) writeTextOutput(data []byte) error {
+	out := os.Stdout
+	if r.output != "-" {
+		var err error
+		if out, err = os.Create(r.output); err != nil {
+			return err
+		}
+		defer out.Close()
+	}
+	if _, err := out.Write(data); err != nil {
+		return err
+	}
+
+	if len(data) > 0 && data[len(data)-1] != '\n' {
+		out.WriteString("\n")
+	}
+	return nil
 }
---
+++ go/src/infra/tools/dirmeta/cli/cmd_export.go
@@ -0,0 +1,87 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cli
+
+import (
+	"context"
+
+	"google.golang.org/protobuf/encoding/protojson"
+
+	"github.com/maruel/subcommands"
+	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/data/text"
+	"go.chromium.org/luci/common/errors"
+
+	"infra/tools/dirmeta"
+)
+
+func cmdExport() *subcommands.Command {
+	return &subcommands.Command{
+		UsageLine: `export`,
+		ShortDesc: "export metadata from a directory tree",
+		LongDesc: text.Doc(`
+			Export metadata from a directory tree to stdout or to a file.
+
+			The output format is JSON form of chrome.dir_metadata.Mapping protobuf
+			message.
+		`),
+		CommandRun: func() subcommands.CommandRun {
+			r := &exportRun{}
+			r.RegisterOutputFlag()
+			r.Flags.StringVar(&r.root, "root", ".", "Path to the root directory")
+			r.Flags.BoolVar(&r.expand, "expand", false, `Expand the mapping, i.e. inherit values`)
+			r.Flags.BoolVar(&r.reduce, "reduce", false, `Reduce the mapping, i.e. remove all redundant information`)
+			return r
+		},
+	}
+}
+
+type exportRun struct {
+	baseCommandRun
+	root           string
+	expand, reduce bool
+}
+
+func (r *exportRun) parseInput(args []string) error {
+	if len(args) != 0 {
+		return errors.Reason("unexpected positional arguments: %q", args).Err()
+	}
+
+	if r.expand && r.reduce {
+		return errors.Reason("-expand and -reduce are mutually exclusive").Err()
+	}
+
+	return nil
+}
+
+func (r *exportRun) Run(a subcommands.Application, args []string, env subcommands.Env) int {
+	ctx := cli.GetContext(a, r, env)
+
+	if err := r.parseInput(args); err != nil {
+		return r.done(ctx, err)
+	}
+
+	return r.done(ctx, r.run(ctx))
+}
+
+func (r *exportRun) run(ctx context.Context) error {
+	mapping, err := dirmeta.ReadMapping(r.root)
+	if err != nil {
+		return err
+	}
+
+	if r.expand {
+		mapping = mapping.Expand()
+	} else if r.reduce {
+		mapping = mapping.Reduce()
+	}
+
+	data, err := protojson.Marshal(mapping.Proto())
+	if err != nil {
+		return err
+	}
+
+	return r.writeTextOutput(data)
+}
