470223e44b6e20af681f5aa67100e1fc316f4f70
Nodir Turakulov <nodir@chromium.org>
2020-07-11 01:25:01 -0700

    [dirmeta] Add compute subcommand
    Subcommand "compute" accepts a root, and 0 or more target directories.
    For each target directory, it reads metadata for each directory on the
    path from the root to the target.
    It reads each directory at most once.
    Then it computes inherited metadata for each target directory
    and prints the mapping in the same format as "export" subcommand.
    
    This CL also refactors reading functions read.go into a methods
    of MappingReader struct.
    
    Also this fixes a bug that error was ignored in os.Walk function.
    
    Change-Id: Id3097199fe5d11c6fe271cae9e9abf96565261da

--- go/src/infra/tools/dirmeta/read_test.go
+++ go/src/infra/tools/dirmeta/read_test.go
@@ -13,26 +13,41 @@ import (
 	. "go.chromium.org/luci/common/testing/assertions"
 )
 
-func TestReadMapping(t *testing.T) {
+func TestReadFull(t *testing.T) {
 	t.Parallel()
 
-	Convey(`ReadMapping`, t, func() {
-		actual, err := ReadMapping("testdata/root")
-		So(err, ShouldBeNil)
-		So(actual.Proto(), ShouldResembleProto, &dirmetapb.Mapping{
-			Dirs: map[string]*dirmetapb.Metadata{
-				".": {
-					TeamEmail: "chromium-review@chromium.org",
-				},
-				"subdir_with_owners": {
-					TeamEmail: "team-email@chromium.org",
-					Os:        dirmetapb.OS_IOS,
-					Monorail: &dirmetapb.Monorail{
-						Project:   "chromium",
-						Component: "Some>Component",
+	Convey(`MappingReader`, t, func() {
+		var r MappingReader
+
+		Convey(`ReadFull`, func() {
+			r.Root = "testdata/root"
+			err := r.ReadFull()
+			So(err, ShouldBeNil)
+			So(r.Mapping.Proto(), ShouldResembleProto, &dirmetapb.Mapping{
+				Dirs: map[string]*dirmetapb.Metadata{
+					".": {
+						TeamEmail: "chromium-review@chromium.org",
+					},
+					"subdir_with_owners": {
+						TeamEmail: "team-email@chromium.org",
+						Os:        dirmetapb.OS_IOS,
+						Monorail: &dirmetapb.Monorail{
+							Project:   "chromium",
+							Component: "Some>Component",
+						},
 					},
 				},
-			},
+			})
+		})
+
+		Convey(`ReadInheritance`, func() {
+			r.Root = "testdata/inheritance"
+			err := r.ReadTowards("testdata/inheritance/a/b")
+			So(err, ShouldBeNil)
+			So(r.Dirs, ShouldHaveLength, 3)
+			So(r.Dirs, ShouldContainKey, ".")
+			So(r.Dirs, ShouldContainKey, "a")
+			So(r.Dirs, ShouldContainKey, "a/b")
 		})
 	})
 }
