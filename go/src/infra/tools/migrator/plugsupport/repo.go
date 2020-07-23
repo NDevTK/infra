d901d4ec9a2a62a76112e743cfae9d82d2df03f7
Robert Iannucci <iannucci@chromium.org>
2020-07-22 22:38:48 -0700

    [migrator] Add implementation for Repo().Project().
    R=nodir@chromium.org, tandrii@chromium.org, vadimsh@chromium.org
    
    Change-Id: Ia8dd4e67e24f715efa327af8a4308a6bb1841ea7

--- go/src/infra/tools/migrator/plugsupport/repo.go
+++ go/src/infra/tools/migrator/plugsupport/repo.go
@@ -12,6 +12,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"path/filepath"
 	"strings"
 	"sync"
 
@@ -34,7 +35,11 @@ type repo struct {
 }
 
 func (r *repo) Project() migrator.Project {
-	panic("IMPLEMENT PROJECT()")
+	return &localProject{
+		id:  migrator.ReportID{Project: r.projPB.Id},
+		dir: filepath.Join(r.root, r.relGeneratedConfigRoot),
+		ctx: r.ctx,
+	}
 }
 
 func (r *repo) ConfigRoot() string          { return "/" + r.relConfigRoot }
