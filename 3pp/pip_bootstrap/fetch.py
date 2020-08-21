e9924a06c60640b9aa0f16182d0957d71e5aa800
Brian Ryner <bryner@google.com>
2020-08-21 14:51:45 +1000

    Explicitly exclude setuptools 45.0.0, which is not really python2-compatible. Formatting changes are from 'git cl format'.
    Change-Id: I2a23105b6bf3cb0b83736a462a42025a7cd11aa5

--- 3pp/pip_bootstrap/fetch.py
+++ 3pp/pip_bootstrap/fetch.py
@@ -12,6 +12,11 @@ import pkg_resources
 import requests
 
 
+_SETUPTOOLS_BAD_VERSIONS = [
+    '45.0.0',  # Advertises python_version='py2.py3', but also requires >= 3.5
+]
+
+
 def _get_wheel_url(pkgname, version):
   r = requests.get('https://pypi.org/pypi/%s/json' % pkgname)
   r.raise_for_status()
@@ -21,26 +26,27 @@ def _get_wheel_url(pkgname, version):
   raise AssertionError('could not find wheel for %s @ %s' % (pkgname, version))
 
 
-def _get_version(pkgname):
+def _get_version(pkgname, bad_versions=None):
+  bad_versions = bad_versions or []
   r = requests.get('https://pypi.org/pypi/%s/json' % pkgname)
   r.raise_for_status()
   # Find the latest python2-compatible version.
   releases = r.json()['releases']
   versions = [pkg_resources.parse_version(v) for v in releases.keys()]
   for version in sorted(versions, reverse=True):
-    for filedata in releases[str(version)]:
-      if (filedata['packagetype'] == 'bdist_wheel' and
+    version_str = str(version)
+    for filedata in releases[version_str]:
+      if (version_str not in bad_versions and
+          filedata['packagetype'] == 'bdist_wheel' and
           not filedata['yanked'] and filedata['python_version'] != 'py3'):
-        return version
+        return version_str
   raise AssertionError('could not find a compatible version for %s' % pkgname)
 
 
 def do_latest():
-  print 'pip%s.setuptools%s.wheel%s' % (
-    _get_version('pip'),
-    _get_version('setuptools'),
-    _get_version('wheel')
-  )
+  print 'pip%s.setuptools%s.wheel%s' % (_get_version(
+      'pip'), _get_version('setuptools', bad_versions=_SETUPTOOLS_BAD_VERSIONS),
+                                        _get_version('wheel'))
 
 
 def do_checkout(version, checkout_path):
