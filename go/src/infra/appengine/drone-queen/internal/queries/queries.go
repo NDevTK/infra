d0341a7439a19519ef5ce7e012b455306716d46b
Eshwar Narayan <eshwarn@google.com>
2020-12-23 11:43:25 -0800

    DroneQueen: Add dut hive field for DeclareDutsRequest
    BUG=b:176211353
    Test=make test
    
    Change-Id: I70648f9249deaf6ac8a7b78b5be490c5e9f63e37

--- go/src/infra/appengine/drone-queen/internal/queries/queries.go
+++ go/src/infra/appengine/drone-queen/internal/queries/queries.go
@@ -112,8 +112,6 @@ func AssignNewDUTs(ctx context.Context, d entities.DroneID, li *api.ReportDroneR
 		return nil, errors.Annotate(err, "assign new DUTs to %v", d).Err()
 	}
 	dutsNeeded := uint32ToInt(li.GetDutCapacity()) - len(currentDUTs)
-
-	// TODO (anhdle): Update this during full Satlab implementation.
 	newDUTs, err := getUnassignedDUTs(ctx, int32(dutsNeeded), li.GetHive())
 	if err != nil {
 		return nil, errors.Annotate(err, "assign new DUTs to %v", d).Err()
