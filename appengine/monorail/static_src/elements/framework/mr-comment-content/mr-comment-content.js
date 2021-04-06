9b1a69c3696b69f4e359182dbac3d34f66581961
Kevin Weng <kweng@google.com>
2021-04-06 11:17:43 -0700

    [🚝] DO NOT SUBMIT, Playground of various markdown libraries and xss protection libaries
    Change-Id: I2967d80bfd8f8013c7e60e3ae05bf5717b83758f

--- appengine/monorail/static_src/elements/framework/mr-comment-content/mr-comment-content.js
+++ appengine/monorail/static_src/elements/framework/mr-comment-content/mr-comment-content.js
@@ -3,12 +3,14 @@
 // found in the LICENSE file.
 
 import {LitElement, html, css} from 'lit-element';
+import {unsafeHTML} from 'lit-html/directives/unsafe-html.js';
 import {ifDefined} from 'lit-html/directives/if-defined';
 import {autolink} from 'autolink.js';
 import {connectStore} from 'reducers/base.js';
 import * as issueV0 from 'reducers/issueV0.js';
 import * as projectV0 from 'reducers/projectV0.js';
 import {SHARED_STYLES} from 'shared/shared-styles.js';
+import {renderMarkdown, sanitizeMarkdown} from 'shared/md-helper.js';
 
 /**
  * `<mr-comment-content>`
@@ -68,9 +70,21 @@ export class MrCommentContent extends connectStore(LitElement) {
 
   /** @override */
   render() {
+    // const options = { mdLibrary: 'showdown', sanitize: false, xssLibrary: 'js-xss' };
+    // const options = { mdLibrary: 'marked', sanitize: false };
+    // const options = { mdLibrary: 'showdown', sanitize: false };
+
+    // const showdownHTML = renderMarkdown(this.content, { mdLibrary: 'showdown', sanitize: true, xssLibrary: 'DOMPurify' });
+    const markedHTML = renderMarkdown(this.content, { mdLibrary: 'marked', sanitize: true, xssLibrary: 'DOMPurify' });
+    // const markdownItHTML = renderMarkdown(this.content, { mdLibrary: 'markdown-it', sanitize: true, xssLibrary: 'DOMPurify' });
+
+    // const intermediary = renderMarkdown(this.content, { mdLibrary: 'marked', sanitize: false });
+    // const filteredHTML = sanitizeMarkdown(intermediary, { xssLibrary: 'js-xss' });
+
     const runs = autolink.markupAutolinks(
         this.content, this.commentReferences, this.projectName,
         this.revisionUrlFormat);
+
     const templates = runs.map((run) => {
       switch (run.tag) {
         case 'b':
@@ -89,7 +103,91 @@ export class MrCommentContent extends connectStore(LitElement) {
           return html`<span class="line">${run.content}</span>`;
       }
     });
-    return html`${templates}`;
+
+    // suppose there is only one text run, the entirey of the Markdown content
+    const mdAutolinkChunks = autolink.autolinkChunk(markedHTML, this.commentReferences, this.projectName, this.revisionUrlFormat)
+    console.log('autolinkChunk(markedHTML)');
+    console.log(mdAutolinkChunks);
+    
+    const mdAutolinkContent = mdAutolinkChunks.reduce((acc, chunk) => acc + chunk.content, '')
+    // const mdAutolinkContent = mdAutolinkChunks.reduce((acc, chunk) => { return acc + chunk.content; }, '')
+    console.log('mdAutolinkContent')
+    console.log(mdAutolinkContent)
+
+
+
+
+    // const rawAutoMark = runs.map((run) => {
+    //   switch (run.tag) {
+    //     case 'b':
+    //       return html`<b class="line">${run.content}</b>`;
+    //     case 'br':
+    //       return html`<br>`;
+    //     case 'a':
+    //       return html`<a
+    //         class="line"
+    //         target="_blank"
+    //         href=${run.href}
+    //         class=${run.css}
+    //         title=${ifDefined(run.title)}
+    //       >${run.content}</a>`;
+    //     default:
+    //       const mdRunContent = renderMarkdown(run.content, { mdLibrary: 'marked', sanitize: true, xssLibrary: 'DOMPurify' });
+    //       // console.log(mdRunContent)
+    //       // return unsafeHTML`<span class="line">${mdRunContent}</span>`;
+    //       return html`${unsafeHTML(mdRunContent)}`;
+    //   }
+    // });
+
+    // const rawMarkAutoRuns = autolink.markupAutolinks(markedHTML, this.commentReferences, this.projectName, this.revisionUrlFormat)
+    // const rawMarkAuto = rawMarkAutoRuns.map((run) => {
+    //   switch (run.tag) {
+    //     case 'b':
+    //       return html`<b class="line">${run.content}</b>`;
+    //     case 'br':
+    //       return html`<br>`;
+    //     case 'a':
+    //       return html`<a
+    //         class="line"
+    //         target="_blank"
+    //         href=${run.href}
+    //         class=${run.css}
+    //         title=${ifDefined(run.title)}
+    //       >${run.content}</a>`;
+    //     default:
+    //       return html`${unsafeHTML(run.content)}`;
+    //   }
+    // });
+
+    // const shouldRenderMarkdown = someHelpers.shouldRenderMarkdown({ project: '', issueToggle: true })
+    // if (shouldRenderMarkdown) {
+    //   try {
+    //     return html`${mdRenderHelper(content)}`
+    // } else {
+    //   return html`${templates}`;
+    // }
+
+    // return html`<div>
+    //   <h1>Original content</h1>${templates}
+    //   <hr>
+    //   <h1>Unfiltered content</h1>${unsafeHTML(markdownHTML)}
+    //   <hr>
+    //   <h1>Markdown content</h1>${unsafeHTML(sanitizedHTML)}
+    //  </div>`
+
+    return html`<div>
+      <h2>Raw</h2>${this.content}
+      <hr>
+
+      <h2>Classic</h2>${templates}
+      <hr>
+
+      <h2>raw --> Markdown (no autolink)</h2>
+      ${unsafeHTML(markedHTML)}
+      <hr>
+
+
+     </div>`
   }
 
   /** @override */
