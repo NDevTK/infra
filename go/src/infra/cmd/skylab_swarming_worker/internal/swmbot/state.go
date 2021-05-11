44fef103454291c3ff5ea8c0cef9852907449205
Garry Wang <xianuowang@chromium.org>
2021-05-10 18:51:14 -0700

    SSW: Support scheduling unit in SSW
    BUG=b:181696942
    TEST=go test ./... and go build
    
    Change-Id: I772b196e0db2196222fc553fe9255b7281bf49d0

--- go/src/infra/cmd/skylab_swarming_worker/internal/swmbot/state.go
+++ go/src/infra/cmd/skylab_swarming_worker/internal/swmbot/state.go
@@ -10,27 +10,27 @@ import (
 	"infra/cros/dutstate"
 )
 
-// LocalState contains persistent bot information that is cached on the
+// LocalDUTState contains persistent DUT information that is cached on the
 // Skylab drone.
-type LocalState struct {
+type LocalDUTState struct {
 	HostState               dutstate.State          `json:"-"`
 	ProvisionableLabels     ProvisionableLabels     `json:"provisionable_labels"`
 	ProvisionableAttributes ProvisionableAttributes `json:"provisionable_attributes"`
 }
 
-// ProvisionableLabels stores provisionable labels for a bot and host.
+// ProvisionableLabels stores provisionable labels for a DUT.
 type ProvisionableLabels map[string]string
 
-// ProvisionableAttributes stores provisionable attributes for a bot and host.
+// ProvisionableAttributes stores provisionable attributes for a DUT.
 type ProvisionableAttributes map[string]string
 
 // Marshal returns the encoding of the BotInfo.
-func Marshal(bi *LocalState) ([]byte, error) {
+func Marshal(bi *LocalDUTState) ([]byte, error) {
 	return json.Marshal(bi)
 }
 
 // Unmarshal decodes BotInfo from the encoded data.
-func Unmarshal(data []byte, bi *LocalState) error {
+func Unmarshal(data []byte, bi *LocalDUTState) error {
 	if err := json.Unmarshal(data, bi); err != nil {
 		return err
 	}
