4757f5c98d0d9e9873cf6fd1b418ab8d3a19068d
Garry Wang <xianuowang@chromium.org>
2021-05-10 18:17:06 -0700

    SSW: Split DUT harness from bot harness logic
    BUG=b:181696942
    TEST=go test ./... and go build
    
    Change-Id: I13c4ca92af8e9c6db6e3e4f751db4d40d0186da1

--- go/src/infra/cmd/skylab_swarming_worker/internal/swmbot/harness/options.go
+++ go/src/infra/cmd/skylab_swarming_worker/internal/swmbot/harness/options.go
@@ -4,15 +4,15 @@
 
 package harness
 
-// Option is passed to Open to configure the harness.
-type Option func(*Info)
+// Option is passed to Open to configure the DUT harness.
+type Option func(*DUTHarness)
 
 // UpdateInventory returns an Option that enables inventory updates.
 // A task name to be associated with the inventory update should be
 // provided.
 func UpdateInventory(name string) Option {
-	return func(i *Info) {
-		i.labelUpdater.taskName = name
-		i.labelUpdater.updateLabels = true
+	return func(dh *DUTHarness) {
+		dh.labelUpdater.TaskName = name
+		dh.labelUpdater.UpdateLabels = true
 	}
 }
