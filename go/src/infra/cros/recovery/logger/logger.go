af3b22288a9da54965ef2215ff57e69c9d7dc358
Otabek Kasimov <otabek@google.com>
2021-07-27 19:19:44 -0700

    recovery: Add indent for logger.
    BUG=b:187878465
    TEST=run local
    
    Change-Id: If583471bebf4f96358351bfb2155b53ff8ebe3ea

--- go/src/infra/cros/recovery/logger/logger.go
+++ go/src/infra/cros/recovery/logger/logger.go
@@ -5,6 +5,10 @@
 // Package logger provides an abstract representation of logging interfaces used by recovery lib.
 package logger
 
+import (
+	"log"
+)
+
 // Logger represents a simple interface for logging data.
 type Logger interface {
 	// Debug log message at Debug level.
@@ -15,4 +19,50 @@ type Logger interface {
 	Warning(format string, args ...interface{})
 	// Error is like Debug, but logs at Error level.
 	Error(format string, args ...interface{})
+	// Indenter provides access to logger intender.
+	GetIndenter() Indenter
+}
+
+// NewLogger creates default logger.
+func NewLogger() Logger {
+	return &logger{}
+}
+
+// logger provides default implementation of Logger interface.
+type logger struct {
+	indenter Indenter
+}
+
+// Debug log message at Debug level.
+func (l *logger) Debug(format string, args ...interface{}) {
+	l.print(format, args...)
+}
+
+// Info is like Debug, but logs at Info level.
+func (l *logger) Info(format string, args ...interface{}) {
+	l.print(format, args...)
+}
+
+// Warning is like Debug, but logs at Warning level.
+func (l *logger) Warning(format string, args ...interface{}) {
+	l.print(format, args...)
+}
+
+// Error is like Debug, but logs at Error level.
+func (l *logger) Error(format string, args ...interface{}) {
+	l.print(format, args...)
+}
+
+// Indenter provides access to logger intender.
+func (l *logger) GetIndenter() Indenter {
+	if l.indenter == nil {
+		l.indenter = NewIndenter()
+	}
+	return l.indenter
+}
+
+// Default logging logic for all levels.
+func (l *logger) print(format string, args ...interface{}) {
+	indent := GetIntentString(l.GetIndenter(), "\t")
+	log.Printf(indent+format, args...)
 }
