61e2868a635c2d8c046f9dd30c9d6d3222df597c
Olivia Sturman <oliviasturman@google.com>
2021-07-29 15:13:02 -0700

    Monorail: added details step for the Issue Wizard. More styling to come soon.
    Change-Id: I37d619f020237b35d0636581d46cd360f77a207b

--- appengine/monorail/static_src/react/issue-wizard/SelectMenu.test.tsx
+++ appengine/monorail/static_src/react/issue-wizard/SelectMenu.test.tsx
@@ -9,7 +9,7 @@ import {assert} from 'chai';
 
 import SelectMenu from './SelectMenu.tsx';
 
-describe.only('SelectMenu', () => {
+describe('SelectMenu', () => {
   it('renders', async () => {
     const {container} = render(<SelectMenu />);
 
