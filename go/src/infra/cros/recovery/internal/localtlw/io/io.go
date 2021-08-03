93d8f037daf44bdb7769fdb7d14c99d9637a1ef2
Vineet Joshi <vkjoshi@google.com>
2021-08-03 12:40:29 -0700

    [recovery]: Copy file from local machine to remote device.
    This commit is a part of the PARIS project. It provides an
    implementation of the method CopyFileTo of the Access interface.
    
    This method copies the local source file mentioned in a structure
    passed as a function parameter over to the remote destination
    location, also mentioned in this structure parameter.
    
    BUG=b:193437315
    TEST=build, manual testing.
    
    Change-Id: Ic3bc4d5886c67671fcbd872d5b7d13ba078051e6

--- go/src/infra/cros/recovery/internal/localtlw/io/io.go
+++ go/src/infra/cros/recovery/internal/localtlw/io/io.go
@@ -7,12 +7,14 @@ package io
 import (
 	"context"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strconv"
+	"sync"
 
 	"go.chromium.org/luci/common/errors"
 
@@ -48,7 +50,7 @@ func CopyFileFrom(ctx context.Context, pool *sshpool.Pool, req *tlw.CopyRequest)
 	if err := validateInputParams(ctx, pool, req); err != nil {
 		return errors.Annotate(err, "copy file from").Err()
 	}
-	if err := ensureDirExists(ctx, req.PathDestination); err != nil {
+	if err := ensurePathExists(ctx, req.PathDestination, true, true); err != nil {
 		return errors.Annotate(err, "copy file from").Err()
 	}
 
@@ -105,6 +107,89 @@ func CopyFileFrom(ctx context.Context, pool *sshpool.Pool, req *tlw.CopyRequest)
 	return nil
 }
 
+// CopyFileTo copies a single file from local machine to remote
+// device. req contains the complete path of the source file on the
+// local machine, and the complete path of the destination directory
+// on the remote device where the source file will be copied.
+func CopyFileTo(ctx context.Context, pool *sshpool.Pool, req *tlw.CopyRequest) error {
+	if err := validateInputParams(ctx, pool, req); err != nil {
+		return errors.Annotate(err, "copy file to").Err()
+	}
+	if err := ensurePathExists(ctx, req.PathSource, false, false); err != nil {
+		return errors.Annotate(err, "copy file to: error while checking whether the source file exists").Err()
+	}
+
+	addr := net.JoinHostPort(req.Resource, strconv.Itoa(defaultSSHPort))
+	client, err := pool.Get(addr)
+	if err != nil {
+		return errors.Annotate(err, "copy file to: failed to get client %q from pool", addr).Err()
+	}
+	defer pool.Put(addr, client)
+	session, err := client.NewSession()
+	if err != nil {
+		return errors.Annotate(err, "copy file to: failed to create SSH session").Err()
+	}
+	defer session.Close()
+
+	// Read the input path on the local machine and create a
+	// compressed tar archive. Then write it to stdout. Here, the '-C'
+	// flag changes the working directory to the location where the
+	// input exists. This ensures that the archive includes paths only
+	// relative to this directory.
+	lCmd := exec.CommandContext(ctx, tarCmd, "-c", "--gzip", "-C", filepath.Dir(req.PathSource), filepath.Base(req.PathSource))
+	p, err := lCmd.StdoutPipe()
+	if err != nil {
+		return errors.Annotate(err, "copy file to: could not obtain the stdout pipe").Err()
+	}
+	if err := lCmd.Start(); err != nil {
+		return errors.Annotate(err, "copy file to: could not execute local command %q", lCmd).Err()
+	}
+	defer lCmd.Wait()
+	p2, err2 := session.StdinPipe()
+	if err2 != nil {
+		return errors.Annotate(err, "copy file to: error with obtaining stdin pipe for the SSH Session").Err()
+	}
+	uploadErrors := make(chan error)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func(wg1 *sync.WaitGroup) {
+		defer wg1.Done()
+		if _, err := io.Copy(p2, p); err != nil {
+			uploadErrors <- errors.Annotate(err, "copy file to: error with copying contents from local stdout to remote stdin").Err()
+		}
+		defer p2.Close()
+	}(&wg)
+
+	// Read the stdin on the remote device and extract to the
+	// destination path. The '-C' flag changes the current directory
+	// to the destination path, and ensures that the output is placed
+	// there.
+	rCmd := fmt.Sprintf("%s -x --gzip -C %s", tarCmd, req.PathDestination)
+	wg.Add(1)
+	go func(wg2 *sync.WaitGroup) {
+		defer wg2.Done()
+		if err := session.Start(rCmd); err != nil {
+			uploadErrors <- errors.Annotate(err, "copy file to: remote device could not read the uploaded contents").Err()
+		} else if err := session.Wait(); err != nil {
+			uploadErrors <- errors.Annotate(err, "copy file to: remote command did not exit cleanly").Err()
+		}
+	}(&wg)
+	wg.Wait()
+
+	select {
+	case e, ok := <-uploadErrors:
+		if ok {
+			return errors.Annotate(e, "copy file to").Err()
+		} else {
+			// No one is closing the channel, but we want
+			// to defensively handle this case.
+			return nil
+		}
+	default:
+		return nil
+	}
+}
+
 func validateInputParams(ctx context.Context, pool *sshpool.Pool, req *tlw.CopyRequest) error {
 	if pool == nil {
 		return errors.New("validate input params: ssh pool is not initialized")
@@ -121,26 +206,44 @@ func validateInputParams(ctx context.Context, pool *sshpool.Pool, req *tlw.CopyR
 	return nil
 }
 
-// ensureDirExists checks whether the directory 'd' exists, or not. If
-// it does not exist, the function creates it. It returns any error
-// encountered during checking or creating the directory.
-func ensureDirExists(ctx context.Context, d string) error {
-	s, err := os.Stat(d)
+// ensurePathExists checks whether the path 'p' exists, and whether it
+// is of the type indicated by 'd'. If the directory does not exist,
+// the function will create it if 'c' is true. It returns any error
+// encountered during checking existence and type of 'p' or during
+// directory creation.
+func ensurePathExists(ctx context.Context, p string, d bool, c bool) error {
+	s, err := os.Stat(p)
 	if err != nil {
 		if os.IsNotExist(err) {
-			log.Debug(ctx, "Ensure directory exists: creating directory %q.", d)
-			return os.MkdirAll(d, dirPermission)
+			if d && c {
+				log.Debug(ctx, "Ensure path exists: creating directory %q.", p)
+				return os.MkdirAll(p, dirPermission)
+			}
+			return errors.Annotate(err, "ensure path exists: %q does not exist", p).Err()
 		}
 		// This means that 'err' is not known to report
 		// whether or not the file or directory already
 		// exists. Hence we cannot proceed with checking
 		// whether the directory pre-exists, or creating
 		// directory.
-		return errors.Annotate(err, "ensure directory exists: cannot determine if %q exists", d).Err()
+		return errors.Annotate(err, "ensure path exists: cannot determine if %q exists", p).Err()
 	}
-	if s.IsDir() {
-		log.Debug(ctx, "Ensure directory exists: directory %q already exists.", d)
-		return nil
+
+	if d {
+		if s.IsDir() {
+			log.Debug(ctx, "Ensure path exists: directory %q exists.", p)
+			return nil
+		} else {
+			log.Debug(ctx, "Ensure path exists: path %q is a file, and not a directory.", p)
+			return errors.Annotate(err, "ensure path exists: %q is a file, and not a directory", p).Err()
+		}
+	} else {
+		if s.IsDir() {
+			log.Debug(ctx, "Ensure path exists: path %q is a directory, and not a file.", p)
+			return errors.Annotate(err, "ensure path exists: %q is a directory, and not a file", p).Err()
+		} else {
+			log.Debug(ctx, "Ensure path exists: file %q exists.", p)
+			return nil
+		}
 	}
-	return fmt.Errorf("ensure directory exists: cannot create directory %q, it is a pre-existing file", d)
 }
