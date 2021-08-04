09b67a58159ba87802fe6821e7f27ebf6725c096
Gregory Nisbet <gregorynisbet@google.com>
2021-08-04 15:49:20 -0700

    [satlab] Add -satlab-id flag and fix satlab-add-dut issues.
    Fix issues preventing satlab add dut from working end to end.
    
    BUG=b/193249901
    TEST=make test AND ./satlab add dut -satlab-id ... ...
    
    Change-Id: I53c1f13226734211b7a6c27cbcdcd7d07d8aa80a

---
+++ go/src/infra/cros/cmd/satlab/internal/common/common.go
@@ -0,0 +1,38 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package common
+
+import (
+	"fmt"
+	"strings"
+)
+
+const SatlabID = "satlab-id"
+const SkipDNS = "skip-dns"
+
+var IgnoreInternalFlags = map[string]bool{
+	SatlabID: false,
+	SkipDNS:  false,
+}
+
+func WithInternalFlags(o ...map[string]bool) map[string]bool {
+	m := make(map[string]bool)
+	for k, v := range IgnoreInternalFlags {
+		m[k] = v
+	}
+	for _, additional := range o {
+		for k, v := range additional {
+			m[k] = v
+		}
+	}
+	return m
+}
+
+func MaybePrepend(satlabPrefix string, content string) string {
+	if strings.HasPrefix(content, satlabPrefix) {
+		return content
+	}
+	return fmt.Sprintf("%s-%s", satlabPrefix, content)
+}
--- go/src/infra/cros/cmd/satlab/internal/tasks/satlab_delete_dut.go
+++ go/src/infra/cros/cmd/satlab/internal/tasks/satlab_delete_dut.go
@@ -15,6 +15,7 @@ import (
 	"go.chromium.org/luci/common/errors"
 
 	"infra/cros/cmd/satlab/internal/commands"
+	"infra/cros/cmd/satlab/internal/common"
 	"infra/cros/cmd/satlab/internal/parse"
 	"infra/cros/cmd/satlab/internal/paths"
 )
@@ -22,15 +23,31 @@ import (
 // DeleteDUT deletes a DUT. It takes a possibly-empty path to the service account credentials,
 // the prefix for the satlab box in question, and the result of parsing the command line flags given to the
 // command initially.
-func DeleteDUT(serviceAccountJSON string, satlab string, p *parse.CommandParseResult) error {
+func DeleteDUT(serviceAccountJSON string, satlabPrefix string, p *parse.CommandParseResult) error {
 	if p == nil {
 		return errors.New("command parse cannot be nil")
 	}
-	// TODO(gregorynisbet): add prefix
+
+	positionalArgs := []string{}
+	for _, item := range p.PositionalArgs {
+		positionalArgs = append(positionalArgs, common.MaybePrepend(satlabPrefix, item))
+	}
+	flags := make(map[string][]string)
+	for k, v := range p.Flags {
+		flags[k] = []string{v}
+	}
+	for k := range p.NullaryFlags {
+		flags[k] = nil
+	}
+
 	args := (&commands.CommandWithFlags{
 		Commands:       []string{paths.ShivasPath, "delete", "dut"},
-		PositionalArgs: p.PositionalArgs,
-	}).ToCommand()
+		PositionalArgs: positionalArgs,
+		Flags:          flags,
+	}).ApplyFlagFilter(
+		true,
+		common.IgnoreInternalFlags,
+	).ToCommand()
 	command := exec.Command(args[0], args[1:]...)
 	command.Stdin = os.Stdin
 	command.Stdout = os.Stdout
