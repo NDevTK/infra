f31cf1b63e1cd60f39daf3d155d94d4468d30535
Gregory Nisbet <gregorynisbet@google.com>
2021-08-24 10:55:19 -0700

    [recovery] Add Metrics API.
    Metrics is the subset of the Karte API that is used by the recovery library.
    This allows the recovery library to have more than one backing implementation
    of the Karte API. The recovery library will have at least two implementations of
    this API, a basic one that logs JSON at the debug level, and a more complex one
    that calls Karte.
    
    BUG=b/183724382
    TEST=make test
    
    Change-Id: I278fe47ab70047c6ad09e0dbbdebb94aaa463267

---
+++ go/src/infra/cros/recovery/logger/metrics_test.go
@@ -0,0 +1,64 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package logger
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+// TestNewMetrics verifies that the default metrics logger writes a serialized message to
+// the provided logger at the debug level.
+//
+// TODO(gregorynisbet): Drop this test once the default logger implementation is more substantial.
+func TestNewMetrics(t *testing.T) {
+	ctx := context.Background()
+	expected := []string{
+		(`` +
+			`{` + "\n" +
+			`    "Kind": "b",` + "\n" +
+			`    "SwarmingTaskID": "a",` + "\n" +
+			`    "AssetTag": "",` + "\n" +
+			`    "StartTime": "0001-01-01T00:00:00Z",` + "\n" +
+			`    "StopTime": "0001-01-01T00:00:00Z",` + "\n" +
+			`    "Status": "",` + "\n" +
+			`    "FailReason": "",` + "\n" +
+			`    "Observations": [` + "\n" +
+			`        {` + "\n" +
+			`            "MetricKind": "c",` + "\n" +
+			`            "ValueType": "",` + "\n" +
+			`            "Value": ""` + "\n" +
+			`        }` + "\n" +
+			`    ]` + "\n" +
+			`}` + "\n"),
+	}
+	var actual []string
+	l := NewMetrics(newFakeLogger(func(level string, format string, args []interface{}) {
+		if level != "debug" {
+			t.Errorf("expected log level to be \"debug\" not %q", level)
+		}
+		actual = append(actual, fmt.Sprintf(format, args...))
+	}))
+
+	l.Record(
+		ctx,
+		&Action{
+			SwarmingTaskID: "a",
+			Kind:           "b",
+			Observations: []*Observation{
+				{
+					MetricKind: "c",
+				},
+			},
+		},
+	)
+
+	if diff := cmp.Diff(expected, actual); diff != "" {
+		t.Errorf("unexpected diff: %s", diff)
+	}
+}
