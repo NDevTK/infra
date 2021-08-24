e5e75e497de4c13fad86c8775791c04277e0ca3a
Gregory Nisbet <gregorynisbet@google.com>
2021-08-24 10:55:19 -0700

    [recovery] Add Metrics API.
    Metrics is the subset of the Karte API that is used by the recovery library.
    This allows the recovery library to have more than one backing implementation
    of the Karte API. The recovery library will have at least two implementations of
    this API, a basic one that logs JSON at the debug level, and a more complex one
    that calls Karte.
    
    BUG=b/183724382
    TEST=make test
    
    Change-Id: I278fe47ab70047c6ad09e0dbbdebb94aaa463267

---
+++ go/src/infra/cros/recovery/logger/metrics_impl.go
@@ -0,0 +1,41 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package logger
+
+import (
+	"context"
+	"encoding/json"
+
+	"go.chromium.org/luci/common/errors"
+)
+
+// metrics is a default Metric implementation that logs all events
+// to the logger.
+type metrics struct {
+	logger Logger
+}
+
+// NewMetrics creates a default metric sink.
+func NewMetrics(l Logger) Metrics {
+	return &metrics{
+		logger: l,
+	}
+}
+
+// Create marshals an action as JSON and logs it at the debug level.
+func (m *metrics) Create(ctx context.Context, action *Action) (*Action, error) {
+	a, err := json.MarshalIndent(action, "", "    ")
+	if err != nil {
+		// TODO(gregorynisbet): Check if action is nil.
+		return nil, errors.Annotate(err, "record action for asset %q", action.AssetTag).Err()
+	}
+	m.logger.Debug("Create action %q: %s\n", action.ActionKind, string(a))
+	return action, nil
+}
+
+// Search lists the actions matching a given criterion.
+func (m *metrics) Search(ctx context.Context, q *Query) (*QueryResult, error) {
+	return nil, errors.New("list actions matching: not yet implemented")
+}
