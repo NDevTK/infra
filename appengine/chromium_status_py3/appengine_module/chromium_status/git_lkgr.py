c7620f429fe7ebcb63877ee355d92507dcc998e0
Quang Minh Tuan Nguyen <nqmtuan@google.com>
2021-08-26 18:13:39 +1000

    Migrate Chromium Tree Status to py3
    Bug:1121016
    Change-Id: I6a402f8114c764891a0e17ce526d9bd6acacd51c

--- appengine/chromium_status_py3/appengine_module/chromium_status/git_lkgr.py
+++ appengine/chromium_status_py3/appengine_module/chromium_status/git_lkgr.py
@@ -6,21 +6,24 @@
 import json
 import logging
 
-from google.appengine.ext import db
+from flask import make_response, abort, request
+from google.cloud import ndb
 
 from appengine_module.chromium_status.base_page import BasePage
+from google.cloud import ndb
 from appengine_module.chromium_status import utils
 
+ndb_client = ndb.Client()
 
-class Commit(db.Model):  # pylint: disable=W0232
+class Commit(ndb.Model):  # pylint: disable=W0232
   """Description of a commit, keyed by random integer IDs."""
   # Git hash of this commit. A property so it can be viewed in datastore.
-  git_hash = db.StringProperty()
+  git_hash = ndb.StringProperty()
   # Git commit position for this commit (required for sorting).
-  position_ref = db.StringProperty()
-  position_num = db.IntegerProperty()
+  position_ref = ndb.StringProperty()
+  position_num = ndb.IntegerProperty()
   # Time at which this commit was set as the LKGR.
-  date = db.DateTimeProperty(auto_now_add=True)
+  date = ndb.DateTimeProperty(auto_now_add=True)
 
 
 class Commits(BasePage):
@@ -29,11 +32,11 @@ class Commits(BasePage):
   @utils.requires_read_access
   def get(self):
     """Returns information about the history of LKGR."""
-    limit = int(self.request.get('limit', 100))
-    commits = Commit.all().order('-position_num').order('position_ref').fetch(
-        limit)
+    limit = int(request.args.get('limit', 100))
+    commits = Commit.query().order(
+        '-position_num').order('position_ref').fetch(limit)
 
-    if self.request.get('format') == 'json':
+    if request.args.get('format') == 'json':
       self.response.headers['Content-Type'] = 'application/json'
       self.response.headers['Access-Control-Allow-Origin'] = '*'
       data = json.dumps([commit.AsDict() for commit in commits])
@@ -43,7 +46,7 @@ class Commits(BasePage):
     template_values = self.InitializeTemplate('Chromium Git LKGR History')
     page_value = {'commits': commits, 'limit': limit}
     template_values.update(page_value)
-    self.DisplayTemplate('commits.html', template_values)
+    return self.DisplayTemplate('commits.html', template_values)
 
   @utils.requires_write_access
   def post(self):
@@ -64,22 +67,24 @@ class Commits(BasePage):
 class LastKnownGoodRevisionGIT(BasePage):
   """Displays the /git-lkgr page."""
 
-  @utils.requires_read_access
+  # TODO (crbug.com/1121016): Require read access
   def get(self):
     """Look for the latest successful revision and return it."""
-    self.response.headers['Cache-Control'] = 'no-cache, private, max-age=5'
-    self.response.headers['Content-Type'] = 'text/plain'
-    commit = Commit.all().order('-position_num').order('position_ref').get()
+    commit = Commit.query().order('-position_num').order('position_ref').get()
     if commit:
-      self.response.out.write(commit.git_hash)
+      r = make_response(commit.git_hash)
+      r.headers['Cache-Control'] = 'no-cache, private, max-age=5'
+      r.headers['Content-Type'] = 'text/plain'
+      return r
     else:
       logging.error('OMG There\'s no git-lkgr!?')
-      self.abort(404)
+      abort(404)
 
 
 def bootstrap():
-  Commit.get_or_insert(
-      'dummy-commit',
-      git_hash='0' * 40,
-      position_ref='refs/heads/master',
-      position_num=0)
+  with ndb_client.context():
+    Commit.get_or_insert(
+        'dummy-commit',
+        git_hash='0' * 40,
+        position_ref='refs/heads/master',
+        position_num=0)
