cd8fe0359c3be8b83a695fbd75ad90bb16a602b2
Gregory Nisbet <gregorynisbet@google.com>
2021-09-20 10:58:36 -0700

    [karte] Wrap errors dependency.
    In preparation for a new GRPC conversion library, add the internal/errors package.
    
    BUG=b/200578943
    TEST=make test
    
    Change-Id: Id7923a25df2c4213ab4509c405285958148da032

---
+++ go/src/infra/cros/karte/internal/errors/errors.go
@@ -0,0 +1,32 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package errors
+
+import (
+	"fmt"
+
+	"go.chromium.org/luci/common/errors"
+)
+
+// Annotator exposes a subset of the LUCI error Annotator.
+type Annotator interface {
+	Err() error
+}
+
+// New creates a new error with the given tags.
+// TODO(gregorynisbet): Consider replacing with calls to Reason exclusively.
+func New(msg string, tags ...errors.TagValueGenerator) error {
+	return errors.New(msg, tags...)
+}
+
+// Annotate annotates an error.
+func Annotate(err error, reason string, args ...interface{}) *errors.Annotator {
+	return errors.Annotate(err, reason, args...)
+}
+
+// Errorf creates a new error given a format string.
+func Errorf(format string, a ...interface{}) error {
+	return fmt.Errorf(format, a...)
+}
