13c6730dc05d104c9a381d0b4b45ca8b9a19a771
Oliver Newman <olivernewman@google.com>
2021-10-08 15:03:56 -0400

    [recipe_autoroller] Respect recipes.cfg self_approve_method
    crrev.com/c/3213966 added this field to recipes.cfg. It defaults to
    BOT_COMMIT_APPROVE so the recipe autoroller will still set Bot-Commit+1
    by default, but now it also has support for setting CR+1 or CR+2 instead
    to support Gerrit hosts that don't use Bot-Commit.
    
    Change-Id: I95f3fcffad0e939cb44ed61650a9e5dbeeabf6b7
    Bug: 1205925

--- recipes/recipe_modules/recipe_autoroller/api.py
+++ recipes/recipe_modules/recipe_autoroller/api.py
@@ -9,7 +9,7 @@ import re
 from google.protobuf import json_format as jsonpb
 
 from recipe_engine import recipe_api
-from PB.recipe_engine.recipes_cfg import RepoSpec, DepRepoSpecs
+from PB.recipe_engine.recipes_cfg import AutorollRecipeOptions, DepRepoSpecs, RepoSpec
 
 
 class RepoData(object):
@@ -319,7 +319,13 @@ class RecipeAutorollerApi(recipe_api.RecipeApi):
     upload_args = ['--send-mail']
     if roll_result['trivial']:
       s = spec.autoroll_recipe_options.trivial
-      upload_args.append('--set-bot-commit')
+      if s.self_approve_method == AutorollRecipeOptions.TrivialOptions.BOT_COMMIT_APPROVE:
+        upload_args.append('--set-bot-commit')
+      elif s.self_approve_method == AutorollRecipeOptions.TrivialOptions.CODE_REVIEW_1_APPROVE:
+        upload_args.extend(['-o', '-l=Code-Review+1'])
+      elif s.self_approve_method == AutorollRecipeOptions.TrivialOptions.CODE_REVIEW_2_APPROVE:
+        upload_args.extend(['-o', '-l=Code-Review+2'])
+
       if s.tbr_emails:
         upload_args.extend(['-r', self.m.random.choice(s.tbr_emails)])
       upload_args.append('--r-owners')
