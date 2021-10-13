4e4d4a39d8c73be38f7e945ec13e1d356debfc60
Patrick Meiring <meiring@google.com>
2021-10-13 06:13:57 +0000

    [Weetbix] Add methods for saving and retreiving clustering state.
    BUG=chromium:1243174
    TEST=unit tests
    
    Change-Id: Ib34d1bfdeaff8f0aa2ffc472e2e9ee7863666293

--- go/src/infra/appengine/weetbix/internal/clustering/constants.go
+++ go/src/infra/appengine/weetbix/internal/clustering/constants.go
@@ -5,3 +5,9 @@ import "regexp"
 // ProjectRe matches validly formed LUCI Project names.
 // From https://source.chromium.org/chromium/infra/infra/+/main:luci/appengine/components/components/config/common.py?q=PROJECT_ID_PATTERN
 var ProjectRe = regexp.MustCompile(`^[a-z0-9\-_]{1,40}$`)
+
+// chunkRe matches validly formed chunk IDs.
+var ChunkRe = regexp.MustCompile(`^[0-9a-f]{1,32}$`)
+
+// algorithmRe matches validly formed clustering algorithm names.
+var AlgorithmRe = regexp.MustCompile(`^[0-9a-z\-.]{1,32}$`)
