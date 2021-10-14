92f36f2f0f0b3b3b01a2d30310b873b52fa91548
Patrick Meiring <meiring@google.com>
2021-10-14 07:10:49 +0000

    [Weetbix] Add framework for clustering.
    Including implementations for reason and testname-based clustering.
    
    BUG=chromium:1243174
    TEST=None
    
    Change-Id: Id1984ca5e73f2fd2eaf1aea5bc24ce98a80de8aa

---
+++ go/src/infra/appengine/weetbix/internal/clustering/algorithms/cluster.go
@@ -0,0 +1,73 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package algorithms
+
+import (
+	"time"
+
+	"infra/appengine/weetbix/internal/clustering"
+	"infra/appengine/weetbix/internal/clustering/algorithms/failurereason"
+	"infra/appengine/weetbix/internal/clustering/algorithms/testname"
+	cpb "infra/appengine/weetbix/internal/clustering/proto"
+)
+
+// Algorithm represents the interface that each clustering algorithm must
+// implement.
+type Algorithm interface {
+	Name() string
+	Cluster(failure *cpb.Failure) []byte
+}
+
+// AlgorithmsVersion is the version of the set of algorithms used.
+// Changing the set of algorithms below (including add, update or
+// deletion of an algorithm) should result in this version being
+// incremented.
+const AlgorithmsVersion = 1 + failurereason.AlgorithmVersion + testname.AlgorithmVersion
+
+// algs is the set of clustering algorithms known to Weetbix.
+// When this an algorithm is added or removed from the set,
+// or when an algorithm is updated, ensure the AlgorithmsVersion
+// above increments.
+var algs = []Algorithm{
+	&failurereason.Algorithm{},
+	&testname.Algorithm{},
+}
+
+// ClusterResults represents the results of clustering test failures.
+type ClusterResults struct {
+	// RuleVersion is the version of failure association rules used
+	// to cluster test results. This is the Spanner commit timestamp
+	// of the most recent failure association rule used in clustering.
+	RuleVersion time.Time
+	// Clusters each test result is in, one slice of ClusterRefs
+	// for each test result.
+	Clusters [][]*clustering.ClusterID
+}
+
+// Cluster clusters the given test failures using all registered
+// clustering algorithms.
+func Cluster(failures []*cpb.Failure) *ClusterResults {
+	var result [][]*clustering.ClusterID
+	for _, f := range failures {
+		var ids []*clustering.ClusterID
+		for _, a := range algs {
+			id := a.Cluster(f)
+			if id == nil {
+				continue
+			}
+			ids = append(ids, &clustering.ClusterID{
+				Algorithm: a.Name(),
+				ID:        id,
+			})
+		}
+		result = append(result, ids)
+	}
+	return &ClusterResults{
+		// TODO(crbug.com/1243174): Set when failure association rules
+		// are implemented.
+		RuleVersion: time.Date(1900, time.January, 1, 0, 0, 0, 0, time.UTC),
+		Clusters:    result,
+	}
+}
