d515e7597025e057d9feeaabd9e70b06bd3b9df7
Yunzhi Yu <yunzhiyu@google.com>
2021-10-16 14:22:27 -0700

    Recovery: Add two more storage related verifiers
    1. cros_has_enough_storage_space
    2. cros_has_enough_inodes
    
    BUG=b:197527180
    TEST=manual test
    
    
    Change-Id: Ib2fb58bd4dcfe6fd271775fcc113a03d9cb7ec2a

--- go/src/infra/cros/recovery/internal/execs/cros/storage_execs.go
+++ go/src/infra/cros/recovery/internal/execs/cros/storage_execs.go
@@ -6,6 +6,10 @@ package cros
 
 import (
 	"context"
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
 
 	"go.chromium.org/luci/common/errors"
 
@@ -55,6 +59,74 @@ func auditStorageSMARTExec(ctx context.Context, args *execs.RunArgs, actionArgs 
 	return nil
 }
 
+// hasEnoughStorageSpaceExec confirms the given path has at least the amount of free space specified by the actionArgs arguments.
+// provides arguments should be in the formart of:
+// ["../../path:x"]
+// x is the number of GB of the disk space.
+func hasEnoughStorageSpaceExec(ctx context.Context, args *execs.RunArgs, actionArgs []string) error {
+	path := strings.Split(actionArgs[0], ":")[0]
+	pathMinSpaceInGB, convertErr := strconv.ParseFloat(strings.Split(actionArgs[0], ":")[1], 64)
+	if convertErr != nil {
+		return errors.Annotate(convertErr, "has enough storage space: convert stateful path min space").Err()
+	}
+	if err := pathHasEnoughValue(ctx, args, args.ResourceName, path, "disk space", pathMinSpaceInGB); err != nil {
+		return errors.Annotate(err, "has enough storage space").Err()
+	}
+	return nil
+}
+
+// hasEnoughInodesExec confirms the given path has at least the amount of free inodes specified by the actionArgs arguments.
+// provides arguments should be in the formart of:
+// ["../../path:x"]
+// x is the number of kilos of inodes.
+func hasEnoughInodesExec(ctx context.Context, args *execs.RunArgs, actionArgs []string) error {
+	path := strings.Split(actionArgs[0], ":")[0]
+	pathMinKiloInodes, convertErr := strconv.ParseFloat(strings.Split(actionArgs[0], ":")[1], 64)
+	if convertErr != nil {
+		return errors.Annotate(convertErr, "has enough storage inodes: convert stateful path min kilo inodes").Err()
+	}
+	err := pathHasEnoughValue(ctx, args, args.ResourceName, path, "inodes", pathMinKiloInodes*1000)
+	return errors.Annotate(err, "has enough storage inodes").Err()
+}
+
+// pathHasEnoughValue is a helper function that checks the given path's free disk space / inodes is no less than the min disk space /indoes specified.
+func pathHasEnoughValue(ctx context.Context, args *execs.RunArgs, dutName string, path string, typeOfSpace string, minSpaceNeeded float64) error {
+	if !IsPathExist(ctx, args, path) {
+		return errors.Reason("path has enough value: %s: path: %q not exist", typeOfSpace, path).Err()
+	}
+	var cmd string
+	if typeOfSpace == "disk space" {
+		oneMB := math.Pow(10, 6)
+		log.Info(ctx, "Checking for >= %f (GB/inodes) of %s under %s on machine %s", minSpaceNeeded, typeOfSpace, path, dutName)
+		cmd = fmt.Sprintf(`df -PB %.f %s | tail -1`, oneMB, path)
+	} else {
+		// checking typeOfSpace == "inodes"
+		cmd = fmt.Sprintf(`df -Pi %s | tail -1`, path)
+	}
+	r := args.NewRunner(dutName)
+	output, err := r(ctx, cmd)
+	if err != nil {
+		return errors.Annotate(err, "path has enough value: %s", typeOfSpace).Err()
+	}
+	outputList := strings.Fields(output)
+	free, err := strconv.ParseFloat(outputList[3], 64)
+	if err != nil {
+		log.Error(ctx, err.Error())
+		return errors.Annotate(err, "path has enough value: %s", typeOfSpace).Err()
+	}
+	if typeOfSpace == "diskspace" {
+		mbPerGB := math.Pow(10, 3)
+		free = float64(free) / mbPerGB
+	}
+	if free < minSpaceNeeded {
+		return errors.Reason("path has enough value: %s: Not enough free %s on %s - %f (GB/inodes) free, want %f (GB/inodes)", typeOfSpace, typeOfSpace, path, free, minSpaceNeeded).Err()
+	}
+	log.Info(ctx, "Found %f (GB/inodes) >= %f (GB/inodes) of %s under %s on machine %s", free, minSpaceNeeded, typeOfSpace, path, dutName)
+	return nil
+}
+
 func init() {
 	execs.Register("cros_audit_storage_smart", auditStorageSMARTExec)
+	execs.Register("cros_has_enough_storage_space", hasEnoughStorageSpaceExec)
+	execs.Register("cros_has_enough_inodes", hasEnoughInodesExec)
 }
