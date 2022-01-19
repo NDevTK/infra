e5354b10b6e23c204b9eb97f39ea22db86c4ea8f
Yuxuan Chen <yuxuanch@google.com>
2022-01-19 11:01:16 -0800

    create Issue Wizard Utils file this file is used to host all the util function used to phrase issue wizard metadata bug: monorail:6891 Change-Id: I5cd937cf371bd31c1c425ebdd93428d234f8a586
    Change-Id: I5cd937cf371bd31c1c425ebdd93428d234f8a586

---
+++ appengine/monorail/static_src/react/issue-wizard/IssueWizardUtils.tsx
@@ -0,0 +1,29 @@
+import {customQuestion, IssueCategory} from "./IssueWizardTypes";
+
+// this function is used to get the issue list belong to different user group
+// when a user group is selected a list of related issue catelog will show up
+export function GetIssueCategoryMap (issues: IssueCategory[]):Map<string, string[]> {
+  const issueUserNameAndTopicMap = new Map<string, string[]>();
+
+  issues.forEach((issue) => {
+    if (issue.enabled) {
+      const currentIssueUser = issue.user;
+      const currentIssueName = issue.name;
+      const topics = issueUserNameAndTopicMap.get(currentIssueUser) ?? [];
+      topics.push(currentIssueName);
+      issueUserNameAndTopicMap.set(currentIssueUser, topics);
+    }
+  });
+
+  return issueUserNameAndTopicMap;
+}
+
+// this function is used to get the customer questions belong to different issue category
+// the customer question page will render base on these data
+export function GetIssueCustomQuestionsMap(issues: IssueCategory[]):Map<string,customQuestion[] | null> {
+  const issueCustomQuestionsMap = new Map<string,customQuestion[] | null>();
+  issues.forEach((issue) => {
+    issueCustomQuestionsMap.set(issue.name, issue.customQuestions ?? null);
+  })
+  return issueCustomQuestionsMap;
+}
