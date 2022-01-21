654401d7d6f1ed0bd67690751515cacd3abf92d2
Gregory Nisbet <gregorynisbet@google.com>
2022-01-21 13:25:01 -0800

    Fix valid pool name heuristic
    This heuristic is used in
    `cmd/skylab/internal/userinput/device_specs.go`, which is used by shivas
    and possibly other tools.
    
    The actual regex in question includes a typo `A-z` instead of `A-Z`. Fix
    this.
    
    BUG=None
    TEST=make test
    
    Change-Id: Ifea10870a0664bb2862bff9587c313fa3e59f20f

--- go/src/infra/libs/skylab/common/heuristics/heuristics_test.go
+++ go/src/infra/libs/skylab/common/heuristics/heuristics_test.go
@@ -6,13 +6,15 @@ package heuristics
 
 import (
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 // TestLooksLikeCrosskBotName tests identification of bots.
 func TestLooksLikeCrosskBotName(t *testing.T) {
 	t.Parallel()
 
-	cases := struct {
+	cases := []struct {
 		name string
 		in   string
 		out  bool
@@ -40,6 +42,45 @@ func TestLooksLikeCrosskBotName(t *testing.T) {
 			t.Parallel()
 			expected := tt.out
 			actual := LooksLikeCrosskBotName(tt.in)
+			if diff := cmp.Diff(expected, actual); diff != "" {
+				t.Errorf("unexpected diff (-want +got): %s", diff)
+			}
+		})
+	}
+}
+
+// TestLooksLikeValidPool tests whether strings are correctly identified as being valid pools.
+func TestLooksLikeValidPool(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name string
+		in   string
+		out  bool
+	}{
+		{
+			name: "empty string",
+			in:   "",
+			out:  false,
+		},
+		{
+			name: "has [",
+			in:   "a[",
+			out:  false,
+		},
+		{
+			name: "valid identifier",
+			in:   "valid_identifier4",
+			out:  true,
+		},
+	}
+
+	for _, tt := range cases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			expected := tt.out
+			actual := LooksLikeValidPool(tt.in)
 			if diff := cmp.Diff(expected, actual); diff != "" {
 				t.Errorf("unexpected diff (-want +got): %s", diff)
 			}
