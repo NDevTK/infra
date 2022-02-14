a94bb545d1c8cb5af9b5343612b872af6c9a93ed
Gowri Denduluri <gowriden@google.com>
2022-02-13 16:41:14 -0800

    [UFS] - Sending custom status types in CheckFleetTestsPolicyResponse to help identify errors in crosfleet CLI..
    These status types will be used to identify different types of failures
    when validating public tests.
    
    Bug:b/209476191
    Change-Id: I8cb6b20112aa2f4992451c1bfc0ee80976959514

--- go/src/infra/unifiedfleet/app/frontend/fleet_tests_policy.go
+++ go/src/infra/unifiedfleet/app/frontend/fleet_tests_policy.go
@@ -16,15 +16,46 @@ import (
 
 // CheckFleetTestsPolicy returns whether the given the test parameters are for a valid test.
 func (fs *FleetServerImpl) CheckFleetTestsPolicy(ctx context.Context, req *api.CheckFleetTestsPolicyRequest) (response *ufsAPI.CheckFleetTestsPolicyResponse, err error) {
-	validTestResponse := &ufsAPI.CheckFleetTestsPolicyResponse{
-		IsTestValid: true,
+	isPublicUser, err := controller.IsPublicGroupMember(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if !isPublicUser {
+		return &ufsAPI.CheckFleetTestsPolicyResponse{
+			IsTestValid: true,
+			Status:      api.CheckFleetTestsPolicyResponse_PRIVATE_USER,
+		}, nil
 	}
 
 	// Check test parameters
+	status := api.CheckFleetTestsPolicyResponse_UNSPECIFIED
 	err = controller.IsValidTest(ctx, req)
-	if err != nil {
-		logging.Errorf(ctx, "Error validating test parameters : %s", err.Error())
+	if err == nil {
+		return &ufsAPI.CheckFleetTestsPolicyResponse{
+			IsTestValid: true,
+			Status:      status,
+		}, nil
+	}
+
+	switch err.(type) {
+	case *controller.InvalidBoardError:
+		logging.Infof(ctx, "Returning Invalid board error")
+		status = api.CheckFleetTestsPolicyResponse_NOT_A_PUBLIC_BOARD
+	case *controller.InvalidModelError:
+		logging.Infof(ctx, "Returning Invalid model error")
+		status = api.CheckFleetTestsPolicyResponse_NOT_A_PUBLIC_MODEL
+	case *controller.InvalidTestError:
+		logging.Infof(ctx, "Returning Invalid test error")
+		status = api.CheckFleetTestsPolicyResponse_NOT_A_PUBLIC_TEST
+	case *controller.InvalidImageError:
+		logging.Infof(ctx, "Returning Invalid image error")
+		status = api.CheckFleetTestsPolicyResponse_NOT_A_PUBLIC_IMAGE
+	default:
 		return nil, err
 	}
-	return validTestResponse, nil
+	return &ufsAPI.CheckFleetTestsPolicyResponse{
+		IsTestValid:   false,
+		Status:        status,
+		StatusMessage: err.Error(),
+	}, nil
 }
