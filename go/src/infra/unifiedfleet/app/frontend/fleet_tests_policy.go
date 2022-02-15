5233984b962a3958283c87ea7b6a882ae0092c0a
Gowri Denduluri <gowriden@google.com>
2022-02-15 00:40:53 -0800

    [UFS] - Sending custom status types in CheckFleetTestsPolicyResponse to help identify errors in crosfleet CLI..
    These status types will be used to identify different types of failures
    when validating public tests.
    
    Bug:b/209476191
    Change-Id: I8cb6b20112aa2f4992451c1bfc0ee80976959514

--- go/src/infra/unifiedfleet/app/frontend/fleet_tests_policy.go
+++ go/src/infra/unifiedfleet/app/frontend/fleet_tests_policy.go
@@ -8,23 +8,40 @@ import (
 	"context"
 
 	api "infra/unifiedfleet/api/v1/rpc"
-	ufsAPI "infra/unifiedfleet/api/v1/rpc"
 	"infra/unifiedfleet/app/controller"
 
 	"go.chromium.org/luci/common/logging"
 )
 
 // CheckFleetTestsPolicy returns whether the given the test parameters are for a valid test.
-func (fs *FleetServerImpl) CheckFleetTestsPolicy(ctx context.Context, req *api.CheckFleetTestsPolicyRequest) (response *ufsAPI.CheckFleetTestsPolicyResponse, err error) {
-	validTestResponse := &ufsAPI.CheckFleetTestsPolicyResponse{
-		IsTestValid: true,
+func (fs *FleetServerImpl) CheckFleetTestsPolicy(ctx context.Context, req *api.CheckFleetTestsPolicyRequest) (response *api.CheckFleetTestsPolicyResponse, err error) {
+	// Check test parameters
+	var statusCode api.TestStatus_Code
+	response = &api.CheckFleetTestsPolicyResponse{}
+	err = controller.IsValidTest(ctx, req)
+	if err == nil {
+		response.TestStatus = &api.TestStatus{
+			Code: api.TestStatus_OK,
+		}
+		return response, nil
 	}
 
-	// Check test parameters
-	err = controller.IsValidTest(ctx, req)
-	if err != nil {
-		logging.Errorf(ctx, "Error validating test parameters : %s", err.Error())
+	logging.Errorf(ctx, "Returning error %s", err.Error())
+	switch err.(type) {
+	case *controller.InvalidBoardError:
+		statusCode = api.TestStatus_NOT_A_PUBLIC_BOARD
+	case *controller.InvalidModelError:
+		statusCode = api.TestStatus_NOT_A_PUBLIC_MODEL
+	case *controller.InvalidTestError:
+		statusCode = api.TestStatus_NOT_A_PUBLIC_TEST
+	case *controller.InvalidImageError:
+		statusCode = api.TestStatus_NOT_A_PUBLIC_IMAGE
+	default:
 		return nil, err
 	}
-	return validTestResponse, nil
+	response.TestStatus = &api.TestStatus{
+		Code:    statusCode,
+		Message: err.Error(),
+	}
+	return response, nil
 }
