98871fde835105942ac01e1269825ced7a721ce9
Yunzhi Yu <yunzhiyu@google.com>
2022-03-18 18:21:26 -0700

    recovery: Use Karte's Metrics to record DUT's last cr50 fw reflash time.
    This cl adds functionality to enable Karte in Paris to track the last time
    a DUT reflashes its cr50 fw.
    
    One Exec action was added: isTimeToReflashCr50FwExec (to determine whether it's ok to reflash)
    
    One Exec action was modified: reflashCr50FwExec (to dump into karte after cr50 fw reflash)
    
    BUG=b:216567871
    TEST=manual test
    
    Change-Id: I1b3e4c6ed78e9a814f309499f95983927f85bbcf

---
+++ go/src/infra/cros/recovery/logger/metrics/kinds.go
@@ -0,0 +1,11 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package metrics
+
+const (
+	// LastRecordedCr50FwReflashTimeKeyword is the name/kind in the karte metrics
+	// used for query or update the cr 50 fw reflash information.
+	LastRecordedCr50FwReflashTimeKeyword = "cr50_flash"
+)
