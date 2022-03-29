d41510e313a85e0f9813f28af28cef63d3e7dea7
Weizhong Xia <weizhong@google.com>
2022-03-29 18:27:49 +0000

    Add libcrypt.so to the httpd-php cipd package
    The docker environment uses libcrypt.so.2, which isn't
    available where we run the resulting binary.
    
    Bug: 1155141
    Change-Id: I2c6e1005e164475d8329251611b02db5acc30c80

--- 3pp/httpd-php/install.sh
+++ 3pp/httpd-php/install.sh
@@ -10,6 +10,9 @@
 # path on Mac
 
 set -e
+set -x
+
+uname -a
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -21,7 +24,7 @@ libxml2_version="2.9.12"
 openssl_version="1.1.1j"
 pcre_version="8.41"
 php_version="7.3.31"
-zlib_version="1.2.11"
+zlib_version="1.2.12"
 
 out="$1"
 build="$PWD/build"
@@ -34,6 +37,8 @@ mkdir "${src}"
 
 cd "${src}"
 
+CC_BACKUP=$CC
+unset CC
 echo "Building zlib"
 tar xf "../zlib-${zlib_version}.tar.gz"
 cd "zlib-${zlib_version}"
@@ -41,6 +46,7 @@ cd "zlib-${zlib_version}"
 make -j"${jobs}"
 make install
 cd ..
+CC=CC_BACKUP
 
 echo "Building OpenSSL"
 tar xf "../openssl-${openssl_version}.tar.gz"
@@ -261,4 +267,11 @@ then
       exit 1
     fi
   done
+fi
+
+if [[ $OSTYPE == linux* ]]
+then
+  # The docker environment uses libcrypt.so.2, which isn't available
+  # where we run the resulting binary.
+  cp /usr/local/lib/libcrypt.so.2 "${out}/lib"
 fi
