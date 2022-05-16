5d31e4e6367c5ca419d1059985ead445b2d41ebb
Gregory Nisbet <gregorynisbet@google.com>
2022-05-16 20:44:13 +0000

    [karte] Add package doc comemnts for RPC API.
    BUG=b:231636964
    TEST=make test
    
    Change-Id: I6d6654faec084610be2ccbbd8e5cda141066f858
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3641442
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Reviewed-by: Ashish Gandhi <ashishgandhi@google.com>
    Cr-Commit-Position: refs/heads/main@{#49031}

---
+++ go/src/infra/cros/karte/api/bigquery/doc.go
@@ -0,0 +1,12 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package kbqpb is the bigquery proto API of karte.
+//
+// These protos should implement the ValueSaver interface, which allows
+// them to control the names of their fields.
+//
+// The protos also control the schema of the BigQuery tables belonging to Karte.
+// These protos are intentionally very simple. Karte uses flat BigQuery tables in order to make ad hoc SQL queries more ergonomic.
+package kbqpb
---
+++ go/src/infra/cros/karte/api/doc.go
@@ -0,0 +1,16 @@
+// Package kartepb is the RPC interface of Karte.
+//
+// - CreateAction
+// - CreateObservation
+// - PersistAction
+// - PersistActionRange
+// - ListActions
+// - ListObservations
+// - UpdateAction
+//
+//
+// Karte also has a cron interface.
+//
+// - PersistToBigquery
+//
+package kartepb
