78ea10fd85913df3dac5ddbb3cbc2619b84ea526
Gregory Nisbet <gregorynisbet@google.com>
2022-05-23 17:17:19 +0000

    Reland "[crosadm] Add support for getting the most specific prod and latest permilles."
    This reverts commit 666377ef9a683acdc433d06a3d1ed359e7057f72.
    
    Reason for revert: b:233237505 Reland once we unsplit traffic to crosadm
    
    Original change's description:
    > Revert "[crosadm] Add support for getting the most specific prod and latest permilles."
    >
    > This reverts commit 8b08f5e9f60934cea922a49ba25883109426f6ab.
    >
    > Reason for revert: b:233237505
    >
    > Original change's description:
    > > [crosadm] Add support for getting the most specific prod and latest permilles.
    > >
    > > Add support for getting specific prod and latest permilles based on specific
    > > hostname override.
    > >
    > > BUG=b:232150759
    > > TEST=make test
    > >
    > > Change-Id: I0312c0f2bf0c482986ec31675a350e6d7b57c502
    > > Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3648778
    > > Reviewed-by: Otabek Kasimov <otabek@google.com>
    > > Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    > > Reviewed-by: Ashish Gandhi <ashishgandhi@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#49131}
    >
    > Bug: b:232150759
    > Change-Id: Ide54cfa4045663ec7c17282b93366c9a49407944
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3656149
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Otabek Kasimov <otabek@google.com>
    > Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    > Cr-Commit-Position: refs/heads/main@{#49150}
    
    Bug: b:232150759
    Change-Id: I5cc1d6ee0e2d6bdec3c12d3dedc40584558b1c7b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3653016
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Reviewed-by: Ashish Gandhi <ashishgandhi@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#49215}

--- go/src/infra/appengine/crosskylabadmin/internal/app/config/config_helpers.go
+++ go/src/infra/appengine/crosskylabadmin/internal/app/config/config_helpers.go
@@ -4,18 +4,89 @@
 
 package config
 
+import (
+	"context"
+	"regexp"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
+)
+
 // PermilleData contains information on what portion of traffic to opt
 // into Prod and Latest.
 type PermilleData struct {
+	Source string
 	Prod   float64
 	Latest float64
 }
 
-// ComputeProdPermille computes the most applicable prod permille
-// for a device.
-func (x *RolloutConfig) ComputePermilleData(hostname string) (PermilleData, error) {
-	return PermilleData{
+func validatePattern(pattern string) error {
+	if len(pattern) < 2 {
+		return errors.Reason("pattern is too short").Err()
+	}
+	if pattern == "^$" {
+		return errors.Reason(`pattern "^$" is not useful`).Err()
+	}
+	if pattern[0] == '^' || pattern[len(pattern)-1] == '$' {
+		return nil
+	}
+	return errors.Reason(`pattern missing "^" or "$" anchor`).Err()
+}
+
+// matches returns true if and only if pattern and hostname are both nonempty and
+// the hostname is an instance of the pattern.
+//
+// We return an non-nil error if and only if the pattern is empty, the hostname is empty,
+// or the regular expression pattern fails to compile.
+func matches(pattern string, hostname string) (bool, error) {
+	if err := validatePattern(pattern); err != nil {
+		return false, errors.Annotate(err, "matches").Err()
+	}
+	if hostname == "" {
+		return false, errors.Reason("matches: hostname cannot be empty").Err()
+	}
+	r, err := regexp.Compile(pattern)
+	if err != nil {
+		return false, errors.Annotate(err, "matches").Err()
+	}
+	return r.MatchString(hostname), nil
+}
+
+// getLastMatch returns the last match in the config.
+func (x *RolloutConfig) getLastMatch(hostname string) (*PermilleData, error) {
+	patterns := x.GetPattern()
+	// We want to enable the users to write the patterns with more general patterns at the top
+	// and more specific patterns at the bottom.
+	//
+	// In this setting, it is correct to iterate backwards through the list and stop as soon
+	// as we see a match.
+	for i := -1 + len(patterns); i >= 0; i-- {
+		ok, err := matches(patterns[i].GetPattern(), hostname)
+		if err != nil {
+			return nil, errors.Annotate(err, "get specific pattern").Err()
+		}
+
+		if ok {
+			return &PermilleData{
+				Source: patterns[i].GetPattern(),
+				Prod:   float64(patterns[i].GetProdPermille()),
+				Latest: float64(patterns[i].GetLatestPermille()),
+			}, nil
+		}
+	}
+	return &PermilleData{
+		Source: "",
 		Prod:   float64(x.GetProdPermille()),
 		Latest: float64(x.GetLatestPermille()),
 	}, nil
+}
+
+// ComputeProdPermille computes the most applicable prod permille
+// for a device.
+func (x *RolloutConfig) ComputePermilleData(ctx context.Context, hostname string) (*PermilleData, error) {
+	d, err := x.getLastMatch(hostname)
+	if err != nil {
+		logging.WithError(err).Errorf(ctx, "malformed config file")
+	}
+	return d, nil
 }
