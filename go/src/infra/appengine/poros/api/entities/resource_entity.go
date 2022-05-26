365ab60bed190de669d0242d1084a4419bd5c4c9
Eswaran Krishnamoorthy <eswarank@google.com>
2022-05-26 01:52:27 +0000

    Added Resource entity in poros
    Created the reource entity to store the image information which will be
    used in asset.
    
    Bug: 1329194
    Change-Id: Ic587b02fa81ce0f1ffcc14094c785c712e0aa598
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3668124
    Reviewed-by: Xiang Ji <jxiang@google.com>
    Commit-Queue: Eswaran Krishnamoorthy <eswarank@google.com>
    Cr-Commit-Position: refs/heads/main@{#49335}

---
+++ go/src/infra/appengine/poros/api/entities/resource_entity.go
@@ -0,0 +1,31 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package entities
+
+import (
+	"time"
+)
+
+type ResourceEntity struct {
+	// Unique identifier of the resource
+	ResourceId string `gae:"$id"`
+	// Name of the resource
+	Name string
+	// Description of the resource
+	Description string
+	// Type of the resource
+	Type string
+	// TODO: crbug/1328854 move the image info as part of property later phases
+	// image associated to the machine
+	Image string
+	// User who created the record.
+	CreatedBy string
+	// Timestamp for the creation of the record.
+	CreatedAt time.Time
+	// Timestamp for the last update of the record.
+	ModifiedAt time.Time
+	// User who modified the record.
+	ModifiedBy string
+}
