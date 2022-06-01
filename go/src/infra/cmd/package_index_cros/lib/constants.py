88c708ba5aabf90306733fb2b35e2875e9bca9ea
Christoph Schlosser <cschlosser@google.com>
2022-06-01 11:09:09 +0000

    Update temporary unsupported packages
    Add packages that aren't buildable currently for amd64-generic, betty
    or hatch.
    
    Bug: b:233172208
    Change-Id: I04cd41fe38945f4adaf00fff2c6345287db96c09
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3666856
    Auto-Submit: Christoph Schlosser <cschlosser@chromium.org>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: Christoph Schlosser <cschlosser@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#49418}

--- go/src/infra/cmd/package_index_cros/lib/constants.py
+++ go/src/infra/cmd/package_index_cros/lib/constants.py
@@ -12,6 +12,11 @@ CACHE_PACKAGES_PATH_TEMPALATE = os.path.join('/', 'tmp',
 # Set of packages that should be fine to work with but are not handled properly
 # yet.
 TEMPORARY_UNSUPPORTED_PACKAGES = {
+    # Reason: There are no ebuilds to satisfy ">=dev-lang/python-2.7.5-r2:2.7[threads(+)]".
+    # Probably perfectly fine packages otherwise.
+    'app-benchmarks/glmark2',
+    'chromeos-base/autotest-deps-glmark2',
+
     # Reason: sys-devel/arc-build fails, but I cannot figure out which package
     # triggers it.
     # Perfectly fine package otherwise.
@@ -27,6 +32,9 @@ TEMPORARY_UNSUPPORTED_PACKAGES = {
     'chromeos-base/arc-sensor-service',
     'chromeos-base/arc-setup',
     'chromeos-base/arcvm-boot-notification-server',
+    # Reason: Required 'USE=arcpp' or 'USE=arcvm' fails builds
+    # Probably a perfectly fine package otherwise.
+    'chromeos-base/arc-base',
     # A bit strange package using files from platform2/vm_tools, but should be
     # buildable.
     'chromeos-base/arcvm-forward-pstore',
@@ -38,6 +46,14 @@ TEMPORARY_UNSUPPORTED_PACKAGES = {
     # If it can be built but has empty compile_commands, there should be no
     # harm, need to be NO_LOCAL_SOURCE otherwise.
     'chromeos-base/arc-sdcard',
+
+    # Reason: Cannot find build or temp dir
+    # Probably a perfectly fine package otherwise.
+    'chromeos-base/arc-myfiles',
+    'chromeos-base/arc-removable-media',
+    'chromeos-base/arc-common-scripts',
+    'chromeos-base/arcvm-common-scripts',
+    'chromeos-base/arcvm-mount-media-dirs',
 
     # TODO: notify owners.
     # Reason: include path is misspelled vs actual dir: nNCache vs
@@ -52,10 +68,33 @@ TEMPORARY_UNSUPPORTED_PACKAGES = {
     # field which makes merge complicated.
     'chromeos-base/bootid-logger',
 
+    # Reason: Cannot find temp dir
+    # Probably a perfectly fine package otherwise.
+    'chromeos-base/cdm-oemcrypto',
+    'chromeos-base/cdm-oemcrypto-hw-test',
+
+    # Reason: REQUIRED_USE=any-of: test factory_netboot_ramfs factory_shim_ramfs hypervisor_ramfs recovery_ramfs minios_ramfs
+    # Probably a perfectly fine package otherwise.
+    'chromeos-base/chromeos-initramfs',
+
     # Has cryptohome-flatbuffers-binding gn target which has sources field which
     # is almost the same as the target in chromeos-base/cryptohome except that
     # it uses paths generated for this package.
     'chromeos-base/cryptohome-dev-utils',
+
+    # Reason: REQUIRED_USE=fuzzer fails builds.
+    # Probably a perfectly fine package otherwise.
+    'chromeos-base/cups-fuzz',
+
+    # Reason: Required 'USE=cheets' fails builds.
+    # Probably a perfectly fine package otherwise.
+    'chromeos-base/demo_mode_resources',
+
+    # Reason: Missing dependency //diagnostics/mojom/public:cros_healthd_mojo_connectivity_bindings
+    'chromeos-base/diagnostics-test',
+
+    # Reason: Several missing headers referenced and typos in types.
+    'chromeos-base/factory_runtime_probe',
 
     # Reason: sys-cluster/fcp dependency fails build.
     # Perfectly fine package otherwise.
@@ -86,9 +125,20 @@ TEMPORARY_UNSUPPORTED_PACKAGES = {
     # merge complicated.
     'chromeos-base/ml-cmdline',
 
+    # Reason: Mismatched proto versions in generated files and referenced library.
+    # Probably a perfectly fine package otherwise.
+    'chromeos-base/modemfwd',
+
+    # Reason: media_perception_impl.cc:86:13: error: no member named 'AdaptCallbackForRepeating' in namespace 'base'
+    'chromeos-base/mri_package',
+
     # Reason: /etc/init/ocr_service.conf: missing 'oom score' line
     # Perfectly fine package otherwise.
     'chromeos-base/ocr',
+
+    # Reason: Cannot find temp dir
+    # Perfectly fine package otherwise.
+    'chromeos-base/smogcheck',
 
     # Reason: REQUIRED_USE="kvm_guest" fails build.
     # Perfectly fine package otherwise.
@@ -99,19 +149,62 @@ TEMPORARY_UNSUPPORTED_PACKAGES = {
     # another seccomp.
     'chromeos-base/touch_firmware_calibration',
 
+    # Reason: Dependency chromeos-base/vm_guest_tools can't be built due to missing REQUIRED_USE.
+    # Probably a perfectly fine package otherwise.
+    'chromeos-base/tremplin',
+
+    # Reason: Given path does not exist: /build/hatch/usr/include/u2f/client vs /build/hatch/usr/include/u2f/client
+    # Perfectly fine package otherwise.
+    'chromeos-base/u2fd',
+
     # Reason: compilation errors because base::WriteFileDescriptor.
     # Should be solved by using older libchrome or updating the package.
     # Perfectly fine package otherwise.
     'chromeos-base/ureadahead-diff',
 
+    # Reason: Required 'USE=vulkan' fails build
+    # Probably a perfectly fine package otherwise.
+    'chromeos-base/vkbench',
+
     # Reason: REQUIRED_USE="kvm_guest" fails build.
     # Perfectly fine package otherwise.
     'chromeos-base/vm_guest_tools',
+
+    # Reason: Lint error, needs a tmpfiles.d configuration.
+    # Probably a perfectly fine package otherwise.
+    'chromeos-base/webserver',
+
+    # Reason: Require dev-lang/python with USE: +sqlite
+    # Probably perfectly fine packages otherwise.
+    'chromeos-base/zephyr-build-tools',
+    'dev-python/hypothesis',
 
     # Reason: Fails build because it cannot find src/aosp/external/perfetto.
     # It's a go package that pretends to be an actual package. Should be
     # properly ignored.
     'dev-go/perfetto-protos',
+
+    # Reason: There are no ebuilds built with USE flags to satisfy "dev-libs/gmp[static-libs]" has to USE: +static-libs
+    # Probably a perfectly fine package otherwise.
+    'dev-util/shellcheck',
+
+    # Reason: Masked packages for dependency ebuild x11-libs/arc-libdrm are missing keywords
+    # Probably a perfectly fine package otherwise.
+    'media-libs/arc-mesa-freedreno',
+    'media-libs/arc-mesa-virgl',
+    'media-libs/arcvm-mesa-freedreno',
+
+    # Reason: Required 'USE=cheets' fails builds
+    # Probably a perfectly fine package otherwise.
+    'media-libs/arc-mesa-iris',
+
+    # Reason: Several headers can't be found
+    # Probably a perfectly fine package otherwise.
+    'media-libs/cros-camera-hdrnet-tests',
+
+    # Reason: There are no ebuilds to satisfy "media-libs/libcamera-configs"
+    # Probably a perfectly fine package otherwise.
+    'media-libs/libcamera',
 
     # Reason: Requires media-libs/intel-ipu6-camera-bins which is missing.
     # Perfectly fine package otherwise.
@@ -120,6 +213,18 @@ TEMPORARY_UNSUPPORTED_PACKAGES = {
     # Reason: Compilation errors due to some script.
     # Perfectly fine package otherwise.
     'media-libs/cros-camera-libjda_test',
+
+    # Reason: REQUIRED_USE either rockchip or rockchip_v2.
+    # Probably a perfectly fine package otherwise.
+    'media-libs/libv4lplugins',
+
+    # Reason: Cannot find temp dir
+    # Perfectly fine package otherwise.
+    'net-print/brother_mlaser',
+
+    # Reason: Multiple package instances within a single package slot have been pulled in
+    # Perfectly fine package otherwise.
+    'sci-libs/tensorflow',
 }
 
 # Set of packages that are not currently supported when building with tests.
