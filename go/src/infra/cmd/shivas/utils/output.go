fda4f332f2a4b6f174d828504fc30381f6a0ba2c
Xixuan Wu <xixuan@chromium.org>
2022-06-10 15:20:14 +0000

    Revert "[shivas] shivas get dut -json should skip delimiters."
    This reverts commit f9f6489b07d4c6111b43035364c75ca5a8082d55.
    
    Reason for revert: break browser lab manual workflow
    
    Original change's description:
    > [shivas] shivas get dut -json should skip delimiters.
    >
    > Make `shivas get` produce output that is interpretable by `shivas update`.
    > Do this by suppressing "[" and "]" when we want to print exactly one item.
    >
    > BUG=b:193566796
    > TEST=shivas get dut --json [redacted] AND observe output
    >
    > Change-Id: Iced0d910887b4b8e85122b69e9e6d372e8a24462
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3693944
    > Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    > Reviewed-by: Vineet Joshi <vkjoshi@google.com>
    > Reviewed-by: Justin Suen <justinsuen@google.com>
    > Cr-Commit-Position: refs/heads/main@{#49628}
    
    Bug: b:193566796
    Change-Id: I8140963bf23262068ae86fb3b19f2a79b517e8d9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3700310
    Reviewed-by: Elliott Friedman <friedman@google.com>
    Reviewed-by: Jonathan Pittman <jmpittman@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Vineet Joshi <vkjoshi@google.com>
    Commit-Queue: Jonathan Pittman <jmpittman@google.com>
    Cr-Commit-Position: refs/heads/main@{#49652}

--- go/src/infra/cmd/shivas/utils/output.go
+++ go/src/infra/cmd/shivas/utils/output.go
@@ -1372,14 +1372,11 @@ func PrintDutsShort(res []proto.Message, keysOnly bool) {
 
 // PrintMachineLSEsJSON prints the machinelse details in json format.
 func PrintMachineLSEsJSON(res []proto.Message, emit bool) {
-	shouldPrintDelimiters := len(res) != 1
 	machinelses := make([]*ufspb.MachineLSE, len(res))
 	for i, r := range res {
 		machinelses[i] = r.(*ufspb.MachineLSE)
 	}
-	if shouldPrintDelimiters {
-		fmt.Print("[")
-	}
+	fmt.Print("[")
 	for i, m := range machinelses {
 		m.Name = ufsUtil.RemovePrefix(m.Name)
 		PrintProtoJSON(m, emit)
@@ -1388,9 +1385,7 @@ func PrintMachineLSEsJSON(res []proto.Message, emit bool) {
 			fmt.Println()
 		}
 	}
-	if shouldPrintDelimiters {
-		fmt.Println("]")
-	}
+	fmt.Println("]")
 }
 
 func machineLSEFullOutputStrs(lse *ufspb.MachineLSE, dhcp *ufspb.DHCPConfig) []string {
