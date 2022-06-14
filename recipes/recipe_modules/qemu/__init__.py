e9015fc5d56db2d30e48ea4da24ad7d7f71284e5
Anushruth S <anushruth@google.com>
2022-06-14 18:01:28 +0000

    [QEMU] Add support to create virtual disks
    Bug: b/232831070
    
    Adds support to create virtual disk images
    
    Change-Id: Id57bfdca75c928446db13b8e43d93874bb5f1930
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3656454
    Commit-Queue: Anushruth Sasivehalli <anushruth@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Elliott Friedman <friedman@google.com>
    Cr-Commit-Position: refs/heads/main@{#49705}

--- recipes/recipe_modules/qemu/__init__.py
+++ recipes/recipe_modules/qemu/__init__.py
@@ -5,6 +5,9 @@
 DEPS = [
     'recipe_engine/cipd',
     'recipe_engine/path',
+    'recipe_engine/raw_io',
+    'recipe_engine/step',
+    'recipe_engine/file',
 ]
 
 PYTHON_VERSION_COMPATIBILITY = 'PY2+3'
