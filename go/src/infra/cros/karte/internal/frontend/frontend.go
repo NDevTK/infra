7d8040ce8763400f09423857620a401d7fda86df
Gregory Nisbet <gregorynisbet@google.com>
2022-06-17 00:05:17 +0000

    [karte] Fix error message when persisting batches.
    BUG=None
    TEST=make test
    
    Change-Id: I4549bd32b0204c698129df758c1e62bd29d06171
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3708038
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Cr-Commit-Position: refs/heads/main@{#49783}

--- go/src/infra/cros/karte/internal/frontend/frontend.go
+++ go/src/infra/cros/karte/internal/frontend/frontend.go
@@ -246,7 +246,7 @@ func (k *karteFrontend) persistActionRangeImpl(ctx context.Context, client bqPer
 		// We do not need to worry about retries. The default implementation will retry for us in a reasonable way.
 		if err := insertCb(ctx, batch); err != nil {
 			logging.Errorf(ctx, "cannot insert action: %s", err)
-			return nil, status.Errorf(codes.Aborted, "error persisting single record: %s", err)
+			return nil, status.Errorf(codes.Aborted, "error persisting batch: %s", err)
 		}
 	}
 
