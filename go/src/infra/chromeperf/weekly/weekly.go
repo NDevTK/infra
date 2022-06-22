c459cd254765243a084c1050e04b7c0753e580ee
seanmccullough <seanmccullough@google.com>
2022-06-21 17:36:32 -0700

    [on-call rotation util] weekly meeting Incoming Issues list generator
    generates markdown, but you can pipe that into whatever md-to-html
    tool you like.
    
    example: https://g3mark-service-playground.corp.google.com/?markdownz=pZNta9swEMe/yuFXGyR1nbRJYxhjDwz6oksgsDdbaB37bB21T5oe0mV1vntPyUL7YpSxCCQk3f2ln053j0mp2SP7JE96WFiCHq6dCwjXn2W6DF1X2C30P7iH4XAI+3HfnqcHq2iHmWx+L5XVHYUuz8bj6cX5bPVGeW9cnqbr0Lizo/lM2yY16XGZUrzVpRX6gtr3VL37I38bz/x2BUuDWOkOPdoRLNDWsFRoqa5XOUzAYmPROdLsgBjcS+dX8MaTi1PwRL7Hizgftf8ipmBxBR+4spoqaDRUuKESHWgGE6EfCmGqi7aFgitYEBtN7IGFVx5R6g0ew/1X4slkdn4CcZRH4rLVoYJ1oLYStBrcIZQgyVBTA6pwsEZkqEVK3IAjLhEu03H2CttoNp2ewBbl//LZ2X9+dja6ujwBL8ojng18u0YulRTGvaD8DCQwUMCd2Xql+Q7WxFIzA3hQVCpgDa3mBi3gL3J+nwldwVtwW+exc9Ang8QiV2jnxsc3Jflj4pS2XkJ/v8RYmp/mNzfzr+KoqFGtdNn0NuAgaWxh1IZ+H9a7QWIsGqsl5dzzcbvdEw==
    Change-Id: I00832a7fee7cc991166005a18d619c61d8173ddb

---
+++ go/src/infra/chromeperf/weekly/weekly.go
@@ -0,0 +1,56 @@
+// Copyright 2022 The Chromium Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package weekly
+
+import (
+	"context"
+
+	"github.com/maruel/subcommands"
+	"go.chromium.org/luci/auth"
+	"go.chromium.org/luci/auth/client/authcli"
+	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/logging/gologger"
+	"go.chromium.org/luci/hardcoded/chromeinfra"
+)
+
+func application(p Param) *cli.Application {
+	p.Auth = chromeinfra.DefaultAuthOptions()
+	return &cli.Application{
+		Name:  "weekly",
+		Title: "A CLI for chrome browser perf engprod weekly updates.",
+		Context: func(ctx context.Context) context.Context {
+			return gologger.StdConfig.Use(ctx)
+		},
+		Commands: []*subcommands.Command{
+			cmdReport(p),
+			authcli.SubcommandLogin(p.Auth, "auth-login", false),
+			authcli.SubcommandLogout(p.Auth, "auth-logout", false),
+			authcli.SubcommandInfo(p.Auth, "auth-info", false),
+
+			subcommands.CmdHelp,
+		},
+	}
+}
+
+// Param includes the parameters to use for the CLI application.
+type Param struct {
+	DefaultServiceDomain, OIDCProviderURL string
+	Auth                                  auth.Options
+}
+
+// Main invokes the subcommands for the application.
+func Main(p Param, args []string) int {
+	return subcommands.Run(application(p), nil)
+}
