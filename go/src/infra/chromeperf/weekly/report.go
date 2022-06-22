9e584af94436e4e4a6f0fdae84a70eb03fcc0e75
seanmccullough <seanmccullough@google.com>
2022-06-21 17:36:32 -0700

    [on-call rotation util] weekly meeting Incoming Issues list generator
    generates markdown, but you can pipe that into whatever md-to-html
    tool you like.
    
    Example run:
    ```
     go run main.go report-incoming -days 7
    
    | Pri | Issue ID | Summary |
    | --- | -------- | ------- |
    | Pri-1 | [chromium:1337409](https://bugs.chromium.org/p/chromium/issues/detail?id=1337409) | [V8 Speedometer2 Perf Sheriff]: 6 regressions in speedometer2 |
    | Pri-1 | [chromium:1337364](https://bugs.chromium.org/p/chromium/issues/detail?id=1337364) | [PerfBotFailure] Android go devices on perf waterfall and Pinpoint need recovery |
    | Pri-1 | [chromium:1336690](https://bugs.chromium.org/p/chromium/issues/detail?id=1336690) | cloud builds of sheriff config has been failing since 5/31 |
    | Pri-1 | [chromium:1332977](https://bugs.chromium.org/p/chromium/issues/detail?id=1332977) | [V8 Speedometer2 Perf Sheriff]: 1 regressions in speedometer2 |
    | Pri-1 | [chromium:1331285](https://bugs.chromium.org/p/chromium/issues/detail?id=1331285) | run_benchmark requires a `python` binary, which no longer exists on many systems |
    ```
    
    example html: https://g3mark-service-playground.corp.google.com/?markdownz=pZNta9swEMe/yuFXGyR1nbRJYxhjDwz6oksgsDdbaB37bB21T5oe0mV1vntPyUL7YpSxCCQk3f2ln053j0mp2SP7JE96WFiCHq6dCwjXn2W6DF1X2C30P7iH4XAI+3HfnqcHq2iHmWx+L5XVHYUuz8bj6cX5bPVGeW9cnqbr0Lizo/lM2yY16XGZUrzVpRX6gtr3VL37I38bz/x2BUuDWOkOPdoRLNDWsFRoqa5XOUzAYmPROdLsgBjcS+dX8MaTi1PwRL7Hizgftf8ipmBxBR+4spoqaDRUuKESHWgGE6EfCmGqi7aFgitYEBtN7IGFVx5R6g0ew/1X4slkdn4CcZRH4rLVoYJ1oLYStBrcIZQgyVBTA6pwsEZkqEVK3IAjLhEu03H2CttoNp2ewBbl//LZ2X9+dja6ujwBL8ojng18u0YulRTGvaD8DCQwUMCd2Xql+Q7WxFIzA3hQVCpgDa3mBi3gL3J+nwldwVtwW+exc9Ang8QiV2jnxsc3Jflj4pS2XkJ/v8RYmp/mNzfzr+KoqFGtdNn0NuAgaWxh1IZ+H9a7QWIsGqsl5dzzcbvdEw==
    Change-Id: I00832a7fee7cc991166005a18d619c61d8173ddb

---
+++ go/src/infra/chromeperf/weekly/report.go
@@ -0,0 +1,157 @@
+// Copyright 2022 The Chromium Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package weekly
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+	"sync"
+	"text/template"
+
+	"infra/monorail"
+
+	"github.com/maruel/subcommands"
+	"go.chromium.org/luci/auth"
+	"go.chromium.org/luci/auth/client/authcli"
+	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/errors"
+	"google.golang.org/grpc/credentials"
+)
+
+var (
+	MonorailAPIURL = "https://monorail-prod.appspot.com/_ah/api/monorail/v1"
+	markdownTempl  = template.Must(template.New("test").Parse(`
+| Pri | Issue ID | Summary |
+| --- | -------- | ------- |
+{{- range .}}
+| {{.Pri}} | [{{.ProjectID}}:{{.ID}}](https://bugs.chromium.org/p/{{.ProjectID}}/issues/detail?id={{.ID}}) | {{.Summary}} |
+{{- end}}
+	`))
+)
+
+// clientFactory encapsulates the dialing and caching of http transports.
+type clientFactory struct {
+	tlsCreds    credentials.TransportCredentials
+	baseAuth    *auth.Authenticator
+	idTokenAuth *auth.Authenticator
+
+	initOnce sync.Once
+}
+
+func (f *clientFactory) init(ctx context.Context, opts auth.Options) {
+	f.initOnce.Do(func() {
+		f.tlsCreds = credentials.NewTLS(nil)
+
+		f.baseAuth = auth.NewAuthenticator(ctx, auth.SilentLogin, opts)
+
+		opts.UseIDTokens = true
+		f.idTokenAuth = auth.NewAuthenticator(ctx, auth.InteractiveLogin, opts)
+	})
+}
+
+func (f *clientFactory) http() (*http.Client, error) {
+	return f.baseAuth.Client()
+}
+
+type reportCmd struct {
+	subcommands.CommandRunBase
+	clientFactory clientFactory
+	authFlags     authcli.Flags
+	params        Param
+
+	days     int
+	Monorail monorail.MonorailClient
+}
+
+func (r *reportCmd) httpClient(ctx context.Context) (*http.Client, error) {
+	opts, err := r.authFlags.Options()
+	if err != nil {
+		return nil, err
+	}
+	r.clientFactory.init(ctx, opts)
+	httpClient, err := r.clientFactory.http()
+	switch {
+	case err == auth.ErrLoginRequired:
+		return nil, errors.New("Login required: run `weekly auth-login` or use the -service-account-json flag")
+	case err != nil:
+		return nil, err
+	}
+	return httpClient, nil
+}
+
+func (dc *reportCmd) RegisterFlags(p Param) {
+	dc.Flags.IntVar(&dc.days, "days", 7, "number of days to look back")
+	dc.authFlags.Register(&dc.Flags, p.Auth)
+}
+
+type tmplItem struct {
+	ProjectID string
+	ID        int32
+	Summary   string
+	Pri       string
+}
+
+func (dc *reportCmd) Run(a subcommands.Application, args []string, env subcommands.Env) int {
+	ctx := cli.GetContext(a, nil, env)
+	htc, err := dc.httpClient(ctx)
+	if err != nil {
+		fmt.Fprintf(a.GetErr(), "ERROR: %s\n", err)
+		return -1
+	}
+	dc.Monorail = monorail.NewEndpointsClient(htc, MonorailAPIURL)
+	req := &monorail.IssuesListRequest{
+		ProjectId: "chromium",
+		Can:       monorail.IssuesListRequest_ALL,
+		// This URL is borrowed from the link at go/berf-triage-queue and modified to:
+		// - include all issue statuses
+		// - limit results to issues modified in the past N (default 7) days as set by the -days CLI flag.
+		Q: fmt.Sprintf("component:Speed>Dashboard,Speed>Bisection,Speed>Benchmarks>Waterfall pri:0,1,2,3 opened>2021-8-1 -label:Browser-Perf-EngProd modified>today-%d", dc.days),
+	}
+	resp, err := dc.Monorail.IssuesList(ctx, req)
+	if err != nil {
+		fmt.Fprintf(a.GetErr(), "ERROR: %s\n", err)
+		return -1
+	}
+	data := []tmplItem{}
+	for _, issue := range resp.Items {
+		pri := ""
+		for _, l := range issue.Labels {
+			if strings.HasPrefix(l, "Pri-") {
+				pri = l
+			}
+		}
+		data = append(data, tmplItem{issue.ProjectId, issue.Id, issue.Summary, pri})
+	}
+	markdownTempl.Execute(a.GetOut(), data)
+	if err != nil {
+		fmt.Fprintf(a.GetErr(), "ERROR: %s\n", err)
+		return -1
+	}
+	return 0
+}
+
+func cmdReport(p Param) *subcommands.Command {
+	return &subcommands.Command{
+		UsageLine: "report",
+		ShortDesc: "Summarize issues from the past week's on-call rotation formatted as a markdown table.",
+		CommandRun: func() subcommands.CommandRun {
+			c := &reportCmd{params: p}
+			c.RegisterFlags(p)
+			return c
+		},
+	}
+}
