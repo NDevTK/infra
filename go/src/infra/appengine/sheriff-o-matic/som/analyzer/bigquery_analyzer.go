44b7f1ec923c8ec3c223348c8f0eda2a2e48d8c4
Quang Minh Tuan Nguyen <nqmtuan@google.com>
2022-06-24 02:15:40 +0000

    [SoM] Re-add Angle tree
    This is the same CL as https://chromium-review.googlesource.com/c/infra/infra/+/3571148. As I couldn't create a reland of that CL, I made a new CL instead.
    
    This is made possible because we have reduced the running cost of SoM.
    
    Bug: 1288021
    Change-Id: I1833a4dc0e4aff75c40fb76f79a4c7d6373176eb
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3716435
    Reviewed-by: Weiwei Lin <weiweilin@google.com>
    Commit-Queue: Weiwei Lin <weiweilin@google.com>
    Auto-Submit: Tuan Nguyen <nqmtuan@google.com>
    Cr-Commit-Position: refs/heads/main@{#49877}

--- go/src/infra/appengine/sheriff-o-matic/som/analyzer/bigquery_analyzer.go
+++ go/src/infra/appengine/sheriff-o-matic/som/analyzer/bigquery_analyzer.go
@@ -76,6 +76,10 @@ LIMIT
 	1000
 `
 
+const angleFailuresQuery = selectFromWhere + `
+"angle" in UNNEST(SheriffRotations)
+`
+
 func chromeBrowserFilterFunc(tree string) func(r failureRow) bool {
 	return func(r failureRow) bool {
 		return sliceContains(r.SheriffRotations, tree)
@@ -173,6 +177,9 @@ func generateSQLQuery(ctx context.Context, tree string, appID string) (string, e
 	}
 	if tree == "fuchsia" {
 		return fmt.Sprintf(fuchsiaFailuresQuery, appID, "fuchsia", bbProjectFilter), nil
+	}
+	if tree == "angle" {
+		return fmt.Sprintf(angleFailuresQuery, appID, "angle"), nil
 	}
 	return "", fmt.Errorf("invalid tree %q", tree)
 }
