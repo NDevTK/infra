824d046dd2c211598bcd0895717e6e6cbf3f2159
Anushruth S <anushruth@google.com>
2022-07-07 18:29:55 -0700

    [WIB] Add support for online windows customization - pin_customizations
    Bug: b/182061277
    
    Add online_windows_customization with support for pinning artifacts.
    
    Change-Id: I66db76d8d1b643659384739d85e69465ecaa4dc0

--- recipes/recipe_modules/windows_scripts_executor/__init__.py
+++ recipes/recipe_modules/windows_scripts_executor/__init__.py
@@ -3,21 +3,11 @@
 # found in the LICENSE file.
 
 DEPS = [
-    'depot_tools/gclient',
-    'depot_tools/bot_update',
-    'depot_tools/git',
-    'depot_tools/gitiles',
-    'depot_tools/gsutil',
-    'recipe_engine/context',
-    'recipe_engine/cipd',
-    'recipe_engine/step',
-    'recipe_engine/path',
-    'recipe_engine/python',
-    'recipe_engine/json',
-    'recipe_engine/file',
-    'recipe_engine/archive',
-    'recipe_engine/raw_io',
-    'powershell'
+    'depot_tools/gclient', 'depot_tools/bot_update', 'depot_tools/git',
+    'depot_tools/gitiles', 'depot_tools/gsutil', 'recipe_engine/context',
+    'recipe_engine/cipd', 'recipe_engine/step', 'recipe_engine/path',
+    'recipe_engine/python', 'recipe_engine/json', 'recipe_engine/file',
+    'recipe_engine/archive', 'recipe_engine/raw_io', 'powershell', 'qemu'
 ]
 
 PYTHON_VERSION_COMPATIBILITY = 'PY3'
