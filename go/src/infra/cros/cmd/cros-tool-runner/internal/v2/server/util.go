efbf4870d3e423b103382b7f5ce39be8bf3a61e5
Ming Kong <mingkong@google.com>
2022-07-11 22:19:34 +0000

    Add podman support. Podman is the underlying command used on drones under the alias of docker.
    Design doc: go/cft-container-services
    
    BUG=b:237382879
    TEST=unit tests
    
    Change-Id: Ic864d058a794b1b1428b5ab492124e2eb5ccc268

--- go/src/infra/cros/cmd/cros-tool-runner/internal/v2/server/util.go
+++ go/src/infra/cros/cmd/cros-tool-runner/internal/v2/server/util.go
@@ -46,13 +46,21 @@ func (*serverUtils) reverse(s []string) []string {
 // mapToCode maps common docker error messages to gRPC status codes.
 func (*serverUtils) mapToCode(errMsg string) codes.Code {
 	switch {
-	// TODO(mingkong): add "No such container"
+	// docker errors
 	case strings.HasPrefix(errMsg, "Error: No such network"):
+		return codes.NotFound
+	case strings.HasPrefix(errMsg, "Error: No such container"):
 		return codes.NotFound
 	case strings.Contains(errMsg, "operation is not permitted"):
 		return codes.PermissionDenied
-	// TODO(mingkong): make this match more specific
 	case strings.Contains(errMsg, "already exists"):
+		return codes.AlreadyExists
+	// podman errors
+	case strings.HasPrefix(errMsg, "Error: error inspecting object: no such network"):
+		return codes.NotFound
+	case strings.HasPrefix(errMsg, "Error: error inspecting object: no such container"):
+		return codes.NotFound
+	case strings.Contains(errMsg, "is already used"):
 		return codes.AlreadyExists
 	default:
 		log.Println("unable to map error message to a known code:", errMsg)
@@ -67,9 +75,14 @@ func (u *serverUtils) toStatusError(stderrOutput string) error {
 
 // toStatusErrorWithMapper converts stderr output string to gRPC status error using a custom code mapping function
 func (u *serverUtils) toStatusErrorWithMapper(stderrOutput string, mapper func(string) codes.Code) error {
+	log.Println("stderr:", stderrOutput)
 	errMsg := u.firstLine(stderrOutput)
-	log.Println("first line from stderr:", errMsg)
 	return status.Error(mapper(errMsg), errMsg)
+}
+
+// notFound returns an NotFound gRPC status error
+func (*serverUtils) notFound(reason string) error {
+	return status.Error(codes.NotFound, reason)
 }
 
 // invalidArgument returns an InvalidArgument gRPC status error
