c8fee2e1c8a636a4eb00247d73c5827658b68038
Otabek Kasimov <otabek@google.com>
2022-07-18 03:12:28 +0000

    recovery: Split servo power-cycle exec to separate file
    BUG=b:200997211
    TEST=go test ./..
    
    Change-Id: I457ad774e3864a70dca8d4fcce1e48531e48b3ca
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3768682
    Auto-Submit: Otabek Kasimov <otabek@google.com>
    Reviewed-by: Vineet Joshi <vkjoshi@google.com>
    Commit-Queue: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/main@{#50336}

--- go/src/infra/cros/recovery/internal/execs/servo/servo_execs.go
+++ go/src/infra/cros/recovery/internal/execs/servo/servo_execs.go
@@ -1,4 +1,4 @@
-// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Copyright 2021 The ChromiumOS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -864,54 +864,6 @@ func servoPowerStateResetExec(ctx context.Context, info *execs.ExecInfo) error {
 	return nil
 }
 
-// servoPowerCycleRootServoExec resets(power-cycle) the servo via smart usbhub.
-func servoPowerCycleRootServoExec(ctx context.Context, info *execs.ExecInfo) error {
-	argsMap := info.GetActionArgs(ctx)
-	// Timeout for resetting the servo. Default to be 30s.
-	resetTimeout := argsMap.AsDuration(ctx, "reset_timeout", 30, time.Second)
-	// Timeout to wait after resetting the servo. Default to be 20s.
-	waitTimeout := argsMap.AsDuration(ctx, "wait_timeout", 20, time.Second)
-	run := info.DefaultRunner()
-	var smartUsbhubPresent = false
-	defer func() { info.GetChromeos().GetServo().SmartUsbhubPresent = smartUsbhubPresent }()
-	servoSerial := info.GetChromeos().GetServo().SerialNumber
-	// Get the usb devnum before the reset.
-	preResetDevnum, err := topology.GetServoUsbDevnum(ctx, run, servoSerial)
-	if err != nil {
-		return errors.Annotate(err, "servo power cycle root servo: find the servo").Err()
-	}
-	log.Infof(ctx, "Servo usb devnum before reset: %s", preResetDevnum)
-	// Resetting servo.
-	log.Infof(ctx, "Resetting servo through smart usbhub.")
-	if _, err := run(ctx, resetTimeout, "servodtool", "device", "-s", servoSerial, "power-cycle"); err != nil {
-		log.Warningf(ctx, `Failed to reset servo with serial: %s. Please ignore this error if the DUT is not connected to a smart usbhub`, servoSerial)
-		return errors.Annotate(err, "servo power cycle root servo").Err()
-	}
-	// Since we are able to run the power cycle servodtool command
-	// It implies the smartUsb is present.
-	smartUsbhubPresent = true
-	log.Debugf(ctx, "Wait %v for servo to come back from reset.", waitTimeout)
-	time.Sleep(waitTimeout)
-	// Reset authorized flag fror servo-hub for servo v4p1 only.
-	if ResetUsbkeyAuthorized(ctx, run, servoSerial, info.GetChromeos().GetServo().ServodType) != nil {
-		return errors.Annotate(err, "servo power cycle root servo").Err()
-	}
-	// Get the usb devnum after the reset.
-	postResetDevnum, err := topology.GetServoUsbDevnum(ctx, run, servoSerial)
-	if err != nil {
-		return errors.Annotate(err, "servo power cycle root servo: after rest").Err()
-	}
-	log.Infof(ctx, "Servo usb devnum after reset: %s", postResetDevnum)
-	if preResetDevnum == "" || postResetDevnum == "" {
-		log.Infof(ctx, "Servo reset completed but unable to verify devnum change!")
-	} else if preResetDevnum != postResetDevnum {
-		log.Infof(ctx, "Reset servo with serial %s completed successfully!", servoSerial)
-	} else {
-		log.Infof(ctx, "Servo reset completed but devnum is still not changed!")
-	}
-	return nil
-}
-
 const (
 	// rebootCmd is the reboot command that is used to restart the servo v3.
 	rebootCmd = "sync & sleep 5; reboot & sleep 60; reboot -f & sleep 10; reboot -nf & sleep 10; telinit 6"
@@ -980,6 +932,5 @@ func init() {
 	execs.Register("servo_servod_cc_toggle", servoServodCCToggleExec)
 	execs.Register("servo_set_ec_uart_cmd", servoSetEcUartCmdExec)
 	execs.Register("servo_power_state_reset", servoPowerStateResetExec)
-	execs.Register("servo_power_cycle_root_servo", servoPowerCycleRootServoExec)
 	execs.Register("servo_host_v3_reboot", servoHostV3RebootExec)
 }
---
+++ go/src/infra/cros/recovery/internal/execs/servo/servo_power_cycle_execs.go
@@ -0,0 +1,71 @@
+// Copyright 2022 The ChromiumOS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package servo
+
+import (
+	"context"
+	"time"
+
+	"go.chromium.org/luci/common/errors"
+
+	"infra/cros/recovery/internal/execs"
+	"infra/cros/recovery/internal/execs/servo/topology"
+	"infra/cros/recovery/internal/log"
+)
+
+// servoPowerCycleRootServoExec resets(power-cycle) the servo via smart usbhub.
+func servoPowerCycleRootServoExec(ctx context.Context, info *execs.ExecInfo) error {
+	argsMap := info.GetActionArgs(ctx)
+	// Timeout for resetting the servo. Default to be 30s.
+	resetTimeout := argsMap.AsDuration(ctx, "reset_timeout", 30, time.Second)
+	// Timeout to wait after resetting the servo. Default to be 20s.
+	waitTimeout := argsMap.AsDuration(ctx, "wait_timeout", 20, time.Second)
+	run := info.DefaultRunner()
+	servoInfo := info.GetChromeos().GetServo()
+	var smartUsbhubPresent = false
+	defer func() {
+		servoInfo.SmartUsbhubPresent = smartUsbhubPresent
+	}()
+	servoSerial := servoInfo.SerialNumber
+	// Get the usb devnum before the reset.
+	preResetDevnum, err := topology.GetServoUsbDevnum(ctx, run, servoSerial)
+	if err != nil {
+		return errors.Annotate(err, "servo power cycle root servo: find the servo").Err()
+	}
+	log.Infof(ctx, "Servo usb devnum before reset: %s", preResetDevnum)
+	// Resetting servo.
+	log.Infof(ctx, "Resetting servo through smart usbhub.")
+	if _, err := run(ctx, resetTimeout, "servodtool", "device", "-s", servoSerial, "power-cycle"); err != nil {
+		log.Warningf(ctx, `Failed to reset servo with serial: %s. Please ignore this error if the DUT is not connected to a smart usbhub`, servoSerial)
+		return errors.Annotate(err, "servo power cycle root servo").Err()
+	}
+	// Since we are able to run the power cycle servodtool command
+	// It implies the smartUsb is present.
+	smartUsbhubPresent = true
+	log.Debugf(ctx, "Wait %v for servo to come back from reset.", waitTimeout)
+	time.Sleep(waitTimeout)
+	// Reset authorized flag fror servo-hub for servo v4p1 only.
+	if ResetUsbkeyAuthorized(ctx, run, servoSerial, info.GetChromeos().GetServo().ServodType) != nil {
+		return errors.Annotate(err, "servo power cycle root servo").Err()
+	}
+	// Get the usb devnum after the reset.
+	postResetDevnum, err := topology.GetServoUsbDevnum(ctx, run, servoSerial)
+	if err != nil {
+		return errors.Annotate(err, "servo power cycle root servo: after rest").Err()
+	}
+	log.Infof(ctx, "Servo usb devnum after reset: %s", postResetDevnum)
+	if preResetDevnum == "" || postResetDevnum == "" {
+		log.Infof(ctx, "Servo reset completed but unable to verify devnum change!")
+	} else if preResetDevnum != postResetDevnum {
+		log.Infof(ctx, "Reset servo with serial %s completed successfully!", servoSerial)
+	} else {
+		log.Infof(ctx, "Servo reset completed but devnum is still not changed!")
+	}
+	return nil
+}
+
+func init() {
+	execs.Register("servo_power_cycle_root_servo", servoPowerCycleRootServoExec)
+}
