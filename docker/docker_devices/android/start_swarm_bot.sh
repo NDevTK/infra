78f25df465fc5a14fbfbd0fdb40c550a85e64929
Ben Pastene <bpastene@chromium.org>
2022-08-10 23:18:59 +0000

    Fetch luci_machine_token & add that when curling bot zip on docker bots
    Right now it relies on IP allowlisting to allow the curl to go
    through. By using luci_machine_token on the bot, we can get rid of that
    need.
    
    Bug: 965226
    Change-Id: Ie8210d5f87f4877d8164297622a07e207e063096
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3790044
    Reviewed-by: Jonathan Pittman <jmpittman@google.com>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#50837}

--- docker/docker_devices/android/start_swarm_bot.sh
+++ docker/docker_devices/android/start_swarm_bot.sh
@@ -6,8 +6,9 @@ SWARM_DIR=/b/swarming
 SWARM_ZIP=swarming_bot.zip
 
 DEPOT_TOOLS_DIR=/b/depot_tools
-DEPOT_TOOLS_URL="https://chromium.googlesource.com/chromium/tools/depot_tools.git"
-DEPOT_TOOLS_REV="da3a29e13e816459234b0b08ed1059300bae46dd"
+DEPOT_TOOLS_URL='https://chromium.googlesource.com/chromium/tools/depot_tools.git'
+DEPOT_TOOLS_REV='da3a29e13e816459234b0b08ed1059300bae46dd'
+LUCI_MACHINE_TOKEN_FILE='/var/lib/luci_machine_tokend/token.json'
 
 # Wait until this container has access to a device before starting the bot.
 START=$(/bin/date +%s)
@@ -33,11 +34,17 @@ su -c "cd $DEPOT_TOOLS_DIR && \
        /usr/bin/git fetch origin $DEPOT_TOOLS_REV && \
        /usr/bin/git reset --hard FETCH_HEAD" chrome-bot
 
+curl_header_args=()
+if [[ -r "${LUCI_MACHINE_TOKEN_FILE}" ]]; then
+  luci_token="$(jq -r ".luci_machine_token" "${LUCI_MACHINE_TOKEN_FILE}")"
+  curl_header_args=("--header" "X-Luci-Machine-Token: ${luci_token}")
+fi
+
 mkdir -p $SWARM_DIR
 chown chrome-bot:chrome-bot $SWARM_DIR
 cd $SWARM_DIR
 rm -rf swarming_bot*.zip
-su -c "/usr/bin/curl -sSLOJ $SWARM_URL" chrome-bot
+sudo -u chrome-bot -- /usr/bin/curl -sSLOJ "${curl_header_args[@]}" "${SWARM_URL}"
 
 echo "Starting $SWARM_ZIP"
 # Run the swarming bot in the background, and immediately wait for it. This
--- docker/swarm_docker/start_swarm_bot.sh
+++ docker/swarm_docker/start_swarm_bot.sh
@@ -4,12 +4,19 @@ trap "exit 10" SIGUSR1
 
 SWARM_DIR=/b/swarming
 SWARM_ZIP=swarming_bot.zip
+LUCI_MACHINE_TOKEN_FILE='/var/lib/luci_machine_tokend/token.json'
+
+curl_header_args=()
+if [[ -r "${LUCI_MACHINE_TOKEN_FILE}" ]]; then
+  luci_token="$(jq -r ".luci_machine_token" "${LUCI_MACHINE_TOKEN_FILE}")"
+  curl_header_args=("--header" "X-Luci-Machine-Token: ${luci_token}")
+fi
 
 mkdir -p $SWARM_DIR
 /bin/chown chrome-bot:chrome-bot $SWARM_DIR
 cd $SWARM_DIR
 rm -rf swarming_bot*.zip
-/bin/su -c "/usr/bin/curl -sSLOJ $SWARM_URL" chrome-bot
+sudo -u chrome-bot -- /usr/bin/curl -sSLOJ "${curl_header_args[@]}" "${SWARM_URL}"
 
 # Initialize tuntap for qemu running inside of the docker instance on arm64.
 if [[ $(arch) = "aarch64" ]]; then
