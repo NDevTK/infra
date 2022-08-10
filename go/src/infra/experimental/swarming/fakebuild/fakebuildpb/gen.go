5fcbaa2542f9df22b4e4e3361db8c28d35a5c616
Vadim Shtayura <vadimsh@chromium.org>
2022-08-10 00:04:44 +0000

    [swarming] Add a luciexe binary that just emits steps and sleeps.
    Will be launched en-mass on cr-buildbucket-dev+chromium-swarm-dev
    to see what happens. Will probably be extended later to do more
    things.
    
    R=iannucci@chromium.org, chanli@chromium.org
    BUG=1350632
    
    Change-Id: I8dd5842539497e625826bccf610a4ffc4e84c755
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3821746
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Chan Li <chanli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#50805}

---
+++ go/src/infra/experimental/swarming/fakebuild/fakebuildpb/gen.go
@@ -0,0 +1,8 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//go:generate cproto
+
+// Package fakebuildpb contains protos with inputs and outputs of fakebuild.
+package fakebuildpb
---
+++ go/src/infra/experimental/swarming/fakebuild/main.go
@@ -0,0 +1,43 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Binary fakebuild is a luciexe binary that pretends to do some work.
+//
+// To be used for Swarming and Buildbucket load testing.
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/data/rand/mathrand"
+	"go.chromium.org/luci/luciexe/build"
+
+	"infra/experimental/swarming/fakebuild/fakebuildpb"
+)
+
+func main() {
+	mathrand.SeedRandomly()
+
+	inputs := &fakebuildpb.Inputs{}
+
+	build.Main(inputs, nil, nil, func(ctx context.Context, args []string, st *build.State) error {
+		for i := 0; i < int(inputs.Steps); i++ {
+			sleepStep(ctx, inputs, i)
+		}
+		return nil
+	})
+}
+
+func sleepStep(ctx context.Context, inputs *fakebuildpb.Inputs, idx int) {
+	secs := inputs.SleepMinSec + rand.Int63n(inputs.SleepMaxSec-inputs.SleepMinSec)
+
+	step, ctx := build.StartStep(ctx, fmt.Sprintf("Step %d: sleep %d", idx+1, secs))
+	defer func() { step.End(nil) }()
+
+	clock.Sleep(ctx, time.Duration(secs)*time.Second)
+}
