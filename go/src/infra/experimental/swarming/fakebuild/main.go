cd61d5c911f8be93149270f9fdabe38481b15dae
Chan Li <chanli@chromium.org>
2022-08-15 21:21:56 +0000

    [fakebuild] Add steps to search builds
    Add steps to mimic CV search build requests to load test SearchBuilds' performance in Firestore.
    
    Change-Id: I80f711d2165b492af99d9f8823009b86577584a3
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3831763
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Chan Li <chanli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#50926}

--- go/src/infra/experimental/swarming/fakebuild/main.go
+++ go/src/infra/experimental/swarming/fakebuild/main.go
@@ -13,8 +13,9 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/jsonpb"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
 
 	"go.chromium.org/luci/auth"
 	"go.chromium.org/luci/buildbucket"
@@ -24,7 +25,6 @@ import (
 	"go.chromium.org/luci/common/data/rand/mathrand"
 	"go.chromium.org/luci/common/data/strpair"
 	"go.chromium.org/luci/common/errors"
-	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/grpc/prpc"
 	"go.chromium.org/luci/hardcoded/chromeinfra"
 	"go.chromium.org/luci/lucictx"
@@ -42,7 +42,24 @@ func main() {
 		for i := 0; i < int(inputs.Steps); i++ {
 			sleepStep(ctx, inputs, i)
 		}
-		return scheduleChildBuilds(ctx, inputs)
+
+		httpClient, err := auth.NewAuthenticator(ctx, auth.SilentLogin, chromeinfra.DefaultAuthOptions()).Client()
+		switch {
+		case err == auth.ErrLoginRequired:
+			return errors.New("Login required: run `bb auth-login`")
+		case err != nil:
+			return err
+		}
+		bbClient := bbpb.NewBuildsPRPCClient(&prpc.Client{
+			C:       httpClient,
+			Host:    "cr-buildbucket-dev.appspot.com",
+			Options: prpc.DefaultOptions(),
+		})
+
+		if err := scheduleChildBuilds(ctx, bbClient, inputs); err != nil {
+			return err
+		}
+		return searchBuilds(ctx, bbClient, inputs)
 	})
 }
 
@@ -65,25 +82,29 @@ func sleepStep(ctx context.Context, inputs *fakebuildpb.Inputs, idx int) {
 	clock.Sleep(ctx, time.Duration(secs)*time.Second)
 }
 
+func gerritChange(change, patchset int64) *bbpb.GerritChange {
+	return &bbpb.GerritChange{
+		Host:     "chromium-review.googlesource.com",
+		Project:  "chromium/src",
+		Change:   change,
+		Patchset: patchset,
+	}
+}
+
 func generateGerritChangesAndTags(req *bbpb.ScheduleBuildRequest) {
 	clNum := rand.Int63n(5000000)
 
 	var changes []*bbpb.GerritChange
 	tags := strpair.Map{}
 	for i := 1; i <= 4; i++ {
-		changes = append(changes, &bbpb.GerritChange{
-			Host:     "chromium-review.googlesource.com",
-			Project:  "chromium/src",
-			Change:   clNum,
-			Patchset: int64(i),
-		})
+		changes = append(changes, gerritChange(clNum, int64(i)))
 		tags.Add("buildset", fmt.Sprintf("patch/gerrit/chromium-review.googlesource.com/%d/%d", clNum, i))
 	}
 	req.GerritChanges = changes
 	req.Tags = protoutil.StringPairs(tags)
 }
 
-func generateRequest(builder *bbpb.BuilderID, batchSize int) *bbpb.BatchRequest {
+func generateScheduleRequest(builder *bbpb.BuilderID, batchSize int) *bbpb.BatchRequest {
 	req := &bbpb.BatchRequest{
 		Requests: []*bbpb.BatchRequest_Request{},
 	}
@@ -105,19 +126,23 @@ func scheduleOneBatch(ctx context.Context, bbClient bbpb.BuildsClient, idx, batc
 	step, ctx := build.StartStep(ctx, fmt.Sprintf("schedule children (%d)", idx))
 	defer func() { step.End(nil) }()
 
-	req := generateRequest(cbs.Builder, batchSize)
+	req := generateScheduleRequest(cbs.Builder, batchSize)
 	res, err := bbClient.Batch(ctx, req)
 	if err != nil {
 		return errors.Annotate(err, "batch %d", idx).Err()
 	}
-	logging.Debugf(ctx, "response: %s", proto.MarshalTextString(res))
+	log := step.Log("response")
+	marsh := jsonpb.Marshaler{Indent: "  "}
+	if err = marsh.Marshal(log, res); err != nil {
+		return errors.Annotate(err, "failed to marshal proto").Err()
+	}
 
 	secs := randomSecs(cbs.SleepMinSec, cbs.SleepMaxSec)
 	clock.Sleep(ctx, time.Duration(secs)*time.Second)
 	return nil
 }
 
-func scheduleChildBuilds(ctx context.Context, inputs *fakebuildpb.Inputs) error {
+func scheduleChildBuilds(ctx context.Context, bbClient bbpb.BuildsClient, inputs *fakebuildpb.Inputs) error {
 	cbs := inputs.GetChildBuilds()
 	if cbs == nil {
 		return nil
@@ -141,19 +166,6 @@ func scheduleChildBuilds(ctx context.Context, inputs *fakebuildpb.Inputs) error 
 		}
 	}
 
-	httpClient, err := auth.NewAuthenticator(ctx, auth.SilentLogin, chromeinfra.DefaultAuthOptions()).Client()
-	switch {
-	case err == auth.ErrLoginRequired:
-		return errors.New("Login required: run `bb auth-login`")
-	case err != nil:
-		return err
-	}
-	bbClient := bbpb.NewBuildsPRPCClient(&prpc.Client{
-		C:       httpClient,
-		Host:    "cr-buildbucket-dev.appspot.com",
-		Options: prpc.DefaultOptions(),
-	})
-
 	step, ctx := build.StartStep(ctx, "schedule children")
 	defer func() { step.End(nil) }()
 
@@ -170,6 +182,64 @@ func scheduleChildBuilds(ctx context.Context, inputs *fakebuildpb.Inputs) error 
 		}
 		if err := scheduleOneBatch(ctx, bbClient, i, int(batchSize), cbs); err != nil {
 			return err
+		}
+	}
+	return nil
+}
+
+func searchBuild(ctx context.Context, bbClient bbpb.BuildsClient, sbs *fakebuildpb.SearchBuilds, idx int) error {
+	step, ctx := build.StartStep(ctx, fmt.Sprintf("search builds (%d)", idx))
+	defer func() { step.End(nil) }()
+
+	clNum := rand.Int63n(5000000)
+	patchset := rand.Int63n(20)
+
+	// Mimic CV's SearchBuilds RPCs.
+	req := &bbpb.SearchBuildsRequest{
+		Predicate: &bbpb.BuildPredicate{
+			GerritChanges:       []*bbpb.GerritChange{gerritChange(clNum, patchset)},
+			IncludeExperimental: true,
+		},
+		Mask: &bbpb.BuildMask{
+			Fields: &fieldmaskpb.FieldMask{
+				Paths: []string{
+					"builder",
+					"create_time",
+					"id",
+					"output.properties",
+					"status",
+					"status_details",
+					"summary_markdown",
+					"update_time",
+					"input.gerrit_changes",
+					"infra.buildbucket.requested_properties",
+				},
+			},
+		},
+	}
+	res, err := bbClient.SearchBuilds(ctx, req)
+	if err != nil {
+		return errors.Annotate(err, "search %d", idx).Err()
+	}
+	log := step.Log("response")
+	marsh := jsonpb.Marshaler{Indent: "  "}
+	if err = marsh.Marshal(log, res); err != nil {
+		return errors.Annotate(err, "failed to marshal proto").Err()
+	}
+
+	secs := randomSecs(sbs.SleepMinSec, sbs.SleepMaxSec)
+	clock.Sleep(ctx, time.Duration(secs)*time.Second)
+	return nil
+}
+
+func searchBuilds(ctx context.Context, bbClient bbpb.BuildsClient, inputs *fakebuildpb.Inputs) error {
+	sbs := inputs.GetSearchBuilds()
+	if sbs == nil {
+		return nil
+	}
+	for i := 0; i < int(sbs.Steps); i++ {
+		if err := searchBuild(ctx, bbClient, sbs, i); err != nil {
+			return errors.Annotate(err, "search build %d", i).Err()
 		}
 	}
 	return nil
