7465155f9ca670c81e916aea2290a54890b85707
Greg NISBET <gregorynisbet@google.com>
2022-08-31 21:06:04 -0700

    [crosadm] Add a stable version deleter.
    The satlab entries are in another table already, as it turns out.
    Therefore, it is 100% correct to just delete the entries before adding them
    from the file.
    
    BUG=b:241988093
    TEST=make test
    
    Change-Id: Ie30c0d26a548467f9b294345a3ba9af1986a2342

--- go/src/infra/appengine/crosskylabadmin/internal/app/frontend/stable_version.go
+++ go/src/infra/appengine/crosskylabadmin/internal/app/frontend/stable_version.go
@@ -396,6 +396,14 @@ func dumpStableVersionToDatastoreImpl(ctx context.Context, getFile func(context.
 		return nil, errors.Annotate(err, "parse json").Err()
 	}
 	m := getStableVersionRecords(ctx, stableVersions)
+
+	// The satlab versions are stored in another table. We can safely just delete everything.
+	for _, kind := range []string{dssv.CrosStableVersionKind, dssv.FirmwareStableVersionKind, dssv.FaftStableVersionKind} {
+		if err := dssv.DeleteAll(ctx, kind); err != nil {
+			return nil, errors.Annotate(err, "dump stable version: clearing entries of kind %q", kind).Err()
+		}
+	}
+
 	merr := errors.NewMultiError()
 	if err := dssv.PutManyCrosStableVersion(ctx, m.cros); err != nil {
 		merr = append(merr, errors.Annotate(err, "put cros stable version").Err())
