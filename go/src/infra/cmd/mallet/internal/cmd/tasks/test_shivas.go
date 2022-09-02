201450858623d8c218756cb0f6a6e1a779f6b083
Greg NISBET <gregorynisbet@google.com>
2022-09-02 01:43:27 +0000

    [mallet] [shivas] Add shivas integration test
    BUG=b:244193145
    TEST=make test
    
    Change-Id: I15ad99cb9be548f78736ca0eb759cc55a6999e39
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3866795
    Reviewed-by: Anushruth Sasivehalli <anushruth@chromium.org>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Cr-Commit-Position: refs/heads/main@{#51292}

--- go/src/infra/cmd/mallet/internal/cmd/tasks/test_shivas.go
+++ go/src/infra/cmd/mallet/internal/cmd/tasks/test_shivas.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"github.com/maruel/subcommands"
 	"go.chromium.org/luci/auth/client/authcli"
@@ -45,6 +46,13 @@ type testShivasRun struct {
 	dir string
 }
 
+func (c *testShivasRun) devShivas() string {
+	if c.dir == "" {
+		return ""
+	}
+	return filepath.Join(c.dir, "dev-shivas")
+}
+
 func (c *testShivasRun) Run(a subcommands.Application, args []string, env subcommands.Env) int {
 	if err := c.innerRun(a, args, env); err != nil {
 		cmdlib.PrintError(a, err)
@@ -60,9 +68,64 @@ func (c *testShivasRun) innerRun(a subcommands.Application, args []string, env s
 	if c.dir == "" {
 		return errors.Reason("test shivas: argument -dir must be provided").Err()
 	}
-	if err := exec.Command(filepath.Join(c.dir, "dev-shivas"), "help").Run(); err != nil {
+
+	if err := exec.Command(c.devShivas(), "help").Run(); err != nil {
 		return errors.Annotate(err, `test shivas: "dev-shivas help" failed`).Err()
 	}
+	if err := c.addRemoveDUT(); err != nil {
+		return errors.Annotate(err, "test shivas").Err()
+	}
+
 	fmt.Fprintf(a.GetErr(), "OK\n")
+	return nil
+}
+
+// addRemoveDUT tests adding a DUT and then removing it.
+//
+// In order to test this interaction, we first add a rack, then an asset, then a DUT.
+// At the end we tear everything down in the opposite order.
+//
+// Keep this test simple. This is supposed to be a model test that other tests will be based on.
+func (c *testShivasRun) addRemoveDUT() error {
+	const zone = "cros_googler_desk"
+	const rack = "937d6144-9a54-4967-a31b-9c134a823150"
+	const asset = "3fab83ca-1295-454b-9881-7d0e8b37fbe5"
+	const dut = "49e48288-4826-4074-a10a-99e1858ad78e"
+	const eve = "eve"
+
+	f := func(e error) error {
+		if e == nil {
+			return errors.Reason("command exited zero but another problem was detected").Err()
+		}
+		return e
+	}
+
+	cleanup := func() {
+		exec.Command(c.devShivas(), "delete", "dut", "-yes", "-namespace", "os", dut).Run()
+		exec.Command(c.devShivas(), "delete", "asset", "-yes", "-namespace", "os", asset).Run()
+		exec.Command(c.devShivas(), "delete", "rack", "-yes", "-namespace", "os", rack).Run()
+	}
+
+	cleanup()
+	defer cleanup()
+
+	if out, err := exec.Command(c.devShivas(), "add", "rack", "-namespace", "os", "-zone", zone, "-name", rack).CombinedOutput(); !strings.Contains(string(out), "Success") || err != nil {
+		return errors.Annotate(f(err), "add remove DUT: add rack: %q", out).Err()
+	}
+	if out, err := exec.Command(c.devShivas(), "add", "asset", "-namespace", "os", "-zone", zone, "-type", "dut", "-rack", rack, "-name", asset).CombinedOutput(); !strings.Contains(string(out), "Success") || err != nil {
+		return errors.Annotate(f(err), "add remove DUT: add asset: %q", out).Err()
+	}
+	if out, err := exec.Command(c.devShivas(), "add", "dut", "-namespace", "os", "-asset", asset, "-board", eve, "-model", eve, "-name", dut).CombinedOutput(); !strings.Contains(string(out), "Success") || err != nil {
+		return errors.Annotate(f(err), "add remove DUT: add dut: %q", out).Err()
+	}
+	if out, err := exec.Command(c.devShivas(), "delete", "dut", "-yes", "-namespace", "os", dut).CombinedOutput(); err != nil {
+		return errors.Annotate(f(err), "add remove DUT: delete dut: %q", out).Err()
+	}
+	if out, err := exec.Command(c.devShivas(), "delete", "asset", "-yes", "-namespace", "os", asset).CombinedOutput(); err != nil {
+		return errors.Annotate(f(err), "add remove DUT: delete asset: %q", out).Err()
+	}
+	if out, err := exec.Command(c.devShivas(), "delete", "rack", "-yes", "-namespace", "os", rack).CombinedOutput(); err != nil {
+		return errors.Annotate(f(err), "add remove DUT: delete rack: %q", out).Err()
+	}
 	return nil
 }
