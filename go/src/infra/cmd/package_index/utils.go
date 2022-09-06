4761ed5c4e24047204257fa2ee60fd080140464f
Gavin Mak <gavinmak@google.com>
2022-09-06 22:24:17 +0000

    Reland "Handle chroot paths from new ChromeOS cs builders"
    This is a reland of commit c3257e54ccb3f0fff04075d0d266a2e611f0ad43
    
    Original change's description:
    > Handle chroot paths from new ChromeOS cs builders
    >
    > New ChromeOS builders have their chroot paths at
    > "cache/cros_chroot/chroot/build/${board}/foo". Modify package_index to
    > handle these new paths s.t. xrefs to chroot gen files work.
    >
    > Bug: 1284439
    > Change-Id: I87bfb84f99da3fc733cbb8c05621351b362027c4
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3867536
    > Commit-Queue: Gavin Mak <gavinmak@google.com>
    > Reviewed-by: Joanna Wang <jojwang@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#51302}
    
    Bug: 1284439
    Change-Id: Ibb55dbc5f503495936685438dda8de56ac802c14
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3877982
    Commit-Queue: Gavin Mak <gavinmak@google.com>
    Reviewed-by: Joanna Wang <jojwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#51343}

--- go/src/infra/cmd/package_index/utils.go
+++ go/src/infra/cmd/package_index/utils.go
@@ -195,14 +195,25 @@ func setVnameForFile(vnameProto *kpb.VName, filepath, defaultCorpus string) {
 		filepath = filepath[4:]
 	}
 
-	// By default for OS, generated files are in chroot/build/${board}/ and
-	// src/out/${board}/. But in the codesearch repo, these files are at
-	// gen/${board}/chroot/build/${board} and gen/${board}/src/out/${board}/.
+	// By default for OS, generated files are in:
+	//   * cache/cros_chroot/chroot/build/${board}/
+	//   * src/out/${board}/
+	//
+	// But in the codesearch repo, these files are at:
+	//   * gen/${board}/chroot/build/${board}/
+	//   * gen/${board}/src/out/${board}/
+	//
 	// For references to work correctly, set vname to point to files in the repo.
-	if *projectFlag == "chromiumos" &&
-		(strings.HasPrefix(filepath, "chroot/build/") || strings.HasPrefix(filepath, "src/out/")) {
-		board := strings.Split(filepath, "/")[2]
-		filepath = fmt.Sprintf("gen/%s/%s", board, filepath)
+	if *projectFlag == "chromiumos" {
+		split := strings.Split(filepath, "/")
+
+		if strings.HasPrefix(filepath, "cache/cros_chroot/") {
+			filepath = fmt.Sprintf("gen/%s/%s", split[4], strings.Join(split[2:], "/"))
+		}
+
+		if strings.HasPrefix(filepath, "src/out/") {
+			filepath = fmt.Sprintf("gen/%s/%s", split[2], filepath)
+		}
 	}
 
 	vnameProto.Corpus = defaultCorpus
