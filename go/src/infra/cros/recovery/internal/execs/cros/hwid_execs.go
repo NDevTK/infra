480df1a0876d074efd5cbf9bc660f161173d6d4f
Otabek Kasimov <otabek@google.com>
2022-09-15 19:28:58 +0000

    recovery: Adds action to recovery HWID from inventory
    BUG=b:246794387
    TEST=run local action against bad DUTs
    
    Change-Id: I013f5270d6f79d631b806fa1db177e71bd029ba2
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3898357
    Commit-Queue: Vineet Joshi <vkjoshi@google.com>
    Reviewed-by: Vineet Joshi <vkjoshi@google.com>
    Auto-Submit: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/main@{#51579}

--- go/src/infra/cros/recovery/internal/execs/cros/hwid_execs.go
+++ go/src/infra/cros/recovery/internal/execs/cros/hwid_execs.go
@@ -6,6 +6,7 @@ package cros
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"go.chromium.org/luci/common/errors"
@@ -47,7 +48,36 @@ func matchHWIDToInvExec(ctx context.Context, info *execs.ExecInfo) error {
 	return nil
 }
 
+// updateHWIDFromInvExec updates HWID from inventory to host.
+func updateHWIDFromInvExec(ctx context.Context, info *execs.ExecInfo) error {
+	run := info.DefaultRunner()
+	originalHwid := info.GetChromeos().GetHwid()
+	tempFileName := fmt.Sprintf("/tmp/bios_%s.bin", info.GetActiveResource())
+	log.Debugf(ctx, "Update HWID from host: Try to read AP to file: %q", tempFileName)
+	if out, err := run(ctx, time.Minute, "flashrom", "-p", "host", "-r", "-i", fmt.Sprintf("GBB:%s", tempFileName)); err != nil {
+		return errors.Annotate(err, "update HWID from host").Err()
+	} else {
+		log.Debugf(ctx, "Update HWID from host: read AP (output): %s", out)
+	}
+	// Update HWID to the file inside temp file.
+	log.Debugf(ctx, "Update HWID from host: update HWID %q in AP to file %q", originalHwid, tempFileName)
+	if out, err := run(ctx, time.Minute, "futility", "gbb", "--set", "--hwid", fmt.Sprintf("%q", originalHwid), tempFileName); err != nil {
+		return errors.Annotate(err, "update HWID from host").Err()
+	} else {
+		log.Debugf(ctx, "Update HWID from host: updated HWID in AP file (output): %s", out)
+	}
+	// Write updated AP file back to host.
+	log.Debugf(ctx, "Update HWID from host: flash AP file %q back", tempFileName)
+	if out, err := run(ctx, time.Minute, "flashrom", "-p", "host", "-w", "-i", fmt.Sprintf("GBB:%s", tempFileName)); err != nil {
+		return errors.Annotate(err, "update HWID from host").Err()
+	} else {
+		log.Debugf(ctx, "Update HWID from host: AP file flashed to the host (output): %s", out)
+	}
+	return nil
+}
+
 func init() {
 	execs.Register("cros_update_hwid_to_inventory", updateHWIDToInvExec)
 	execs.Register("cros_match_hwid_to_inventory", matchHWIDToInvExec)
+	execs.Register("cros_update_hwid_from_inventory_to_host", updateHWIDFromInvExec)
 }
