3750a3a6ad99c2147b0de5635d8496f0d13108aa
Alan Zhao <ayzhao@google.com>
2022-09-23 11:00:55 -0700

    WIP test adding 7z as a 3pp package
    Change-Id: I8ab40542b78aef9c8da2ac0fd19ddfc5b5f47182

---
+++ 3pp/7z/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright 2022 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# set -e
+# set -x
+# set -o pipefail
+#
+PREFIX="$1"
+CHECKOUT_DIR=$(pwd)
+
+cd $CHECKOUT_DIR/CPP/7zip/Bundles/Alone
+make -j -f ../../cmpl_gcc.mak
+cp b/g/7za $PREFIX
+
+cd $CHECKOUT_DIR/CPP/7zip/Bundles/Alone2
+make -j -f ../../cmpl_gcc.mak
+cp b/g/7zz $PREFIX
+
+cd $CHECKOUT_DIR/CPP/7zip/Bundles/Alone7z
+make -j -f ../../cmpl_gcc.mak
+cp b/g/7zr $PREFIX
