32f93344dbf2806305445fff4afdd7da8e6796de
Alan Zhao <ayzhao@google.com>
2022-09-22 19:43:08 -0700

    WIP test adding 7z as a 3pp package
    Change-Id: I8ab40542b78aef9c8da2ac0fd19ddfc5b5f47182

---
+++ 3pp/7z/install_win.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright 2022 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# set -e
+# set -x
+# set -o pipefail
+#
+PREFIX="$1"
+
+echo FIRST PATH
+echo $PATH
+echo `which cl`
+echo `which ml64`
+
+PATH=$(echo $PATH | sed 's/:\/usr\/bin//g'):/usr/bin
+
+echo changed PATH
+echo $PATH
+
+cd CPP/7zip/Bundles/Alone2
+nmake PLATFORM=x64
+
+if [ -d x86 ]
+then
+  cp x86/7zz.exe $PREFIX
+else
+  cp x64/7zz.exe $PREFIX
+fi
