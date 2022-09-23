6c4ee6da615e5ef8ed653be5776d355a41d3b55a
Alan Zhao <ayzhao@google.com>
2022-09-23 10:26:27 -0700

    WIP test adding 7z as a 3pp package
    Change-Id: I8ab40542b78aef9c8da2ac0fd19ddfc5b5f47182

---
+++ 3pp/7z/install_mac.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright 2022 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# set -e
+# set -x
+# set -o pipefail
+#
+PREFIX="$1"
+PWD=$(pwd)
+
+cd $PWD/CPP/7zip/Bundles/Alone
+make -j -f ../../cmpl_clang.mak
+cp b/g/7za $PREFIX
+
+cd $PWD/CPP/7zip/Bundles/Alone2
+make -j -f ../../cmpl_clang.mak
+cp b/g/7zz $PREFIX
+
+cd $PWD/CPP/7zip/Bundles/Alone7z
+make -j -f ../../cmpl_clang.mak
+cp b/g/7zr $PREFIX
---
+++ 3pp/7z/install_win_64.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Copyright 2022 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# set -e
+# set -x
+# set -o pipefail
+#
+PREFIX="$1"
+PWD=$(PWD)
+
+# Move /usr/bin to the end of PATH because otherwise nmake will use
+# /usr/bin/link, which doesn't work, instead of the MSVC linker.
+PATH=$(echo $PATH | sed 's/:\/usr\/bin//g'):/usr/bin
+
+cd $PWD/CPP/7zip/Bundles/Alone
+nmake PLATFORM=x64
+cp x64/7za.exe $PREFIX
+
+cd $PWD/CPP/7zip/Bundles/Alone2
+nmake PLATFORM=x64
+cp x64/7zz.exe $PREFIX
+
+cd $PWD/CPP/7zip/Bundles/Alone7z
+nmake PLATFORM=x64
+cp x64/7zr.exe $PREFIX
