ea5d951451dac9347426419498aa109857d371ba
Alan Zhao <ayzhao@google.com>
2022-09-26 11:26:27 -0700

    Add 7z as a Chrome infra 3pp cipd package
    7z is required when packaging Chromium for Windows, but it's currently
    checked in as a binary in the Chromium repo. This CL correctly bundles
    7z as a CIPD package so we can remove the copy checked into Chromium.
    
    This is also used to fix an issue on Linux where Linux->Windows cross
    builds fail because the version of 7z on the bots is too old, so we
    provide an up-to-date Linux binary in CIPD.
    
    Bug: chromium:1143303
    Change-Id: I8ab40542b78aef9c8da2ac0fd19ddfc5b5f47182

---
+++ 3pp/7z/install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Copyright 2022 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+set -x
+set -o pipefail
+
+PREFIX="$1"
+BASE_DIR=$(pwd)
+
+if [[ $_3PP_PLATFORM =~ "windows-*"  ]]; then
+  # Move /usr/bin to the end of PATH because otherwise nmake will use
+  # # /usr/bin/link, which doesn't work, instead of the MSVC linker.
+  PATH=$(echo $PATH | sed 's/:\/usr\/bin//g'):/usr/bin
+  if [[ $_3PP_PLATFORM = "windows-386" ]]
+  then
+    BUILD_DIR=x86
+  else
+    BUILD_DIR=x64
+  fi
+  BUILD_CMD = "nmake PLATFORM=$BUILD_DIR"
+elif [[ $_3PP_PLATFORM =~ "mac-*" ]]; then
+  BUILD_CMD="make -j -f ../../cmpl_clang.mak"
+  BUILD_DIR="b/c"
+elif [[ $_3PP_PLATFORM = "linux-amd64" ]]; then
+  BUILD_CMD="make -j -f ../../cmpl_gcc.mak"
+  BUILD_DIR="b/g"
+else
+  echo "Unsupported architecture: $_3PP_PLATFORM"
+  exit 1
+fi
+
+cd $BASE_DIR/CPP/7zip/Bundles/Alone
+$BUILD_CMD
+cp $BUILD_DIR/7za?(.exe) $PREFIX
+
+cd $BASE_DIR/CPP/7zip/Bundles/Alone2
+$BUILD_CMD
+cp $BUILD_DIR/7zz?(.exe) $PREFIX
+
+cd $BASE_DIR/CPP/7zip/Bundles/Alone7z
+$BUILD_CMD
+cp $BUILD_DIR/7zr?(.exe) $PREFIX
+
