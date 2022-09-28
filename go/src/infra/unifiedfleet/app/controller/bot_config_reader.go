777642ef793c6a1c65d8580b31215ac89ee734ed
Gowri Denduluri <gowriden@google.com>
2022-09-28 20:56:03 +0000

    [UFS] Updating Bot Id Parsing Logic.
    Parsing Bot Id's as they may use bash brace expansion syntax and storing bot configs in datastore for Machine entities.
    
    Bug: b/248039146
    Test: make dev; go test;
    
    Change-Id: I79bc073ba7289b8d55831fd0a640098b21931a28
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3917049
    Reviewed-by: Justin Suen <justinsuen@google.com>
    Commit-Queue: Gowri Denduluri <gowriden@google.com>
    Cr-Commit-Position: refs/heads/main@{#51845}

--- go/src/infra/unifiedfleet/app/controller/bot_config_reader.go
+++ go/src/infra/unifiedfleet/app/controller/bot_config_reader.go
@@ -7,16 +7,26 @@ package controller
 import (
 	"context"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"go.chromium.org/luci/common/logging"
 	"google.golang.org/protobuf/encoding/prototext"
 
 	"infra/unifiedfleet/app/config"
 	"infra/unifiedfleet/app/external"
+	"infra/unifiedfleet/app/model/registration"
+
+	ufspb "infra/unifiedfleet/api/v1/models"
 
 	configpb "go.chromium.org/luci/swarming/proto/config"
 )
 
+const (
+	POOL_PREFIX = "pool:"
+)
+
+// ImportENCBotConfig imports Bot Config files and stores the bot configs for ownership data in the DataStore.
 func ImportENCBotConfig(ctx context.Context) error {
 	es, err := external.GetServerInterface(ctx)
 	if err != nil {
@@ -42,4 +52,82 @@ func ImportENCBotConfig(ctx context.Context) error {
 		}
 	}
 	return nil
+}
+
+// ParseBotConfig parses the Bot Config files and stores the ownership data in the Data store for every bot in the config.
+func ParseBotConfig(ctx context.Context, config *configpb.BotsCfg, swarmingInstance string) {
+	for _, botGroup := range config.BotGroup {
+		if len(botGroup.BotId) == 0 {
+			continue
+		}
+		botsIds := []string{}
+		for _, id := range botGroup.BotId {
+			if strings.Contains(id, "{") {
+				// Parse the BotId Range
+				botsIds = append(botsIds, parseBotIds(id)...)
+			} else {
+				botsIds = append(botsIds, id)
+			}
+		}
+
+		pool := ""
+		for _, dim := range botGroup.GetDimensions() {
+			// Extract pool from the bot dimensions
+			if strings.HasPrefix(dim, POOL_PREFIX) {
+				pool = strings.TrimPrefix(dim, POOL_PREFIX)
+				break
+			}
+		}
+		ownershipData := &ufspb.OwnershipData{
+			PoolName:         pool,
+			SwarmingInstance: swarmingInstance,
+		}
+
+		// Update the ownership data for the botIds collected so far.
+		for _, botId := range botsIds {
+			// TODO(b/248039146) - Bot can be one of machine, machine_lse or vm. Store ownership accordingly
+			_, err := registration.UpdateMachineOwnership(ctx, botId, ownershipData)
+			logging.Debugf(ctx, "Failed to update ownership for bot id %s - %v", botId, err)
+		}
+	}
+}
+
+// parseBotIds parses a range of bot Ids from the input string and returns an array of bot Ids
+func parseBotIds(idExpr string) []string {
+	prefix := ""
+	suffix := ""
+	botsIds := []string{}
+	if strings.Contains(idExpr, "{") && strings.Contains(idExpr, "}") && strings.Index(idExpr, "{") < strings.Index(idExpr, "}") {
+		// Get the prefix and suffix but trimming string after and before '{' and '}'
+		prefix = strings.Split(idExpr, "{")[0]
+		suffix = strings.Split(idExpr, "}")[1]
+
+		// Get the number range by trimming prefix and suffix
+		numRange := strings.TrimSuffix(strings.TrimPrefix(idExpr, prefix+"{"), "}"+suffix)
+		nums := strings.Split(numRange, ",")
+
+		for _, num := range nums {
+			if !strings.Contains(num, "..") {
+				botsIds = append(botsIds, prefix+num+suffix)
+			} else {
+				// Expand the range of numbers. Ex 1..10 will be expanded to number 1 to 10 inclusive
+				rangeIds := strings.Split(num, "..")
+				if len(rangeIds) != 2 {
+					continue
+				}
+				start, starterr := strconv.Atoi(rangeIds[0])
+				end, enderr := strconv.Atoi(rangeIds[1])
+
+				// Skip this range is it not correctly formed
+				if starterr != nil || enderr != nil || start > end {
+					continue
+				}
+
+				for i := start; i <= end; i++ {
+					botsIds = append(botsIds, fmt.Sprintf("%s%d%s", prefix, i, suffix))
+				}
+			}
+		}
+	}
+	return botsIds
 }
--- go/src/infra/unifiedfleet/app/controller/bot_config_reader_test.go
+++ go/src/infra/unifiedfleet/app/controller/bot_config_reader_test.go
@@ -8,13 +8,19 @@ import (
 	"go.chromium.org/luci/appengine/gaetesting"
 	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/common/logging/gologger"
+	"go.chromium.org/luci/gae/service/datastore"
+
+	ufspb "infra/unifiedfleet/api/v1/models"
+
+	configpb "go.chromium.org/luci/swarming/proto/config"
 
 	"infra/unifiedfleet/app/config"
 	"infra/unifiedfleet/app/external"
-
-	"go.chromium.org/luci/gae/service/datastore"
+	"infra/unifiedfleet/app/model/registration"
 )
 
+// encTestingContext creates a testing context which mocks the logging and datastore services.
+// Also loads a custom config, which will allow the loading of a dummy bot config file
 func encTestingContext() context.Context {
 	c := gaetesting.TestingContextWithAppID("dev~infra-unified-fleet-system")
 	c = gologger.StdConfig.Use(c)
@@ -36,13 +42,96 @@ func encTestingContext() context.Context {
 	datastore.GetTestable(c).Consistent(true)
 	return c
 }
+
+// Dummy config for bots
+func mockBotConfig(botRange string, pool string) *configpb.BotsCfg {
+	return &configpb.BotsCfg{
+		BotGroup: []*configpb.BotGroup{
+			{
+				BotId:      []string{botRange},
+				Dimensions: []string{"pool:" + pool},
+			},
+		},
+	}
+}
+
+// Dummy ChromeBrowserMachine
+func mockChromeBrowserMachine(id, name string) *ufspb.Machine {
+	return &ufspb.Machine{
+		Name: id,
+		Device: &ufspb.Machine_ChromeBrowserMachine{
+			ChromeBrowserMachine: &ufspb.ChromeBrowserMachine{
+				Description: name,
+			},
+		},
+	}
+}
+
 func TestImportENCBotConfig(t *testing.T) {
+	// Tests the functionality for importing bot configs from the config files
 	t.Parallel()
 	ctx := encTestingContext()
-	Convey("Is Valid Public Chromium Test", t, func() {
+	Convey("Import ENC Bot Config", t, func() {
 		Convey("happy path", func() {
 			err := ImportENCBotConfig(ctx)
 			So(err, ShouldBeNil)
+		})
+	})
+}
+
+func TestParseBotConfig(t *testing.T) {
+	// Tests the functionality for parsing and storing bot configs in Datastore
+	t.Parallel()
+	ctx := encTestingContext()
+	Convey("Parse ENC Bot Config", t, func() {
+		Convey("happy path", func() {
+			resp, err := registration.CreateMachine(ctx, mockChromeBrowserMachine("test1-1", "test1"))
+			So(resp, ShouldNotBeNil)
+			So(err, ShouldBeNil)
+
+			ParseBotConfig(ctx, mockBotConfig("test{1,2}-1", "abc"), "testSwarming")
+
+			resp, err = registration.GetMachine(ctx, "test1-1")
+			So(resp, ShouldNotBeNil)
+			So(err, ShouldBeNil)
+			So(resp.Ownership, ShouldNotBeNil)
+			So(resp.Ownership.PoolName, ShouldEqual, "abc")
+			So(resp.Ownership.SwarmingInstance, ShouldEqual, "testSwarming")
+		})
+		Convey("Does not update non existent bots", func() {
+			ParseBotConfig(ctx, mockBotConfig("test{2,3}-1", "abc"), "testSwarming")
+
+			resp, err := registration.GetMachine(ctx, "test2-1")
+			So(resp, ShouldBeNil)
+			So(err, ShouldNotBeNil)
+			So(err.Error(), ShouldContainSubstring, "NotFound")
+		})
+	})
+}
+
+func TestParseBotIds(t *testing.T) {
+	// Tests the functionality for parsing botId strings
+	t.Parallel()
+	Convey("Parse ENC Bot Config", t, func() {
+		Convey("Parse comma separated and ranges", func() {
+			ids := parseBotIds("mac{9,10..11,12}-483")
+			So(ids, ShouldResemble, []string{"mac9-483", "mac10-483", "mac11-483", "mac12-483"})
+		})
+		Convey("Parse multiple ranges", func() {
+			ids := parseBotIds("mac{9,10..11,18..20}-483")
+			So(ids, ShouldResemble, []string{"mac9-483", "mac10-483", "mac11-483", "mac18-483", "mac19-483", "mac20-483"})
+		})
+		Convey("Parse invalid range - ignores invalid range", func() {
+			ids := parseBotIds("mac{9,10..11,22..20}-483")
+			So(ids, ShouldResemble, []string{"mac9-483", "mac10-483", "mac11-483"})
+		})
+		Convey("Parse mal formed range - ignores malformed range", func() {
+			ids := parseBotIds("mac{9,10..11,..20}-483")
+			So(ids, ShouldResemble, []string{"mac9-483", "mac10-483", "mac11-483"})
+		})
+		Convey("Parse non digit characters in range - ignores", func() {
+			ids := parseBotIds("mac{9,10,11..a}-483")
+			So(ids, ShouldResemble, []string{"mac9-483", "mac10-483"})
 		})
 	})
 }
