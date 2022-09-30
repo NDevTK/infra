93762460cbe208fd555c90eecae2c8cc24818b88
Chenlin Fan <fancl@chromium.org>
2022-09-30 17:13:31 +1000

    pkgbuild: mock build and storage for tests
    Add mock interfaces to avoid triggering build in unit tests.
    
    Bug:1334893
    Change-Id: I855e68d1d7f299ecf26416afa3ac5756ffcafd14

---
+++ go/src/infra/libs/cipkg/utilities/testing/mock.go
@@ -0,0 +1,80 @@
+package testing
+
+import (
+	"context"
+	"infra/libs/cipkg"
+	"time"
+)
+
+// Testing utilities for mocking common cipkg interfaces.
+
+// MockBuild is used as the build function for utilities.Builder.
+// MockBuild.Build can be passed to Builder.BuildAll for collecting packages'
+// information.
+type MockBuild struct {
+	Packages []cipkg.Package
+}
+
+func NewMockBuild() *MockBuild {
+	return &MockBuild{
+		Packages: make([]cipkg.Package, 0),
+	}
+}
+
+func (b *MockBuild) Build(p cipkg.Package) error {
+	b.Packages = append(b.Packages, p)
+	return nil
+}
+func (b *MockBuild) Reset() { b.Packages = b.Packages[:0] }
+
+// MockStorage and MockPackage implements cipkg.Storage interface. It stores
+// metadata and derivation in the memory. It doesn't allocate any "real" storage
+// in the filesystem.
+type MockStorage struct {
+	pkgs map[string]cipkg.Package
+}
+
+func NewMockStorage() cipkg.Storage {
+	return &MockStorage{
+		pkgs: make(map[string]cipkg.Package),
+	}
+}
+
+func (s *MockStorage) Get(id string) cipkg.Package { return s.pkgs[id] }
+func (s *MockStorage) Add(drv cipkg.Derivation, metadata cipkg.PackageMetadata) cipkg.Package {
+	pkg := &MockPackage{
+		derivation: drv,
+		metadata:   metadata,
+		available:  false,
+	}
+	s.pkgs[drv.ID()] = pkg
+	return pkg
+}
+func (s *MockStorage) Prune(ctx context.Context, ttl time.Duration, max int) {}
+
+type MockPackage struct {
+	derivation cipkg.Derivation
+	metadata   cipkg.PackageMetadata
+	available  bool
+}
+
+func (p *MockPackage) Derivation() cipkg.Derivation    { return p.derivation }
+func (p *MockPackage) Metadata() cipkg.PackageMetadata { return p.metadata }
+func (p *MockPackage) Directory() string               { return p.derivation.ID() }
+func (p *MockPackage) Build(f func(cipkg.Package) error) error {
+	if err := f(p); err != nil {
+		return err
+	}
+	p.available = true
+	return nil
+}
+func (p *MockPackage) TryRemove() (ok bool, err error) {
+	if !p.available {
+		return false, nil
+	}
+	p.available = false
+	return true, nil
+}
+func (p *MockPackage) Available() (ok bool, mtime time.Time) { return p.available, time.Time{} }
+func (p *MockPackage) RLock() error                          { return nil }
+func (p *MockPackage) RUnlock() error                        { return nil }
