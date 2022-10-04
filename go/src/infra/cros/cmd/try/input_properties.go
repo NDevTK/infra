4448236e2777a9e6b80d0f5b105c5b6032131a1b
Jack Neus <jackneus@google.com>
2022-10-04 22:34:48 +0000

    cros try: add setProperty
    BUG=b:250046249
    TEST=go test, go run
    
    Change-Id: I41281c88fb37378adb4aba6f0c8b2786b92f051a
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3933976
    Commit-Queue: Jack Neus <jackneus@google.com>
    Reviewed-by: Greg Edelston <gredelston@google.com>
    Cr-Commit-Position: refs/heads/main@{#51953}

--- go/src/infra/cros/cmd/try/input_properties.go
+++ go/src/infra/cros/cmd/try/input_properties.go
@@ -8,6 +8,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"reflect"
+	"strings"
 
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/led/job"
@@ -59,4 +61,68 @@ func writeStructToFile(s *structpb.Struct) (*os.File, error) {
 		return nil, err
 	}
 	return file, nil
+}
+
+func setProperty(s *structpb.Struct, key string, value interface{}) error {
+	// Inner function for recursing over each component of the key ('.' separated).
+	setPropertyInner := func(s *structpb.Struct, toProcess []string, value interface{}) error {
+		processed := []string{}
+		fields := s.Fields
+		for k, v := range fields {
+			if k == toProcess[0] {
+				// Found the key.
+				processed = append(processed, k)
+				toProcess = toProcess[1:]
+				// If there's no more key to process, assign the value.
+				if len(toProcess) == 0 {
+					structValue, err := structpb.NewValue(value)
+					if err != nil {
+						return errors.Annotate(err, "Could not convert value: ").Err()
+					}
+					fields[k] = structValue
+					return nil
+				}
+				// If there's more key to process, recurse.
+				if v.GetStructValue() == nil {
+					return fmt.Errorf("The value for %s is not a struct, cannot resolve key %s.",
+						strings.Join(processed, ","), k)
+				} else {
+					return setProperty(v.GetStructValue(), strings.Join(toProcess, "."), value)
+				}
+			}
+		}
+		// Couldn't find an existing key, create one.
+		for len(toProcess) > 1 {
+			var nextComponent string
+			nextComponent, toProcess = toProcess[0], toProcess[1:]
+			emptyStruct, err := structpb.NewStruct(map[string]interface{}{})
+			if err != nil {
+				return err
+			}
+			fields[nextComponent] = structpb.NewStructValue(emptyStruct)
+			fields = fields[nextComponent].GetStructValue().Fields
+		}
+		structValue, err := structpb.NewValue(value)
+		if err != nil {
+			return errors.Annotate(err, "Could not convert value: ").Err()
+		}
+		fields[toProcess[0]] = structValue
+		return nil
+	}
+
+	// If the value is a slice, make sure it's a slice of interfaces.
+	// This way this function can be called with []string, []int, etc. values.
+	if s := reflect.ValueOf(value); s.Kind() == reflect.Slice {
+		if s.IsNil() {
+			value = nil
+		} else {
+			slice := make([]interface{}, s.Len())
+			for i := 0; i < s.Len(); i++ {
+				slice[i] = s.Index(i).Interface()
+			}
+			value = slice
+		}
+	}
+
+	return setPropertyInner(s, strings.Split(key, "."), value)
 }
--- go/src/infra/cros/cmd/try/input_properties_test.go
+++ go/src/infra/cros/cmd/try/input_properties_test.go
@@ -8,13 +8,16 @@ import (
 	"fmt"
 	"testing"
 
-	"google.golang.org/protobuf/types/known/structpb"
+	"infra/cros/internal/assert"
 	"infra/cros/internal/cmd"
+
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 // TestGetBuilderInputProps tests GetBuilderInputProps.
 // The most interesting logic to test is where it permits certain json.UnmarshalTypeErrors.
 func TestGetBuilderInputProps(t *testing.T) {
+	t.Parallel()
 	const (
 		invalidJSON = "{'is-this-valid-json?': False"
 		// experiment_reasons have enums as integers
@@ -126,4 +129,58 @@ func TestGetBuilderInputProps(t *testing.T) {
 			t.Errorf("#%d: Unexpected input props: got %+v; want %+v", i, propsStruct, tc.expectedInputProps)
 		}
 	}
+}
+
+func TestSetProperty(t *testing.T) {
+	t.Parallel()
+	s, err := structpb.NewStruct(map[string]interface{}{
+		"$chromeos/my_module": map[string]interface{}{
+			"my_prop": 100,
+		},
+		"my_other_prop": "101",
+	})
+	assert.NilError(t, err)
+
+	setProperty(s, "$chromeos/my_module.my_prop", 200)
+	setProperty(s, "$chromeos/my_module.new_prop.foo", []string{"a", "b", "c"})
+	setProperty(s, "my_other_prop", "201")
+
+	fields := s.GetFields()
+	assert.IntsEqual(t, int(fields["$chromeos/my_module"].GetStructValue().GetFields()["my_prop"].GetNumberValue()), 200)
+
+	myProp := fields["$chromeos/my_module"].GetStructValue().GetFields()["new_prop"].GetStructValue().GetFields()["foo"].GetListValue().AsSlice()
+	myPropStr := make([]string, len(myProp))
+	for i := range myProp {
+		myPropStr[i] = myProp[i].(string)
+	}
+	assert.StringArrsEqual(t, myPropStr, []string{"a", "b", "c"})
+
+	assert.StringsEqual(t, fields["my_other_prop"].GetStringValue(), "201")
+}
+
+func TestSetProperty_error(t *testing.T) {
+	t.Parallel()
+	s, err := structpb.NewStruct(map[string]interface{}{
+		"$chromeos/my_module": map[string]interface{}{
+			"my_prop": 100,
+		},
+		"my_other_prop": "101",
+	})
+	assert.NilError(t, err)
+
+	invalidValue := struct {
+		String string
+		Number int
+	}{
+		"foo",
+		123,
+	}
+	err = setProperty(s, "$chromeos/my_module.my_prop", invalidValue)
+	assert.ErrorContains(t, err, "invalid type")
+
+	err = setProperty(s, "totally_new_prop", invalidValue)
+	assert.ErrorContains(t, err, "invalid type")
+
+	err = setProperty(s, "my_other_prop.invalid_nest", 123)
+	assert.ErrorContains(t, err, "not a struct")
 }
