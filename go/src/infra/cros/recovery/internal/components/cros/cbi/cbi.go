5419c15b7775420a1b6ebd6994505b95d50d92da
Sean Carpenter <seancarpenter@google.com>
2022-10-13 13:09:31 -0700

    Add Read CBI Support to Paris
    This code is still not connected to a Critical Action, and is functionally a NO-OP. Ran unit tests for Mallet and Recovery, and then ran against a single Dedede DUT (and manually verified that CBI contents were read) using a custom plan that only references CBI actions: https://paste.googleplex.com/6609060378968064. Broader testing will be conducted before connecting this change to a critical action.
    
    See go/fleet-recovery-developer for more information on how to build and test Paris changes in general.
    
    BUG=b/235000813
    TEST=manual
    
    Change-Id: Iba316f9bc04a827f768b30b4c355a6dadb898905

--- go/src/infra/cros/recovery/internal/components/cros/cbi/cbi.go
+++ go/src/infra/cros/recovery/internal/components/cros/cbi/cbi.go
@@ -8,11 +8,13 @@ package cbi
 import (
 	"context"
 	"regexp"
+	"strconv"
+	"strings"
 	"time"
 
 	"infra/cros/recovery/internal/components"
-	"infra/cros/recovery/internal/log"
 
+	labapi "go.chromium.org/chromiumos/config/go/test/lab/api"
 	"go.chromium.org/luci/common/errors"
 )
 
@@ -24,11 +26,20 @@ type CBILocation struct {
 }
 
 const (
-	locateCBICommand = "ectool locatechip"
-	cbiChipType      = "0" // Maps to CBI in the `ectool locatechip` utility
-	cbiIndex         = "0" // Gets the first CBI chip (there is only ever one) on the DUT.
-	locateCBIRegex   = `Port:\s(\d+).*Address:\s(0x\w+)`
+	locateCBICommand   = "ectool locatechip"
+	cbiChipType        = "0" // Maps to CBI in the `ectool locatechip` utility
+	cbiIndex           = "0" // Gets the first CBI chip (there is only ever one) on the DUT.
+	transferCBICommand = "ectool i2cxfer"
+	cbiSize            = 256 // How many bytes of memory are stored in CBI.
+
+	// How many bytes can be read from CBI in a single operation.
+	// THIS VALUE SHOULD BE TREATED AS A HARD LIMIT. Exceeding this limit may
+	// result in undefined behavior.
+	readIncrement = 64
 )
+
+var readCBIRegex = regexp.MustCompile(`0x[[:xdigit:]]{1,2}|00`) // Match bytes printed in hex format (e.g. 00, 0x12, 0x3)
+var locateCBIRegex = regexp.MustCompile(`Port:\s(\d+).*Address:\s(0x\w+)`)
 
 // GetCBILocation uses the `ectool locatechip` utility to get the CBILocation
 // from the DUT. Will return an error if the DUT doesn't support CBI or if it
@@ -36,29 +47,59 @@ const (
 func GetCBILocation(ctx context.Context, run components.Runner) (*CBILocation, error) {
 	locateCBIOutput, err := run(ctx, time.Second*30, locateCBICommand, cbiChipType, cbiIndex)
 	if err != nil {
-		return nil, errors.Annotate(err, "Unable to determine if CBI is present on the DUT").Err()
+		return nil, errors.Annotate(err, "get CBI location: unable to determine if CBI is present on the DUT").Err()
 	}
 
 	cbiLocation, err := buildCBILocation(locateCBIOutput)
-	if err != nil {
-		return nil, err
-	}
-
-	log.Infof(ctx, "Found CBI contents on the DUT")
-	return cbiLocation, err
+	return cbiLocation, errors.Annotate(err, "get CBI location").Err()
 }
 
 // buildCBILocation creates a CBILocation struct from the text output of an
 // `ectool locatechip` call. Will return an error if the locateCBIOutput doesn't
 // contain both the address and the port needed to access the CBI contents.
 func buildCBILocation(locateCBIOutput string) (*CBILocation, error) {
-	r, _ := regexp.Compile(locateCBIRegex)
-	match := r.FindStringSubmatch(locateCBIOutput)
-	if len(match) != 3 {
-		return nil, errors.Reason("No CBI contents were found on the DUT").Err()
+	match := locateCBIRegex.FindStringSubmatch(locateCBIOutput)
+	if len(match) < 3 {
+		return nil, errors.Reason("build CBI location: no CBI contents were found on the DUT").Err()
 	}
 	return &CBILocation{
 		port:    match[1],
 		address: match[2],
 	}, nil
+}
+
+// ReadCBIContents reads all <cbiSize> bytes from the CBI chip on the DUT in
+// <readIncrement> sized reads using the ectool i2cxfer utility and returns a
+// fully formed CBI proto.
+func ReadCBIContents(ctx context.Context, run components.Runner, cbiLocation *CBILocation) (*labapi.Cbi, error) {
+	hexContents := []string{}
+	for offset := 0; offset < cbiSize; offset += readIncrement {
+		cbiContents, err := run(ctx, time.Second*10, transferCBICommand, cbiLocation.port, cbiLocation.address, strconv.Itoa(readIncrement), strconv.Itoa(offset))
+		if err != nil {
+			return nil, errors.Annotate(err, "read CBI contents: unable to read CBI contents").Err()
+		}
+
+		hexBytes, err := parseBytesFromCBIContents(cbiContents, readIncrement)
+		if err != nil {
+			return nil, err
+		}
+
+		hexContents = append(hexContents, hexBytes...)
+	}
+	return &labapi.Cbi{RawContents: strings.Join(hexContents, " ")}, nil
+}
+
+// parseBytesFromCBIContents reads <numBytesToRead> number of bytes from the
+// raw output from a call to `ectool i2cxfer` and returns a slice of bytes
+// in hex format (the same format returned from `ectool i2cxfer`).
+// e.g.
+// cbiContents = "Read bytes: 0x43, 0x42, 0x49"
+// numBytesToRead = 2
+// hexBytes = ["0x43", "0x42"]
+func parseBytesFromCBIContents(cbiContents string, numBytesToRead int) ([]string, error) {
+	hexBytes := readCBIRegex.FindAllString(cbiContents, numBytesToRead)
+	if len(hexBytes) != numBytesToRead {
+		return nil, errors.Reason("parse bytes from CBI contents: wrong amount: expected %d bytes but read %d bytes instead. CBI contents found: %s", numBytesToRead, len(hexBytes), cbiContents).Err()
+	}
+	return hexBytes, nil
 }
--- go/src/infra/cros/recovery/internal/execs/cros/cros_cbi.go
+++ go/src/infra/cros/recovery/internal/execs/cros/cros_cbi.go
@@ -20,11 +20,34 @@ func repairCBI(ctx context.Context, info *execs.ExecInfo) error {
 	return nil
 }
 
-// cbiIsCorrupt checks if the CBI contents on the DUT match what was previously
-// stored in UFS. If not, then the CBI contents have been corrupted.
-// TODO(b/235000813) Implement
-func cbiIsCorrupt(ctx context.Context, info *execs.ExecInfo) error {
+// ufsContainsCBIContents checks if any CBI Contents were previously stored for
+// this DUT in UFS.
+func ufsContainsCBIContents(ctx context.Context, info *execs.ExecInfo) error {
+	if info.GetChromeos().GetCbi() == nil {
+		return errors.Reason("UFS contains CBI contents: no previous CBI contents were found in UFS").Err()
+	}
 	return nil
+}
+
+// cbiContentsDoNotMatch checks if the CBI contents on the DUT do not match what
+// was previously stored in UFS. If they do not match, then the CBI contents
+// have been corrupted. Throws an error if no previously stored CBI contents are
+// found.
+func cbiContentsDoNotMatch(ctx context.Context, info *execs.ExecInfo) error {
+	cbiLocation, err := cbi.GetCBILocation(ctx, info.NewRunner(info.GetDut().Name))
+	if err != nil {
+		return errors.Annotate(err, "CBI contents do not match").Err()
+	}
+
+	dutCBI, err := cbi.ReadCBIContents(ctx, info.NewRunner(info.GetDut().Name), cbiLocation)
+	if err != nil {
+		return errors.Annotate(err, "CBI contents do not match").Err()
+	}
+
+	if info.GetChromeos().GetCbi().RawContents != dutCBI.RawContents {
+		return nil
+	}
+	return errors.Reason("CBI contents do not match: CBI contents on DUT match the contents stored in UFS").Err()
 }
 
 // cbiIsPresent checks if CBI contents are found on the DUT.
@@ -34,13 +57,14 @@ func cbiIsPresent(ctx context.Context, info *execs.ExecInfo) error {
 		return errors.Annotate(err, "CBI is present").Err()
 	}
 	if cbiLocation == nil {
-		return errors.Reason("No CBI contents were found on the DUT, but encountered no error. This shouldn't ever happen. Please submit a bug.").Err()
+		return errors.Reason("CBI is present: no CBI contents were found on the DUT, but encountered no error - please submit a bug").Err()
 	}
 	return nil
 }
 
 func init() {
 	execs.Register("cros_repair_cbi", repairCBI)
-	execs.Register("cros_cbi_is_corrupt", cbiIsCorrupt)
+	execs.Register("cros_cbi_contents_do_not_match", cbiContentsDoNotMatch)
+	execs.Register("cros_ufs_contains_cbi_contents", ufsContainsCBIContents)
 	execs.Register("cros_cbi_is_present", cbiIsPresent)
 }
