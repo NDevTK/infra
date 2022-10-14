f8f7b56088eb5f02583c25dadd1d0406d8fe2185
Greg NISBET <gregorynisbet@google.com>
2022-10-14 01:52:13 +0000

    [karte] Test CreateAction with a clock.
    Add a test specifically for creating a test action with an explicitly
    given clock setting.
    
    BUG=b:236182267
    TEST=make test
    
    Change-Id: Ia881cf7b89c74dd597f63240ad27c8802f8dcff8
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3942595
    Reviewed-by: Anushruth Sasivehalli <anushruth@chromium.org>
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Cr-Commit-Position: refs/heads/main@{#52195}

---
+++ go/src/infra/cros/karte/internal/frontend/create_test.go
@@ -0,0 +1,44 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package frontend
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"go.chromium.org/luci/appengine/gaetesting"
+	"go.chromium.org/luci/common/clock"
+	"go.chromium.org/luci/common/clock/testclock"
+	"go.chromium.org/luci/gae/service/datastore"
+
+	kartepb "infra/cros/karte/api"
+	"infra/cros/karte/internal/identifiers"
+	"infra/cros/karte/internal/scalars"
+)
+
+// TestCreateActionWithClock tests creating an action with the testing clock set to 10 seconds after
+// the beginning of time (UTC midnight on 1970-01-01).
+func TestCreateActionWithClock(t *testing.T) {
+	Convey("test create action with clock", t, func() {
+		ctx := gaetesting.TestingContext()
+		ctx = identifiers.Use(ctx, identifiers.NewDefault())
+		testClock := testclock.New(time.Unix(10, 0))
+		ctx = clock.Set(ctx, testClock)
+		datastore.GetTestable(ctx).Consistent(true)
+		k := NewKarteFrontend()
+
+		action, err := k.CreateAction(ctx, &kartepb.CreateActionRequest{
+			Action: &kartepb.Action{},
+		})
+		So(err, ShouldBeNil)
+		So(action.Name[0:10], ShouldEqual, "zzzzUzzzzz")
+		action.Name = ""
+		So(action, ShouldResemble, &kartepb.Action{
+			CreateTime: scalars.ConvertTimeToTimestampPtr(time.Unix(10, 0)),
+			SealTime:   scalars.ConvertTimeToTimestampPtr(time.Unix(43210, 0)),
+		})
+	})
+}
