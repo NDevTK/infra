58ee9310ddd6c24956bca3c5a7e2bc9c64a9e7f1
Otabek Kasimov <otabek@google.com>
2022-10-17 17:00:12 +0000

    recovery: Update BtPeer plan and apply new recovery action
    Set 10 seconds as it takes time to recove the service.
    
    BUG=b:251247913
    TEST=run local and test on bad devices
    
    Change-Id: I33c36a72ecac36e12a58c9a9e384fc4cf235ba71
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3956033
    Auto-Submit: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Bryan Ni <bniche@google.com>
    Reviewed-by: Bryan Ni <bniche@google.com>
    Cr-Commit-Position: refs/heads/main@{#52230}

--- go/src/infra/cros/recovery/config/config_btpeer.go
+++ go/src/infra/cros/recovery/config/config_btpeer.go
@@ -11,20 +11,82 @@ import (
 func btpeerRepairPlan() *Plan {
 	return &Plan{
 		CriticalActions: []string{
-			"btpeer_state_broken",
+			"Set state: BROKEN",
 			"Device is pingable",
-			"cros_ssh",
-			"check_server",
-			"btpeer_state_working",
+			"Device is SSHable",
+			"Chameleond is responsive",
+			"Set state: WORKING",
 		},
 		Actions: map[string]*Action{
-			"check_server": {
-				Docs:     []string{"To check if devices is responsive we request not empty list of detected statuses."},
-				ExecName: "btpeer_get_detected_statuses",
+			"Set state: BROKEN": {
+				Docs: []string{
+					"The device state BROKEN.",
+				},
+				ExecName:   "btpeer_state_broken",
+				RunControl: RunControl_RUN_ONCE,
+			},
+			"Set state: WORKING": {
+				Docs: []string{
+					"The device state WORKING.",
+				},
+				ExecName:   "btpeer_state_working",
+				RunControl: RunControl_RUN_ONCE,
 			},
 			"Device is pingable": {
-				ExecTimeout: &durationpb.Duration{Seconds: 15},
+				Docs: []string{
+					"Wait device to be pingable.",
+					"Waiting time 15 seconds.",
+				},
 				ExecName:    "cros_ping",
+				ExecTimeout: &durationpb.Duration{Seconds: 15},
+				RunControl:  RunControl_ALWAYS_RUN,
+			},
+			"Device is SSHable": {
+				Docs: []string{
+					"Try to wait device to be sshable.",
+					"Waiting time 150 seconds.",
+				},
+				ExecName:   "cros_ssh",
+				RunControl: RunControl_ALWAYS_RUN,
+			},
+			"Chameleond is responsive": {
+				Docs: []string{
+					"Verify chameleond is responsive.",
+					"Expected to receive not empty list of detected statuses.",
+				},
+				ExecName: "btpeer_get_detected_statuses",
+				RecoveryActions: []string{
+					"Restart chameleond and wait",
+				},
+			},
+			"Restart chameleond and wait": {
+				Docs: []string{
+					"Restart chameleond and wait for service to be ready.",
+				},
+				Dependencies: []string{
+					"Restart chameleond command",
+					"Sleep for chameleond restart",
+				},
+				ExecName: "sample_pass",
+			},
+			"Restart chameleond command": {
+				Docs: []string{
+					"Restart chameleond service.",
+				},
+				ExecName: "cros_run_shell_command",
+				ExecExtraArgs: []string{
+					"sudo service chameleond restart",
+				},
+			},
+			"Sleep for chameleond restart": {
+				Docs: []string{
+					"When we restart chameleond we need wait about 10 seconds to be recovered.",
+				},
+				ExecName: "sample_sleep",
+				ExecExtraArgs: []string{
+					"sleep:10",
+				},
+				RunControl: RunControl_ALWAYS_RUN,
 			},
 		},
 	}
