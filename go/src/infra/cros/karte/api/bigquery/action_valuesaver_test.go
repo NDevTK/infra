6319299170c83ca214f304373bad1b71fdf8c282
Greg NISBET <gregorynisbet@google.com>
2022-10-18 20:41:45 +0000

    [karte] Remove goconvey from action_valuesaver_test.go
    BUG=b:253329179
    TEST=make test
    
    Change-Id: I44dba4385d20480bb7666b4cf2c381283244341d
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3952756
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Reviewed-by: Allen Li <ayatane@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#52280}

--- go/src/infra/cros/karte/api/bigquery/action_valuesaver_test.go
+++ go/src/infra/cros/karte/api/bigquery/action_valuesaver_test.go
@@ -8,7 +8,6 @@ import (
 	"testing"
 
 	"cloud.google.com/go/bigquery"
-	. "github.com/smartystreets/goconvey/convey"
 )
 
 // TestActionIsValueSaver tests that a pointer to an action implements the ValueSaver interface.
@@ -22,16 +21,22 @@ func TestActionIsValueSaver(t *testing.T) {
 // to make sure that they are exported correctly.
 func TestActionSave(t *testing.T) {
 	t.Parallel()
-	Convey("test action save", t, func() {
-		Convey("model", func() {
-			m, _, err := (&Action{Name: "aaaaa", Model: "hi"}).Save()
-			So(err, ShouldBeNil)
-			So(m["model"], ShouldEqual, "hi")
-		})
-		Convey("board", func() {
-			m, _, err := (&Action{Name: "aaaaa", Board: "hi"}).Save()
-			So(err, ShouldBeNil)
-			So(m["board"], ShouldEqual, "hi")
-		})
+	t.Run("model", func(t *testing.T) {
+		m, _, err := (&Action{Name: "aaaaa", Model: "hi"}).Save()
+		if err != nil {
+			t.Errorf("unexpected err: %s", err)
+		}
+		if m["model"] != "hi" {
+			t.Errorf("unexpected model: %q", m["model"])
+		}
+	})
+	t.Run("board", func(t *testing.T) {
+		m, _, err := (&Action{Name: "aaaaa", Board: "hi"}).Save()
+		if err != nil {
+			t.Errorf("unexpected err: %s", err)
+		}
+		if m["board"] != "hi" {
+			t.Errorf("unexpected board: %q", m["board"])
+		}
 	})
 }
