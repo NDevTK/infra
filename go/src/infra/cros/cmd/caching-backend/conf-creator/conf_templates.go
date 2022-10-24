b03d839801c957975cbad092f53cc8876bf1105b
Congbin Guo <guocb@google.com>
2022-10-24 19:08:13 +0000

    fix(cache): Nginx logs the correct content-length
    The upstream content length could be the size of a slice, instead of the
    whole file.
    
    BUG=b:253102399
    TEST=local test
    
    Change-Id: I3d5ad76e83d5fb0b02ce28b474f8892072f1c8bd
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3970864
    Auto-Submit: Congbin Guo <guocb@chromium.org>
    Reviewed-by: Bryan Ni <bniche@google.com>
    Commit-Queue: Bryan Ni <bniche@google.com>
    Cr-Commit-Position: refs/heads/main@{#52422}

--- go/src/infra/cros/cmd/caching-backend/conf-creator/conf_templates.go
+++ go/src/infra/cros/cmd/caching-backend/conf-creator/conf_templates.go
@@ -53,7 +53,7 @@ http {
   include       /etc/nginx/mime.types;
   default_type  application/octet-stream;
   log_format main '$remote_addr - $remote_user [$time_iso8601] "$request" '
-                  '$status $body_bytes_sent "$upstream_http_content_length" '
+                  '$status $body_bytes_sent "$sent_http_content_length" '
                   '$request_time "$http_referer" '
                   '"$http_user_agent" "$http_x_forwarded_for" $upstream_cache_status';
   proxy_cache_path  /var/cache/nginx levels=1:2 keys_zone=google-storage:80m
