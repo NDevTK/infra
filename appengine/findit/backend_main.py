e97ef74aa8598a512bba3cc9b8eb301e15905507
Prakhar <pasthana@google.com>
2022-10-28 06:31:52 +0000

    Delete waterfall backend module
    The corresponding UI page was deleted in https://chromium-review.googlesource.com/c/infra/infra/+/3979403
    
    Change-Id: I45bfb9841b147b539d7609b714f56eddc2361c54
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/3985397
    Reviewed-by: Zhaoyang Li <zhaoyangli@chromium.org>
    Auto-Submit: Prakhar Asthana <pasthana@google.com>
    Commit-Queue: Prakhar Asthana <pasthana@google.com>
    Cr-Commit-Position: refs/heads/main@{#52540}

--- appengine/findit/backend_main.py
+++ appengine/findit/backend_main.py
@@ -7,42 +7,11 @@ import webapp2
 import gae_ts_mon
 
 from gae_libs import appengine_util
-from gae_libs.pipelines import pipeline_handlers
-from handlers import build_ahead
-from handlers import calculate_confidence_scores
-from handlers import check_reverted_cls
-from handlers import collect_tree_closures
-from handlers import obscure_emails
-from handlers import process_failure_analysis_requests
-from handlers import process_flake_analysis_request
 from handlers.flake.detection import detect_flakes
 from handlers.flake.detection import process_flakes
 from handlers.flake.detection import update_flake_counts
 from handlers.flake.reporting import generate_report
 
-# For appengine pipeline running on backend module.
-pipeline_backend_application = pipeline_handlers._APP
-if appengine_util.IsInProductionApp():
-  gae_ts_mon.initialize_prod(pipeline_backend_application)
-
-# "waterfall-backend" module.
-waterfall_backend_web_pages_handler_mappings = [
-    ('/waterfall/cron/calculate-confidence-scores',
-     calculate_confidence_scores.CalculateConfidenceScores),
-    ('/waterfall/cron/check-reverted-cls', check_reverted_cls.CheckRevertedCLs),
-    ('/waterfall/cron/collect-tree-closures',
-     collect_tree_closures.CollectTreeClosures),
-    ('/waterfall/cron/obscure-emails', obscure_emails.ObscureEmails),
-    ('/waterfall/cron/periodic-build-ahead', build_ahead.BuildAhead),
-    ('/waterfall/task/process-failure-analysis-requests',
-     process_failure_analysis_requests.ProcessFailureAnalysisRequests),
-    ('/waterfall/task/process-flake-analysis-request',
-     process_flake_analysis_request.ProcessFlakeAnalysisRequest),
-]
-waterfall_backend_web_application = webapp2.WSGIApplication(
-    waterfall_backend_web_pages_handler_mappings, debug=False)
-if appengine_util.IsInProductionApp():
-  gae_ts_mon.initialize_prod(waterfall_backend_web_application)
 
 # "flake-detection-backend" module.
 flake_detection_backend_web_pages_handler_mappings = [
