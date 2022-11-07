03f13ae96827974fcae3fd82905e8d9f729a3fe9
Benjamin Gordon <bmgordon@chromium.org>
2022-11-07 18:09:06 +0000

    [tricium] Update shellcheck to 0.7.1-r53
    Binary downloaded from the Chrome OS SDK version 2022.11.01.161729.
    
    Bug: None
    Test: make testrun; PATH=$PWD/bin/shellcheck:$PATH make test
    Change-Id: I3a23b2b0537ded5fd1eaedb5f996c700d55a17fc
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4003984
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>
    Commit-Queue: Benjamin Gordon <bmgordon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#52710}

--- go/src/infra/tricium/functions/shellcheck/fetch_shellcheck.sh
+++ go/src/infra/tricium/functions/shellcheck/fetch_shellcheck.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -e
+# Copyright 2022 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 # When updating the version, you must update the SHA512 sum as well, e.g.:
 # shasum -a 512 "${ARCHIVE}" > "${ARCHIVE}.sum"
-VERSION=0.7.1-r51
-SDK_VERSION=2022.06.19.122518
+VERSION=0.7.1-r53
+SDK_VERSION=2022.11.01.161729
 ARCHIVE="shellcheck-${VERSION}.tbz2"
 URL="https://storage.googleapis.com/chromeos-prebuilt/host/amd64/amd64-host/chroot-${SDK_VERSION}/packages/dev-util/${ARCHIVE}"
 SUMFILE="${ARCHIVE}.sum"
