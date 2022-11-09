9ffa416c228887f4782bcd0345be6e3a81f94d25
Ming Kong <mingkong@google.com>
2022-11-09 17:39:43 +0000

    Add docker exec command support.
    This is prework of go/cft-port-discovery
    
    BUG=b:256886221
    TEST=unit tests
    
    Change-Id: Id4d0dc6c94217d6e52a9b33982d9365522167b0c
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4017452
    Auto-Submit: Ming Kong <mingkong@google.com>
    Reviewed-by: Azizur Rahman <azrahman@google.com>
    Commit-Queue: Azizur Rahman <azrahman@google.com>
    Cr-Commit-Position: refs/heads/main@{#52770}

---
+++ go/src/infra/cros/cmd/cros-tool-runner/internal/v2/commands/docker_exec.go
@@ -0,0 +1,23 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package commands
+
+import (
+	"context"
+)
+
+// DockerExec represents `docker exec`.
+// Example of ExecCommand: ["bash", "-c", "echo $HOME && echo $PATH"] prints the
+// value of environment variables $HOME and $PATH inside the container.
+type DockerExec struct {
+	Name        string   // name of container
+	ExecCommand []string // command to be executed
+}
+
+func (c *DockerExec) Execute(ctx context.Context) (string, string, error) {
+	args := []string{"exec", c.Name}
+	args = append(args, c.ExecCommand...)
+	return execute(ctx, dockerCmd, args)
+}
