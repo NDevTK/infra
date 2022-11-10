1916a48b47c117daefc0728cfce3fc8e935be4d6
Vadim Shtayura <vadimsh@chromium.org>
2022-11-10 18:04:39 +0000

    Hit badssl.com in tests only when running locally.
    This is a follow up to
    https://chromium-review.googlesource.com/c/4014858
    
    R=bryner@google.com
    BUG=1143145
    
    Change-Id: Ic91f90676f90a04e9641608d9d5f27f1f9c5da5a
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4018933
    Reviewed-by: Brian Ryner <bryner@google.com>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#52812}

--- infra/tools/dockerbuild/test/testdata/check_requests.py
+++ infra/tools/dockerbuild/test/testdata/check_requests.py
@@ -7,27 +7,34 @@
 # pylint 1.5 happy.
 from __future__ import print_function
 
-import sys
+import os
 import requests
+import sys
 
 
+# Cases to check when running locally and on a bot.
 TEST_CASES = [
     # OK.
     ('https://clients3.google.com/generate_204', 204),
+]
 
+
+# These are checked only when running not on a bot. They are too flaky to run
+# on a bot as presubmit or postsubmit tests.
+NON_BOT_TEST_CASES = [
     # The cert for extended-validation.badssl.com (ironically?) expired. Disable
     # its test in the meantime.
     #('https://extended-validation.badssl.com', 200),
 
-    # Bad certs. Unfortunately badssl.com is too flaky to use in tests.
-    # ('https://expired.badssl.com', requests.exceptions.SSLError),
-    # ('https://wrong.host.badssl.com', requests.exceptions.SSLError),
-    # ('https://self-signed.badssl.com', requests.exceptions.SSLError),
-    # ('https://untrusted-root.badssl.com', requests.exceptions.SSLError),
+    # Bad certs.
+    ('https://expired.badssl.com', requests.exceptions.SSLError),
+    ('https://wrong.host.badssl.com', requests.exceptions.SSLError),
+    ('https://self-signed.badssl.com', requests.exceptions.SSLError),
+    ('https://untrusted-root.badssl.com', requests.exceptions.SSLError),
 
     # 'requests' is known to accept revoked certificates.
     # https://github.com/kennethreitz/requests/issues/3770
-    # ('https://revoked.badssl.com', requests.exceptions.SSLError),
+    ('https://revoked.badssl.com', requests.exceptions.SSLError),
 ]
 
 
@@ -40,8 +47,13 @@ def get_code_or_err(url):
 
 
 def tests_succeed():
+  cases = []
+  cases.extend(TEST_CASES)
+  if os.environ.get('SWARMING_HEADLESS') != '1':
+    cases.extend(NON_BOT_TEST_CASES)
+
   ok = True
-  for url, exp in TEST_CASES:
+  for url, exp in cases:
     res = get_code_or_err(url)
     if isinstance(exp, int):
       if exp != res:
