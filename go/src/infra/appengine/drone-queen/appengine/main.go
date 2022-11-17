dedbe7e641b07d28168f3db0f463ed285d35522c
Allen Li <ayatane@chromium.org>
2022-11-16 17:39:24 -0800

    drone-queen: Add trace exporter
    BUG=b:259104564
    TEST=go test
    
    Change-Id: I275de9d18b0fa8bcdbb628c8817505d745b4d071

--- go/src/infra/appengine/drone-queen/appengine/main.go
+++ go/src/infra/appengine/drone-queen/appengine/main.go
@@ -15,11 +15,21 @@
 package main
 
 import (
+	"context"
+	"os"
+
+	texporter "github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace"
+	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/config/server/cfgmodule"
 	"go.chromium.org/luci/server"
 	"go.chromium.org/luci/server/cron"
 	"go.chromium.org/luci/server/gaeemulation"
 	"go.chromium.org/luci/server/module"
+	"go.opentelemetry.io/contrib/detectors/gcp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 
 	"infra/appengine/drone-queen/internal/config"
 	icron "infra/appengine/drone-queen/internal/cron"
@@ -34,10 +44,41 @@ func main() {
 		cfgmodule.NewModuleFromFlags(),
 	}
 	server.Main(nil, modules, func(srv *server.Server) error {
+		ctx := srv.Context
+		projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
+		if exp, err := texporter.New(texporter.WithProjectID(projectID)); err == nil {
+			tp := trace.NewTracerProvider(
+				trace.WithBatcher(exp),
+				trace.WithResource(newResource(ctx)),
+			)
+			srv.RegisterCleanup(func(ctx context.Context) {
+				if err := tp.Shutdown(ctx); err != nil {
+					logging.Infof(ctx, "Error shutting down TracerProvider: %s", err)
+				}
+			})
+			otel.SetTracerProvider(tp)
+		} else {
+			logging.Infof(ctx, "Error setting up trace exporter: %s", err)
+		}
+
 		icron.InstallHandlers()
 		srv.RegisterUnaryServerInterceptor(middleware.UnaryTrace)
 		srv.RegisterUnaryServerInterceptor(config.UnaryConfig)
 		frontend.RegisterServers(srv)
 		return nil
 	})
+}
+
+func newResource(ctx context.Context) *resource.Resource {
+	// This should never error.
+	// Even if it does, try to keep running normally.
+	r, _ := resource.New(
+		ctx,
+		resource.WithDetectors(gcp.NewDetector()),
+		resource.WithTelemetrySDK(),
+		resource.WithAttributes(
+			semconv.ServiceNameKey.String("drone-queen"),
+		),
+	)
+	return r
 }
