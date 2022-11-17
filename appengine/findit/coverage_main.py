7517be110ed7f0665534dd4de99f3f4f53efedc8
Prakhar <pasthana@google.com>
2022-11-17 05:48:48 +0000

    Add scaffolding to run author-coverage job
    Bug: 1371204
    Change-Id: I0695c64a1bb341415d7a493df33fe6dd80a1684f
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4024004
    Reviewed-by: Zhaoyang Li <zhaoyangli@chromium.org>
    Commit-Queue: Prakhar Asthana <pasthana@google.com>
    Cr-Commit-Position: refs/heads/main@{#52972}

--- appengine/findit/coverage_main.py
+++ appengine/findit/coverage_main.py
@@ -8,6 +8,7 @@ import gae_ts_mon
 
 from gae_libs import appengine_util
 
+from handlers.code_coverage import create_author_coverage
 from handlers.code_coverage import create_referenced_coverage
 from handlers.code_coverage import export_absolute_coverage
 from handlers.code_coverage import export_incremental_coverage
@@ -22,6 +23,8 @@ from handlers.code_coverage import update_postsubmit_report
 experimental_coverage_worker_handler_mappings = [
     ('.*/coverage/task/gerrit-filter-coverage.*',
      export_gerrit_filter_coverage.ExportCoverageMetrics),
+    ('.*/coverage/task/author-coverage.*',
+     create_author_coverage.CreateAuthorCoverageMetrics),
 ]
 experimental_coverage_worker_application = webapp2.WSGIApplication(
     experimental_coverage_worker_handler_mappings, debug=False)
@@ -58,6 +61,8 @@ code_coverage_backend_handler_mappings = [
      export_gerrit_filter_coverage.ExportAllCoverageMetrics),
     ('.*/coverage/cron/referenced-coverage',
      create_referenced_coverage.CreateReferencedCoverageMetricsCron),
+    ('.*/coverage/cron/author-coverage',
+     create_author_coverage.CreateAuthorCoverageMetricsCron),
     ('.*/coverage/task/postsubmit-report/update',
      update_postsubmit_report.UpdatePostsubmitReport),
 ]
