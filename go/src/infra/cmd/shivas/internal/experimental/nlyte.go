42e080eb9414b0407688e6a18cc623c8536549d6
Daniel Lee <danclee@google.com>
2022-11-23 00:23:07 +0000

    Add parseRecord.
    BUG=B/260134576, B/260134576
    TEST=NONE
    
    Change-Id: I8911ad59271f7fa38d7299fa67f471cd683f1074
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4036056
    Commit-Queue: Daniel Lee <danclee@google.com>
    Reviewed-by: Xixuan Wu <xixuan@google.com>
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/main@{#53093}

--- go/src/infra/cmd/shivas/internal/experimental/nlyte.go
+++ go/src/infra/cmd/shivas/internal/experimental/nlyte.go
@@ -6,14 +6,17 @@ package experimental
 
 import (
 	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/maruel/subcommands"
+	"go.chromium.org/luci/auth/client/authcli"
+
 	"infra/cmd/shivas/site"
 	"infra/cmdsupport/cmdlib"
 	ufspb "infra/unifiedfleet/api/v1/models"
 	ufsAPI "infra/unifiedfleet/api/v1/rpc"
 	ufsUtil "infra/unifiedfleet/app/util"
-
-	"github.com/maruel/subcommands"
-	"go.chromium.org/luci/auth/client/authcli"
 )
 
 // DumpNlyteCmd dumps updated entries from Nlyte to UFS.
@@ -56,6 +59,41 @@ func (c *dumpNlyte) Run(a subcommands.Application, args []string, env subcommand
 }
 
 func (c *dumpNlyte) innerRun(a subcommands.Application, args []string, env subcommands.Env) error {
+	return nil
+}
+
+// parseRecord parses the input record and update the input Asset with parsed info.
+func (c *dumpNlyte) parseRecord(ctx context.Context, record *ufspb.AssetAndHostInfo, asset *ufspb.Asset) error {
+	if record.GetAssetName() == "" {
+		return fmt.Errorf("Failed to parse record: Empty AssetName\n")
+	}
+
+	// TODO: (b/260134576) Use field masks to specify the fields to update as that is a better way to update.
+	// Add more field masks as we increase the amount of tracked field in Nlyte.
+	asset.Location.Position = strconv.Itoa(int(record.GetCabinetUNumber()))
+	// TODO: (b/260134131) Rack implementation right now is just a place holder because we don't have row and col info in Nlyte yet.
+	asset.Location.Rack = fmt.Sprintf("rack-%s", strconv.Itoa(int(record.GetCabinetAssetId())))
+	asset.Location.RackId = record.GetCabinetAssetId()
+	asset.Location.LabId = record.GetLocationGroup().GetLocationGroupId()
+	asset.Location.FullLocationName = record.GetLocationGroup().GetFullLocationName()
+
+	for _, customField := range record.GetAssetInfo().GetCustomFields() {
+		if customField.GetFieldName() == "Asset Type" {
+			if val, ok := ufspb.AssetType_value[customField.GetFieldStringValue()]; ok {
+				asset.Type = ufspb.AssetType(val)
+				continue
+			}
+			return fmt.Errorf("Failed to parse record: Invalid asset type: %s\n", customField.GetFieldStringValue())
+		} else if customField.GetFieldName() == "Zone" {
+			if val, ok := ufspb.Zone_value[customField.GetFieldStringValue()]; ok {
+				asset.Location.Zone = ufspb.Zone(val)
+				asset.Realm = ufsUtil.ToUFSRealm(ufspb.Zone(val).String())
+				continue
+			}
+			return fmt.Errorf("Failed to parse record: Invalid zone type: %s\n", customField.GetFieldStringValue())
+		}
+	}
+
 	return nil
 }
 
