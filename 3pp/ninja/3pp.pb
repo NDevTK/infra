82d753f674f4e19077eb2cce78b2dd6ac4f175f0
Brian Ryner <bryner@google.com>
2022-11-23 05:40:30 +0000

    Explicitly use python3 when configuring ninja.
    Also merge the two install scripts for convenience.
    
    Bug: 1293206
    Change-Id: I6ef3f141ad9cc0237af7d9aec9ca487bfbe83b06
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4049265
    Commit-Queue: Chenlin Fan <fancl@chromium.org>
    Reviewed-by: Chenlin Fan <fancl@chromium.org>
    Auto-Submit: Brian Ryner <bryner@google.com>
    Cr-Commit-Position: refs/heads/main@{#53112}

--- 3pp/ninja/3pp.pb
+++ 3pp/ninja/3pp.pb
@@ -9,14 +9,18 @@ create {
       repo: "https://chromium.googlesource.com/external/github.com/ninja-build/ninja"
       tag_pattern: "v%s"
     }
-    patch_version: "chromium.5"
+    patch_version: "chromium.6"
+  }
+  build {
+    tool: "tools/cpython38"
   }
 }
 
 create {
   platform_re: "mac-.*|linux-amd64"
   build {
-    tool: "tools/re2c"
+    tool: "tools/cpython38"
+    tool: "tools/re2c"  # Required if we ever patch ninja source
     tool: "tools/sed"
   }
 }
@@ -24,16 +28,10 @@ create {
 create {
   platform_re: "linux-arm.*|linux-mips.*"
   build {
+    tool: "tools/cpython38"
     tool: "tools/ninja"  # Depend on the bootstrapped version when cross-compiling
-    tool: "tools/re2c"
+    tool: "tools/re2c"  # Required if we ever patch ninja source
     tool: "tools/sed"
-  }
-}
-
-create {
-  platform_re: "windows-.*|mac-.*|linux-amd64"
-  build {
-    install: "install_bootstrap.sh"
   }
 }
 
--- 3pp/ninja/install.sh
+++ 3pp/ninja/install.sh
@@ -9,9 +9,26 @@ set -o pipefail
 
 PREFIX="$1"
 
-# Cross compiling; rely on `ninja` in $PATH.
-./configure.py
-ninja -j $(nproc)
+if [[ $_3PP_PLATFORM == mac* ]]; then
+  XCODE_SDK_PATH=$(xcrun --show-sdk-path)
+  # the min version is configured in mac_sdk.gni
+  # https://crrev.com/e840c4b48a861be294f206bd694ebce986ddbb88/build/config/mac/mac_sdk.gni#28
+  MACOSX_VERSION_MIN="10.13"
+  MACOSX_FLAGS="-isysroot${XCODE_SDK_PATH} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  CFLAGS="${CFLAGS} ${MACOSX_FLAGS}"
+  LDFLAGS="${LDFLAGS} ${MACOSX_FLAGS}"
+fi
+
+if [[ $_3PP_TOOL_PLATFORM != $_3PP_PLATFORM ]]; then
+  # Cross compiling; rely on `ninja` in $PATH.
+  python3 ./configure.py
+  ninja -j $(nproc)
+  # Can't run tests when cross-compiling.
+else
+  CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" python3 ./configure.py --bootstrap
+  ./ninja -j $(nproc) all
+  ./ninja_test
+fi
 if [[ $_3PP_PLATFORM == windows* ]]; then
   cp ninja.exe "$PREFIX"
 else
