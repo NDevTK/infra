a3fe33a9b712f236793da8b676d20fa8752334d3
Gowri Denduluri <gowriden@google.com>
2022-12-12 21:13:04 +0000

    [UFS] Saving Ownership per bot name in datastore.
    This will be a new table to cache per bot ownership configs to reduce the time taken to import from starlark generated config files.
    
    Bug: b/261949810
    Test: make dev; go test;
    
    Change-Id: I22d8d0e4a308c6d1d4db27cde8075af88ed6290a

---
+++ go/src/infra/unifiedfleet/app/model/inventory/ownership_test.go
@@ -0,0 +1,173 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package inventory
+
+import (
+	ufspb "infra/unifiedfleet/api/v1/models"
+	"testing"
+
+	"go.chromium.org/luci/appengine/gaetesting"
+	"go.chromium.org/luci/gae/service/datastore"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// Tests the functionality for Creating/Updating Ownership data per bot in the datastore
+func TestPutOwnershipData(t *testing.T) {
+	t.Parallel()
+	ctx := gaetesting.TestingContextWithAppID("go-test")
+	datastore.GetTestable(ctx).Consistent(true)
+
+	t.Run("add non-existent Bot ownership", func(t *testing.T) {
+		ownershipData := &ufspb.OwnershipData{
+			PoolName:         "pool1",
+			SwarmingInstance: "test-swarming",
+			SecurityLevel:    "untrusted",
+			Customer:         "browser",
+		}
+		expectedName := "test"
+		assetType := "machine"
+		got, err := PutOwnershipData(ctx, ownershipData, expectedName, assetType)
+		if err != nil {
+			t.Fatalf("PutOwnershipData failed: %s", err)
+		}
+		p, err := got.GetProto()
+		if err != nil {
+			t.Fatalf("Unmarshalling ownership data from datatstore failed: %s", err)
+		}
+		pm := p.(*ufspb.OwnershipData)
+		if got.Name != expectedName {
+			t.Errorf("PutOwnershipData returned unexpected name:\n%s", got.Name)
+		}
+		if pm.PoolName != ownershipData.PoolName {
+			t.Errorf("PutOwnershipData returned unexpected result for Pool Name:\n%v", pm.PoolName)
+		}
+		if pm.Customer != ownershipData.Customer {
+			t.Errorf("PutOwnershipData returned unexpected result for Customer:\n%v", pm.Customer)
+		}
+		if pm.SecurityLevel != ownershipData.SecurityLevel {
+			t.Errorf("PutOwnershipData returned unexpected result for Pool Name:\n%v", pm.SecurityLevel)
+		}
+		if pm.SwarmingInstance != ownershipData.SwarmingInstance {
+			t.Errorf("PutOwnershipData returned unexpected result for Pool Name:\n%v", pm.SwarmingInstance)
+		}
+	})
+
+	t.Run("add existing Bot Ownership", func(t *testing.T) {
+		ownershipData := &ufspb.OwnershipData{
+			PoolName:         "pool1",
+			SwarmingInstance: "test-swarming",
+			SecurityLevel:    "untrusted",
+			Customer:         "browser",
+		}
+		assetType := "machine"
+		expectedName := "test"
+		got, err := PutOwnershipData(ctx, ownershipData, expectedName, assetType)
+		if err != nil {
+			t.Fatalf("PutOwnershipData failed: %s", err)
+		}
+
+		updated_ownershipData := &ufspb.OwnershipData{
+			PoolName:         "pool2",
+			SwarmingInstance: "test-swarming",
+			SecurityLevel:    "untrusted",
+			Customer:         "flex",
+		}
+		// Update ownership
+		got, err = PutOwnershipData(ctx, updated_ownershipData, expectedName, assetType)
+		if err != nil {
+			t.Fatalf("PutOwnershipData failed: %s", err)
+		}
+		p, err := got.GetProto()
+		if err != nil {
+			t.Fatalf("Unmarshalling ownership data from datatstore failed: %s", err)
+		}
+		pm := p.(*ufspb.OwnershipData)
+		if got.Name != expectedName {
+			t.Errorf("PutOwnershipData returned unexpected name:\n%s", got.Name)
+		}
+		if pm.PoolName != updated_ownershipData.PoolName {
+			t.Errorf("PutOwnershipData returned unexpected result for Pool Name:\n%v", pm.PoolName)
+		}
+		if pm.Customer != updated_ownershipData.Customer {
+			t.Errorf("PutOwnershipData returned unexpected result for Customer:\n%v", pm.Customer)
+		}
+		if pm.SecurityLevel != updated_ownershipData.SecurityLevel {
+			t.Errorf("PutOwnershipData returned unexpected result for Pool Name:\n%v", pm.SecurityLevel)
+		}
+		if pm.SwarmingInstance != updated_ownershipData.SwarmingInstance {
+			t.Errorf("PutOwnershipData returned unexpected result for Pool Name:\n%v", pm.SwarmingInstance)
+		}
+	})
+
+	t.Run("add empty bot name", func(t *testing.T) {
+		_, err := PutOwnershipData(ctx, &ufspb.OwnershipData{}, "", "")
+		if err == nil {
+			t.Errorf("PutOwnershipData succeeded with empty name")
+		}
+		if c := status.Code(err); c != codes.Internal {
+			t.Errorf("Unexpected error when calling PutOwnershipData: %s", err)
+		}
+	})
+}
+
+// Tests the functionality for getting Ownership data per bot from the DataStore
+func TestGetOwnershipData(t *testing.T) {
+	t.Parallel()
+	ctx := gaetesting.TestingContextWithAppID("go-test")
+	datastore.GetTestable(ctx).Consistent(true)
+
+	t.Run("get OwnershipData by existing ID", func(t *testing.T) {
+		ownershipData := &ufspb.OwnershipData{
+			PoolName:         "pool1",
+			SwarmingInstance: "test-swarming",
+			SecurityLevel:    "untrusted",
+			Customer:         "browser",
+		}
+		assetType := "machine"
+		expectedName := "test"
+		_, err := PutOwnershipData(ctx, ownershipData, expectedName, assetType)
+		if err != nil {
+			t.Fatalf("PutOwnershipData failed: %s", err)
+		}
+
+		got, err := GetOwnershipData(ctx, expectedName)
+		if err != nil {
+			t.Fatalf("GetOwnershipData failed: %s", err)
+		}
+		if got.Name != expectedName {
+			t.Errorf("GetOwnershipData returned unexpected Name:\n%s", got.Name)
+		}
+		p, err := got.GetProto()
+		if err != nil {
+			t.Fatalf("Unmarshalling ownership data from datatstore failed: %s", err)
+		}
+		pm := p.(*ufspb.OwnershipData)
+
+		if pm.PoolName != ownershipData.PoolName {
+			t.Errorf("PutOwnershipData returned unexpected result for Pool Name:\n%v", pm.PoolName)
+		}
+		if pm.Customer != ownershipData.Customer {
+			t.Errorf("PutOwnershipData returned unexpected result for Customer:\n%v", pm.Customer)
+		}
+		if pm.SecurityLevel != ownershipData.SecurityLevel {
+			t.Errorf("PutOwnershipData returned unexpected result for Pool Name:\n%v", pm.SecurityLevel)
+		}
+		if pm.SwarmingInstance != ownershipData.SwarmingInstance {
+			t.Errorf("PutOwnershipData returned unexpected result for Pool Name:\n%v", pm.SwarmingInstance)
+		}
+	})
+
+	t.Run("get OwnershipData by non-existent ID", func(t *testing.T) {
+		const expectedName = "test2"
+		_, err := GetOwnershipData(ctx, expectedName)
+		if err == nil {
+			t.Errorf("GetOwnershipData succeeded with non-existent ID: %s", expectedName)
+		}
+		if c := status.Code(err); c != codes.NotFound {
+			t.Errorf("Unexpected error when calling GetOwnershipData: %s", err)
+		}
+	})
+}
