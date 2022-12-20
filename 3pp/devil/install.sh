381b98827875cb2b644dd0b9e6a09e6a8a27461d
Haiyang Pan <hypan@google.com>
2022-12-20 23:29:07 +0000

    Add 3pp package for catapult/devil.
    devil is a library to interact with Android devices and lives under
    catapult project.
    We package the minimum amount of files that are required to run
    standalong devil in this 3pp package.
    
    Bug: 1379427
    Change-Id: I419084f1ef267610c0603ab1b622a5e6d040139e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4062315
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Reviewed-by: Brian Ryner <bryner@google.com>
    Cr-Commit-Position: refs/heads/main@{#53654}

---
+++ 3pp/devil/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright 2022 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+set -x
+set -o pipefail
+
+PREFIX="$1"
+
+# All of devil along with its dependencies in catapult.
+declare -a target_dirs=(
+  "common/py_utils"
+  "dependency_manager"
+  "devil"
+  "third_party/gsutil"
+)
+
+for target_dir in "${target_dirs[@]}"; do
+  cp -rf  "$target_dir" "$PREFIX"/
+done
