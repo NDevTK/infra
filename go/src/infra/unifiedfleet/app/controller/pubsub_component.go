bf0f5eeb0a8dc7c9ca49bd26f26d2b801740020f
Julio Hurtado <juahurta@google.com>
2022-12-21 20:40:05 +0000

    UFS: add more and clearer logging for pubsub experiment.
    BUG=b:220373240
    TEST=N/A
    
    Change-Id: I2d6256ea016b3ee18d2579e056c0be7a3c8db0bb

--- go/src/infra/unifiedfleet/app/controller/pubsub_component.go
+++ go/src/infra/unifiedfleet/app/controller/pubsub_component.go
@@ -16,10 +16,11 @@ import (
 // createPubSubTopicClient returns back an instance of a Pub/Sub client for the
 // given target.
 func createPubSubTopicClient(ctx context.Context, topicID string) (*pubsub.Topic, error) {
+	logging.Debugf(ctx, "pubsub_stream: Creating topic %s for pubsub publishing.", topicID)
 	// Create client for Pub/Sub publishing.
 	client := external.GetPubSub(ctx)
 	if client == nil {
-		return nil, fmt.Errorf("Pubsub client is nil.")
+		return nil, fmt.Errorf("pubsub_stream: Pubsub client is nil.")
 	}
 
 	// Associate the Pub/Sub client with the correct topicID.
@@ -28,23 +29,23 @@ func createPubSubTopicClient(ctx context.Context, topicID string) (*pubsub.Topic
 	// Check if the topic exists on the project.go test.
 	exists, err := topic.Exists(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("Error when checking if topic %s exist error: %s", topicID, err.Error())
+		return nil, fmt.Errorf("pubsub_stream: Error when checking if topic %s exist error: %s", topicID, err.Error())
 	}
 
 	// Attempt to create the topic if it doesn't exist.
 	if !exists {
 		topic, err = client.CreateTopic(ctx, topicID)
 		if err != nil {
-			return nil, fmt.Errorf("Topic %s doesn't exist and failed to be created. error: %s", topicID, err.Error())
+			return nil, fmt.Errorf("pubsub_stream: Topic %s doesn't exist and failed to be created. error: %s", topicID, err.Error())
 		}
 	}
-
+	logging.Debugf(ctx, "pubsub_stream: Topic for %s successfully made.", topicID)
 	return topic, nil
 }
 
 // publish wraps all the steps required to send a message to a Pub/Sub topic.
 func publish(ctx context.Context, topicId string, msgs [][]byte) error {
-	logging.Debugf(ctx, "Pubsub: attempting to publish %d messages", len(msgs))
+	logging.Debugf(ctx, "pubsub_stream: attempting to publish %d messages.", len(msgs))
 	topic, err := createPubSubTopicClient(ctx, topicId)
 	if err != nil {
 		return err
@@ -60,6 +61,7 @@ func publish(ctx context.Context, topicId string, msgs [][]byte) error {
 	}
 
 	// Block and wait to check for publishing errors.
+	logging.Debugf(ctx, "pubsub_stream: waiting on results from %d messages.", len(msgs))
 	for _, result := range results {
 		_, err = result.Get(ctx)
 		// Rather than flood errors, fail fast.
@@ -68,6 +70,6 @@ func publish(ctx context.Context, topicId string, msgs [][]byte) error {
 		}
 	}
 
-	logging.Debugf(ctx, "Pubsub: published %d messages", len(results))
+	logging.Debugf(ctx, "pubsub_stream: published %d messages.", len(results))
 	return nil
 }
