e4cfbcdd9a49081b3d4ecc035f75835787734bfc
Tatsuhisa Yamaguchi <yamaguchi@google.com>
2023-01-20 12:05:29 +0000

    Add unit test for rate_counter.
    This is a folow-up of crrev.com/c/4121816.
    Add a unit test for rate_counter.go introduced by the previous change.
    
    BUG=b:261523033
    TEST=go test ./...
    
    Change-Id: I965ba0b37a4a92801027995e42a067bf37be2d48
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4160492
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Reviewed-by: Greg Edelston <gredelston@google.com>
    Cr-Commit-Position: refs/heads/main@{#54067}

---
+++ go/src/infra/cros/cmd/upload_debug_symbols/rate_counter_test.go
@@ -0,0 +1,34 @@
+// Copyright 2023 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestRateCounter(t *testing.T) {
+	t0 := time.Date(2022, 12, 22, 0, 0, 0, 0, time.UTC)
+	r := NewRateCounter(10 * time.Second)
+
+	verify := func(elapsed_seconds, expected int) {
+		if n := r.GetRate(t0.Add(time.Duration(elapsed_seconds) * time.Second)); n != expected {
+			t.Errorf("GetRate(at %ds)=%d, want %d", elapsed_seconds, n, expected)
+		}
+	}
+
+	verify(1, 0)
+
+	r.Add(t0.Add(time.Second))
+	r.Add(t0.Add(2 * time.Second))
+	r.Add(t0.Add(4 * time.Second))
+	r.Add(t0.Add(8 * time.Second))
+	verify(8, 4)
+
+	r.Add(t0.Add(16 * time.Second))
+	verify(16, 2)
+	verify(19, 1)
+	verify(27, 0)
+}
