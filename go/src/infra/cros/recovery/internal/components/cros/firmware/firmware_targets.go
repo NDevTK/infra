2c17b4d2e033a274546890b75a7aa6c9d6050ec7
Sam McNally <sammc@chromium.org>
2023-02-01 00:32:36 +0000

    recovery: Add firmware overwrite target for more yaviks(o) UFS devices
    BUG=b:266006526
    TEST=go test ./...
    
    Change-Id: I31a8510a597129b3abca78cc76a6f57784163621
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4206733
    Reviewed-by: Garry Wang <xianuowang@chromium.org>
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#54308}

--- go/src/infra/cros/recovery/internal/components/cros/firmware/firmware_targets.go
+++ go/src/infra/cros/recovery/internal/components/cros/firmware/firmware_targets.go
@@ -49,6 +49,9 @@ var targetOverridebyHwid = map[string]string{
 	"NIRWEN-ZZCR B2B-B2A-B2A-W7H":         "nivviks_ufs",
 	"NEREID-ZZCR A4B-B2C-E3E-F2A-A2B-48T": "nereid_hdmi",
 	"YAVIKS-BVSW B3B-A2C-B2A-Q6W-I3T":     "yaviks_ufs",
+	"YAVIKS-BVSW C4B-C2C-C3B-H4H-66N":     "yaviks_ufs",
+	"YAVIKSO-OFRL C4C-C2F-D3B-W3P-N4E":    "yaviks_ufs",
+	"YAVIKSO-OFRL C4C-B2D-D3B-Q3E-X66":    "yaviks_ufs",
 }
 
 // ecExemptedModels holds a map of models that doesn't have EC firmware.
