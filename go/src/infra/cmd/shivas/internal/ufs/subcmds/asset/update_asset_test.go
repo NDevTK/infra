92c847a94e9f04909b7bcc70af64a0868c51a874
Eli <elijahtrexler@google.com>
2023-02-01 21:43:22 +0000

    Migrate `asset` commands to new namespace checks
    Add/Delete/Update/Get are straightforward. Rename was more complex due
    to how these commands are generated.
    
    Test Plan=manual, unit tests
    BUG=b/267461567
    
    Change-Id: Id33183eeb24af1ca4c6f6b82dc5a95660381a554

---
+++ go/src/infra/cmd/shivas/internal/ufs/subcmds/asset/update_asset_test.go
@@ -0,0 +1,65 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package asset
+
+import (
+	"testing"
+
+	ufsUtil "infra/unifiedfleet/app/util"
+)
+
+// TestUpdateAssetNamespace tests the output of getNamespace to ensure the
+// namespace is correctly transformed and/or an error is thrown
+func TestUpdateAssetNamespace(t *testing.T) {
+	tests := []struct {
+		name              string
+		passedNamespace   string
+		expectedNamespace string
+		expectedErr       bool
+	}{
+		{
+			"if no namespace passed, default to OS",
+			"",
+			ufsUtil.OSNamespace,
+			false,
+		},
+		{
+			"if namespace passed, use that",
+			ufsUtil.OSPartnerNamespace,
+			ufsUtil.OSPartnerNamespace,
+			false,
+		},
+		{
+			"if invalid namespace passed, throw error regardless",
+			"fake",
+			"fake",
+			true,
+		},
+		{
+			"ensure browser namespace is invalid",
+			ufsUtil.BrowserNamespace,
+			ufsUtil.BrowserNamespace,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := updateAsset{}
+			c.envFlags.Register(&c.Flags)
+			err := c.GetFlags().Set("namespace", tt.passedNamespace)
+			if err != nil {
+				t.Errorf("Error setting namespace: %s", err)
+			}
+
+			ns, err := c.getNamespace()
+			if ns != tt.expectedNamespace {
+				t.Errorf("Expected namespace: %s, got namespace: %s", tt.expectedNamespace, ns)
+			}
+			if (err != nil) != tt.expectedErr {
+				t.Errorf("Expected error: %t, got error: %t", (err != nil), tt.expectedErr)
+			}
+		})
+	}
+}
