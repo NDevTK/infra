6f1a7571cd2ad8e53394641627285251c83979d9
Brian Ryner <bryner@google.com>
2023-02-13 04:51:56 +0000

    Fix missing clock_gettime symbol in zstd on linux-armv6.
    Bug: 1415180
    Change-Id: I6369201021c8d205bf11c71cdd78124f77ba48ee
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4241459
    Reviewed-by: Chenlin Fan <fancl@chromium.org>
    Commit-Queue: Brian Ryner <bryner@google.com>
    Auto-Submit: Brian Ryner <bryner@google.com>
    Cr-Commit-Position: refs/heads/main@{#54625}

--- 3pp/libzstd/install.sh
+++ 3pp/libzstd/install.sh
@@ -9,4 +9,10 @@ set -o pipefail
 
 PREFIX="$1"
 
+# clock_gettime() moved to libc in glibc 2.17, however our linux-armv6
+# image is on a much older glibc 2.13 that has it in -lrt.
+if [[ $_3PP_PLATFORM == linux-armv6l ]]; then
+  export LDLIBS=-lrt
+fi
+
 make -j $(nproc) install prefix="$PREFIX"
