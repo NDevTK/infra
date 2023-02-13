721d69e90e6325564f0e6e71601af6c6a8397a18
Chenlin Fan <fancl@chromium.org>
2023-02-13 01:56:46 +0000

    vpython_ng: add vpython3
    Add vpython3 variant for python3 only tasks.
    
    Bug: 1334893
    Change-Id: I1f1f97f23c43acd5ccb8cc6a9c19521cc098958b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4235718
    Reviewed-by: Brian Ryner <bryner@google.com>
    Commit-Queue: Brian Ryner <bryner@google.com>
    Auto-Submit: Chenlin Fan <fancl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#54621}

---
+++ go/src/infra/tools/vpython_ng/cmd/vpython3/main.go
@@ -0,0 +1,122 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"infra/tools/vpython_ng/pkg/application"
+	"infra/tools/vpython_ng/pkg/python"
+	"infra/tools/vpython_ng/pkg/wheels"
+
+	"go.chromium.org/luci/common/errors"
+)
+
+type PythonRuntime struct {
+	Version     string
+	Executable  string
+	CIPDName    string
+	SpecPattern string
+	Virtualenv  string
+}
+
+func GetPythonRuntime(ver string) *PythonRuntime {
+	switch ver {
+	case "3.8":
+		return &PythonRuntime{
+			Version:     "3.8",
+			Executable:  "python3",
+			CIPDName:    "cpython3",
+			SpecPattern: ".vpython3",
+			Virtualenv:  "version:2@16.7.12.chromium.7",
+		}
+	default:
+		return &PythonRuntime{
+			Version:     ver,
+			Executable:  "python3",
+			CIPDName:    "cpython3",
+			SpecPattern: ".vpython3",
+			Virtualenv:  "version:2@20.17.1.chromium.8",
+		}
+	}
+}
+
+func main() {
+	rt := GetPythonRuntime("3.8")
+
+	app := application.Application{
+		PruneThreshold:    7 * 24 * time.Hour, // One week.
+		MaxPrunesPerSweep: 3,
+
+		Environments: os.Environ(),
+		Arguments:    os.Args[1:],
+	}
+	app.Initialize()
+
+	app.Must(app.ParseEnvs())
+	app.Must(app.ParseArgs())
+
+	if app.Bypass {
+		// no-op for tool mode if we are bypassing vpython
+		if app.ToolMode != "" {
+			return
+		}
+		app.PythonExecutable = rt.Executable
+		app.Must(app.ExecutePython())
+		return
+	}
+
+	app.Must(app.LoadSpec())
+
+	// Update the Python Runtime based on vpython spec, if specified.
+	if v := app.VpythonSpec.PythonVersion; v != "" {
+		rt = GetPythonRuntime(v)
+	}
+
+	bundle := rt.Version
+	if app.InterpreterPath != "" {
+		bundle = app.InterpreterPath
+	}
+	cpython, err := python.CPythonFromPath(bundle, rt.CIPDName)
+	if err != nil {
+		app.Fatal(err)
+	}
+	app.PythonExecutable = rt.Executable
+
+	env := python.Environment{
+		Executable: rt.Executable,
+		CPython:    cpython,
+		Virtualenv: python.VirtualenvFromCIPD(rt.Virtualenv),
+	}
+	wheel, err := wheels.FromSpec(app.VpythonSpec, env.Pep425Tags())
+	if err != nil {
+		app.Fatal(err)
+	}
+	venv := env.WithWheels(wheel)
+
+	if !app.Help && app.ToolMode != "" {
+		app.Must(func() error {
+			switch app.ToolMode {
+			case "install":
+				app.PruneThreshold = 0
+				return app.BuildVENV(venv)
+			case "verify":
+				return wheels.Verify(app.VpythonSpec)
+			default:
+				return errors.Reason("unknown -vpython-tool command: %s", app.ToolMode).Err()
+			}
+		}())
+		return
+	}
+
+	if app.Help {
+		// Continue to execute python to print its help message after vpython's.
+		fmt.Println(app.Usage)
+	}
+	app.Must(app.BuildVENV(venv))
+	app.Must(app.ExecutePython())
+}
