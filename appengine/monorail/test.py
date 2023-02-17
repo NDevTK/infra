a5984d854ef3dce4bc70e3f2291ff0837c70b1bf
Dave Tu <dtu@google.com>
2023-02-16 16:54:06 -0800

    🚝 Add Python 3 test script.
    To run:
    $ make pytest
    OR
    $ python3 -m test
    
    This Python script allows us to ensure the imports and environment
    variables are correct before starting the unit tests.
    
    Bug: monorail:8285
    Change-Id: I198783514916c09c1c9dcc05aa662507ed3f886c

---
+++ appengine/monorail/test.py
@@ -0,0 +1,25 @@
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Sets up Monorail's test environment and runs tests."""
+
+import os
+import sys
+
+import pytest
+
+# gae_ts_mon's __init__.py does some import magic to create an infra_libs
+# package, so we need to import it before importing Monorail packages.
+import gae_ts_mon
+
+import import_utils
+
+if __name__ == '__main__':
+  os.environ['GAE_RUNTIME'] = 'python3'
+  os.environ['GAE_APPLICATION'] = 'testbed-test'
+  os.environ['SERVER_SOFTWARE'] = 'test'
+
+  import_utils.FixImports()
+
+  args = ['--ignore', 'components', '--ignore', 'gae_ts_mon'] + sys.argv[1:]
+  pytest.main(args)
