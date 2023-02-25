eba04bb8212b5996adbb32c029a8eb60b3c496c7
Ming Kong <mingkong@google.com>
2023-02-25 06:52:22 +0000

    vmlab: implement image API to unblock test runner v2 integration work
    This CL partially implements go/cros-image-importer
    The code might be used for go/cros-vm-cloud-native-image-sync
    
    BUG=b:250958383
    TEST=unit test
    
    Change-Id: Idf7e6830db1ba45400708e8672187515ef313a13

---
+++ go/src/infra/libs/vmlab/internal/image/cloudsdk/cloudsdk.go
@@ -0,0 +1,204 @@
+package cloudsdk
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	compute "cloud.google.com/go/compute/apiv1"
+	"cloud.google.com/go/compute/apiv1/computepb"
+	"github.com/googleapis/gax-go/v2"
+	"infra/libs/vmlab/api"
+)
+
+const (
+	project      = "betty-cloud-prototype"
+	license      = "https://www.googleapis.com/compute/v1/projects/vm-options/global/licenses/enable-vmx"
+	timeout      = 10 * time.Minute
+	pollInterval = 30 * time.Second
+)
+
+// cloudsdkImageApi implementation api.ImageApi using Cloud SDK.
+// go/cros-image-importer
+type cloudsdkImageApi struct{}
+
+// New constructs a new api.ImageApi with CloudSDK backend.
+func New() (api.ImageApi, error) {
+	return &cloudsdkImageApi{}, nil
+}
+
+// computeImagesClient interfaces partial GCE client API
+type computeImagesClient interface {
+	Get(context.Context, *computepb.GetImageRequest, ...gax.CallOption) (*computepb.Image, error)
+	Insert(context.Context, *computepb.InsertImageRequest, ...gax.CallOption) (*compute.Operation, error)
+}
+
+func (c *cloudsdkImageApi) GetImage(buildPath string, wait bool) (*api.GceImage, error) {
+	imageName, err := convertName(buildPath)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to convert image name from GCS to GCE format: %v", err)
+	}
+
+	gceImage := &api.GceImage{
+		Project: project,
+		Name:    imageName,
+		Source:  getGcsImagePath(buildPath),
+	}
+
+	var ctx = context.Background()
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	client, err := compute.NewImagesRESTClient(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("NewImagesRESTClient: %v", err)
+	}
+	defer client.Close()
+
+	op, gceImage, _ := c.handle(client, gceImage)
+
+	if wait {
+		if op != nil {
+			_ = op.Wait(ctx)
+		} else {
+			return c.poll(ctx, client, gceImage)
+		}
+	}
+	return c.describeImage(client, gceImage)
+}
+
+// handle retrieves image info, and imports the image when not found.
+func (c *cloudsdkImageApi) handle(client computeImagesClient, gceImage *api.GceImage) (*compute.Operation, *api.GceImage, error) {
+	gceImage, _ = c.describeImage(client, gceImage)
+
+	if gceImage.GetStatus() == api.GceImage_NOT_FOUND {
+		op, err := c.importImage(client, gceImage)
+		if err == nil {
+			gceImage.Status = api.GceImage_PENDING
+		}
+		return op, gceImage, err
+	}
+
+	return nil, gceImage, nil
+}
+
+// poll retrieves image info periodically until error, success, or timeout.
+func (c *cloudsdkImageApi) poll(ctx context.Context, client computeImagesClient, gceImage *api.GceImage) (*api.GceImage, error) {
+	return gceImage, poll(ctx, func(ctx context.Context) (bool, error) {
+		_, gceImage, err := c.handle(client, gceImage)
+		if gceImage.GetStatus() == api.GceImage_READY {
+			return true, err
+		}
+		if err != nil {
+			return false, err
+		}
+		return false, nil
+	}, pollInterval)
+}
+
+// describeImage detects the readiness of the given GCE image: READ, PENDING, or
+// NOT_FOUND, which means import is needed.
+func (c *cloudsdkImageApi) describeImage(client computeImagesClient, gceImage *api.GceImage) (*api.GceImage, error) {
+	ctx := context.Background()
+
+	// Create the gceImage
+	req := &computepb.GetImageRequest{
+		Image:   gceImage.Name,
+		Project: gceImage.Project,
+	}
+	i, err := client.Get(ctx, req)
+
+	gceImage.Status = api.GceImage_NOT_FOUND
+	if i != nil {
+		switch i.GetStatus() {
+		case "READY":
+			gceImage.Status = api.GceImage_READY
+		case "PENDING":
+			gceImage.Status = api.GceImage_PENDING
+		}
+	}
+
+	return gceImage, err
+}
+
+// importImage imports an image from GCS into GCE
+func (c *cloudsdkImageApi) importImage(client computeImagesClient, image *api.GceImage) (*compute.Operation, error) {
+	ctx := context.Background()
+
+	// Create the image
+	req := &computepb.InsertImageRequest{
+		ImageResource: &computepb.Image{
+			Licenses: []string{license},
+			Name:     &image.Name,
+			RawDisk: &computepb.RawDisk{
+				Source: &image.Source,
+			},
+		},
+		Project: image.Project,
+	}
+
+	return client.Insert(ctx, req)
+}
+
+// convertName constructs GCE image name from the build path in GCS.
+// buildPath is the path between "chromeos-image-archive/" and "/chromiumos_test_image_gce.tar.gz"
+// as found in autotest_keyvals in CtrRequest. Examples:
+// betty-arc-r-cq/R108-15164.0.0-71927-8801111609984657185
+// betty-arc-r-release/R108-15178.0.0
+// The image name convention is to swap the order of two directories, rejoin
+// with --, replace any . and _ with -, in lowercase and maximum length of 63.
+// After conversion, the above examples would become:
+// r108-15164-0-0-71927-8801111609984657185--betty-arc-r-cq
+// r108-15178-0-0--betty-arc-r-release
+func convertName(buildPath string) (string, error) {
+	// Split the source build path
+	bucket, build, found := strings.Cut(buildPath, "/")
+	if !found {
+		return "", fmt.Errorf("Invalid build path format: %s", buildPath)
+	}
+
+	// Switch order of build and bucket
+	imageName := fmt.Sprintf("%s--%s", build, bucket)
+
+	// Shorten name
+	if len(imageName) > 63 {
+		imageName = imageName[0:62]
+	}
+
+	// Convert to legit GCE name
+	imageName = strings.ToLower(imageName)
+	imageName = strings.ReplaceAll(imageName, ".", "-")
+	imageName = strings.ReplaceAll(imageName, "_", "-")
+
+	return imageName, nil
+}
+
+// getGcsImagePath returns the full path of the tarball on GCS.
+func getGcsImagePath(buildPath string) string {
+	return fmt.Sprintf("https://storage.googleapis.com/chromeos-image-archive/%s/chromiumos_test_image_gce.tar.gz", buildPath)
+}
+
+// poll provides a generic implementation of calling f at interval, exits on
+// error or ctx timeout. f return true to end poll early.
+func poll(ctx context.Context, f func(context.Context) (bool, error), interval time.Duration) error {
+	if _, ok := ctx.Deadline(); !ok {
+		return errors.New("context must have a deadline to avoid infinite polling")
+	}
+	ticker := time.NewTicker(interval)
+	for {
+		select {
+		case <-ticker.C:
+			success, err := f(ctx)
+			if err != nil {
+				return err
+			}
+			if success {
+				return nil
+			}
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+}
---
+++ go/src/infra/libs/vmlab/internal/image/cloudsdk/cloudsdk_test.go
@@ -0,0 +1,337 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cloudsdk
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	compute "cloud.google.com/go/compute/apiv1"
+	"cloud.google.com/go/compute/apiv1/computepb"
+	"github.com/googleapis/gax-go/v2"
+	"go.chromium.org/luci/common/errors"
+	"infra/libs/vmlab/api"
+)
+
+type mockImageClient struct {
+	getFunc    func() (*computepb.Image, error)
+	importFunc func() (*compute.Operation, error)
+}
+
+func (m *mockImageClient) Get(ctx context.Context, req *computepb.GetImageRequest, opts ...gax.CallOption) (*computepb.Image, error) {
+	return m.getFunc()
+}
+
+func (m *mockImageClient) Insert(context.Context, *computepb.InsertImageRequest, ...gax.CallOption) (*compute.Operation, error) {
+	return m.importFunc()
+}
+
+func TestDescribeImageError(t *testing.T) {
+	imageApi := &cloudsdkImageApi{}
+	client := &mockImageClient{getFunc: func() (*computepb.Image, error) {
+		return nil, errors.New("404")
+	}}
+	gceImage := &api.GceImage{
+		Name:    "my-image",
+		Project: "my-project",
+	}
+
+	_, err := imageApi.describeImage(client, gceImage)
+
+	if err == nil {
+		t.Errorf("describeImage() expected error, got nil")
+	}
+	if gceImage.Status != api.GceImage_NOT_FOUND {
+		t.Errorf("describeImage() expected status api.GceImage_NOT_FOUND, got %v", gceImage.Status)
+	}
+}
+
+func TestDescribeImageReady(t *testing.T) {
+	imageApi := &cloudsdkImageApi{}
+	client := &mockImageClient{getFunc: func() (*computepb.Image, error) {
+		status := "READY"
+		i := &computepb.Image{
+			Status: &status,
+		}
+		return i, nil
+	}}
+	gceImage := &api.GceImage{
+		Name:    "my-image",
+		Project: "my-project",
+	}
+
+	gceImage, err := imageApi.describeImage(client, gceImage)
+
+	if err != nil {
+		t.Errorf("describeImage() expected nil error, got %v", err)
+	}
+	if gceImage.Status != api.GceImage_READY {
+		t.Errorf("describeImage() expected status api.GceImage_READY, got %v", gceImage.Status)
+	}
+}
+
+func TestDescribeImagePending(t *testing.T) {
+	imageApi := &cloudsdkImageApi{}
+	client := &mockImageClient{getFunc: func() (*computepb.Image, error) {
+		status := "PENDING"
+		i := &computepb.Image{
+			Status: &status,
+		}
+		return i, nil
+	}}
+	gceImage := &api.GceImage{
+		Name:    "my-image",
+		Project: "my-project",
+	}
+
+	gceImage, err := imageApi.describeImage(client, gceImage)
+
+	if err != nil {
+		t.Errorf("describeImage() expected nil error, got %v", err)
+	}
+	if gceImage.Status != api.GceImage_PENDING {
+		t.Errorf("describeImage() expected status api.GceImage_PENDING, got %v", gceImage.Status)
+	}
+}
+
+func TestHandleReady(t *testing.T) {
+	imageApi := &cloudsdkImageApi{}
+	client := &mockImageClient{getFunc: func() (*computepb.Image, error) {
+		status := "READY"
+		i := &computepb.Image{
+			Status: &status,
+		}
+		return i, nil
+	}}
+	gceImage := &api.GceImage{
+		Name:    "my-image",
+		Project: "my-project",
+	}
+
+	op, gceImage, err := imageApi.handle(client, gceImage)
+
+	if op != nil {
+		t.Errorf("handle() expected nil operation, got %v", op)
+	}
+	if err != nil {
+		t.Errorf("handle() expected nil error, got %v", err)
+	}
+	if gceImage.Status != api.GceImage_READY {
+		t.Errorf("handle() expected status api.GceImage_READY, got %v", gceImage.Status)
+	}
+}
+
+func TestHandlePending(t *testing.T) {
+	imageApi := &cloudsdkImageApi{}
+	client := &mockImageClient{
+		getFunc: func() (*computepb.Image, error) {
+			status := "PENDING"
+			i := &computepb.Image{
+				Status: &status,
+			}
+			return i, nil
+		},
+	}
+	gceImage := &api.GceImage{
+		Name:    "my-image",
+		Project: "my-project",
+	}
+
+	_, gceImage, err := imageApi.handle(client, gceImage)
+
+	if err != nil {
+		t.Errorf("handle() expected nil error, got %v", err)
+	}
+	if gceImage.Status != api.GceImage_PENDING {
+		t.Errorf("handle() expected status api.GceImage_PENDING, got %v", gceImage.Status)
+	}
+}
+
+func TestHandleImportImage(t *testing.T) {
+	imageApi := &cloudsdkImageApi{}
+	expected := &compute.Operation{}
+	client := &mockImageClient{
+		getFunc: func() (*computepb.Image, error) {
+			return nil, errors.New("404")
+		},
+		importFunc: func() (*compute.Operation, error) {
+			return expected, nil
+		},
+	}
+	gceImage := &api.GceImage{
+		Name:    "my-image",
+		Project: "my-project",
+	}
+
+	op, gceImage, err := imageApi.handle(client, gceImage)
+
+	if op != expected {
+		t.Errorf("handle() expected operation %v, got %v", expected, op)
+	}
+	if err != nil {
+		t.Errorf("handle() expected nil error, got %v", err)
+	}
+	if gceImage.Status != api.GceImage_PENDING {
+		t.Errorf("handle() expected status api.GceImage_PENDING, got %v", gceImage.Status)
+	}
+}
+
+func TestHandleImportError(t *testing.T) {
+	imageApi := &cloudsdkImageApi{}
+	client := &mockImageClient{
+		getFunc: func() (*computepb.Image, error) {
+			return nil, errors.New("404")
+		},
+		importFunc: func() (*compute.Operation, error) {
+			return nil, errors.New("Outage")
+		},
+	}
+	gceImage := &api.GceImage{
+		Name:    "my-image",
+		Project: "my-project",
+	}
+
+	_, gceImage, err := imageApi.handle(client, gceImage)
+
+	if err == nil {
+		t.Errorf("handle() expected error, got nil")
+	}
+	if gceImage.Status != api.GceImage_NOT_FOUND {
+		t.Errorf("handle() expected status api.GceImage_NOT_FOUND, got %v", gceImage.Status)
+	}
+}
+
+func TestConvertNameInvalid(t *testing.T) {
+	buildPath := "R108-15164.0.0-71927-8801111609984657185"
+
+	_, err := convertName(buildPath)
+
+	if err == nil {
+		t.Errorf("convertName() expected error, got nil")
+	}
+}
+
+func TestConvertNameCq(t *testing.T) {
+	buildPath := "betty-arc-r-cq/R108-15164.0.0-71927-8801111609984657185"
+	expected, err := convertName(buildPath)
+	if err != nil {
+		t.Errorf("convertName() error: %v", err)
+	}
+
+	actual := "r108-15164-0-0-71927-8801111609984657185--betty-arc-r-cq"
+
+	if expected != actual {
+		t.Errorf("convertName() expected: %s, actual: %s", expected, actual)
+	}
+}
+
+func TestConvertNameRelease(t *testing.T) {
+	buildPath := "betty-arc-r-release/R108-15178.0.0"
+	expected, err := convertName(buildPath)
+	if err != nil {
+		t.Errorf("convertName() error: %v", err)
+	}
+
+	actual := "r108-15178-0-0--betty-arc-r-release"
+
+	if expected != actual {
+		t.Errorf("convertName() expected: %s, actual: %s", expected, actual)
+	}
+}
+
+func TestConvertNameLong(t *testing.T) {
+	buildPath := "betty-arc-r-postsubmit-main/R108-15164.0.0-71927-8801111609984657185"
+	expected, err := convertName(buildPath)
+	if err != nil {
+		t.Errorf("convertName() error: %v", err)
+	}
+
+	actual := "r108-15164-0-0-71927-8801111609984657185--betty-arc-r-postsubm"
+
+	if expected != actual {
+		t.Errorf("convertName() expected: %s, actual: %s", expected, actual)
+	}
+}
+
+func TestPollCountAndTimeout(t *testing.T) {
+	expected := 4
+	count := 1
+	ctx, cancel := context.WithTimeout(context.Background(), 310*time.Millisecond)
+	defer cancel()
+	f := func(ctx context.Context) (bool, error) {
+		count++
+		return false, nil
+	}
+	interval := 100 * time.Millisecond
+
+	_ = poll(ctx, f, interval)
+	actual := count
+
+	if expected != actual {
+		t.Errorf("count expected: %v, actual: %v", expected, actual)
+	}
+}
+
+func TestPollQuitOnError(t *testing.T) {
+	expected := 2
+	count := 1
+	ctx, cancel := context.WithTimeout(context.Background(), 310*time.Millisecond)
+	defer cancel()
+	f := func(ctx context.Context) (bool, error) {
+		count++
+		if count == 2 {
+			return false, errors.New("error on 2")
+		}
+		return false, nil
+	}
+	interval := 100 * time.Millisecond
+
+	err := poll(ctx, f, interval)
+	actual := count
+
+	if expected != actual {
+		t.Errorf("count expected: %v, actual: %v", expected, actual)
+	}
+	if err == nil {
+		t.Errorf("poll() expected error, got nil")
+	}
+}
+
+func TestPollQuitOnSuccess(t *testing.T) {
+	expected := 3
+	count := 1
+	ctx, cancel := context.WithTimeout(context.Background(), 310*time.Millisecond)
+	defer cancel()
+	f := func(ctx context.Context) (bool, error) {
+		count++
+		if count == 3 {
+			return true, nil
+		}
+		return false, nil
+	}
+	interval := 100 * time.Millisecond
+
+	_ = poll(ctx, f, interval)
+	actual := count
+
+	if expected != actual {
+		t.Errorf("count expected: %v, actual: %v", expected, actual)
+	}
+}
+
+func TestPollNoDeadlineError(t *testing.T) {
+	ctx := context.Background()
+	f := func(ctx context.Context) (bool, error) {
+		return false, nil
+	}
+	interval := time.Duration(1)
+
+	err := poll(ctx, f, interval)
+
+	if err == nil {
+		t.Errorf("poll() expected error, got nil")
+	}
+}
