618d9bdcf73d05ae964e6111067027cfad44c1de
Vadim Shtayura <vadimsh@chromium.org>
2023-02-27 12:33:00 -0800

    [go] Update go to 1.20.1.
    This also updates all GAE apps to go120 runtime. Buildbucket
    and some other services has been running on go1.20 for several
    days and seem fine.
    
    R=iannucci@chromium.org, chanli@chromium.org
    BUG=1417544
    
    Change-Id: Iebc930a9bac514fe9d1de9036e7c8f4481cc67b7

--- go/src/infra/cros/cmd/crosgrep/internal/swarming/query/transform_test.go
+++ go/src/infra/cros/cmd/crosgrep/internal/swarming/query/transform_test.go
@@ -56,6 +56,7 @@ func TestBBRecordRoundTrip(t *testing.T) {
 	}
 
 	for _, testCase := range cases {
+		testCase := testCase
 		t.Run(testCase.name, func(t *testing.T) {
 			t.Parallel()
 			original := testCase.data
