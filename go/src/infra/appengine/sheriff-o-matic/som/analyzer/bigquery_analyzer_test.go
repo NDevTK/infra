06efb9b334fbc9372b1d6eb5e920232af45a6d79
Sven Zheng <svenzheng@chromium.org>
2023-03-07 23:17:27 +0000

    [SoM][Lacros] Remove lacros skylab
    It was set up in crbug.com/1305093. The experimental
    sheriff was done and skylab builders are now monitored
    by chromium sheriff and os gardeners.
    So it's time to remove it from SOM to avoid confusions.
    
    Bug: 1422310
    Change-Id: Ieee808844916472acb3111958fe0bba484d1af86
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4316728
    Reviewed-by: Tuan Nguyen <nqmtuan@google.com>
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#55190}

--- go/src/infra/appengine/sheriff-o-matic/som/analyzer/bigquery_analyzer_test.go
+++ go/src/infra/appengine/sheriff-o-matic/som/analyzer/bigquery_analyzer_test.go
@@ -1446,8 +1446,6 @@ func TestGetFilterFuncForTree(t *testing.T) {
 		So(err, ShouldBeNil)
 		_, err = getFilterFuncForTree("chromium.clang")
 		So(err, ShouldBeNil)
-		_, err = getFilterFuncForTree("lacros_skylab")
-		So(err, ShouldBeNil)
 		_, err = getFilterFuncForTree("another")
 		So(err, ShouldNotBeNil)
 	})
