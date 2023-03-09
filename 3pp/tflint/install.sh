ec44534f6ee1d38b18a721966f402c1a616d77cb
Mike Frysinger <vapier@chromium.org>
2023-03-09 16:59:44 +0000

    3pp: Update license boilerplate text in source code files
    Normally we don't do this, but enough changes have accumulated that
    we're doing a tree-wide one-off update of the name & style.
    
    Bug: 1098010
    Change-Id: Ica9db80f854e529ba97fa094e1e7c45fecab688a
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4322086
    Reviewed-by: Brian Ryner <bryner@google.com>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#55268}

--- 3pp/tflint/install.sh
+++ 3pp/tflint/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2021 The Chromium Authors. All rights reserved.
+# Copyright 2021 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -26,7 +26,7 @@ PREFIX=$(realpath "$1")
     mkdir "${PREFIX}"/plugins
     cat <<EOF> "${PREFIX}"/tflint
 #!/usr/bin/env bash
-# Copyright 2021 The Chromium Authors. All rights reserved.
+# Copyright 2021 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
