eb0d01c9dbd53be32e5ac68666064ca6cc66f8ba
Tiffany Zhang <zhangtiff@google.com>
2023-03-17 18:36:25 +0000

    Add Monorail v3 AppScript method to get all components for a project.
    I'd like to make it possible to get all the components for a Monorail project, so that I can write some utility AppScripts thati nteract with components.
    
    Bug: b/273828658
    Change-Id: I00c595ed000d27411116187495b4d96094ffaebf
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4349317
    Reviewed-by: Dave Tu <dtu@chromium.org>
    Commit-Queue: Tiffany Zhang <zhangtiff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#55487}

--- appengine/monorail/api/v3/apps-script-client/ProjectService.js
+++ appengine/monorail/api/v3/apps-script-client/ProjectService.js
@@ -4,6 +4,8 @@
 
 /* eslint-disable no-unused-vars */
 
+const MAX_COMPONENT_PAGE_SIZE = 100;
+
 /**
  * Creates a ComponentDef.
  * @param {string} projectName The resource name of the parent project.
@@ -49,4 +51,32 @@ function deleteComponentDef(componentName) {
   };
   const url = URL + 'monorail.v3.Projects/DeleteComponentDef';
   return run_(url, message);
+}
+
+/**
+ * Lists all ComponentDefs for a project. Automatically traverses through pages
+ * to get all components.
+ * @param {string=} projectName Resource name of the project to fetch components for.
+ * @param {string=} includeDeprecated Where to include deprecated components.
+ * @return {Array<ComponentDef>}
+ */
+function listComponentDefs(projectName = 'projects/chromium', includeDeprecated=false) {
+  const url = URL + 'monorail.v3.Projects/ListComponentDefs';
+  let components = [];
+
+  let response;
+  do {
+    const message = {
+      'parent': projectName,
+      'pageSize': MAX_COMPONENT_PAGE_SIZE,
+      'pageToken': response ? response.nextPageToken : undefined,
+    };
+    response = run_(url, message);
+    components = [...components, ...response.componentDefs];
+  } while (response.nextPageToken);
+
+  if (!includeDeprecated) {
+    components = components.filter((c) => c.state === 'ACTIVE');
+  }
+  return components;
 }
