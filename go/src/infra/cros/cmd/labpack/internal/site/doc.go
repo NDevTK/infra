9e81f8907a2c089f40c16eb28f3b75cd2e9398ff
Greg NISBET <gregorynisbet@google.com>
2023-03-17 21:52:49 +0000

    [staticcheck] Add doc comment to site package
    BUG=None
    TEST=staticcheck
    
    Change-Id: Id1c64d3ba65f482c80a596e795915db8a1180e11
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4348093
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/main@{#55496}

---
+++ go/src/infra/cros/cmd/labpack/internal/site/doc.go
@@ -0,0 +1,10 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package site contains variables and general purpose utilities related to the
+// labpack command line tool.
+//
+// For example, it includes the default user agent and default UFS RPC
+// options.
+package site
