ebdfd48ef8397c5cad39f7573d0c631d5353fc73
Greg NISBET <gregorynisbet@google.com>
2023-03-18 00:43:15 +0000

    [labpack] Add package doc for logger.
    BUG=None
    TEST=make test
    
    Change-Id: I26dd2e45412b44a38ac16a3a1a60814a2f44b776
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4348097
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Cr-Commit-Position: refs/heads/main@{#55501}

---
+++ go/src/infra/cros/cmd/labpack/logger/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package logger creates a log stream for the labpack executable.
+package logger
