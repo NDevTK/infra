60af4181fde6aeacc56b1323152747e61fd598ad
Chris DeLaGarza <cdelagarza@google.com>
2023-03-20 19:43:26 +0000

    cros_test_runner: overwrite logpath with artifacts path
    Ensure that execution requests that contain an artifacts path are respected in server mode.
    
    BUG=b:272380720
    TEST=./cros_test_runner server
    
    Change-Id: Id06f945d68342de32a864ef74482df9d437744b0
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4353076
    Reviewed-by: Jaques Clapauch <jaquesc@google.com>
    Commit-Queue: Chris DeLaGarza <cdelagarza@google.com>
    Cr-Commit-Position: refs/heads/main@{#55530}

--- go/src/infra/cros/cmd/cros_test_runner/service/cros_test_runner_server.go
+++ go/src/infra/cros/cmd/cros_test_runner/service/cros_test_runner_server.go
@@ -112,7 +112,9 @@ func (server *CrosTestRunnerServer) Execute(ctx context.Context, req *skylab_tes
 		return out, fmt.Errorf("failed to create new cros-test-runner service: %s", err)
 	}
 
-	out, err = service.Execute(ctx, server.metadata.LogPath)
+	logPath := path.Join(server.metadata.LogPath, req.ArtifactsPath)
+
+	out, err = service.Execute(ctx, logPath)
 	if err != nil {
 		log.Printf("execution failed: %s", err)
 		return out, fmt.Errorf("execution failed: %s", err)
