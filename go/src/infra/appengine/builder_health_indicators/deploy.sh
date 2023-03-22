063d8de79d27b8fcef7cb8132ad77a7b6e158806
Greg Guterman <guterman@google.com>
2023-03-22 22:44:34 +0000

    Builder health indicators gae app and cron scaffolding
    Bug: 1401558
    Change-Id: I9a4fa83e90b3f375fa1012313880b9d45d28e4d0

---
+++ go/src/infra/appengine/builder_health_indicators/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+gae.py upload --switch --app-id=cr-builder-health-indicators
