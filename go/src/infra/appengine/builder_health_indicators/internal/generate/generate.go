ed1a7bd3b24662ab733d5b6652e770f1270f172a
Greg Guterman <guterman@google.com>
2023-03-23 17:46:49 +0000

    Builder health indicators gae app and cron scaffolding
    Bug: 1401558
    Change-Id: I9a4fa83e90b3f375fa1012313880b9d45d28e4d0

---
+++ go/src/infra/appengine/builder_health_indicators/internal/generate/generate.go
@@ -0,0 +1,13 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package generate
+
+import (
+	"context"
+)
+
+func Generate(ctx context.Context) error {
+	return nil
+}
