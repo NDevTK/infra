6288a35a415b3921c4480e859965341401b92b65
Andrew Lamb <andrewlamb@chromium.org>
2023-03-27 22:52:30 +0000

    build_poller: Don't request all Build fields.
    - This was likely the cause of 500 errors from Buildbucket, e.g.
    https://ci.chromium.org/ui/p/chromeos/builders/cq/cq-orchestrator/b8785811709714084385/overview.
    Only request the default fields + tags, to match the current
    behavior of orch_menu: https://source.chromium.org/chromiumos/chromiumos/codesearch/+/main:infra/recipes/recipe_modules/orch_menu/api.py.
    .
    
    - Also fix small issue with handling the case where `-json` is "-".
    Currently it also creates a file called "-".
    
    BUG=b:261873853
    TEST=CQ
    TEST=Re-ran command that cause the issue. Failed before this CL, passes
    after.
    
    Change-Id: I9a6d91dbccc44e7be18edeed30abdf76b6e50f65

--- go/src/infra/cros/lib/buildbucket/poll.go
+++ go/src/infra/cros/lib/buildbucket/poll.go
@@ -10,6 +10,25 @@ import (
 	"google.golang.org/protobuf/types/known/fieldmaskpb"
 )
 
+// Default fields returned by Buildbucket, should be kept in sync with the set
+// of default fields in https://source.chromium.org/chromium/infra/infra/+/main:recipes-py/recipe_modules/buildbucket/api.py.
+var defaultFields = []string{
+	"builder",
+	"create_time",
+	"created_by",
+	"critical",
+	"end_time",
+	"id",
+	"input",
+	"number",
+	"output",
+	"start_time",
+	"status",
+	"update_time",
+}
+
+var requestFields = append(defaultFields, "tags")
+
 // PollForOutputProp polls until all of buildIds are completed or have set
 // outputProp. Between each call to Buildbucket, the fn. sleeps for interval.
 // This functionality is similar to the `bb collect` command, but allows polling
@@ -24,27 +43,32 @@ func PollForOutputProp(
 	outputProp string,
 	interval time.Duration,
 ) (map[int64]*bbpb.Build, error) {
-	// For simplicity, keep a single map of builds that have either completed or
-	// set the requested output prop, and request all builds in each batch. This
-	// means that if a build goes from running with output prop set ->
-	// completed, the updated build will be put into the map. In the future, we
-	// could keep a separate map of builds that are running with the output prop
-	// set if needed.
-	batchReq := &bbpb.BatchRequest{}
-	for _, id := range buildIds {
-		batchReq.Requests = append(batchReq.Requests, &bbpb.BatchRequest_Request{
-			Request: &bbpb.BatchRequest_Request_GetBuild{
-				GetBuild: &bbpb.GetBuildRequest{
-					Id:     id,
-					Fields: &fieldmaskpb.FieldMask{Paths: []string{"*"}},
-				},
-			},
-		})
-	}
-
+	// Keep two sets of builds, one with builds that are completed, one with
+	// builds that have the output property set, but are not completed. Builds
+	// that are completed will not be requested in later iterations.
 	completedBuilds := make(map[int64]*bbpb.Build)
+	propSetBuilds := make(map[int64]*bbpb.Build)
 
 	for {
+		batchReq := &bbpb.BatchRequest{}
+		for _, id := range buildIds {
+			if _, found := completedBuilds[id]; found {
+				logging.Debugf(ctx, "build %d already completed, not requesting", id)
+				continue
+			}
+
+			batchReq.Requests = append(batchReq.Requests, &bbpb.BatchRequest_Request{
+				Request: &bbpb.BatchRequest_Request_GetBuild{
+					GetBuild: &bbpb.GetBuildRequest{
+						Id: id,
+						// Also request tags, to match the behavior in
+						// https://source.chromium.org/chromiumos/chromiumos/codesearch/+/main:infra/recipes/recipe_modules/orch_menu/api.py.
+						Fields: &fieldmaskpb.FieldMask{Paths: append(defaultFields, "tags")},
+					},
+				},
+			})
+		}
+
 		batchResp, err := client.Batch(ctx, batchReq)
 		if err != nil {
 			return nil, err
@@ -64,19 +88,41 @@ func PollForOutputProp(
 			// If the build has completed or set the output prop, add it to
 			// completedBuilds.
 			if (build.GetStatus() & bbpb.Status_ENDED_MASK) != 0 {
-				logging.Infof(ctx, "build %d completed", build.GetId())
-				completedBuilds[build.GetId()] = build
+				logging.Infof(ctx, "build %d completed", build.Id)
+				completedBuilds[build.Id] = build
+
+				if _, found := propSetBuilds[build.Id]; found {
+					logging.Debugf(ctx, "build %d in propSetBuilds, moved to completedBuilds", build.Id)
+					delete(propSetBuilds, build.Id)
+				}
 			} else if _, containsOutputProp := build.GetOutput().GetProperties().GetFields()[outputProp]; containsOutputProp {
-				logging.Infof(ctx, "build %d has output prop (%q)", build.GetId(), outputProp)
-				completedBuilds[build.GetId()] = build
+				logging.Infof(ctx, "build %d has output prop (%q)", build.Id, outputProp)
+				propSetBuilds[build.Id] = build
 			} else {
-				logging.Infof(ctx, "still waiting for build %d", build.GetId())
+				logging.Infof(ctx, "still waiting for build %d to complete or set output property", build.Id)
 			}
+		}
+
+		// Check that a given build is not in both of the sets, this indicates
+		// an invalid state.
+		for _, id := range buildIds {
+			_, buildInCompleted := completedBuilds[id]
+			_, buildInPropSet := propSetBuilds[id]
+
+			if buildInCompleted && buildInPropSet {
+				panic(fmt.Sprintf("build %d in both completedBuilds and propSetBuilds, invalid state", id))
+			}
+		}
+
+		// Check that the sum of the size of the sets combined is not greater
+		// than the length of buildIds, this indicates an invalid state.
+		if len(completedBuilds)+len(propSetBuilds) > len(buildIds) {
+			panic("sum of builds in completedBuilds and propSetBuilds > buildIds, invalid state")
 		}
 
 		// Once all requested builds are completed or set the output prop,
 		// break. Otherwise sleep for interval.
-		if len(completedBuilds) == len(buildIds) {
+		if len(completedBuilds)+len(propSetBuilds) == len(buildIds) {
 			break
 		}
 
@@ -84,5 +130,12 @@ func PollForOutputProp(
 		time.Sleep(interval)
 	}
 
-	return completedBuilds, nil
+	allBuilds := make(map[int64]*bbpb.Build)
+	for id, build := range completedBuilds {
+		allBuilds[id] = build
+	}
+	for id, build := range propSetBuilds {
+		allBuilds[id] = build
+	}
+	return allBuilds, nil
 }
