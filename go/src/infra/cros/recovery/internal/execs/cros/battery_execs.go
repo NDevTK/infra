bab8ca76098d5d59a01e37dfb8a725d90cab71a7
Otabek Kasimov <otabek@google.com>
2023-04-06 16:48:24 +0000

    recovery: Fix typo in the info message
    BUG=None
    TEST=build
    
    Change-Id: I19dae4b9887522066556dcf5254509c396de0253
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4400254
    Reviewed-by: Gowri Denduluri <gowriden@google.com>
    Commit-Queue: Gowri Denduluri <gowriden@google.com>
    Auto-Submit: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/main@{#55946}

--- go/src/infra/cros/recovery/internal/execs/cros/battery_execs.go
+++ go/src/infra/cros/recovery/internal/execs/cros/battery_execs.go
@@ -35,7 +35,7 @@ func auditBatteryExec(ctx context.Context, info *execs.ExecInfo) error {
 		return errors.Reason("audit battery: dut battery did not detected or state cannot extracted").Err()
 	}
 	if hardwareState == tlw.HardwareState_HARDWARE_NEED_REPLACEMENT {
-		log.Infof(ctx, "Detected issue with storage on the DUT.")
+		log.Infof(ctx, "Detected issue with battery on the DUT.")
 		batteryInfo.State = tlw.HardwareState_HARDWARE_NEED_REPLACEMENT
 		log.Debugf(ctx, "Audit Battery Exec: setting dut state to %s", string(dutstate.NeedsReplacement))
 		info.GetDut().State = dutstate.NeedsReplacement
