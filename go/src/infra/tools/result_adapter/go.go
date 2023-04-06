117ee99b20cc61031f1c48809a0ec13b9e6a38be
Dmitri Shuralyov <dmitshur@google.com>
2023-04-06 13:16:12 -0400

    [result_adapter] associate test output from Go test with its test record
    Make it so that a test record for a single Go test shows only the output
    that said test produced, rather than each test record repeating the same
    output for the entire Go package.
    
    Bug: 1431319
    Change-Id: Ie2c8e1c65d041db40f06693fb246c0dcbcb3426f

--- go/src/infra/tools/result_adapter/go.go
+++ go/src/infra/tools/result_adapter/go.go
@@ -85,19 +85,7 @@ func goTestJsonToTestRecords(ctx context.Context, data []byte, copyTestOutput io
 			ordered = append(ordered, currentRecord)
 			byID[currentRecord.TestID] = currentRecord
 		}
-
-		var parent *TestRecord = nil
-		if tEvt.Test != "" {
-			parent = byID[tEvt.Package]
-			if parent == nil {
-				parent = &TestRecord{TestID: tEvt.Package}
-				ordered = append(ordered, parent)
-				byID[parent.TestID] = parent
-			}
-		}
-
-		currentRecord.ingest(tEvt, parent)
-
+		currentRecord.ingest(tEvt)
 		if copyTestOutput != nil {
 			copyTestOutput.WriteString(tEvt.Output)
 		}
@@ -140,11 +128,10 @@ func testRecordsToTestProtos(ctx context.Context, ordered []*TestRecord, byID ma
 				tr.Status = resultpb.TestStatus_ABORT
 			}
 		}
-		parentRecord := byID[record.PackageName]
-		if parentRecord.Output.Len() > 0 {
+		if record.Output.Len() > 0 {
 			a := sinkpb.Artifact{}
 			a.Body = &sinkpb.Artifact_Contents{
-				Contents: []byte(parentRecord.Output.String()),
+				Contents: []byte(record.Output.String()),
 			}
 			tr.Artifacts = map[string]*sinkpb.Artifact{"output": &a}
 			tr.SummaryHtml = `<p><text-artifact artifact-id="output"></p>`
@@ -198,12 +185,12 @@ type TestRecord struct {
 
 // ingest updates the fields of the test record according to the contents of
 // the given test event.
-// Output events will be added to the package's test record (the test's "parent")
-// instead of the record for the specific test.
-// Tests running in parallel may cause test2json to associate the output of one
-// with a different test as all simultaneous tests in the same package race for
-// access to stdout.
-func (tr *TestRecord) ingest(te *TestEvent, parent *TestRecord) {
+// Output events from a specific test will be associated with the corresponding
+// test record.
+// Tests running in parallel that choose not to use t.Log/t.Error and instead
+// write to stdout/stderr directly may result in output being associated with
+// the wrong test. See https://go.dev/issue/23036#issuecomment-355669573.
+func (tr *TestRecord) ingest(te *TestEvent) {
 	if te.Test == "" {
 		tr.IsPackage = true
 	}
@@ -217,11 +204,7 @@ func (tr *TestRecord) ingest(te *TestEvent, parent *TestRecord) {
 	case "run":
 		tr.Started = te.Time
 	case "output":
-		if parent == nil {
-			tr.Output.WriteString(te.Output)
-		} else {
-			parent.Output.WriteString(te.Output)
-		}
+		tr.Output.WriteString(te.Output)
 	default:
 		tr.Result = te.Action
 		if te.Elapsed > 0 {
