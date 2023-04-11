e439afa0d2a3078c224f5ca447bae419bb0f30af
Joanna Wang <jojwang@google.com>
2023-04-11 23:09:30 +0000

    Remove infra_superproject recipe.
    Bug: 1415507
    Change-Id: If173bdb507b8d31603718e372ffd3ce9571c19de
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4416174
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Auto-Submit: Joanna Wang <jojwang@chromium.org>
    Commit-Queue: Joanna Wang <jojwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#56054}

--- recipes/recipes/infra_superproject_tester.py
+++ recipes/recipes/infra_superproject_tester.py
@@ -13,56 +13,8 @@ DEPS = [
 def RunSteps(api):
   """Runs infra{_internal} builds for infra_superproject changes."""
 
-  reqs = []
-  reqs.append(
-      api.buildbucket.schedule_request(
-          'infra-internal-try-frontend',
-          project='infra_internal',
-          bucket='try',
-          swarming_parent_run_id=api.swarming.task_id,
-      ))
-
-  reqs.append(
-      api.buildbucket.schedule_request(
-          'infra-internal-tester-bionic-64',
-          project='infra_internal',
-          bucket='try',
-          swarming_parent_run_id=api.swarming.task_id,
-      ))
-
-  # Note: infra_superproject is in the infra_internal LUCI project and
-  # we cannot assign a task from a different swarming instance as the
-  # swarming_parent_run_id.
-  reqs.append(
-      api.buildbucket.schedule_request(
-          'infra-try-bionic-64',
-          project='infra',
-          bucket='try',
-      ))
-
-  reqs.append(
-      api.buildbucket.schedule_request(
-          'infra-try-mac',
-          project='infra',
-          bucket='try',
-      ))
-
-  reqs.append(
-      api.buildbucket.schedule_request(
-          'infra-try-win',
-          project='infra',
-          bucket='try',
-      ))
-
-  reqs.append(
-      api.buildbucket.schedule_request(
-          'infra-try-frontend',
-          project='infra',
-          bucket='try',
-      ))
-
-  api.buildbucket.run(
-      reqs, raise_if_unsuccessful=True, step_name='schedule builds')
+  # TODO(crbug.com/1415507): schedule infra and infra_internal builds
+  return
 
 
 def GenTests(api):
