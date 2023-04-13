75beecbf5047973a68ea9d486fa6b95f82e4f521
Greg Guterman <guterman@google.com>
2023-04-13 21:32:41 +0000

    Builder health indicators command line app and cipd build
    The builder that'll run this is in https://crrev.com/i/5699530
    
    Bug: 1401558
    Change-Id: I9a4fa83e90b3f375fa1012313880b9d45d28e4d0

---
+++ go/src/infra/builder_health_indicators/main.go
@@ -0,0 +1,37 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+	"os"
+
+	"infra/builder_health_indicators/cmd/generate_indicators"
+
+	"github.com/maruel/subcommands"
+	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/flag/fixflagpos"
+	"go.chromium.org/luci/common/logging/gologger"
+)
+
+var logCfg = gologger.LoggerConfig{
+	Format: `%{message}`,
+	Out:    os.Stderr,
+}
+
+func main() {
+	app := &cli.Application{
+		Name:  "builder-health-indicators",
+		Title: "Builder Health Indicators track Chromium builders' long term health",
+		Context: func(ctx context.Context) context.Context {
+			return logCfg.Use(ctx)
+		},
+		Commands: []*subcommands.Command{
+			generate.CmdGenerate(),
+		},
+	}
+
+	os.Exit(subcommands.Run(app, fixflagpos.FixSubcommands(os.Args[1:])))
+}
