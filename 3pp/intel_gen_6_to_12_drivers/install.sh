4cff07b7c7e16cfed0b9c757881019a091aa1e60
Brian Sheedy <bsheedy@chromium.org>
2023-04-19 17:23:47 -0700

    Add Win Intel Gen 6-12 drivers
    Adds a 3pp config for getting Windows Intel drivers for Intel integrated
    GPUs from gen 6 to gen 12 as a zip file. This will ultimately be used
    to help automatically generate GPU Windows images when new drivers are
    released.
    
    Bug: b:277120952
    Change-Id: Ia3b52c213c500deaf031d0285dba287894b02b45

---
+++ 3pp/intel_gen_6_to_12_drivers/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+PREFIX="$1"
+
+ls $(pwd)
+ls PREFIX
+
+exit 1
