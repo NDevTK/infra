f4d71394e7b235006a5a7a2808d9ebdde0cdaec7
Dave Tu <dtu@google.com>
2023-04-27 15:34:58 -0700

    🚝 Restart instance if memory usage is too high.
    Bug: monorail:5271
    Change-Id: I4646ac423093dec0861b54091690cbe2e07cd9ca

---
+++ appengine/monorail/framework/teardown.py
@@ -0,0 +1,31 @@
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""A handler run on Flask request teardown."""
+
+import logging
+
+from google.appengine.api import runtime
+from googleapiclient.discovery import build
+from oauth2client.client import GoogleCredentials
+
+import settings
+
+_MAXIMUM_MEMORY_USAGE = 2000
+
+
+def Teardown(_exc):
+  if settings.local_mode:
+    return
+
+  # Check memory usage and delete it if it's too high.
+  memory_usage = runtime.memory_usage().average10m
+  if memory_usage >= _MAXIMUM_MEMORY_USAGE:
+    logging.critical('Deleting instance using %d MB of memory.' % memory_usage)
+    credentials = GoogleCredentials.get_application_default()
+    appengine = build('appengine', 'v1', credentials=credentials)
+    appengine.apps().services().versions().instances().delete(
+        appsId=os.environ.get('GAE_APPLICATION').split('~')[-1],
+        servicesId=os.environ.get('GAE_SERVICE'),
+        versionsId=os.environ.get('GAE_VERSION'),
+        instancesId=os.environ.get('GAE_INSTANCE')).execute()
