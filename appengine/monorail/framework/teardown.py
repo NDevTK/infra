c7e4c1623aa1b7fed9c34cf72384657ba7f8fefc
Dave Tu <dtu@google.com>
2023-05-02 17:00:46 +0000

    🚝 Only delete an instance once for using too much memory.
    An instance may still serve requests for 5 seconds after it's been
    stopped. But subsequent requests may trigger more calls to
    instance.delete(), which will fail with a 404 error since the instance
    has already been deleted. That causes the last few requests to return
    500 errors. Catch and suppress the 404 error instead.
    
    I also tried using instance.get() to to ensure that the instance exists
    before deletion. But I found that instance.get() would still return the
    same instance information even after instance deletion. So catching
    the 404 error is the more surefire way of making sure this works
    consistently.
    
    The downside of this approach is that each failed deletion will still
    add an additional entry to the Cloud Audit log.
    
    Bug: monorail:5271
    Change-Id: I5142e4ae10cab01cabc1a10a5d9a8955e02bf619
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4497532
    Auto-Submit: Dave Tu <dtu@chromium.org>
    Reviewed-by: Andrew Chang <andrewjc@google.com>
    Reviewed-by: Yuxuan Chen <yuxuanch@google.com>
    Commit-Queue: Andrew Chang <andrewjc@google.com>
    Cr-Commit-Position: refs/heads/main@{#56458}

--- appengine/monorail/framework/teardown.py
+++ appengine/monorail/framework/teardown.py
@@ -7,8 +7,9 @@ import logging
 import os
 
 from google.appengine.api import runtime
-from googleapiclient.discovery import build
-from oauth2client.client import GoogleCredentials
+from googleapiclient import discovery
+from googleapiclient import errors
+from oauth2client import client
 
 import settings
 
@@ -19,14 +20,22 @@ def Teardown(_exc):
   if settings.local_mode:
     return
 
-  # Check memory usage and delete it if it's too high.
+  # Stop the instance if it's using too much memory.
   memory_usage = runtime.memory_usage().average10m
-  if memory_usage >= _MAXIMUM_MEMORY_USAGE:
-    logging.critical('Deleting instance using %d MB of memory.' % memory_usage)
-    credentials = GoogleCredentials.get_application_default()
-    appengine = build('appengine', 'v1', credentials=credentials)
-    appengine.apps().services().versions().instances().delete(
-        appsId=os.environ.get('GAE_APPLICATION').split('~')[-1],
-        servicesId=os.environ.get('GAE_SERVICE'),
-        versionsId=os.environ.get('GAE_VERSION'),
-        instancesId=os.environ.get('GAE_INSTANCE')).execute()
+  if memory_usage < _MAXIMUM_MEMORY_USAGE:
+    return
+
+  credentials = client.GoogleCredentials.get_application_default()
+  appengine = discovery.build('appengine', 'v1', credentials=credentials)
+  delete = appengine.apps().services().versions().instances().delete(
+      appsId=os.environ.get('GAE_APPLICATION').split('~')[-1],
+      servicesId=os.environ.get('GAE_SERVICE'),
+      versionsId=os.environ.get('GAE_VERSION'),
+      instancesId=os.environ.get('GAE_INSTANCE'))
+  try:
+    delete.execute()
+  except errors.HttpError as e:
+    if e.status_code != 404:
+      raise
+  else:
+    logging.critical('Deleted instance using %d MB of memory.' % memory_usage)
