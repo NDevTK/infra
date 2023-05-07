ebf759921f003b19c08d7ce345004f1f936370c8
Yuxuan Chen <yuxuanch@google.com>
2023-05-07 15:02:29 -0700

    redirect the issue list and new new issue link
    bug: b/279782705
    
    Change-Id: Ifea64476f3e2274ead0d42ed7753e02a44e1944b

---
+++ appengine/monorail/redirect/redirect_utils.py
@@ -0,0 +1,10 @@
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Utils for redirect."""
+
+PROJECT_REDIRECT_MAP = {}
+
+
+def GetRedirectURL(project_name):
+  return PROJECT_REDIRECT_MAP.get(project_name, None)
