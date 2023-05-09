58cfd04f077071f3532501a20fde3b74af1b3a70
Struan Shrimpton <sshrimp@google.com>
2023-05-09 04:33:14 +0000

    Remove unused modules in test-resources backend
    A lot of these modules were added in an attempt to get rpcexplorer
    working but aren't actually needed. Some of these might be needed when
    we implement a sign in outside of rpcexplorer but we're not using them
    yet. Also set up the routing to enable rpcexplorer
    
    Bug: 1425974
    Change-Id: Icf3cf155c7a3e6aae27cfc4027e65fd3af9794de
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4506796
    Commit-Queue: Struan Shrimpton <sshrimp@google.com>
    Reviewed-by: Riley Wong <rgw@google.com>
    Cr-Commit-Position: refs/heads/main@{#56572}

--- go/src/infra/appengine/test-resources/main.go
+++ go/src/infra/appengine/test-resources/main.go
@@ -16,13 +16,7 @@ import (
 	"go.chromium.org/luci/server/auth/openid"
 	"go.chromium.org/luci/server/auth/rpcacl"
 	"go.chromium.org/luci/server/cron"
-	"go.chromium.org/luci/server/encryptedcookies"
-	"go.chromium.org/luci/server/gaeemulation"
 	"go.chromium.org/luci/server/module"
-	"go.chromium.org/luci/server/secrets"
-
-	// Store auth sessions in the datastore.
-	_ "go.chromium.org/luci/server/encryptedcookies/session/datastore"
 
 	"infra/appengine/test-resources/api"
 	"infra/appengine/test-resources/internal/testmetrics"
@@ -50,9 +44,6 @@ func main() {
 
 	modules := []module.Module{
 		cron.NewModuleFromFlags(),
-		encryptedcookies.NewModuleFromFlags(), // Required for auth sessions.
-		gaeemulation.NewModuleFromFlags(),     // Required by encryptedcookies
-		secrets.NewModuleFromFlags(),          // Needed by encryptedcookies.
 	}
 	server.Main(nil, modules, func(srv *server.Server) error {
 		client, err := setupClient(srv)
