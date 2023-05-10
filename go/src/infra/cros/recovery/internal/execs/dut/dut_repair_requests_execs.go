c31a8c04d1785baf6493a34159d6627b70bdb443
Otabek Kasimov <otabek@google.com>
2023-05-10 00:23:26 +0000

    recovery: Checks repair-request before set state ready
    BUG=b:280356682
    TEST=build
    
    Change-Id: Ib94d0898043fc54fae4e34e358f6fbee6dcace64
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4511229
    Reviewed-by: Vineet Joshi <vkjoshi@google.com>
    Commit-Queue: Otabek Kasimov <otabek@google.com>
    Auto-Submit: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/main@{#56602}

---
+++ go/src/infra/cros/recovery/internal/execs/dut/dut_repair_requests_execs.go
@@ -0,0 +1,36 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package dut
+
+import (
+	"context"
+
+	"go.chromium.org/luci/common/errors"
+
+	"infra/cros/recovery/internal/execs"
+	"infra/cros/recovery/internal/log"
+	"infra/cros/recovery/tlw"
+)
+
+// hasNoRepairRequestsExec checks that DUT has no repair_requests.
+func hasNoRepairRequestsExec(ctx context.Context, info *execs.ExecInfo) error {
+	if info.GetDut() == nil {
+		return errors.Reason("has no repair-requests: dut is not provided").Err()
+	}
+	if len(info.GetDut().RepairRequests) == 0 {
+		log.Debugf(ctx, "Total 0 repair-requests.")
+		return nil
+	}
+	for _, rr := range info.GetDut().RepairRequests {
+		if rr != tlw.RepairRequestUnknown {
+			return errors.Reason("has no repair-requests: found %q repair-request", rr).Err()
+		}
+	}
+	return nil
+}
+
+func init() {
+	execs.Register("dut_has_no_repair_requests", hasNoRepairRequestsExec)
+}
