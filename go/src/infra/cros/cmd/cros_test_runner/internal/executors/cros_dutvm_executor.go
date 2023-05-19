6fb9dc6e376c252ed33bce8ff8d455d032195908
Justin Suen <justinsuen@google.com>
2023-05-19 01:21:54 +0000

    cros_test_runner: fix lease DUT VM with public IP
    Rebase error removed the code.
    
    BUG=b/280372317
    TEST=go test ./...; led testing
    
    Change-Id: I06e2d91481c43b9188a9577363c1d6af8f4cf448
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4546123
    Commit-Queue: Ming Kong <mingkong@google.com>
    Reviewed-by: Ming Kong <mingkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#56860}

--- go/src/infra/cros/cmd/cros_test_runner/internal/executors/cros_dutvm_executor.go
+++ go/src/infra/cros/cmd/cros_test_runner/internal/executors/cros_dutvm_executor.go
@@ -231,14 +231,15 @@ func constructGcloudPayload(ctx context.Context, cmd *commands.DutVmLeaseCmd) *v
 		Config: &vmlabapi.Config{
 			Backend: &vmlabapi.Config_GcloudBackend{
 				GcloudBackend: &vmlabapi.Config_GCloudBackend{
-					Project:        "chromeos-gce-tests",
-					Zone:           "us-central1-a",
-					MachineType:    "n2-standard-4",
-					InstancePrefix: "ctsprototype-",
-					PublicIp:       false,
-					Image:          cmd.DutVmGceImage,
-					Network:        "chromeos-gce-tests",
-					Subnet:         "us-central1",
+					Project:             "chromeos-gce-tests",
+					Zone:                "us-central1-a",
+					MachineType:         "n2-standard-4",
+					InstancePrefix:      "ctsprototype-",
+					PublicIp:            true,
+					AlwaysSshInternalIp: true,
+					Image:               cmd.DutVmGceImage,
+					Network:             "chromeos-gce-tests",
+					Subnet:              "us-central1",
 				},
 			},
 		},
