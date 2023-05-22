65364371c9bb03216c146494f5e977f93844beac
Struan Shrimpton <sshrimp@google.com>
2023-05-22 20:48:17 +0000

    Add weekly file based rollups to the test resources dashboard
    Change-Id: I443a15c89180e277e46d5936a5b1f5453d48afe0

---
+++ go/src/infra/appengine/test-resources/sql/update_weekly_file_metrics.sql
@@ -0,0 +1,32 @@
+MERGE INTO %s.test_results.weekly_file_metrics AS T
+USING (
+  SELECT
+    DATE_TRUNC(date, WEEK(SUNDAY)) AS date,
+    component,
+    node_name,
+    ANY_VALUE(is_file) AS is_file,
+    SUM(num_runs) AS num_runs,
+    SUM(num_failures) AS num_failures,
+    SUM(num_flake) AS num_flake,
+    AVG(avg_runtime)	AS avg_runtime,
+    SUM(total_runtime) AS total_runtime,
+  FROM %s.test_results.file_metrics
+  WHERE date BETWEEN
+    -- The date range is inclusive so only go up to the Saturday
+    DATE_TRUNC(DATE(@from_date), WEEK) AND
+    DATE_ADD(DATE_TRUNC(DATE(@to_date), WEEK), INTERVAL 6 DAY)
+  GROUP BY DATE_TRUNC(date, WEEK(SUNDAY)), component, node_name
+  ) AS S
+ON
+  T.date = S.date
+  AND T.component = S.component
+  AND T.node_name = S.node_name
+WHEN MATCHED THEN
+  UPDATE SET
+    num_runs = S.num_runs,
+    num_failures = S.num_failures,
+    num_flake = S.num_flake,
+    avg_runtime = S.avg_runtime,
+    total_runtime = S.total_runtime
+WHEN NOT MATCHED THEN
+  INSERT ROW
