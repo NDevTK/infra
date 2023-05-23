4a7a3650013f4ca93caaff25b304b8997fe91e79
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2023-05-23 17:38:34 +0000

    Build From Tarball: Pass enable_rust=false to GN.
    https://chromium-review.googlesource.com/c/chromium/src/+/4454842
    started setting enable_rust to true by default on official builds,
    leading to failures such as
    https://ci.chromium.org/ui/p/infra/builders/cron/Build%20From%20Tarball/b8780358118255289057/overview
    
    For now, just explicitly disable enable_rust to avoid requiring the
    presence of an external Rust toolchain since it is only required to
    build some test executables at this point.
    
    Bug: 1292038
    Change-Id: I3a24ce4f50b165e67b97db848f7fddaf2917248b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4553877
    Auto-Submit: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#56947}

--- recipes/recipes/build_from_tarball.py
+++ recipes/recipes/build_from_tarball.py
@@ -59,6 +59,11 @@ def RunSteps(api):
           # necessary when unbundling libjpeg.
           'use_system_libjpeg=true',
           'use_v8_context_snapshot=false',
+
+          # |enable_rust| started defaulting to true in official builds in
+          # 115.0.5787.2 (https://crrev.com/c/4454842). Turn it off to avoid
+          # requiring a Rust toolchain for the build.
+          'enable_rust=false',
       ]
 
       # Until the release above (https://crrev.com/c/4108258), this defaulted
