b434976bca222784dc10a9ab08098f22cc14e091
Allen Li <ayatane@chromium.org>
2023-05-31 16:02:34 -0700

    drone-agent: Add config file
    drone-agent is accumulating too many settings (and we need to add
    megadrone now).  Migrate to a config file (with backward compat
    support so we can rollout).
    
    BUG=b:279830861
    
    Change-Id: I86415d03221035ff102680169ff3ac90d2a3c38c

---
+++ go/src/infra/cmd/drone-agent/config.go
@@ -0,0 +1,78 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+// A config represents the config for drone-agent.
+//
+// Note that the YAML library unmarshals using the field name
+// lowercased as the default key.
+type config struct {
+	// QueenService is the address of the drone queen service.
+	QueenService string
+	// SwarmingURL is the URL of the Swarming instance to use.
+	// Should be a full URL without the path, e.g.,
+	// https://host.example.com
+	SwarmingURL string
+
+	// TraceBackend denotes the backend used for OTel traces.
+	// Valid options are:
+	//   - grpc
+	//   - none
+	TraceBackend string
+	// TraceTarget is the destination for traces.
+	TraceTarget string
+
+	// Megadrone config settings
+
+	// EnableMegadrone enables megadrone mode if true.
+	// Most config settings like QueenService are ignored in megadrone mode.
+	EnableMegadrone bool
+	// NumBots sets the number of bots to run in megadrone mode.
+	NumBots string
+}
+
+// parseConfigFile parses the config file for drone-agent.
+// This function always returns a valid config object.
+// Errors are logged.
+//
+// This function also parses the environment and global flag vars to
+// implement backward compatibility.
+func parseConfigFile(path string) *config {
+	var cfg config
+	addBackwardCompatConfig(&cfg)
+	if path == "" {
+		return &cfg
+	}
+	b, err := os.ReadFile(path)
+	if err != nil {
+		log.Printf("Error: parse config file: %s", err)
+		return &cfg
+	}
+	if err := yaml.Unmarshal(b, &cfg); err != nil {
+		log.Printf("Error: parse config file: %s", err)
+		return &cfg
+	}
+	return &cfg
+}
+
+// addBackwardCompatConfig parses the environment and global flag vars
+// and adds settings to the config.  This is for backward
+// compatibility.
+func addBackwardCompatConfig(cfg *config) {
+	// Environment variables.
+	cfg.QueenService = queenService
+	cfg.SwarmingURL = swarmingURL
+
+	// Flags.
+	cfg.TraceBackend = traceBackend
+	cfg.TraceTarget = *traceTarget
+}
