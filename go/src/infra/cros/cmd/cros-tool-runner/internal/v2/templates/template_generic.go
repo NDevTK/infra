43d18840b55b282be3dd30238f0d7e72b15448a8
Chris DeLaGarza <cdelagarza@google.com>
2023-06-05 21:48:16 +0000

    WIP: add generic container template logic
    BUG=none
    TEST=none
    
    Change-Id: I0d752294984cf4ccd80fb69606bb6fcdd2947851

---
+++ go/src/infra/cros/cmd/cros-tool-runner/internal/v2/templates/template_generic.go
@@ -0,0 +1,49 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package templates
+
+import (
+	"fmt"
+
+	"infra/cros/cmd/cros-tool-runner/internal/v2/commands"
+
+	"go.chromium.org/chromiumos/config/go/test/api"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type genericProcessor struct {
+	cmdExecutor cmdExecutor
+}
+
+func newGenericProcessor() *genericProcessor {
+	return &genericProcessor{
+		cmdExecutor: &commands.ContextualExecutor{},
+	}
+}
+
+func (p *genericProcessor) Process(request *api.StartTemplatedContainerRequest) (*api.StartContainerRequest, error) {
+	t := request.GetTemplate().GetGeneric()
+	if t == nil {
+		return nil, status.Error(codes.Internal, "unable to process")
+	}
+
+	artifactVolume := fmt.Sprintf("%s:%s", request.ArtifactDir, t.DockerArtifactDir)
+	volumes := []string{}
+	volumes = append(volumes, artifactVolume)
+	volumes = append(volumes, t.AdditionalVolumes...)
+	additionalOptions := &api.StartContainerRequest_Options{
+		Network: request.Network,
+		Expose:  t.Expose,
+		Volume:  volumes,
+	}
+	startCommand := t.Args
+	return &api.StartContainerRequest{Name: request.Name, ContainerImage: request.ContainerImage, AdditionalOptions: additionalOptions, StartCommand: startCommand}, nil
+}
+
+func (p *genericProcessor) discoverPort(request *api.StartTemplatedContainerRequest) (*api.Container_PortBinding, error) {
+	// delegate to default impl, any template-specific logic should be implemented here.
+	return defaultDiscoverPort(p.cmdExecutor, request)
+}
