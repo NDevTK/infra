03bd3b5dd44e689b4bc24939b4287cae29ccdb5e
Jared Bennett <jaredbennett@google.com>
2023-06-15 13:52:03 -0700

    recovery: Refactor wifi router repair config
    Updates the router repair configuration to use the new router execs
    and structure of separating actions by device type. Technically, this
    performs the same repair steps as before for Gales, with the addition
    of a reboot action.
    
    BUG=b:282836502
    TEST=cd ~/cr/infra/go/src/infra/cros/recovery && go clean && go build \
    && go test ./...
    TEST=successfully ran updated wifi router recovery plan on a testbed
    with gale routers and confirmed that the WifiRouterHost object printed
    after the local recovery completed was expected with the new fields
    populated and the gale reboots successfully
    
    Change-Id: I2cc386038347db31a6d88b8e920161a45f50f180

--- go/src/infra/cros/recovery/config/config_wifirouter.go
+++ go/src/infra/cros/recovery/config/config_wifirouter.go
@@ -1,33 +1,108 @@
-// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 package config
 
 import (
+	"fmt"
+	"strings"
+
+	labapi "go.chromium.org/chromiumos/config/go/test/lab/api"
 	"google.golang.org/protobuf/types/known/durationpb"
+	"infra/cros/recovery/tlw"
 )
 
 func wifiRouterRepairPlan() *Plan {
 	return &Plan{
 		CriticalActions: []string{
-			"wifirouter_state_broken",
-			"Device is pingable",
-			"cros_ssh",
-			"Device is on stable-version",
-			"is_wifirouter_tools_present",
-			"Device has 50 percent tmp diskspace",
-			"Device has 50 percent stateful partition diskspace",
-			"wifirouter_state_working",
+			// Initial actions to prepare for device-specific actions.
+			"Set WifiRouter state to BROKEN",
+			"Device is ping-able",
+			"Identify and set WifiRouter device_type",
+
+			// Actions only executed for specific device types.
+			"Check ChromeOS Gale device",
+
+			// General final actions done for all device types.
+			"Reboot device",
+			"Set WifiRouter state to WORKING",
 		},
 		Actions: map[string]*Action{
-			"is_wifirouter_tools_present": {
-				Docs: []string{
-					"check whether wifirouter critial tools present: ",
-					"tcpdump, hostapd, dnsmasq, netperf, iperf, iw",
+			// Generic AP actions.
+			"Set WifiRouter state to BROKEN": {
+				ExecName: "wifi_router_set_state",
+				ExecExtraArgs: []string{
+					fmt.Sprintf("state:%d", tlw.WifiRouterHost_BROKEN),
 				},
+			},
+			"Set WifiRouter state to WORKING": {
+				ExecName: "wifi_router_set_state",
+				ExecExtraArgs: []string{
+					fmt.Sprintf("state:%d", tlw.WifiRouterHost_WORKING),
+				},
+			},
+			"Device is ping-able": {
+				ExecName:    "cros_ping",
+				ExecTimeout: &durationpb.Duration{Seconds: 15},
+			},
+			"Identify and set WifiRouter device_type": {
+				Docs: []string{
+					"Identifies the device type of the test AP by probing the device over ssh.",
+					"APs that fail to be identified as one of the supported device types will be left in a broken state.",
+				},
+				ExecName: "wifi_router_identify_device_type",
+			},
+			"Reboot device": {
+				Docs: []string{
+					"Reboots the device over ssh and waits for it to be ssh-able again.",
+				},
+				ExecName:    "wifi_router_reboot",
+				ExecTimeout: &durationpb.Duration{Seconds: 200},
+			},
+
+			// Device type conditions.
+			"Is ChromeOS Gale": {
+				ExecName: "wifi_router_device_type_in_list",
+				ExecExtraArgs: []string{
+					buildWifiRouterDeviceTypesArg(labapi.WifiRouterDeviceType_WIFI_ROUTER_DEVICE_TYPE_CHROMEOS_GALE),
+				},
+			},
+
+			// ChromeOS Gale actions.
+			"Check ChromeOS Gale device": {
 				Conditions: []string{
-					"The device: gale/gale",
+					"Is ChromeOS Gale",
+				},
+				Dependencies: []string{
+					"Device is on stable-version",
+					"Device has required wifi tools",
+					"Device has 50 percent tmp diskspace",
+					"Device has 50 percent stateful partition diskspace",
+					"Set WifiRouter model and features to hardcoded values for Gales",
+				},
+				ExecName: "sample_pass",
+			},
+			"Device is on stable-version": {
+				ExecName: "cros_is_on_stable_version",
+				ExecExtraArgs: []string{
+					"device_type:wifi_router",
+				},
+				RecoveryActions: []string{
+					"Provision Gale device to stable version",
+				},
+			},
+			"Set WifiRouter model and features to hardcoded values for Gales": {
+				Docs: []string{
+					"All ChromeOS Gale devices are expected to have the same model and ",
+					"features, so we can set these as hardcoded values.",
+				},
+				ExecName: "wifi_router_update_model_and_features",
+			},
+			"Device has required wifi tools": {
+				Docs: []string{
+					"check whether wifirouter critical tools present: ",
+					"tcpdump, hostapd, dnsmasq, netperf, iperf, iw",
 				},
 				Dependencies: []string{
 					"cros_ssh",
@@ -37,48 +112,13 @@ func wifiRouterRepairPlan() *Plan {
 					"tools:tcpdump,hostapd,dnsmasq,netperf,iperf,iw",
 				},
 				RecoveryActions: []string{
-					"Provision WifiRouter to stable version",
+					"Provision Gale device to stable version",
 				},
 			},
-			"Device is pingable": {
-				ExecName:    "cros_ping",
-				ExecTimeout: &durationpb.Duration{Seconds: 15},
-			},
-			"Device is on stable-version": {
-				Docs: []string{
-					"TODO(b/216192539): extend stable version to peripheral routers",
-					"This is intermittent solution for wifirouter until bug is resolved",
-					"Currently lab only support one type of router device (board=gale,model=gale)",
-				},
-				Dependencies: []string{
-					"The device: gale/gale",
-				},
-				ExecName: "cros_is_on_stable_version",
-				ExecExtraArgs: []string{
-					"device_type:wifi_router",
-				},
-				RecoveryActions: []string{
-					"Provision WifiRouter to stable version",
-				},
-			},
-			"The device: gale/gale": {
-				Docs: []string{
-					"TODO(b:248631855): hardcoded to only accept model=gale, board=gale routers.",
-					"Remove when stable version is ready",
-				},
-				ExecName: "is_wifirouter_board_model_matching",
-				ExecExtraArgs: []string{
-					"board:gale",
-					"model:gale",
-				},
-			},
-			"Provision WifiRouter to stable version": {
+			"Provision Gale device to stable version": {
 				Docs: []string{
 					"Install wifirouter stable os.",
 					"Currently only has one version",
-				},
-				Conditions: []string{
-					"The device: gale/gale",
 				},
 				ExecName: "cros_provision",
 				ExecExtraArgs: []string{
@@ -90,9 +130,6 @@ func wifiRouterRepairPlan() *Plan {
 				Docs: []string{
 					"Check if there are more than 50 percent of diskspace in /tmp",
 				},
-				Dependencies: []string{
-					"Device is on stable-version",
-				},
 				ExecName: "cros_has_enough_storage_space_percentage",
 				ExecExtraArgs: []string{
 					"path:/tmp",
@@ -100,7 +137,7 @@ func wifiRouterRepairPlan() *Plan {
 				},
 				RecoveryActions: []string{
 					"Clean up tmp space",
-					"Provision WifiRouter to stable version",
+					"Provision Gale device to stable version",
 				},
 			},
 			"Clean up tmp space": {
@@ -116,9 +153,6 @@ func wifiRouterRepairPlan() *Plan {
 				Docs: []string{
 					"Check if there are more than 50 percent of diskspace in /mnt/stateful_partition",
 				},
-				Dependencies: []string{
-					"Device is on stable-version",
-				},
 				ExecName: "cros_has_enough_storage_space_percentage",
 				ExecExtraArgs: []string{
 					"path:/mnt/stateful_partition",
@@ -126,7 +160,7 @@ func wifiRouterRepairPlan() *Plan {
 				},
 				RecoveryActions: []string{
 					"Clean up stateful sub space",
-					"Provision WifiRouter to stable version",
+					"Provision Gale device to stable version",
 				},
 			},
 			"Clean up stateful sub space": {
@@ -140,4 +174,12 @@ func wifiRouterRepairPlan() *Plan {
 			},
 		},
 	}
+}
+
+func buildWifiRouterDeviceTypesArg(deviceTypes ...labapi.WifiRouterDeviceType) string {
+	var argValue []string
+	for _, dt := range deviceTypes {
+		argValue = append(argValue, fmt.Sprintf("%d", dt))
+	}
+	return fmt.Sprintf("device_types:%s", strings.Join(argValue, ","))
 }
