09df6d89fb33ff350dd45422de1f3d9790c81b93
Jared Bennett <jaredbennett@google.com>
2023-06-15 13:54:38 -0700

    recovery: Add exec support for AsusWrt routers
    Adds a new router controller for AsusWrt routers and adds new wifirouter
    execs that adds support for repairing AsusWrt routers.
    
    BUG=b:282835106
    TEST=cd ~/cr/infra/go/src/infra/cros/recovery && go clean && go build \
    && go test ./..
    TEST=successfully ran an updated wifi router recovery plan on a testbed
    with Gale routers and confirmed that only the Gale-specific actions
    were followed
    TEST=successfully ran an updated wifi router recovery plan on a testbed
    with an AsusWrt router, confirmed that only the AsusWrt-specific
    actions were followed, and confirmed that the WifiRouterHost object
    printed after the local recovery completed was as expected
    
    Change-Id: Ib024fa1838fc81e901cb5228970d6fc220370035

---
+++ go/src/infra/cros/recovery/internal/execs/wifirouter/controller/asuswrt.go
@@ -0,0 +1,158 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package controller
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+
+	labapi "go.chromium.org/chromiumos/config/go/test/lab/api"
+	"go.chromium.org/luci/common/errors"
+	"infra/cros/recovery/internal/execs/wifirouter/ssh"
+	"infra/cros/recovery/tlw"
+)
+
+var asuswrtModelToFeatures = map[string][]labapi.WifiRouterFeature{
+	"RT-AX92U": {
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_A,
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_B,
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_G,
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_N,
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_AC,
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_AX,
+	},
+	"GT-AX11000": {
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_A,
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_A,
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_B,
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_G,
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_N,
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_AC,
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_AX,
+	},
+	"GT-AXE11000": {
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_A,
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_B,
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_G,
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_N,
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_AC,
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_AX,
+		labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_IEEE_802_11_AX_E,
+	},
+}
+
+const nvramCmd = "/bin/nvram"
+
+const asusWrtSshUser = "admin"
+
+// hostIsAsusWrtRouter checks if the remote host is an AsusWrt router.
+func hostIsAsusWrtRouter(ctx context.Context, sshRunner ssh.Runner) (bool, error) {
+	nvramCmdExists, err := ssh.TestPath(ctx, sshRunner, "-x", nvramCmd)
+	if err != nil {
+		return false, errors.Annotate(err, "failed to check for the existence of the command %q", nvramCmd).Err()
+	}
+	if !nvramCmdExists {
+		return false, nil
+	}
+
+	// Verify that the manufacturer is ASUS.
+	manufacturer, err := hostRunNvramShowForKey(ctx, sshRunner, "wps_mfstring")
+	if err != nil {
+		return false, err
+	}
+	return manufacturer == "ASUSTeK Computer Inc.", nil
+}
+
+func hostRunNvramShowForKey(ctx context.Context, sshRunner ssh.Runner, nvramKey string) (string, error) {
+	nvramShowOutput, err := sshRunner.Run(ctx, 0, nvramCmd, "show")
+	if err != nil {
+		return "", err
+	}
+
+	// Parse value of key.
+	matchRegex := fmt.Sprintf("(?m)^%s=(.+)$", nvramKey)
+	matcher, err := regexp.Compile(matchRegex)
+	if err != nil {
+		return "", errors.Annotate(err, "failed to compile regex string %q", matchRegex).Err()
+	}
+	valueMatch := matcher.FindStringSubmatch(nvramShowOutput)
+	if valueMatch == nil {
+		return "", errors.Reason("failed to parse %q from '%s show' output: %q", nvramKey, nvramCmd, nvramShowOutput).Err()
+	}
+	return valueMatch[1], nil
+}
+
+// AsusWrtRouterController is the RouterController implementation for
+// AsusWrt router devices.
+type AsusWrtRouterController struct {
+	sshRunner      ssh.Runner
+	wifiRouterHost *tlw.WifiRouterHost
+	state          *tlw.AsusWrtRouterControllerState
+}
+
+func newAsusWrtRouterController(sshRunner ssh.Runner, wifiRouterHost *tlw.WifiRouterHost, state *tlw.AsusWrtRouterControllerState) *AsusWrtRouterController {
+	return &AsusWrtRouterController{
+		sshRunner:      sshRunner,
+		wifiRouterHost: wifiRouterHost,
+		state:          state,
+	}
+}
+
+// WifiRouterHost returns the corresponding tlw.WifiRouterHost instance for
+// this router. Changes to this instance are persisted across execs.
+func (c *AsusWrtRouterController) WifiRouterHost() *tlw.WifiRouterHost {
+	return c.wifiRouterHost
+}
+
+// FetchAsusModel retrieves the ASUS model from the device and stores it in the state.
+func (c *AsusWrtRouterController) FetchAsusModel(ctx context.Context) error {
+	var err error
+	c.state.AsusModel, err = hostRunNvramShowForKey(ctx, c.sshRunner, "wps_modelnum")
+	if err != nil {
+		return errors.Annotate(err, "failed to fetch ASUS model from ASUSWRT router").Err()
+	}
+	return nil
+}
+
+// DeviceType returns the labapi.WifiRouterDeviceType of the router.
+func (c *AsusWrtRouterController) DeviceType() labapi.WifiRouterDeviceType {
+	return labapi.WifiRouterDeviceType_WIFI_ROUTER_DEVICE_TYPE_ASUSWRT
+}
+
+// Model returns a unique name for the router model.
+//
+// For AsusWrt routers, this is a combination of the DeviceType and the Asus
+// model name retrieved from the router.
+func (c *AsusWrtRouterController) Model() (string, error) {
+	if c.state.AsusModel == "" {
+		return "", errors.Reason("AsusModel not yet retrieved from device").Err()
+	}
+	return buildModelName(c.DeviceType(), c.state.AsusModel), nil
+}
+
+// Features returns the router features that this router supports.
+//
+// AsusWrt routers have a static mapping of known features for each supported
+// Asus model. This mapping is maintained by the controller and is not retrieved
+// from the device itself.
+func (c *AsusWrtRouterController) Features() ([]labapi.WifiRouterFeature, error) {
+	if c.state == nil || c.state.AsusModel == "" {
+		return nil, errors.Reason("AsusModel not yet retrieved from device").Err()
+	}
+	features, ok := asuswrtModelToFeatures[c.state.AsusModel]
+	if !ok {
+		return []labapi.WifiRouterFeature{
+			labapi.WifiRouterFeature_WIFI_ROUTER_FEATURE_UNKNOWN,
+		}, nil
+	}
+	return features, nil
+}
+
+// Reboot will reboot the router and wait for it to come back up. A non-nil
+// error indicates that the router was rebooted and is ssh-able again.
+func (c *AsusWrtRouterController) Reboot(ctx context.Context) error {
+	return ssh.Reboot(ctx, c.sshRunner)
+}
