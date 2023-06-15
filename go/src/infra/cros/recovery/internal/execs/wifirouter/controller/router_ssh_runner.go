3f716af4378110d31a753a2d542ac3636010d647
Jared Bennett <jaredbennett@google.com>
2023-06-15 13:49:01 -0700

    recovery: Refactor wifi router repair execs
    This refactors the wifi router repair execs to prepare for supporting
    multiple types of routers. The same repair steps are performed for
    Gales, but they are now reorganized. Some new repair execs have been
    added as well, including a device reboot and collection of wifi
    features and device type. These fields will be saved to UFS in a
    later CL.
    
    With this new design, each type of router is managed by a different
    controller implementation. Identification of the router occurs in an
    action prior to any device-specific actions and execs so that the execs
    will know how to properly interact with the router host when they are
    called. Since Gale routers are simple and only one model is supported,
    its controller is mostly made up of static data. Other controllers added
    in later CLs will be more complex.
    
    BUG=b:282836502
    TEST=cd ~/cr/infra/go/src/infra/cros/recovery && go clean && go build \
    && go test ./...
    TEST=successfully ran current wifi router recovery plan on a testbed
    with gale routers
    TEST=successfully ran updated wifi router recovery plan on a testbed
    with gale routers and confirmed that the WifiRouterHost object printed
    after the local recovery completed was expected with the new fields
    populated and the gale reboots successfully
    
    Change-Id: If40402bd0dbd94c436f306fa0da1b9f3444396ff

---
+++ go/src/infra/cros/recovery/internal/execs/wifirouter/controller/router_ssh_runner.go
@@ -0,0 +1,68 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package controller
+
+import (
+	"context"
+	"time"
+
+	"go.chromium.org/luci/common/errors"
+	"google.golang.org/protobuf/types/known/durationpb"
+	"infra/cros/recovery/internal/execs/wifirouter/ssh"
+	"infra/cros/recovery/tlw"
+)
+
+type routerSshRunner struct {
+	sshAccess           ssh.Access
+	resource            string
+	sshUsernameOverride string
+}
+
+func newRouterSshRunner(sshAccess ssh.Access, resource string) ssh.Runner {
+	r := &routerSshRunner{
+		sshAccess: sshAccess,
+		resource:  resource,
+	}
+	return r
+}
+
+// Run executes the given command with its arguments on a host over ssh. The
+// stdout of the command execution is returned. If the command returns a
+// non-zero exit code, a non-nil error is returned.
+//
+// Can be used as a components.Runner.
+func (r *routerSshRunner) Run(ctx context.Context, timeout time.Duration, cmd string, args ...string) (string, error) {
+	runResult := r.RunForResult(ctx, timeout, false, cmd, args...)
+	if runResult.GetExitCode() != 0 {
+		return "", errors.Reason("run of command %q result returned non-zero exit code: %s", runResult.GetCommand(), runResult.GetStderr()).Err()
+	}
+	return runResult.GetStdout(), nil
+}
+
+// RunInBackground starts to execute the given command with its arguments on a
+// host over ssh, but does so in a background process and does not wait for it
+// to finish before returning. A non-nil error is returned when it fails to
+// start the command execution.
+func (r *routerSshRunner) RunInBackground(ctx context.Context, timeout time.Duration, cmd string, args ...string) error {
+	runResult := r.RunForResult(ctx, timeout, true, cmd, args...)
+	if runResult.GetExitCode() != 0 {
+		return errors.Reason("background run of command %q result returned non-zero exit code: %s", runResult.GetCommand(), runResult.GetStderr()).Err()
+	}
+	return nil
+}
+
+// RunForResult executes the given command with its arguments on a host over
+// ssh. A RunResult is returned, which can be used to determine the success
+// of the command execution.
+func (r *routerSshRunner) RunForResult(ctx context.Context, timeout time.Duration, inBackground bool, cmd string, args ...string) ssh.RunResult {
+	return r.sshAccess.Run(ctx, &tlw.RunRequest{
+		SshUsername:  r.sshUsernameOverride,
+		Resource:     r.resource,
+		Timeout:      durationpb.New(timeout),
+		InBackground: inBackground,
+		Command:      cmd,
+		Args:         args,
+	})
+}
