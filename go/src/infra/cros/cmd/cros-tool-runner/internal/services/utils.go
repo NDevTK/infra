a0b77c171f4a705be6e6a42f26b45ba5cbf25c1a
Derek Beckett <dbeckett@chromium.org>
2023-06-23 17:10:16 +0000

    CTRv1: Pull by digest
    Something is causing tags to go flaky, digest more reliable.
    
    BUG=b:288431281
    TEST=led/cq
    
    Change-Id: Ia7695daeca7aeb8c875748f09a7bb90579fcaecc
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4639205
    Reviewed-by: Chris DeLaGarza <cdelagarza@google.com>
    Auto-Submit: Derek Beckett <dbeckett@chromium.org>
    Reviewed-by: Azizur Rahman <azrahman@google.com>
    Commit-Queue: Derek Beckett <dbeckett@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#57684}

--- go/src/infra/cros/cmd/cros-tool-runner/internal/services/utils.go
+++ go/src/infra/cros/cmd/cros-tool-runner/internal/services/utils.go
@@ -60,9 +60,8 @@ func createImagePath(i *build_api.ContainerImageInfo) (string, error) {
 	if len(i.GetTags()) == 0 {
 		return "", errors.Reason("create image path: no tags found").Err()
 	}
-	// TODO: update logic ow to choose tags.
-	tag := i.GetTags()[0]
-	path := fmt.Sprintf("%s/%s/%s:%s", r.GetHostname(), r.GetProject(), i.GetName(), tag)
+
+	path := fmt.Sprintf("%s/%s/%s@%s", r.GetHostname(), r.GetProject(), i.GetName(), i.GetDigest())
 	log.Printf("container image: %q", path)
 	return path, nil
 }
