63255604770a745e5c6926b5394b76c3b2ac9d4b
Robert Iannucci <iannucci@chromium.org>
2023-06-29 15:18:40 +0000

    [cfggrab] Add ability to process service configs.
    Wanted to parse projects.cfg as JSON.
    
    Now you can invoke cfggrab like:
    
        cfggrab -service-cfg service-name/filename
    
    JSON conversion, etc. work as well.
    
    R=vadimsh
    
    Change-Id: I0380cc02ad7e01c892e61bd0870bf7181731e183
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4651595
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Auto-Submit: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#57868}

--- go/src/infra/tools/cfggrab/main.go
+++ go/src/infra/tools/cfggrab/main.go
@@ -7,8 +7,8 @@
 // By default prints all fetched configs to stdout, prefixing each printed
 // line with the project name for easier grepping.
 //
-// If `-output-dir` is given, stores the fetched files into per-project
-// <output-dir>/<project>.cfg.
+// If `-outputDir-dir` is given, stores the fetched files into per-project
+// <outputDir-dir>/<project>.cfg.
 //
 // Usage:
 //
@@ -24,6 +24,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"path"
 	"path/filepath"
 	"sync"
 
@@ -36,7 +37,7 @@ import (
 	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/common/logging/gologger"
-	projectpb "go.chromium.org/luci/common/proto/config"
+	configpb "go.chromium.org/luci/common/proto/config"
 	realmspb "go.chromium.org/luci/common/proto/realms"
 	"go.chromium.org/luci/common/sync/parallel"
 	"go.chromium.org/luci/config"
@@ -54,7 +55,8 @@ import (
 var outputDir = flag.String("output-dir", "-", "Where to store fetched files or - to print them to stdout for grepping")
 var configService = flag.String("config-service-host", chromeinfra.ConfigServiceHost, "Hostname of LUCI Config service to query")
 var convertJSON = flag.Bool("convert-json", false, ("Convert fetched files to JSONPB (best-effort)." +
-	" If output to -, wraps messages in {\"proj\": ..., \"data\": ...}."))
+	" If output-dir is -, wraps messages in {\"proj\": ..., \"data\": ...}."))
+var isServiceCfg = flag.Bool("service-cfg", false, "Treat the config name a service-name/file and load it as service config")
 
 // quick and dirty map for known config file names to the proto message they
 // contain.
@@ -70,7 +72,8 @@ var messageMap map[string]protoreflect.Message = map[string]protoreflect.Message
 	"luci-notify.cfg":        (*notifypb.ProjectConfig)(nil).ProtoReflect(),
 	"luci-scheduler-dev.cfg": (*schedulerpb.ProjectConfig)(nil).ProtoReflect(),
 	"luci-scheduler.cfg":     (*schedulerpb.ProjectConfig)(nil).ProtoReflect(),
-	"project.cfg":            (*projectpb.ProjectCfg)(nil).ProtoReflect(),
+	"project.cfg":            (*configpb.ProjectCfg)(nil).ProtoReflect(),
+	"projects.cfg":           (*configpb.ProjectsCfg)(nil).ProtoReflect(),
 	"realms-dev.cfg":         (*realmspb.RealmsCfg)(nil).ProtoReflect(),
 	"realms.cfg":             (*realmspb.RealmsCfg)(nil).ProtoReflect(),
 	"tricium-dev.cfg":        (*triciumpb.ProjectConfig)(nil).ProtoReflect(),
@@ -79,48 +82,49 @@ var messageMap map[string]protoreflect.Message = map[string]protoreflect.Message
 
 var stdoutLock sync.Mutex
 
-func main() {
-	ctx := context.Background()
-	ctx = gologger.StdConfig.Use(ctx)
+type source struct {
+	cfgSet config.Set
+	path   string
+}
 
+func (s source) String() string {
+	return fmt.Sprintf("%s/%s", s.cfgSet, s.path)
+}
+
+func main() {
 	flag.Parse()
 	if len(flag.Args()) != 1 {
 		fmt.Fprintf(os.Stderr, "Expecting one positional argument with the name of the file to fetch.\n")
 		os.Exit(2)
 	}
 
-	path := flag.Arg(0)
+	toLoad := flag.Arg(0)
 
 	var msg protoreflect.Message
 	if *convertJSON {
 		var ok bool
-		if msg, ok = messageMap[flag.Arg(0)]; !ok {
-			fmt.Fprintf(os.Stderr, "Cannot convert %q to JSON: unknown file", path)
+		fileName := path.Base(toLoad)
+		if msg, ok = messageMap[fileName]; !ok {
+			fmt.Fprintf(os.Stderr, "Cannot convert %q to JSON: unknown file", toLoad)
 			os.Exit(2)
 		}
 	}
 
-	if err := run(ctx, path, *outputDir, msg); err != nil {
+	ctx := context.Background()
+	ctx = gologger.StdConfig.Use(ctx)
+
+	if err := mainImpl(ctx, toLoad, *outputDir, msg, *isServiceCfg); err != nil {
 		errors.Log(ctx, err)
 		os.Exit(1)
 	}
+
 }
 
-func withConfigClient(ctx context.Context) (context.Context, error) {
-	auth := auth.NewAuthenticator(ctx, auth.SilentLogin, chromeinfra.DefaultAuthOptions())
-	client, err := auth.Client()
-	if err != nil {
-		return nil, err
-	}
-	return cfgclient.Use(ctx, remote.New(*configService, false, func(context.Context) (*http.Client, error) {
-		return client, nil
-	})), nil
-}
-
-func run(ctx context.Context, path, output string, msg protoreflect.Message) error {
-	if output != "-" {
-		if err := os.MkdirAll(output, 0777); err != nil {
-			return err
+func mainImpl(ctx context.Context, toLoad, outputDir string, msg protoreflect.Message, isServiceCfg bool) error {
+	if outputDir != "-" {
+		if err := os.MkdirAll(outputDir, 0777); err != nil {
+			errors.Log(ctx, err)
+			os.Exit(1)
 		}
 	}
 
@@ -129,64 +133,106 @@ func run(ctx context.Context, path, output string, msg protoreflect.Message) err
 		return err
 	}
 
-	projects, err := cfgclient.ProjectsWithConfig(ctx, path)
+	var sources []source
+	if isServiceCfg {
+		service, toLoad := path.Split(toLoad)
+		sources = []source{{config.Set("services/" + path.Clean(service)), toLoad}}
+	} else {
+		sources, err = gatherProjectSources(ctx, toLoad)
+	}
 	if err != nil {
 		return err
 	}
 
+	return processSources(ctx, sources, processConfig(outputDir, msg))
+}
+
+func withConfigClient(ctx context.Context) (context.Context, error) {
+	auth := auth.NewAuthenticator(ctx, auth.SilentLogin, chromeinfra.DefaultAuthOptions())
+	client, err := auth.Client()
+	if err != nil {
+		return ctx, err
+	}
+	return cfgclient.Use(ctx, remote.New(*configService, false, func(context.Context) (*http.Client, error) {
+		return client, nil
+	})), nil
+}
+
+func gatherProjectSources(ctx context.Context, fileName string) ([]source, error) {
+	projects, err := cfgclient.ProjectsWithConfig(ctx, fileName)
+	if err != nil {
+		return nil, err
+	}
+	ret := make([]source, len(projects))
+	for i, proj := range projects {
+		ret[i] = source{
+			config.Set("projects/" + proj),
+			fileName,
+		}
+	}
+	return ret, err
+}
+
+func processSources(ctx context.Context, sources []source, cb func(ctx context.Context, source source, raw []byte) error) error {
 	return parallel.FanOutIn(func(work chan<- func() error) {
-		for _, proj := range projects {
-			proj := proj
+		for _, source := range sources {
+			source := source
 			work <- func() error {
-				if err := processProject(ctx, proj, path, output, msg); err != nil {
-					logging.Errorf(ctx, "Failed when processing %s: %s", proj, err)
+				var blob []byte
+				err := cfgclient.Get(ctx, source.cfgSet, source.path, cfgclient.Bytes(&blob), nil)
+				if err != nil {
 					return err
 				}
-				return nil
+
+				if err = cb(ctx, source, blob); err != nil {
+					logging.Errorf(ctx, "Failed when processing %s/%s: %s", source.cfgSet, source.path, err)
+				}
+				return err
 			}
 		}
 	})
 }
 
-func processProject(ctx context.Context, proj, path, output string, msg protoreflect.Message) error {
-	var blob []byte
-	err := cfgclient.Get(ctx, config.Set("projects/"+proj), path, cfgclient.Bytes(&blob), nil)
-	if err != nil {
-		return err
-	}
-
-	if msg != nil {
-		decoded := msg.New().Interface()
-		if err = prototext.Unmarshal(blob, decoded); err != nil {
-			return errors.Annotate(err, "decoding textpb").Err()
+func processConfig(outputDir string, msg protoreflect.Message) func(context.Context, source, []byte) error {
+	return func(ctx context.Context, source source, raw []byte) (err error) {
+		if msg != nil {
+			decoded := msg.New().Interface()
+			if err = prototext.Unmarshal(raw, decoded); err != nil {
+				return errors.Annotate(err, "decoding textpb").Err()
+			}
+			raw, err = protojson.MarshalOptions{
+				Multiline:     true,
+				Indent:        "  ",
+				UseProtoNames: true,
+			}.Marshal(decoded)
+			if err != nil {
+				return errors.Annotate(err, "encoding jsonpb").Err()
+			}
 		}
-		blob, err = protojson.MarshalOptions{
-			Multiline:     true,
-			Indent:        "  ",
-			UseProtoNames: true,
-		}.Marshal(decoded)
-		if err != nil {
-			return errors.Annotate(err, "encoding jsonpb").Err()
-		}
-	}
 
-	if output != "-" {
+		if outputDir != "-" {
+			stdoutLock.Lock()
+			defer stdoutLock.Unlock()
+			dest := filepath.Join(outputDir, source.String())
+			fmt.Printf("writing %s\n", dest)
+			if err = os.MkdirAll(filepath.Dir(dest), 0777); err == nil {
+				err = ioutil.WriteFile(dest, raw, 0666)
+			}
+			return
+		}
+
 		stdoutLock.Lock()
 		defer stdoutLock.Unlock()
-		fmt.Printf("writing %s\n", proj)
-		return ioutil.WriteFile(filepath.Join(output, proj+".cfg"), blob, 0666)
-	}
-
-	stdoutLock.Lock()
-	defer stdoutLock.Unlock()
-	if msg == nil {
-		// raw
-		for _, line := range bytes.Split(blob, []byte{'\n'}) {
-			fmt.Printf("%s: %s\n", proj, line)
+		if msg == nil {
+			// raw
+			for _, line := range bytes.Split(raw, []byte{'\n'}) {
+				fmt.Printf("%s: %s\n", source.cfgSet, line)
+			}
+		} else {
+			// json
+			fmt.Printf(`{"source": {"configSet": %q, "path": %q}, "data": %s}`, source.cfgSet, source.path, raw)
+			fmt.Println()
 		}
-	} else {
-		// json
-		fmt.Printf("{\"project\": \"%s\", \"data\": %s}\n", proj, blob)
+		return nil
 	}
-	return nil
 }
