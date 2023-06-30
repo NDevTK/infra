d640d93577610bcfa646d462805ddc2e358fbcf7
Dmitri Shuralyov <dmitshur@google.com>
2023-06-30 17:27:18 +0000

    [golangbuild] disable gRPC generation in golangbuildpb
    Regenerating golangbuildpb with the latest versions of protoc
    and protoc-gen-go fails with:
    
    $ go generate ./golangbuildpb
    --go_out: protoc-gen-go: plugins are not supported; use 'protoc --go-grpc_out=...' to generate gRPC
    [...]
    
    cproto can be configured to use protoc-gen-go-grpc instead of
    protoc-gen-go for gRPC generation via its -use-grpc-plugin flag.
    But since we don't use gRPC in golangbuildpb, disable it instead.
    
    Change-Id: I606e8987f0746a2e27ba01cdd48b6f726a062f03
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4649917
    Reviewed-by: Michael Knyszek <mknyszek@google.com>
    Commit-Queue: Dmitri Shuralyov <dmitshur@google.com>
    Cr-Commit-Position: refs/heads/main@{#57934}

--- go/src/infra/experimental/golangbuild/golangbuildpb/gen.go
+++ go/src/infra/experimental/golangbuild/golangbuildpb/gen.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-//go:generate cproto
+//go:generate cproto -disable-grpc
 
 // Package golangbuildpb contains protos with inputs and outputs of golangbuild.
 package golangbuildpb
