6985237006e3b96bf183f12cf3cab76b2ccfa890
Jared Bennett <jaredbennett@google.com>
2023-07-06 15:15:02 -0700

    recovery: Add ssh runner interface for routers
    Adds a Runner interface in a new ssh package that will be used to
    govern ssh command execution. Abstract out TLW and HostAccess
    usage from building ssh commands and receiving their results. Is
    compatibly with the current TLW runner result and implementations
    can use TLW as the runner behind the scenes.
    
    In the future the idea is to generalize this for ssh commands on
    any host, but due to time constraints of dependent features this
    will be limited for router controllers for now. It can be refactored
    for general host access later.
    
    BUG=b:282836502
    TEST=cd ~/cr/infra/go/src/infra/cros/recovery && go clean \
    && go build && go test ./...
    
    Change-Id: I1bd592ffc9f385fbc55d92c8341e42f9d6ab1e75

---
+++ go/src/infra/cros/recovery/internal/execs/wifirouter/ssh/runner.go
@@ -0,0 +1,50 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ssh
+
+import (
+	"context"
+	"time"
+
+	"infra/cros/recovery/tlw"
+)
+
+// Access provides a single method for executing ssh commands on devices.
+type Access interface {
+	// Run executes command on device by SSH related to resource name.
+	Run(ctx context.Context, request *tlw.RunRequest) *tlw.RunResult
+}
+
+// RunResult provides functions for retrieving the result of an ssh command call.
+type RunResult interface {
+	// GetCommand returns the full command executed on the resource over ssh.
+	GetCommand() string
+	// GetStdout returns the stdout of the command execution.
+	GetStdout() string
+	// GetStderr returns the stderr of the command execution.
+	GetStderr() string
+	// GetExitCode returns the exit code of the command execution.
+	GetExitCode() int32
+}
+
+// Runner provides functions for executing ssh commands on a host.
+type Runner interface {
+	// Run executes the given command with its arguments on a host over ssh. The
+	// stdout of the command execution is returned. If the command returns a
+	// non-zero exit code, a non-nil error is returned.
+	//
+	// Can be used as a components.Runner.
+	Run(ctx context.Context, timeout time.Duration, cmd string, args ...string) (string, error)
+
+	// RunInBackground starts to execute the given command with its arguments on a
+	// host over ssh, but does so in a background process and does not wait for it
+	// to finish before returning.
+	RunInBackground(ctx context.Context, timeout time.Duration, cmd string, args ...string)
+
+	// RunForResult executes the given command with its arguments on a host over
+	// ssh. A RunResult is returned, which can be used to determine the success
+	// of the command execution.
+	RunForResult(ctx context.Context, timeout time.Duration, inBackground bool, cmd string, args ...string) RunResult
+}
