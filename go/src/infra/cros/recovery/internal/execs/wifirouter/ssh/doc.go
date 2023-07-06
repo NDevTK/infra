cb10a80fa915774214be2a9aade3c4f5e5aeabf1
Jared Bennett <jaredbennett@google.com>
2023-07-06 15:27:14 -0700

    recovery: Add common ssh command functions
    Adds a handful of utility functions for running common ssh
    commands. All of which will be used by upcoming router
    controllers.
    
    BUG=b:282836502
    TEST=cd ~/cr/infra/go/src/infra/cros/recovery && go clean \
    && go build && go test ./...
    
    Change-Id: I52c34962cadd181a3080ae764638d37faa965d20

--- go/src/infra/cros/recovery/internal/execs/wifirouter/ssh/doc.go
+++ go/src/infra/cros/recovery/internal/execs/wifirouter/ssh/doc.go
@@ -6,4 +6,9 @@
 // commands on hosts.
 //
 // TODO(jaredbennett/otabek): Move this out from under the wifirouter execs to a general location
+//
+// Some common, simple bash command functions are included in this package as
+// well, but more complex or uncommon bash command functions should not be
+// included in this package. Rather, those should be closer to their usage and
+// have unit tests.
 package ssh
