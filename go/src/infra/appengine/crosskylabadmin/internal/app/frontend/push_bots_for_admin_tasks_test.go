1633d8599c14a8055098d1fd0ec7d8a28e558452
Greg NISBET <gregorynisbet@google.com>
2023-07-11 17:37:20 -0700

    [smart-scheduling] Move implementation of push-bots-for-admin-tasks RPC to own file
    This refactoring is part of splitting up https://chromium-review.googlesource.com/c/infra/infra/+/4657226
    
    BUG=b:268078240
    TEST=make test
    
    Change-Id: Ic8a1a794fd904ef4bf1ae08ea35d59e40346fe77

---
+++ go/src/infra/appengine/crosskylabadmin/internal/app/frontend/push_bots_for_admin_tasks_test.go
@@ -0,0 +1,74 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package frontend
+
+import (
+	"context"
+	"testing"
+
+	swarming "go.chromium.org/luci/common/api/swarming/swarming/v1"
+	"go.chromium.org/luci/common/data/strpair"
+
+	fleet "infra/appengine/crosskylabadmin/api/fleet/v1"
+	"infra/appengine/crosskylabadmin/internal/app/clients"
+	"infra/appengine/crosskylabadmin/internal/app/config"
+)
+
+func TestPushBotsForAdminTasksImplSmokeTest(t *testing.T) {
+	tf, validate := newTestFixture(t)
+	defer validate()
+	ctx := tf.C
+	ctx = config.Use(ctx, &config.Config{
+		Swarming: &config.Swarming{
+			BotPool: "fake-bot-pool",
+		},
+	})
+	var swarmingClient clients.SwarmingClient
+	swarmingClient = &fakeSwarmingClient{}
+	req := &fleet.PushBotsForAdminTasksRequest{
+		TargetDutState: fleet.DutState_NeedsRepair,
+	}
+	_, err := pushBotsForAdminTasksImpl(ctx, swarmingClient, req)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+}
+
+type fakeSwarmingClient struct{}
+
+func (sc *fakeSwarmingClient) ListAliveIdleBotsInPool(c context.Context, pool string, dims strpair.Map) ([]*swarming.SwarmingRpcsBotInfo, error) {
+	return []*swarming.SwarmingRpcsBotInfo{
+		{
+			BotId: "fake-bot-1",
+		},
+		{
+			BotId: "fake-bot-2",
+		},
+	}, nil
+}
+
+func (sc *fakeSwarmingClient) ListAliveBotsInPool(context.Context, string, strpair.Map) ([]*swarming.SwarmingRpcsBotInfo, error) {
+	panic("ListAliveBotsInPool")
+}
+
+func (sc *fakeSwarmingClient) ListBotTasks(id string) clients.BotTasksCursor {
+	panic("ListBotTasks")
+}
+
+func (sc *fakeSwarmingClient) ListRecentTasks(c context.Context, tags []string, state string, limit int) ([]*swarming.SwarmingRpcsTaskResult, error) {
+	panic("ListRecentTasks")
+}
+
+func (sc *fakeSwarmingClient) ListSortedRecentTasksForBot(c context.Context, botID string, limit int) ([]*swarming.SwarmingRpcsTaskResult, error) {
+	panic("ListSortedRecentTasksForBot")
+}
+
+func (sc *fakeSwarmingClient) CreateTask(c context.Context, name string, args *clients.SwarmingCreateTaskArgs) (string, error) {
+	panic("CreateTask")
+}
+
+func (sc *fakeSwarmingClient) GetTaskResult(ctx context.Context, tid string) (*swarming.SwarmingRpcsTaskResult, error) {
+	panic("GetTaskResult")
+}
