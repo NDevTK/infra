e438dd4b18d40733455cb5488075a0ebaaf5af81
Greg NISBET <gregorynisbet@google.com>
2023-07-26 20:29:45 +0000

    [recovery] Add go mock
    BUG=b:268078240
    TEST=make test
    
    Change-Id: Iffbc86adebd64f0fa3ada7dbda919a60916d3ebd
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4706286
    Reviewed-by: Garry Wang <xianuowang@chromium.org>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Cr-Commit-Position: refs/heads/main@{#58477}

---
+++ go/src/infra/cros/recovery/logger/metrics/generate-metrics-interface.go
@@ -0,0 +1,7 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package metrics
+
+//go:generate mockgen -copyright_file copyright.txt -source metrics.go -destination mockmetrics/metrics.mock.go -package mockmetrics
