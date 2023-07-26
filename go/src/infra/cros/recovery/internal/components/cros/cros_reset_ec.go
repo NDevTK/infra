093dbda4cf6b92f5f9ac9baebc412f6d5d411e96
Gowri Denduluri <gowriden@google.com>
2023-07-26 16:41:28 +0000

    [Paris] Adding support for "ec_reboot" and "reboot_ec" commands for "ectool".
    Runs the ectool help command to determine whether the board supports "ec_reboot" or "reboot_ec" option and runs the ectool reboot command accordingly.
    
    Bug: b/281453012
    Test: local mallet run
    Change-Id: I40b1a345597be01048160312312f80989256e77e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4706856
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Gowri Denduluri <gowriden@google.com>
    Cr-Commit-Position: refs/heads/main@{#58464}

---
+++ go/src/infra/cros/recovery/internal/components/cros/cros_reset_ec.go
@@ -0,0 +1,58 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cros
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.chromium.org/luci/common/errors"
+
+	"infra/cros/recovery/internal/execs"
+	"infra/cros/recovery/internal/log"
+)
+
+// Finds the command to reboot ec and executes it.
+func RebootECByEcTool(ctx context.Context, r execs.Runner) error {
+	ecRebootCmds := []string{"ec_reboot", "reboot_ec"}
+
+	var rebootCmd = ""
+	for _, cmd := range ecRebootCmds {
+		fmtCmd := fmt.Sprintf("ectool help | grep %s", cmd)
+		output, err := r(ctx, 5*time.Second, fmtCmd)
+		log.Debugf(ctx, "RebootECByEcTool EC Reboot command help output : %s", output)
+		if err != nil {
+			errorCode, ok := errors.TagValueIn(execs.ErrCodeTag, err)
+			if !ok {
+				return errors.Annotate(err, "RebootECByEcTool: cannot find error code").Err()
+			}
+			// The errorCode value stored in the empty interface in the
+			// error tag is of type int32. To compare it with an integer
+			// literal, we need to convert the literal into int32 value,
+			// otherwise the comparison will always return 'false'.
+			if errorCode == int32(1) {
+				continue
+			}
+		}
+		rebootCmd = cmd
+		break
+	}
+	if rebootCmd == "" {
+		return errors.New("RebootECByEcTool: cannot find ectool reboot command through ectool help command")
+	}
+	log.Infof(ctx, "Found EC Reboot command : %s", rebootCmd)
+
+	// Command to reset EC from DUT side.
+	ecResetCmd := fmt.Sprintf("ectool %s cold && exit", rebootCmd)
+	if out, err := r(ctx, 30*time.Second, ecResetCmd); err != nil {
+		// Client closed connected as rebooting.
+		log.Debugf(ctx, "Client exit as device rebooted: %s", err)
+		return errors.Annotate(err, "RebootECByEcTool").Err()
+	} else {
+		log.Debugf(ctx, "Stdout: %s", out)
+	}
+	return nil
+}
--- go/src/infra/cros/recovery/internal/execs/cros/cros_ectool_execs.go
+++ go/src/infra/cros/recovery/internal/execs/cros/cros_ectool_execs.go
@@ -1,4 +1,4 @@
-// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Copyright 2023 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,8 +8,7 @@ import (
 	"context"
 	"time"
 
-	"go.chromium.org/luci/common/errors"
-
+	"infra/cros/recovery/internal/components/cros"
 	"infra/cros/recovery/internal/execs"
 	"infra/cros/recovery/internal/log"
 )
@@ -22,19 +21,10 @@ func resetEcExec(ctx context.Context, info *execs.ExecInfo) error {
 	argsMap := info.GetActionArgs(ctx)
 	// Delay to wait for the ec reset command to be efftive. Default to be 30s.
 	waitTimeout := argsMap.AsDuration(ctx, "wait_timeout", 30, time.Second)
-	// TODO(otabek): Figure out why, in PARIS,
-	// we must add '&& exit' to make ssh proxy return immediately without
-	// waiting for the command to finish execution.
-	//
-	// Command to reset EC from DUT side.
-	const ecResetCmd = "ectool reboot_ec cold && exit"
-	run := info.NewRunner(info.GetDut().Name)
-	if out, err := run(ctx, 30*time.Second, ecResetCmd); err != nil {
-		// Client closed connected as rebooting.
-		log.Debugf(ctx, "Client exit as device rebooted: %s", err)
-		return errors.Annotate(err, "reset ec").Err()
-	} else {
-		log.Debugf(ctx, "Stdout: %s", out)
+
+	// Reset EC from DUT side.
+	if err := cros.RebootECByEcTool(ctx, info.NewRunner(info.GetDut().Name)); err != nil {
+		return err
 	}
 	log.Debugf(ctx, "waiting for %d seconds to let ec reset be effective.", waitTimeout)
 	time.Sleep(waitTimeout)
