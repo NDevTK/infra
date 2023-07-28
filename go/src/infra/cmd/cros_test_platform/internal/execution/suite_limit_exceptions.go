04d0df44a278a928ac520dc49f97255180c84f42
Julio Hurtado <juahurta@google.com>
2023-07-28 23:32:55 +0000

    CTP: extend and grant new exemptions for CTS
    BUG=b:293522184, b:293609501
    TEST=None
    
    Change-Id: Ieb2e57d96fe4ae98957ce8de4184a54a45241831
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4730321
    Reviewed-by: Jard Loucks <jaredloucks@google.com>
    Commit-Queue: Jard Loucks <jaredloucks@google.com>
    Commit-Queue: Julio Hurtado <juahurta@google.com>
    Auto-Submit: Julio Hurtado <juahurta@google.com>
    Cr-Commit-Position: refs/heads/main@{#58566}

--- go/src/infra/cmd/cros_test_platform/internal/execution/suite_limit_exceptions.go
+++ go/src/infra/cmd/cros_test_platform/internal/execution/suite_limit_exceptions.go
@@ -32,20 +32,32 @@ var exceptions = []suiteFilter{
 		expiration: time.Date(2023, time.September, 30, 0, 0, 0, 0, time.UTC),
 	},
 	{
+		suiteName:  "arc-cts-long",
+		expiration: time.Date(2099, time.September, 30, 0, 0, 0, 0, time.UTC),
+	},
+	{
 		suiteName:  "arc-cts-camera-opendut",
 		expiration: time.Date(2023, time.September, 30, 0, 0, 0, 0, time.UTC),
 	},
 	{
 		suiteName:  "arc-cts-hardware",
-		expiration: time.Date(2023, time.September, 30, 0, 0, 0, 0, time.UTC),
+		expiration: time.Date(2024, time.January, 8, 0, 0, 0, 0, time.UTC),
 	},
 	{
 		suiteName:  "arc-cts-qual",
-		expiration: time.Date(2023, time.September, 30, 0, 0, 0, 0, time.UTC),
+		expiration: time.Date(2024, time.January, 8, 0, 0, 0, 0, time.UTC),
+	},
+	{
+		suiteName:  "arc-cts-qual-long",
+		expiration: time.Date(2099, time.January, 8, 0, 0, 0, 0, time.UTC),
 	},
 	{
 		suiteName:  "arc-cts-vm-stable",
 		expiration: time.Date(2023, time.September, 30, 0, 0, 0, 0, time.UTC),
+	},
+	{
+		suiteName:  "arc-cts-vm-stable-long",
+		expiration: time.Date(2099, time.September, 30, 0, 0, 0, 0, time.UTC),
 	},
 	{
 		suiteName:  "arc-gts",
@@ -53,7 +65,11 @@ var exceptions = []suiteFilter{
 	},
 	{
 		suiteName:  "arc-gts-qual",
-		expiration: time.Date(2023, time.September, 30, 0, 0, 0, 0, time.UTC),
+		expiration: time.Date(2024, time.January, 8, 0, 0, 0, 0, time.UTC),
+	},
+	{
+		suiteName:  "arc-gts-qual-long",
+		expiration: time.Date(2099, time.January, 8, 0, 0, 0, 0, time.UTC),
 	},
 	{
 		suiteName:  "arc-sts-full",
