d96fd38e67a55f524f95ad9f5c1f3fcf16e7810e
Jared Bennett <jaredbennett@google.com>
2023-08-03 18:12:40 -0700

    recovery: Add basic OpenWrt router controller
    Adds a new router controller for OpenWrt routers that retrieves
    the model and supported features from the build info file present on
    all ChromeOS OpenWrt test routers. The router identification and
    controller initialization steps have been updated to support this
    new controller type. A new exec has been added to trigger the
    retrieval of the build info file.
    
    BUG=b:282836912
    TEST=cd ~/cr/infra/go/src/infra/cros/recovery && go clean && go build \
    && go test ./...
    TEST=successfully ran the repair plan with an openwrt router with a
    custom config that calls the new exec and verified the router output
    matched the model and supported features for the device
    
    Change-Id: I286da8657a77d9df478cf0878db5d11df3f5844b

---
+++ go/src/infra/cros/recovery/internal/execs/wifirouter/wifirouter_execs_openwrt.go
@@ -0,0 +1,27 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package wifirouter
+
+import (
+	"context"
+
+	"go.chromium.org/luci/common/errors"
+	"infra/cros/recovery/internal/execs"
+)
+
+func fetchOpenWrtBuildInfoExec(ctx context.Context, info *execs.ExecInfo) error {
+	c, err := activeHostOpenWrtRouterController(ctx, info)
+	if err != nil {
+		return errors.Annotate(err, "fetch openwrt build info").Err()
+	}
+	if err := c.FetchDeviceBuildInfo(ctx); err != nil {
+		return errors.Annotate(err, "failed to fetch device build info").Err()
+	}
+	return nil
+}
+
+func init() {
+	execs.Register("wifi_router_openwrt_fetch_build_info", fetchOpenWrtBuildInfoExec)
+}
