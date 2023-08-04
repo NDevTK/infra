0984edab1b1f8f56721da3b7c78520aa6068dafc
Jared Bennett <jaredbennett@google.com>
2023-08-03 18:24:17 -0700

    recovery: Add OpenWrt router image selection
    Adds two new OpenWrt execs which use new support in the OpenWrt
    router controller that allow for identifying the image a router
    should have using the global config and checking whether the
    router has that image installed.
    
    The image selection logic is copied from similar logic in the
    cros_openwrt_image_builder CLI tool, which manages the global config
    file based on its proto definition. Unit tests have been added to
    validate the expected selection behavior.
    
    BUG=b:282836912
    TEST=cd ~/cr/infra/go/src/infra/cros/recovery && go clean && go build \
    && go test ./...
    
    Change-Id: Ibfb63510ae33a0aa5a797f12c627ed22a85fcecc

--- go/src/infra/cros/recovery/internal/execs/wifirouter/wifirouter_execs_openwrt.go
+++ go/src/infra/cros/recovery/internal/execs/wifirouter/wifirouter_execs_openwrt.go
@@ -8,6 +8,7 @@ import (
 	"context"
 
 	"go.chromium.org/luci/common/errors"
+	"infra/cros/recovery/internal/components/cros"
 	"infra/cros/recovery/internal/execs"
 )
 
@@ -33,7 +34,43 @@ func fetchOpenWrtConfigExec(ctx context.Context, info *execs.ExecInfo) error {
 	return nil
 }
 
+func identifyExpectedOpenWrtImageExec(ctx context.Context, info *execs.ExecInfo) error {
+	c, err := activeHostOpenWrtRouterController(ctx, info)
+	if err != nil {
+		return errors.Annotate(err, "identify expected openwrt image").Err()
+	}
+	dut := info.GetDut()
+	if dut == nil {
+		return errors.Reason("dut is not present").Err()
+	}
+	provisionedInfo := info.GetDut().ProvisionedInfo
+	if provisionedInfo == nil {
+		return errors.Reason("Dut.ProvisionedInfo is not present").Err()
+	}
+	if provisionedInfo.CrosVersion == "" {
+		return errors.Reason("Dut.ProvisionedInfo.CrosVersion is empty").Err()
+	}
+	crosReleaseVersion, err := cros.ParseReleaseVersionFromBuilderPath(provisionedInfo.CrosVersion)
+	if err != nil {
+		return errors.Annotate(err, "failed to parse release version from Dut.ProvisionedInfo.CrosVersion %q", provisionedInfo.CrosVersion).Err()
+	}
+	if err := c.IdentifyExpectedImage(ctx, dut.Name, crosReleaseVersion); err != nil {
+		return errors.Annotate(err, "failed to identify expected OpenWrt OS image for device").Err()
+	}
+	return nil
+}
+
+func assertOpenWrtRouterHasExpectedImageExec(ctx context.Context, info *execs.ExecInfo) error {
+	c, err := activeHostOpenWrtRouterController(ctx, info)
+	if err != nil {
+		return errors.Annotate(err, "assert openwrt router has expected image").Err()
+	}
+	return c.AssertHasExpectedImage()
+}
+
 func init() {
 	execs.Register("wifi_router_openwrt_fetch_build_info", fetchOpenWrtBuildInfoExec)
 	execs.Register("wifi_router_openwrt_fetch_config", fetchOpenWrtConfigExec)
+	execs.Register("wifi_router_openwrt_identify_expected_image", identifyExpectedOpenWrtImageExec)
+	execs.Register("wifi_router_openwrt_has_expected_image", assertOpenWrtRouterHasExpectedImageExec)
 }
