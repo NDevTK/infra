e00e5c2cc78c8e31ff3d135d1855f75e42578b91
Jared Bennett <jaredbennett@google.com>
2023-08-05 00:17:32 +0000

    recovery: Add WgetURL to ssh command utils
    Adds an ssh command util function for running the "wget" command on
    remote hosts. In addition to stdout and stderr, the HTTP error code
    may be returned when wget returns a non-zero exit code. This HTTP
    error code is parsed from the stderr. Unit tests have been included
    as well to validate the handling of wget errors.
    
    BUG=b:282836912
    TEST=cd ~/cr/infra/go/src/infra/cros/recovery && go clean && go build \
    && go test ./...
    
    Change-Id: Ifb2839054405e51e071a4a6999fec6617716e763
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4749458
    Commit-Queue: Jared Bennett <jaredbennett@google.com>
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/main@{#58723}

---
+++ go/src/infra/cros/recovery/internal/execs/wifirouter/ssh/mocks/commands_test.go
@@ -0,0 +1,116 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package mocks
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"infra/cros/recovery/internal/execs/wifirouter/ssh"
+	"infra/cros/recovery/tlw"
+)
+
+// TestWgetURL tests the ssh.WgetURL function.
+//
+// Note: This is in the mocks package because it cannot use mocks and be in th
+// package the mocks depend on, as that would introduce an import loop.
+func TestWgetURL(t *testing.T) {
+	const testURL = "https://google.com"
+	tests := []struct {
+		name                      string
+		additionalWgetArgs        []string
+		wgetRunResult             *tlw.RunResult
+		wantHttpErrorResponseCode int
+		wantErr                   bool
+	}{
+		{
+			"no additional args, success response",
+			[]string{},
+			&tlw.RunResult{
+				ExitCode: 0,
+			},
+			0,
+			false,
+		},
+		{
+			"additional args, success response",
+			[]string{"a", "b", "c"},
+			&tlw.RunResult{
+				ExitCode: 0,
+			},
+			0,
+			false,
+		},
+		{
+			"no additional args, success response, non-empty stdout",
+			[]string{},
+			&tlw.RunResult{
+				Stdout:   "test stdout",
+				ExitCode: 0,
+			},
+			0,
+			false,
+		},
+		{
+			"example error response, with http code",
+			[]string{},
+			&tlw.RunResult{
+				Stderr:   "HTTP error 404",
+				ExitCode: 8,
+			},
+			404,
+			true,
+		},
+		{
+			"example non-http error response",
+			[]string{},
+			&tlw.RunResult{
+				Stderr:   "Failed to allocate uclient context",
+				ExitCode: 1,
+			},
+			-1,
+			true,
+		},
+		{
+			"example non-http error response",
+			[]string{},
+			&tlw.RunResult{
+				Stderr:   "Failed to allocate uclient context",
+				ExitCode: 1,
+			},
+			-1,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			mockRunner := NewMockRunner(ctrl)
+			var runCmdArgs []interface{}
+			runCmdArgs = append(runCmdArgs, testURL)
+			for _, arg := range tt.additionalWgetArgs {
+				runCmdArgs = append(runCmdArgs, arg)
+			}
+			mockRunner.EXPECT().
+				RunForResult(gomock.Any(), gomock.Any(), false, "wget", runCmdArgs...).
+				Return(tt.wgetRunResult)
+			gotStdout, gotStderr, gotHttpErrorResponseCode, err := ssh.WgetURL(context.Background(), mockRunner, 0, testURL, tt.additionalWgetArgs...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("WgetURL() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotStdout != tt.wgetRunResult.Stdout {
+				t.Errorf("WgetURL() gotStdout = %v, want %v", gotStdout, tt.wgetRunResult.Stdout)
+			}
+			if gotStderr != tt.wgetRunResult.Stderr {
+				t.Errorf("WgetURL() gotStderr = %v, want %v", gotStderr, tt.wgetRunResult.Stderr)
+			}
+			if gotHttpErrorResponseCode != tt.wantHttpErrorResponseCode {
+				t.Errorf("WgetURL() gotHttpErrorResponseCode = %v, want %v", gotHttpErrorResponseCode, tt.wantHttpErrorResponseCode)
+			}
+		})
+	}
+}
