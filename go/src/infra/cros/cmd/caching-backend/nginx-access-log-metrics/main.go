a6d9f36a5a1782b03ad1e321285f450880e7bd18
Congbin Guo <guocb@google.com>
2023-08-28 18:52:16 +0000

    refactor(caching): parse nginx log in json format
    BUG=b:297599889
    TEST=unit test
    
    Change-Id: Ia733e5404e475ed2206993bfb8e1db9e8871fae5
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4814897
    Auto-Submit: Congbin Guo <guocb@chromium.org>
    Commit-Queue: Congbin Guo <guocb@chromium.org>
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/main@{#59279}

--- go/src/infra/cros/cmd/caching-backend/nginx-access-log-metrics/main.go
+++ go/src/infra/cros/cmd/caching-backend/nginx-access-log-metrics/main.go
@@ -6,12 +6,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"os"
-	"regexp"
-	"strconv"
 	"strings"
 	"time"
 
@@ -28,16 +27,16 @@ import (
 )
 
 type record struct {
-	timestamp     time.Time
+	Timestamp     time.Time `json:"access_time"`
 	hostname      string
-	clientIP      string
-	httpMethod    string
-	path          string
-	status        int
-	bodyBytesSent int
-	expectedSize  int
-	requestTime   float64
-	cacheStatus   string
+	ClientIP      string  `json:"remote_addr"`
+	HttpMethod    string  `json:"method"`
+	Path          string  `json:"uri"`
+	Status        int     `json:"status"`
+	BodyBytesSent int     `json:"bytes_sent"`
+	ExpectedSize  int     `json:"content_length"`
+	RequestTime   float64 `json:"request_time"`
+	CacheStatus   string  `json:"upstream_cache_status"`
 }
 
 var (
@@ -102,92 +101,28 @@ func innerMain() error {
 	return nil
 }
 
-// See https://chromium.googlesource.com/infra/infra/+/refs/heads/main/go/src/infra/cros/cmd/caching-backend/conf-creator/conf_templates.go#55
-// for the detailed log format definition.
-// An example log line:
-// 127.0.0.1 - - [2021-06-09T20:24:39+00:00] "GET /download/abc HTTP/1.1" 200 369 "-" 0.000 "-" "curl/7.66.0" "-" -
-const logLinePattern = `^(?P<client_ip>\S+) - \S+ \[(?P<timestamp>[^\]]+)\] ` +
-	`"(?P<http_method>\S+) (?P<path>\S+)[^"]*" (?P<status>\d+) (?P<body_bytes_sent>\d+) "(?P<upstream_http_content_length>\S+)" ` +
-	`(?P<request_time>\d+[\.\d]+) "[^"]+" "[^"]+" "[^"]+" (?P<cache_status>\S+)`
-
-var logRegex = regexp.MustCompile(logLinePattern)
-
-// parseLine parses a log line and generate a record if the line matches the
-// predefined pattern.
+// parseLine parses a log line and generate a record if the line is want.
 func parseLine(line string) *record {
-	matches := logRegex.FindStringSubmatch(line)
-	if len(matches) == 0 {
+	r := record{}
+	if err := json.Unmarshal([]byte(line), &r); err != nil {
+		log.Printf("Parsing error: %s", err)
 		return nil
 	}
 
-	// groups is a map from group name to its value.
-	groups := make(map[string]string)
-	for i, group := range logRegex.SubexpNames() {
-		// Handle the case of "entire matching group" (i.e. index == 0) and
-		// unnamed group.
-		if i == 0 || group == "" {
-			continue
-		}
-		groups[group] = matches[i]
-	}
-	status, err := strconv.Atoi(groups["status"])
-	if err != nil {
-		log.Printf("Parse status: %s", err)
-		return nil
-	}
 	// Ignore client side errors (4xx) except NotFound(404).
 	// We may receive many client bad requests from some service scanner, so
 	// ignore them.
-	if status >= 400 && status < 500 && status != 404 {
-		log.Printf("Ignore client error %d", status)
+	if r.Status >= 400 && r.Status < 500 && r.Status != 404 {
+		log.Printf("Ignore client error %d", r.Status)
 		return nil
 	}
-
 	// Ignore non cache related requests.
-	path := groups["path"]
-	if ignoredPath(path) {
-		log.Printf("Ignore path %q", path)
-		return nil
-	}
-	timestamp, err := time.Parse("2006-01-02T15:04:05-07:00", groups["timestamp"])
-	if err != nil {
-		log.Printf("Parse timestamp: %s", err)
-		return nil
-	}
-	requestTime, err := strconv.ParseFloat(groups["request_time"], 64)
-	if err != nil {
-		log.Printf("Parse request time: %s", err)
-		return nil
-	}
-	bodyBytesSent, err := strconv.Atoi(groups["body_bytes_sent"])
-	if err != nil {
-		log.Printf("Parse body_bytes_sent: %s", err)
+	if ignoredPath(r.Path) {
+		log.Printf("Ignore path %q", r.Path)
 		return nil
 	}
 
-	var expectedSize int
-	if groups["upstream_http_content_length"] == "-" {
-		// We use -1 to indicate that the size is unknown.
-		expectedSize = -1
-	} else {
-		expectedSize, err = strconv.Atoi(groups["upstream_http_content_length"])
-		if err != nil {
-			log.Printf("Parse upstream_http_content_length: %s", err)
-			return nil
-		}
-	}
-
-	return &record{
-		timestamp:     timestamp,
-		clientIP:      groups["client_ip"],
-		httpMethod:    groups["http_method"],
-		status:        status,
-		path:          path,
-		bodyBytesSent: bodyBytesSent,
-		expectedSize:  expectedSize,
-		requestTime:   requestTime,
-		cacheStatus:   groups["cache_status"],
-	}
+	return &r
 }
 
 // ignoredPath ignores path/URL not related to caching.
@@ -213,16 +148,16 @@ func ignoredPath(path string) bool {
 
 func (i *record) Save() (row map[string]bigquery.Value, insertID string, err error) {
 	row = map[string]bigquery.Value{
-		"timestamp":       i.timestamp,
+		"timestamp":       i.Timestamp,
 		"hostname":        i.hostname,
-		"client_ip":       i.clientIP,
-		"http_method":     i.httpMethod,
-		"status":          i.status,
-		"path":            i.path,
-		"body_bytes_sent": i.bodyBytesSent,
-		"expected_size":   i.expectedSize,
-		"request_time":    i.requestTime,
-		"cache":           i.cacheStatus,
+		"client_ip":       i.ClientIP,
+		"http_method":     i.HttpMethod,
+		"status":          i.Status,
+		"path":            i.Path,
+		"body_bytes_sent": i.BodyBytesSent,
+		"expected_size":   i.ExpectedSize,
+		"request_time":    i.RequestTime,
+		"cache":           i.CacheStatus,
 	}
 	// A unique insert ID can prevent duplicated uploading when the BigQuery
 	// client retries.
@@ -285,20 +220,20 @@ var respBytesPerSecond = metric.NewCumulativeDistribution("chromeos/caching_back
 func reportToTsMon(i *record) {
 	// Extract the rpc part from the path (e.g. "/rpc/path/to/file").
 	rpc := "unknown"
-	if s := strings.SplitN(i.path, "/", 3); len(s) > 2 {
+	if s := strings.SplitN(i.Path, "/", 3); len(s) > 2 {
 		rpc = s[1]
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	respBytesSent.Add(ctx, int64(i.bodyBytesSent), i.hostname, i.httpMethod, rpc, i.status, i.cacheStatus, i.expectedSize == i.bodyBytesSent)
+	respBytesSent.Add(ctx, int64(i.BodyBytesSent), i.hostname, i.HttpMethod, rpc, i.Status, i.CacheStatus, i.ExpectedSize == i.BodyBytesSent)
 
 	// Set the response speed metric. The minimum resolution of Nginx request
 	// time is 1 ms. For shorter cases, we just set the download speed to a
 	// high number as the speed is supposed high enough.
-	if speed := 100. * 1024 * 1024; i.requestTime < 0.001 {
-		respBytesPerSecond.Add(ctx, speed, i.httpMethod, rpc, i.status, i.cacheStatus, i.expectedSize == i.bodyBytesSent)
+	if speed := 100. * 1024 * 1024; i.RequestTime < 0.001 {
+		respBytesPerSecond.Add(ctx, speed, i.HttpMethod, rpc, i.Status, i.CacheStatus, i.ExpectedSize == i.BodyBytesSent)
 	} else {
-		respBytesPerSecond.Add(ctx, float64(i.bodyBytesSent)/i.requestTime, i.httpMethod, rpc, i.status, i.cacheStatus, i.expectedSize == i.bodyBytesSent)
+		respBytesPerSecond.Add(ctx, float64(i.BodyBytesSent)/i.RequestTime, i.HttpMethod, rpc, i.Status, i.CacheStatus, i.ExpectedSize == i.BodyBytesSent)
 	}
 }
