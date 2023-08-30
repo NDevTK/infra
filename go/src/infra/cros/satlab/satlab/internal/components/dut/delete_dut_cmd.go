f01d6ede45ff2cc70ec371e5fa3126469ff5119e
Boris <boris.lok@cienet.corp-partner.google.com>
2023-08-30 07:46:09 +0000

    [Refactoring] Create a SubCommand struct for testing and remove redundant code.
    Change-Id: I7e729cb37bff53cd5e708bfdc23dddee1fb90fde

--- go/src/infra/cros/satlab/satlab/internal/components/dut/delete_dut_cmd.go
+++ go/src/infra/cros/satlab/satlab/internal/components/dut/delete_dut_cmd.go
@@ -21,6 +21,7 @@ import (
 	"infra/cmdsupport/cmdlib"
 	"infra/cros/satlab/common/satlabcommands"
 	"infra/cros/satlab/common/site"
+	"infra/cros/satlab/common/utils/executor"
 	"infra/cros/satlab/satlab/internal/components/ufs"
 	ufsModels "infra/unifiedfleet/api/v1/models"
 	ufsApi "infra/unifiedfleet/api/v1/rpc"
@@ -69,7 +70,7 @@ func (c *deleteDUT) innerRun(a subcommands.Application, positionalArgs []string,
 
 	if c.commonFlags.SatlabID == "" {
 		var err error
-		c.commonFlags.SatlabID, err = satlabcommands.GetDockerHostBoxIdentifier()
+		c.commonFlags.SatlabID, err = satlabcommands.GetDockerHostBoxIdentifier(&executor.ExecCommander{})
 		if err != nil {
 			return errors.Annotate(err, "get dut").Err()
 		}
