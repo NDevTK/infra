d90cf268bfb5aa2b6eb87d53d3495b90c0387910
Boris <boris.lok@cienet.corp-partner.google.com>
2023-08-31 08:33:13 +0000

    [Feature] Move `UpdateDUT` to common.
    Change-Id: I6c79ae8eb9e47246d8859ce28bc990ce92ea3518

---
+++ go/src/infra/cros/satlab/common/dut/shivas/dut_updater.go
@@ -0,0 +1,42 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package shivas
+
+import (
+	"os/exec"
+
+	"infra/cros/satlab/common/commands"
+	"infra/cros/satlab/common/paths"
+	"infra/cros/satlab/common/utils/executor"
+)
+
+// DUTUpdater updates a DUT with the given name.
+type DUTUpdater struct {
+	// Name is a hostname indicate which DUT we want to update
+	Name string
+	// Executor is a command execuotr
+	Executor executor.IExecCommander
+}
+
+// Update invokes shivas with the required arguments to update information
+// about a DUT.
+func (u *DUTUpdater) Update(args map[string][]string) error {
+	flags := make(map[string][]string)
+
+	for k, v := range args {
+		flags[k] = v
+	}
+
+	flags["name"] = []string{u.Name}
+
+	command_args := (&commands.CommandWithFlags{
+		Commands: []string{paths.ShivasCLI, "update", "dut"},
+		Flags:    flags,
+	}).ToCommand()
+	// We ignore the output here because we don't need any information from
+	// the output now.
+	_, err := u.Executor.Exec(exec.Command(command_args[0], command_args[1:]...))
+	return err
+}
