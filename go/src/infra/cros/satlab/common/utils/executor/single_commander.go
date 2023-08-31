74ab80184a7f6f38c5e60a3c23d7a84dbce50d24
Boris <boris.lok@cienet.corp-partner.google.com>
2023-08-31 07:39:45 +0000

    [Refactoring] Move `ReadDNSConents` to common
    Change-Id: I89a12640767b8a266a6baa9ab51549739cdc40fc

--- go/src/infra/cros/satlab/common/utils/executor/single_commander.go
+++ go/src/infra/cros/satlab/common/utils/executor/single_commander.go
@@ -5,8 +5,11 @@ package executor
 
 import (
 	"os/exec"
+	"time"
 )
 
+// IExecCommander is a interface to define which functions
+// we can use in a commander.
 type IExecCommander interface {
 	Exec(*exec.Cmd) ([]byte, error)
 
@@ -15,6 +18,7 @@ type IExecCommander interface {
 	Wait(*exec.Cmd) error
 }
 
+// ExecCommander is a wrapper uses to execute a command.
 type ExecCommander struct{}
 
 func (e *ExecCommander) Exec(cmd *exec.Cmd) ([]byte, error) {
@@ -27,4 +31,42 @@ func (e *ExecCommander) Start(cmd *exec.Cmd) error {
 
 func (e *ExecCommander) Wait(cmd *exec.Cmd) error {
 	return cmd.Wait()
+}
+
+// FakeCommander is used to fake a result when
+// user write some test cases.
+type FakeCommander struct {
+	CmdOutput string
+	Err       error
+}
+
+func (f *FakeCommander) Exec(_ *exec.Cmd) ([]byte, error) {
+	return []byte(f.CmdOutput), f.Err
+}
+
+func (f *FakeCommander) Start(c *exec.Cmd) error {
+	return nil
+}
+
+func (f *FakeCommander) Wait(c *exec.Cmd) error {
+	if c.Stdin != nil {
+		l := 1024
+		data := make([]byte, l)
+		if _, err := c.Stdin.Read(data); err != nil {
+			return err
+		}
+
+		go func() {
+			// take the byte until meet the first \x0
+			idx := 0
+			for ; idx < l && data[idx] != 0; idx++ {
+			}
+
+			if _, err := c.Stdout.Write(data[:idx]); err != nil {
+			}
+		}()
+
+		time.Sleep(time.Millisecond * 200)
+	}
+	return nil
 }
