04af92824fab861474ca415a4cd37b3e6b6ffd09
Fumitoshi Ukai <ukai@google.com>
2023-09-14 15:17:56 +0900

    siso: add e2e_gn_test
    Change-Id: I3c0bd57fea781d8bd0fb9e84d3202bb40b25077e

---
+++ go/src/infra/build/siso/subcmd/ninja/testdata/TestBuild_GN/third_party/llvm-build/Release+Asserts/bin/llvm-ar.py
@@ -0,0 +1,26 @@
+# Copyright 2023 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import sys
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-D', help='output file')
+  parser.add_argument('rsp')
+  options = parser.parse_args()
+
+  data = ''
+  if options.rsp.startswith('@'):
+    rsp = options.rsp.removeprefix('@')
+    with open(rsp) as f:
+      data = f.read()
+  with open(options.D, 'w') as f:
+    f.write('archive of %s' % data)
+  return 0
+
+
+if __name__ == "__main__":
+  sys.exit(main())
