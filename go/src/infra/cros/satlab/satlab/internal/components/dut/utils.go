df52ac911b7fa5bf99be62c8c68d4c224682f237
Boris <boris.lok@cienet.corp-partner.google.com>
2023-09-15 17:15:38 +0000

    [Refactoring] re-construct the satlab_commands.
    Change-Id: I7e729cb37bff53cd5e708bfdc23dddee1fb90fde
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4825827
    Reviewed-by: Eli Trexler <elijahtrexler@google.com>
    Commit-Queue: Varun Thammineni <varunth@google.com>
    Cr-Commit-Position: refs/heads/main@{#59729}

--- go/src/infra/cros/satlab/satlab/internal/components/dut/utils.go
+++ go/src/infra/cros/satlab/satlab/internal/components/dut/utils.go
@@ -14,6 +14,7 @@ import (
 
 	"infra/cros/satlab/common/satlabcommands"
 	"infra/cros/satlab/common/site"
+	"infra/cros/satlab/common/utils/executor"
 
 	"go.chromium.org/luci/common/errors"
 )
@@ -31,7 +32,7 @@ func getDockerHostBoxIdentifier(common site.CommonFlags) (string, error) {
 		return strings.ToLower(common.SatlabID), nil
 	}
 
-	dockerHostBoxIdentifier, err := satlabcommands.GetDockerHostBoxIdentifier()
+	dockerHostBoxIdentifier, err := satlabcommands.GetDockerHostBoxIdentifier(&executor.ExecCommander{})
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to determine -satlab prefix, use %s to pass explicitly\n", common.SatlabID)
 		return "", errors.Annotate(err, "get docker host box").Err()
