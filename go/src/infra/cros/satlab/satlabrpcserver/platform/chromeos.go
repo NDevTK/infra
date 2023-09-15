e3a9783e746223d00ca8653ffa259000e188ee58
Boris <boris.lok@cienet.corp-partner.google.com>
2023-09-15 00:30:51 +0000

    [Refactoring] Remove `GetHostidentifier`, use common instead.
    Change-Id: I8f83d10992d9cb1da8f6fe7b29cb84b0f6b22900

--- go/src/infra/cros/satlab/satlabrpcserver/platform/chromeos.go
+++ go/src/infra/cros/satlab/satlabrpcserver/platform/chromeos.go
@@ -4,50 +4,15 @@
 package platform
 
 import (
-	"context"
-	"log"
-	"os/exec"
-
 	"infra/cros/satlab/common/utils/executor"
-	"infra/cros/satlab/satlabrpcserver/utils"
-	"infra/cros/satlab/satlabrpcserver/utils/constants"
 )
 
 type Chromeos struct {
-	hostIdentifier *HostIdentifier
-	execCommander  executor.IExecCommander
+	execCommander executor.IExecCommander
 }
 
 func NewChromeosPlatform() IPlatform {
 	return &Chromeos{
-		hostIdentifier: nil,
-		execCommander:  &executor.ExecCommander{},
+		execCommander: &executor.ExecCommander{},
 	}
-}
-
-// GetHostIdentifier get the serial number of the device from VPD.
-//
-// Use the serial number from the VPD as the host identifier, if no
-// serial number is set use the ethernet_mac value.
-//
-// Once a value has been read from the VPD cache it in memory for all
-// future calls, the identifier should not change hitting the VPD
-// can be slow/error prone.
-func (c *Chromeos) GetHostIdentifier(ctx context.Context) (string, error) {
-	if c.hostIdentifier != nil {
-		return c.hostIdentifier.ID, nil
-	}
-
-	for _, key := range []string{constants.VPDKeySerialNumber, constants.VPDKeyEthernetMAC} {
-		cmd := exec.CommandContext(ctx, "vpd", "-g", key)
-		out, err := c.execCommander.Exec(cmd)
-		if err != nil {
-			log.Printf("got host identifier by `%v`", key)
-			continue
-		}
-		c.hostIdentifier = &HostIdentifier{ID: string(out)}
-		return c.hostIdentifier.ID, nil
-	}
-
-	return "", utils.HostIdentifierNotFound
 }
