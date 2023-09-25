fed3c1a8767993fdc7a4415adbc7d4e9282f6376
Gowri Denduluri <gowriden@google.com>
2023-09-25 18:40:18 +0000

    [UFS] Add "tast.lacros-from-gcs" to allowed test suites for public tests.
    Bug: None
    Change-Id: I4972e9fe1ecb2beb6d5f264ba52a9da9c971f336
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4892353
    Commit-Queue: Gowri Denduluri <gowriden@google.com>
    Commit-Queue: Yang Yang <echoyang@chromium.org>
    Auto-Submit: Gowri Denduluri <gowriden@google.com>
    Reviewed-by: Yang Yang <echoyang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#59973}

--- go/src/infra/unifiedfleet/app/controller/fleet_tests_policy.go
+++ go/src/infra/unifiedfleet/app/controller/fleet_tests_policy.go
@@ -187,7 +187,7 @@ func isPublicGroupMember(ctx context.Context, req *api.CheckFleetTestsPolicyRequ
 }
 
 func getValidPublicTestNames() []string {
-	return []string{"tast.lacros", "chromium", "chromium_Graphics"}
+	return []string{"tast.lacros", "chromium", "chromium_Graphics", "tast.lacros-from-gcs"}
 }
 
 func validatePublicBoardModel(ctx context.Context, board string, model string) error {
