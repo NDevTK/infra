d7071ace4988bbf15684b5467fdf5ab5b897d05f
Vidit Chitkara <viditchitkara@google.com>
2023-09-28 22:16:20 +0000

    [chrome-test-health] Added necessary APIs for coverage dashboard.
    1. getProjectDefaultConfig
    2. getSummaryCoverageByComponent
    3. getTeams
    
    - Generalized response returned by getSummaryCoverage and
      getSummaryCoverageByComponent
    - Refactored the code to put interfaces, API calls and helper
      functions to separate sections of the coverage.ts file.
    - Added relevant unit tests
    
    Bug: 1471660
    Change-Id: Ie703eaa89121294afdbe959d7bde9b4119ead16a
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4898176
    Reviewed-by: Riley Wong <rgw@google.com>
    Commit-Queue: Vidit Chitkara <viditchitkara@google.com>
    Cr-Commit-Position: refs/heads/main@{#60093}

--- go/src/infra/appengine/chrome-test-health/frontend/src/api/coverage.test.ts
+++ go/src/infra/appengine/chrome-test-health/frontend/src/api/coverage.test.ts
@@ -5,9 +5,13 @@
 import { Auth } from './auth';
 import { prpcClient } from './client';
 import {
+  GetSummaryByComponentRequest,
   GetSummaryCoverageRequest,
-  GetSummaryCoverageResponse,
+  GetTeamsResponse,
+  SummaryNode,
   getSummaryCoverage,
+  getSummaryCoverageByComponent,
+  getTeams,
 } from './coverage';
 
 const auth = new Auth('', new Date('3000-01-01'));
@@ -61,47 +65,330 @@ describe('getSummaryCoverage', () => {
         ],
       },
     });
-    const expected: GetSummaryCoverageResponse = {
-      summary: {
-        dirs: [
+    const expected: SummaryNode[] = [
+      {
+        'name': 'apps/',
+        'path': '//apps/',
+        'summaries': [
           {
-            'name': 'apps/',
-            'path': '//apps/',
-            'summaries': [
-              {
-                'covered': 75,
-                'name': 'branch',
-                'total': 286,
-              },
-              {
-                'covered': 451,
-                'name': 'line',
-                'total': 1028,
-              },
-            ],
-          },
-        ],
-        files: [],
-        path: '//',
-        summaries: [
-          {
-            'covered': 795625,
+            'covered': 75,
             'name': 'branch',
-            'total': 1791268,
+            'total': 286,
           },
           {
-            'covered': 2519159,
+            'covered': 451,
             'name': 'line',
-            'total': 4567235,
+            'total': 1028,
           },
         ],
+        'isDir': true,
+        'children': [] as SummaryNode[],
       },
-    };
+    ];
     const resp = await getSummaryCoverage(auth, dummyRequest);
 
     expect(mockCall.mock.calls.length).toBe(1);
     expect(mockCall.mock.calls[0].length).toBe(4);
     expect(mockCall.mock.calls[0][3]).toEqual(dummyRequest);
+    expect(resp).toEqual(expected);
+  });
+});
+
+describe('getSummaryCoverageByComponent', () => {
+  const dummyRequest: GetSummaryByComponentRequest = {
+    gitiles_host: 'chromium.googlesource.com',
+    gitiles_project: 'chromium/src',
+    gitiles_ref: 'refs/heads/main',
+    gitiles_revision: '03d4e64771cbc97f3ca5e4bbe85490d7cf909a0a',
+    components: [
+      'Blink>CSS',
+    ],
+    unit_tests_only: true,
+    bucket: 'ci',
+    builder: 'linux-code-coverage',
+  };
+  it('returns coverage summary for each component', async () => {
+    const mockCall = jest.spyOn(prpcClient, 'call').mockResolvedValue({
+      'summary': [
+        {
+          'dirs': [
+            {
+              'name': 'css-parser/',
+              'path': '//third_party/blink/web_tests/css-parser/',
+              'summaries': [
+                {
+                  'covered': 0,
+                  'name': 'branch',
+                  'total': 0,
+                },
+                {
+                  'covered': 0,
+                  'name': 'function',
+                  'total': 0,
+                },
+                {
+                  'covered': 0,
+                  'name': 'instantiation',
+                  'total': 0,
+                },
+                {
+                  'covered': 0,
+                  'name': 'line',
+                  'total': 0,
+                },
+                {
+                  'covered': 0,
+                  'name': 'region',
+                  'total': 0,
+                },
+              ],
+            },
+            {
+              'name': 'basic/',
+              'path': '//third_party/blink/web_tests/css1/basic/',
+              'summaries': [
+                {
+                  'covered': 0,
+                  'name': 'branch',
+                  'total': 0,
+                },
+                {
+                  'covered': 0,
+                  'name': 'function',
+                  'total': 0,
+                },
+                {
+                  'covered': 0,
+                  'name': 'instantiation',
+                  'total': 0,
+                },
+                {
+                  'covered': 30,
+                  'name': 'line',
+                  'total': 100,
+                },
+                {
+                  'covered': 0,
+                  'name': 'region',
+                  'total': 0,
+                },
+              ],
+            },
+          ],
+          'files': [
+            {
+              'name': 'test_file.cc',
+              'path': '//third_party/test_file.cc',
+              'summaries': [
+                {
+                  'covered': 0,
+                  'name': 'branch',
+                  'total': 0,
+                },
+                {
+                  'covered': 0,
+                  'name': 'function',
+                  'total': 0,
+                },
+                {
+                  'covered': 0,
+                  'name': 'instantiation',
+                  'total': 0,
+                },
+                {
+                  'covered': 50,
+                  'name': 'line',
+                  'total': 100,
+                },
+                {
+                  'covered': 0,
+                  'name': 'region',
+                  'total': 0,
+                },
+              ],
+            },
+          ],
+          'path': 'Blink>CSS',
+          'summaries': [
+            {
+              'covered': 0,
+              'name': 'branch',
+              'total': 0,
+            },
+            {
+              'covered': 0,
+              'name': 'function',
+              'total': 0,
+            },
+            {
+              'covered': 0,
+              'name': 'instantiation',
+              'total': 0,
+            },
+            {
+              'covered': 0,
+              'name': 'line',
+              'total': 0,
+            },
+            {
+              'covered': 0,
+              'name': 'region',
+              'total': 0,
+            },
+          ],
+        },
+      ],
+    });
+    const expected: SummaryNode[] = [
+      {
+        'name': 'third_party/',
+        'path': '//third_party/',
+        'isDir': true,
+        'children': [
+          {
+            'name': 'blink/',
+            'path': '//third_party/blink/',
+            'isDir': true,
+            'children': [
+              {
+                'name': 'web_tests/',
+                'path': '//third_party/blink/web_tests/',
+                'isDir': true,
+                'children': [
+                  {
+                    'name': 'css-parser/',
+                    'path': '//third_party/blink/web_tests/css-parser/',
+                    'isDir': true,
+                    'children': [],
+                    'summaries': [
+                      {
+                        'name': 'line',
+                        'covered': 0,
+                        'total': 0,
+                      },
+                    ],
+                  },
+                  {
+                    'name': 'css1/',
+                    'path': '//third_party/blink/web_tests/css1/',
+                    'isDir': true,
+                    'children': [
+                      {
+                        'name': 'basic/',
+                        'path': '//third_party/blink/web_tests/css1/basic/',
+                        'isDir': true,
+                        'children': [],
+                        'summaries': [
+                          {
+                            'name': 'line',
+                            'covered': 30,
+                            'total': 100,
+                          },
+                        ],
+                      },
+                    ],
+                    'summaries': [
+                      {
+                        'name': 'line',
+                        'covered': 30,
+                        'total': 100,
+                      },
+                    ],
+                  },
+                ],
+                'summaries': [
+                  {
+                    'name': 'line',
+                    'covered': 30,
+                    'total': 100,
+                  },
+                ],
+              },
+            ],
+            'summaries': [
+              {
+                'name': 'line',
+                'covered': 30,
+                'total': 100,
+              },
+            ],
+          },
+          {
+            'name': 'test_file.cc',
+            'path': '//third_party/test_file.cc',
+            'isDir': false,
+            'children': [],
+            'summaries': [
+              {
+                'name': 'line',
+                'covered': 50,
+                'total': 100,
+              },
+            ],
+          },
+        ],
+        'summaries': [
+          {
+            'name': 'line',
+            'covered': 80,
+            'total': 200,
+          },
+        ],
+      },
+    ];
+    const resp = await getSummaryCoverageByComponent(auth, dummyRequest);
+    expect(mockCall.mock.calls.length).toBe(1);
+    expect(mockCall.mock.calls[0].length).toBe(4);
+    expect(mockCall.mock.calls[0][3]).toEqual(dummyRequest);
+    expect(resp).toEqual(expected);
+  });
+});
+
+describe('getTeams', () => {
+  it('returns list of teams', async () => {
+    const mockCall = jest.spyOn(prpcClient, 'call').mockResolvedValue(
+        {
+          teams: [
+            {
+              'id': '1346050280',
+              'name': '1346050280',
+              'components': [
+                'Internals>Instrumentation',
+                'Internals>Instrumentation>Memory',
+              ],
+            },
+            {
+              'id': '1346052032',
+              'name': '1346052032',
+              'components': [
+                'Infra>OmahaProxy',
+              ],
+            },
+          ],
+        });
+    const expected: GetTeamsResponse = {
+      teams: [
+        {
+          id: '1346050280',
+          name: '1346050280',
+          components: [
+            'Internals>Instrumentation',
+            'Internals>Instrumentation>Memory',
+          ],
+        },
+        {
+          id: '1346052032',
+          name: '1346052032',
+          components: [
+            'Infra>OmahaProxy',
+          ],
+        },
+      ],
+    };
+    const resp = await getTeams(auth);
+
+    expect(mockCall.mock.calls.length).toBe(1);
+    expect(mockCall.mock.calls[0].length).toBe(4);
     expect(resp).toEqual(expected);
   });
 });
--- go/src/infra/appengine/chrome-test-health/frontend/src/api/coverage.ts
+++ go/src/infra/appengine/chrome-test-health/frontend/src/api/coverage.ts
@@ -5,6 +5,8 @@
 import { Auth } from './auth';
 import { prpcClient } from './client';
 
+// ---------------- Interfaces ----------------
+
 export interface CoverageMetric {
   name: string,
   covered: number,
@@ -14,7 +16,9 @@ export interface CoverageMetric {
 export interface SummaryNode {
   name: string,
   path: string,
-  summaries: CoverageMetric[]
+  summaries: CoverageMetric[],
+  children: SummaryNode[],
+  isDir: boolean,
 }
 
 export interface Summary {
@@ -22,6 +26,32 @@ export interface Summary {
   files: SummaryNode[],
   path: string,
   summaries: CoverageMetric[]
+}
+
+export interface Platform {
+  platform: string,
+  bucket: string,
+  builder: string,
+  coverageTool: string,
+  uiName: string,
+  availableRevision: string,
+  avaialbleModifierId: string,
+}
+
+export interface GetProjectDefaultConfigRequest {
+  luci_project: string,
+  revision: string,
+  modifier_id: string,
+}
+
+export interface GetProjectDefaultConfigResponse {
+  host: string,
+  defaultPlatform: string,
+  project: string,
+  ref: string,
+  platforms: Platform[]
+  revision: string,
+  modifierId: string,
 }
 
 export interface GetSummaryCoverageRequest {
@@ -40,16 +70,218 @@ export interface GetSummaryCoverageResponse {
   summary: Summary
 }
 
+export interface GetSummaryByComponentRequest {
+  gitiles_host: string,
+  gitiles_project: string,
+  gitiles_ref: string,
+  gitiles_revision: string,
+  components: string[],
+  unit_tests_only: boolean,
+  bucket: string,
+  builder: string
+}
+
+export interface GetSummaryByComponentsResponse {
+  summary: Summary[],
+}
+
+export interface Team {
+  id: string,
+  name: string,
+  components: string[]
+}
+
+export interface GetTeamsResponse {
+  teams: Team[]
+}
+
+// ---------------- RPC Calls ----------------
+
+export async function getProjectDefaultConfig(
+    auth: Auth,
+    request: GetProjectDefaultConfigRequest,
+):
+  Promise<GetProjectDefaultConfigResponse> {
+  const resp: GetProjectDefaultConfigResponse = await prpcClient.call(
+      auth,
+      'test_resources.Coverage',
+      'GetProjectDefaultConfig',
+      request,
+  );
+  return resp;
+}
+
 export async function getSummaryCoverage(
     auth: Auth,
     request: GetSummaryCoverageRequest,
 ):
-  Promise<GetSummaryCoverageResponse> {
+  Promise<SummaryNode[]> {
   const resp: GetSummaryCoverageResponse = await prpcClient.call(
       auth,
       'test_resources.Coverage',
       'GetCoverageSummary',
       request,
   );
+  const tree: SummaryNode[] = fixGetSummaryCoverageResponse(resp);
+  return tree;
+}
+
+export async function getSummaryCoverageByComponent(
+    auth: Auth,
+    request: GetSummaryByComponentRequest,
+):
+  Promise<SummaryNode[]> {
+  const resp: GetSummaryByComponentsResponse = await prpcClient.call(
+      auth,
+      'test_resources.Coverage',
+      'GetCoverageSummaryByComponents',
+      request,
+  );
+  const tree: SummaryNode[] = fixGetSummaryCoverageByComponentResponse(resp);
+  return tree;
+}
+
+export async function getTeams(auth: Auth): Promise<GetTeamsResponse> {
+  const resp: GetTeamsResponse = await prpcClient.call(
+      auth,
+      'test_resources.Coverage',
+      'GetTeams',
+      {},
+  );
   return resp;
+}
+
+// ---------------- Helper Functions ----------------
+
+function getPathParts(path: string, isDir: boolean) {
+  let pathParts = path.split('/');
+  // For a dir eg: //a/b/ the path will split to ["", "", "a", "b", ""].
+  // For a file eg: //a/b.ext the path will split to ["", "", "a", "b.ext"]
+  // The following code removes empty string elements
+  // at the beginning and the end leaving us with ["a", "b"]
+  // and ["a", "b.ext"] respectively for this example.
+  pathParts = (isDir) ? pathParts.slice(2, -1) : pathParts.slice(2);
+  for (const [i, part] of pathParts.entries()) {
+    if (!isDir && i == pathParts.length - 1) break;
+    pathParts[i] = `${part}/`;
+  }
+  return pathParts;
+}
+
+function isPathPresent(
+    tree: SummaryNode[],
+    path: string,
+    isDir: boolean,
+): boolean {
+  const pathParts = getPathParts(path, isDir);
+  let treeItr = tree;
+  for (const part of pathParts) {
+    let hasPart = false;
+    for (const node of treeItr) {
+      if (node.name === part) {
+        hasPart = true;
+        treeItr = node.children;
+        break;
+      }
+    }
+    if (!hasPart) return false;
+  }
+  return true;
+}
+
+function addPath(
+    tree: SummaryNode[],
+    path: string,
+    isDir: boolean,
+    summaries: CoverageMetric[],
+) {
+  const pathParts = getPathParts(path, isDir);
+  let treeItr = tree;
+  let pathSoFar = '//';
+  for (const [i, part] of pathParts.entries()) {
+    pathSoFar = `${pathSoFar}${part}`;
+    let hasPart = false;
+    let matchingNode: SummaryNode = {
+      name: '',
+      path: '',
+      isDir: false,
+      children: [],
+      summaries: [],
+    };
+    for (const node of treeItr) {
+      if (node.name === part) {
+        hasPart = true;
+        matchingNode = node;
+        break;
+      }
+    }
+    if (!hasPart) {
+      matchingNode = {
+        name: part,
+        path: pathSoFar,
+        isDir: (isDir || i < pathParts.length - 1) ? true : false,
+        children: [] as SummaryNode[],
+        summaries: [
+          { name: 'line', covered: 0, total: 0 },
+        ],
+      };
+      treeItr.push(matchingNode);
+    }
+
+    matchingNode.summaries.forEach((summary) => {
+      const filteredSummary = summaries.filter(
+          (sum) => sum.name === summary.name,
+      )[0];
+      summary.covered += filteredSummary.covered;
+      summary.total += filteredSummary.total;
+    });
+    treeItr = matchingNode.children;
+  }
+}
+
+function fixGetSummaryCoverageResponse(
+    resp: GetSummaryCoverageResponse,
+): SummaryNode[] {
+  const nodes: SummaryNode[] = [];
+  (resp.summary.dirs || []).forEach((dir) => {
+    nodes.push({
+      name: dir.name,
+      path: dir.path,
+      children: [] as SummaryNode[],
+      summaries: dir.summaries,
+      isDir: true,
+    } as SummaryNode);
+  });
+  (resp.summary.files || []).forEach((file) => {
+    nodes.push({
+      name: file.name,
+      path: file.path,
+      children: [] as SummaryNode[],
+      summaries: file.summaries,
+      isDir: false,
+    } as SummaryNode);
+  });
+  return nodes;
+}
+
+function fixGetSummaryCoverageByComponentResponse(
+    resp: GetSummaryByComponentsResponse,
+): SummaryNode[] {
+  const rootNodes: SummaryNode[] = [];
+  resp.summary.forEach((s) => {
+    s.dirs = s.dirs || [] as SummaryNode[];
+    s.files = s.files || [] as SummaryNode[];
+
+    s.dirs.forEach((dir) => {
+      if (!isPathPresent(rootNodes, dir.path, true)) {
+        addPath(rootNodes, dir.path, true, dir.summaries);
+      }
+    });
+    s.files.forEach((file) => {
+      if (!isPathPresent(rootNodes, file.path, false)) {
+        addPath(rootNodes, file.path, false, file.summaries);
+      }
+    });
+  });
+  return rootNodes;
 }
