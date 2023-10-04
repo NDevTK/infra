7a07efe847a9b7dbfe5893a42832af200344640c
Chenlin Fan <fancl@chromium.org>
2023-10-04 23:34:27 +0000

    pkgbuild: add snoopy support
    Change-Id: I6c4f534d6eb8319e4e056661016d3f3652589181

--- go/src/infra/tools/pkgbuild/cmd/edge/main.go
+++ go/src/infra/tools/pkgbuild/cmd/edge/main.go
@@ -5,8 +5,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
+	"path"
+	"path/filepath"
 	"time"
 
 	"infra/tools/pkgbuild/pkg/spec"
@@ -14,16 +17,28 @@ import (
 
 	"go.chromium.org/luci/cipd/client/cipd/platform"
 	"go.chromium.org/luci/cipkg/base/actions"
+	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/common/logging/gologger"
+	"go.chromium.org/luci/common/system/filesystem"
 	"go.chromium.org/luci/hardcoded/chromeinfra"
+	"go.chromium.org/luci/provenance/api/snooperpb/v1"
+	"go.chromium.org/luci/provenance/client"
 )
 
 func main() {
-	ctx := gologger.StdConfig.Use(context.Background())
-	ctx = logging.SetLevel(ctx, logging.Error)
-
+	ctx := context.Background()
 	actions.NewReexecRegistry().Intercept(ctx)
+
+	if err := Main(ctx); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func Main(ctx context.Context) error {
+	ctx = gologger.StdConfig.Use(ctx)
+	ctx = logging.SetLevel(ctx, logging.Error)
 
 	app := Application{
 		LoggingLevel:   logging.Error,
@@ -33,24 +48,22 @@ func main() {
 		Experiment:     false,
 	}
 	if err := app.Parse(os.Args[1:]); err != nil {
-		logging.WithError(err).Errorf(ctx, "failed to parse options")
-		os.Exit(1)
+		return errors.Annotate(err, "failed to parse options").Err()
 	}
 	ctx = logging.SetLevel(ctx, app.LoggingLevel)
 
 	if app.Help {
-		os.Exit(0)
+		// TODO: print help message?
+		return nil
 	}
 
 	if err := stdenv.Init(stdenv.DefaultConfig()); err != nil {
-		logging.WithError(err).Errorf(ctx, "failed to init stdenv")
-		os.Exit(1)
+		return errors.Annotate(err, "failed to init stdenv").Err()
 	}
 
 	b, err := app.NewBuilder(ctx)
 	if err != nil {
-		logging.WithError(err).Errorf(ctx, "failed to init builder")
-		os.Exit(1)
+		return errors.Annotate(err, "failed to init builder").Err()
 	}
 
 	// Build all packages by default
@@ -68,21 +81,97 @@ func main() {
 				logging.Infof(ctx, "skip package %s on %s", name, app.TargetPlatform)
 				continue
 			}
-			logging.WithError(err).Errorf(ctx, "failed to add %s", name)
-			os.Exit(1)
+			return errors.Annotate(err, "failed to add %s", name).Err()
 		}
 	}
 
 	pkgs, err := b.BuildAll(ctx)
 	if err != nil {
-		logging.WithError(err).Errorf(ctx, "failed to build packages")
-		os.Exit(1)
+		return errors.Annotate(err, "failed to build packages").Err()
 	}
 
-	for _, pkg := range pkgs {
-		metadata := pkg.Action.Metadata
-		fmt.Println(metadata.Cipd.Name, metadata.Cipd.Version) // (TODO): Upload package here
+	if app.Upload {
+		if err := upload(ctx, app.Experiment, pkgs); err != nil {
+			return err
+		}
 	}
 
 	app.PackageManager.Prune(ctx, time.Hour*24, 256)
+	return nil
+}
+
+func upload(ctx context.Context, experimental bool, pkgs []actions.Package) error {
+	clt, err := client.MakeProvenanceClient(ctx, "http://localhost:11000")
+	if err != nil {
+		return errors.Annotate(err, "failed to create provenance client").Err()
+	}
+
+	tmp, err := os.MkdirTemp("", "pkgbuild-")
+	if err != nil {
+		return errors.Annotate(err, "failed to create tmp dir").Err()
+	}
+	defer filesystem.RemoveAll(tmp)
+
+	for _, pkg := range pkgs {
+		metadata := pkg.Action.Metadata
+		logging.Infof(ctx, "creating cipd package %s:%s", metadata.Cipd.Name, metadata.Cipd.Version)
+
+		name := metadata.Cipd.Name
+		if experimental {
+			name = path.Join("experimental", name)
+		}
+
+		out := filepath.Join(tmp, pkg.DerivationID+".cipd")
+		Iid, err := createCIPD(name, pkg.Handler.OutputDirectory(), out)
+		if err != nil {
+			return errors.Annotate(err, "failed to build cipd package").Err()
+		}
+
+		// Report package info to server to trigger provenance generation.
+		if _, err := clt.ReportCipd(ctx, &snooperpb.ReportCipdRequest{
+			CipdReport: &snooperpb.CipdReport{
+				PackageName: name,
+				Iid:         Iid,
+			},
+		}); err != nil {
+			// Error during reporting won't block the package build.
+			logging.Warningf(ctx, "report cipd package to snoopy failed: %s: %s", pkg.Action.Metadata.Cipd.Name, err)
+		}
+	}
+
+	return nil
+}
+
+func createCIPD(name, src, dst string) (Iid string, err error) {
+	resultFile := dst + ".json"
+	cmd := CIPDCommand("pkg-build",
+		"-name", name,
+		"-in", src,
+		"-out", dst,
+		"-json-output", resultFile,
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	f, err := os.Open(resultFile)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	var result struct {
+		Result struct {
+			Package    string
+			InstanceID string `json:"instance_id"`
+		}
+	}
+	if err := json.NewDecoder(f).Decode(&result); err != nil {
+		return "", err
+	}
+
+	return result.Result.InstanceID, nil
 }
