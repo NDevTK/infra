d22cc32a01380fe99cca3a24c7a0985f22be26f5
Prakhar <pasthana@google.com>
2023-10-12 21:03:18 +0000

    Remove author coverage.
    This feature was never used.
    
    Bug: 1414535
    Change-Id: I282363701cc5c052ea1839016fcefba5b4922622
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4933082
    Auto-Submit: Prakhar Asthana <pasthana@google.com>
    Commit-Queue: Prakhar Asthana <pasthana@google.com>
    Reviewed-by: Zhaoyang Li <zhaoyangli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#60378}

--- appengine/findit/coverage_main.py
+++ appengine/findit/coverage_main.py
@@ -8,7 +8,6 @@ import gae_ts_mon
 
 from gae_libs import appengine_util
 
-from handlers.code_coverage import create_author_coverage
 from handlers.code_coverage import export_absolute_coverage
 from handlers.code_coverage import export_incremental_coverage
 from handlers.code_coverage import export_gerrit_filter_coverage
@@ -23,8 +22,6 @@ from handlers.code_coverage import update_postsubmit_report
 experimental_coverage_worker_handler_mappings = [
     ('.*/coverage/task/gerrit-filter-coverage.*',
      export_gerrit_filter_coverage.ExportCoverageMetrics),
-    ('.*/coverage/task/author-coverage.*',
-     create_author_coverage.CreateAuthorCoverageMetrics),
 ]
 experimental_coverage_worker_application = webapp2.WSGIApplication(
     experimental_coverage_worker_handler_mappings, debug=False)
@@ -59,8 +56,6 @@ code_coverage_backend_handler_mappings = [
      export_gerrit_filter_coverage.ExportAllCoverageMetricsCron),
     ('.*/coverage/task/all-gerrit-filter-coverage',
      export_gerrit_filter_coverage.ExportAllCoverageMetrics),
-    ('.*/coverage/cron/author-coverage',
-     create_author_coverage.CreateAuthorCoverageMetricsCron),
     ('.*/coverage/task/postsubmit-report/update',
      update_postsubmit_report.UpdatePostsubmitReport),
 ]
