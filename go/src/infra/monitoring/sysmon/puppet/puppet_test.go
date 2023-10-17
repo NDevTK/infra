1862f7fd210efe821517c11ebf17185fb671844a
Elliott Friedman <friedman@google.com>
2023-10-17 11:09:37 -0400

    [sysmon][puppet] update canary detection logic
    Presubmit error on upload needs to be fixed first:
    % vpython3 /home/friedman/src/infra/go/env.py vpython3 /home/friedman/src/infra/go/src/go.chromium.org/luci/scripts/check_go_mod_tidy.py /home/friedman/src/infra/go/src/infra
    go: finding module for package go.chromium.org/luci/cipkg/core
    go: finding module for package go.chromium.org/luci/swarming/client/swarming
    go: finding module for package go.chromium.org/luci/cipkg/base/actions
    go: finding module for package go.chromium.org/luci/cipkg/base/generators
    go: finding module for package go.chromium.org/luci/cipkg/base/workflow
    go: infra/experimental/golangbuild imports
            go.chromium.org/luci/swarming/client/swarming: module go.chromium.org/luci@latest found (v0.0.0-00010101000000-000000000000, replaced by ../go.chromium.org/luci), but does not contain package go.chromium.org/luci/swarming/client/swarming
    go: infra/tools/vpython_ng/cmd/vpython imports
            go.chromium.org/luci/cipkg/base/actions: module go.chromium.org/luci@latest found (v0.0.0-00010101000000-000000000000, replaced by ../go.chromium.org/luci), but does not contain package go.chromium.org/luci/cipkg/base/actions
    go: infra/tools/vpython_ng/pkg/application imports
            go.chromium.org/luci/cipkg/base/generators: module go.chromium.org/luci@latest found (v0.0.0-00010101000000-000000000000, replaced by ../go.chromium.org/luci), but does not contain package go.chromium.org/luci/cipkg/base/generators
    go: infra/tools/vpython_ng/pkg/application imports
            go.chromium.org/luci/cipkg/base/workflow: module go.chromium.org/luci@latest found (v0.0.0-00010101000000-000000000000, replaced by ../go.chromium.org/luci), but does not contain package go.chromium.org/luci/cipkg/base/workflow
    go: infra/tools/vpython_ng/pkg/wheels imports
            go.chromium.org/luci/cipkg/core: module go.chromium.org/luci@latest found (v0.0.0-00010101000000-000000000000, replaced by ../go.chromium.org/luci), but does not contain package go.chromium.org/luci/cipkg/core
    Failed to call ['go', 'mod', 'tidy'], return code 1:
    None
    
    R=abesmith, jmpittman
    
    Bug: b/304819324
    Change-Id: I48620cb5d92fb3763e5379d9d8d89a26f422452f

--- go/src/infra/monitoring/sysmon/puppet/puppet_test.go
+++ go/src/infra/monitoring/sysmon/puppet/puppet_test.go
@@ -148,11 +148,12 @@ func TestMetrics(t *testing.T) {
 			file, err := ioutil.TempFile("", "sysmon-puppet-test")
 			So(err, ShouldBeNil)
 
-			defer file.Close()
-			defer os.Remove(file.Name())
-
-			So(updateIsCanary(c, file.Name()), ShouldBeNil)
-			So(isCanary.Get(c), ShouldBeTrue)
+			Convey("with environment=canary", func() {
+				file.Write([]byte("foo=bar\nenvironment=canary\nblah=blah\n"))
+				file.Sync()
+				So(updateIsCanary(c, file.Name()), ShouldBeNil)
+				So(isCanary.Get(c), ShouldBeTrue)
+			})
 		})
 	})
 
