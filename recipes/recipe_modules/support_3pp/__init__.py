03fcf58f1ebd0783ac9d1e39c875571d41c66f4a
Rob Mohr <mohrr@google.com>
2023-10-26 09:23:14 -0700

    recipes: Use new defer module
    Bug: chromium:1495428
    Change-Id: I92af77241fca03869cac74d67174e1b0b5c44886

--- recipes/recipe_modules/support_3pp/__init__.py
+++ recipes/recipe_modules/support_3pp/__init__.py
@@ -10,6 +10,7 @@ DEPS = [
     'recipe_engine/bcid_reporter',
     'recipe_engine/cipd',
     'recipe_engine/context',
+    'recipe_engine/defer',
     'recipe_engine/file',
     'recipe_engine/json',
     'recipe_engine/path',
