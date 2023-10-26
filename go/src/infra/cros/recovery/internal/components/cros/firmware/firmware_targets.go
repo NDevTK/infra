5aa72c73ce2471620e85ee0571ec19dd6850fedb
Shou-Chieh Hsu <shouchieh@google.com>
2023-10-26 05:28:24 +0000

    recovery: Add firmware overwrite target for Pujjoteen 5G device
    BUG=b:298590144
    TEST=go test ./...
    
    Change-Id: Iabf0cc13f6500822c0794c18ca8dd441f8631a0b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4975241
    Commit-Queue: Shou-Chieh Hsu <shouchieh@chromium.org>
    Reviewed-by: Nagi Marupaka <nmarupaka@google.com>
    Reviewed-by: Garry Wang <xianuowang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#60678}

--- go/src/infra/cros/recovery/internal/components/cros/firmware/firmware_targets.go
+++ go/src/infra/cros/recovery/internal/components/cros/firmware/firmware_targets.go
@@ -56,14 +56,15 @@ var targetOverrideModels = map[string]string{
 // based on models are not sufficient for them.
 var targetOverridebyHwid = map[string]string{
 	// Nissa board.
-	"JOXER-ZELG B3B-C3A-Q6B-I2C-C9Y-A9N":  "joxer_ufs",
-	"NIRWEN-ZZCR B2B-B2A-B2A-W7H":         "nivviks_ufs",
-	"NEREID-ZZCR A4B-B2C-E3E-F2A-A2B-48T": "nereid_hdmi",
-	"YAVIKS-BVSW B3B-A2C-B2A-Q6W-I3T":     "yaviks_ufs",
-	"YAVIKS-BVSW C4B-C2C-C3B-H4H-66N":     "yaviks_ufs",
-	"YAVIKSO-OFRL C4C-C2F-D3B-W3P-N4E":    "yaviks_ufs",
-	"YAVIKSO-OFRL C4C-B2D-D3B-Q3E-X66":    "yaviks_ufs",
-	"YAVILLY-ITWJ B3B-G2H-C8K-79E-W4S":    "yavilla_ufs",
+	"JOXER-ZELG B3B-C3A-Q6B-I2C-C9Y-A9N":         "joxer_ufs",
+	"NIRWEN-ZZCR B2B-B2A-B2A-W7H":                "nivviks_ufs",
+	"NEREID-ZZCR A4B-B2C-E3E-F2A-A2B-48T":        "nereid_hdmi",
+	"PUJJOTEEN-JQII E5B-D2J-E5D-Q2Q-K4E-I5I-A6I": "pujjo_5g",
+	"YAVIKS-BVSW B3B-A2C-B2A-Q6W-I3T":            "yaviks_ufs",
+	"YAVIKS-BVSW C4B-C2C-C3B-H4H-66N":            "yaviks_ufs",
+	"YAVIKSO-OFRL C4C-C2F-D3B-W3P-N4E":           "yaviks_ufs",
+	"YAVIKSO-OFRL C4C-B2D-D3B-Q3E-X66":           "yaviks_ufs",
+	"YAVILLY-ITWJ B3B-G2H-C8K-79E-W4S":           "yavilla_ufs",
 }
 
 // ecExemptedModels holds a map of models that doesn't have EC firmware.
