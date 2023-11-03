0a1b38e7b0d647392eebe2bd74a9b7a89d8393a7
Riley Wong <rgw@google.com>
2023-11-03 16:01:02 +0000

    Change metric testing to use nested tests
    Small change to LoadTestMetrics.test.tsx
    Utilize nesting for brevity.
    
    Bug: 1435094
    Change-Id: Ibee447d5c54cf90dab71c7d71ae6365f3bc4fbf4
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4969877
    Auto-Submit: Riley Wong <rgw@google.com>
    Reviewed-by: Gary Tong <gatong@chromium.org>
    Commit-Queue: Gary Tong <gatong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#60901}

--- go/src/infra/appengine/chrome-test-health/frontend/src/features/resources/tests/LoadTestMetrics.test.tsx
+++ go/src/infra/appengine/chrome-test-health/frontend/src/features/resources/tests/LoadTestMetrics.test.tsx
@@ -235,7 +235,7 @@ describe('merge_test action', () => {
         .toEqual(4);
   });
 
-  it('merge test with footer correctly', () => {
+  describe('with and without footer', () => {
     const metrics = metricsMap({
       '2012-01-02': [
         [MetricType.NUM_RUNS, 1],
@@ -250,33 +250,22 @@ describe('merge_test action', () => {
       metrics: metrics,
       variants: [],
     }];
-    const merged = dataReducer(
-        state,
-        { type: 'merge_test', parentId: '/', tests, footer: <div/> },
-    );
-    expect(merged[0].footer).toBeDefined();
-  });
-
-  it('merge test without footer', () => {
-    const metrics = metricsMap({
-      '2012-01-02': [
-        [MetricType.NUM_RUNS, 1],
-        [MetricType.NUM_FAILURES, 2],
-      ],
+  
+    it('with footer', () => {
+      const merged = dataReducer(
+          state,
+          { type: 'merge_test', parentId: '/', tests, footer: <div/> },
+      );
+      expect(merged[0].footer).toBeDefined();
     });
-    const state: Node[] = [pathNode('/', DirectoryNodeType.DIRECTORY, false)];
-    const tests: TestDateMetricData[] = [{
-      testId: '12',
-      testName: 'name',
-      fileName: 'file',
-      metrics: metrics,
-      variants: [],
-    }];
-    const merged = dataReducer(
-        state,
-        { type: 'merge_test', parentId: '/', tests },
-    );
-    expect(merged[0].footer).toBeUndefined();
+  
+    it('without footer', () => {
+      const merged = dataReducer(
+          state,
+          { type: 'merge_test', parentId: '/', tests },
+      );
+      expect(merged[0].footer).toBeUndefined();
+    });
   });
 });
 
