15b7a8f53cf9b94d8634cc3a35b321aa290021c5
Greg NISBET <gregorynisbet@google.com>
2023-11-07 20:05:20 -0800

    [ufs] Add IP arithmetic
    BUG=b:187899200
    TEST=make test
    
    Change-Id: Ic42c503bc1294538a9e76bf24279beff92c04084

--- go/src/infra/unifiedfleet/app/util/iputil/iputil_test.go
+++ go/src/infra/unifiedfleet/app/util/iputil/iputil_test.go
@@ -5,6 +5,7 @@
 package iputil
 
 import (
+	"math/big"
 	"net"
 	"testing"
 
@@ -54,6 +55,97 @@ func TestRawIncr(t *testing.T) {
 				t.Errorf("unexpected diff (-want +got): %s", diff)
 			}
 			if diff := typed.Diff(overflow, tt.overflow); diff != "" {
+				t.Errorf("unexpected diff (-want +got): %s", diff)
+			}
+		})
+	}
+}
+
+// TestPad that pad correctly adds and truncates paths.
+func TestPad(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		name string
+		in   string
+		n    int
+		out  string
+	}{
+		{
+			name: "happy path",
+			in:   "ab",
+			n:    1,
+			out:  "b",
+		},
+		{
+			name: "less trivial happy path",
+			in:   "abcdef",
+			n:    3,
+			out:  "def",
+		},
+		{
+			name: "null bytes",
+			in:   "abc",
+			n:    4,
+			out:  "\x00abc",
+		},
+	}
+
+	for _, tt := range cases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := string(pad([]byte(tt.in), tt.n))
+			if diff := typed.Diff(got, tt.out); diff != "" {
+				t.Errorf("unexpected diff (-want +got): %s", diff)
+			}
+		})
+	}
+}
+
+// TestAddIP tests performing IP arithmetic.
+func TestAddIP(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		name   string
+		ip     net.IP
+		offset int64
+		want   net.IP
+		ok     bool
+	}{
+		{
+			name:   "0.0.0.1 increment",
+			ip:     MustParseIP("::1"),
+			offset: 1,
+			want:   MustParseIP("::2"),
+			ok:     true,
+		},
+		{
+			name:   "0.0.0.1 decrement",
+			ip:     MustParseIP("::1"),
+			offset: -1,
+			want:   MustParseIP("::"),
+			ok:     true,
+		},
+		{
+			name:   "0.0.0.1 decerment by too much",
+			ip:     MustParseIP("::1"),
+			offset: -2,
+			want:   net.IP{},
+			ok:     false,
+		},
+	}
+
+	for _, tt := range cases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := AddToIP(tt.ip, big.NewInt(tt.offset))
+
+			if diff := typed.Diff(got.String(), tt.want.String()); diff != "" {
+				t.Errorf("unexpected diff (-want +got): %s", diff)
+			}
+			if diff := typed.Diff(got != nil, tt.ok); diff != "" {
 				t.Errorf("unexpected diff (-want +got): %s", diff)
 			}
 		})
