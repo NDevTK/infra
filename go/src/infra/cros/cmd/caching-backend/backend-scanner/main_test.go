d63839fd56ff31bc8301a27e1df30bc2585baa12
Congbin Guo <guocb@google.com>
2023-11-09 16:57:33 -0800

    feat(caching): backend scanner generates backend configuration
    Generate the configuration from template and update it to the config
    map.
    
    BUG=b:310002629
    TEST=unit test
    
    Change-Id: I716c8eeb887bc6747e790d8ac136fe27dba7b9f0

---
+++ go/src/infra/cros/cmd/caching-backend/backend-scanner/main_test.go
@@ -0,0 +1,42 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+package main
+
+import (
+	"log"
+	"testing"
+)
+
+func TestConfigTemplate(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		template string
+		data     interface{}
+	}{
+		{
+			name:     "nginx",
+			template: nginxTemplate,
+			data: &nginxConf{
+				CacheSizeInGB:     100,
+				Port:              1234,
+				L7Port:            4321,
+				L7Servers:         []string{"1.1.1.1", "2.2.2.2"},
+				OtelTraceEndpoint: "http://localhost:5678",
+			},
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			got, err := genConfig("nginx", tc.template, tc.data)
+			if err != nil {
+				t.Errorf("genConfig(%q) err %q, want nil", tc.name, err)
+			}
+			log.Printf("generated conf for %q is:\n%s", tc.name, got)
+		})
+	}
+}
