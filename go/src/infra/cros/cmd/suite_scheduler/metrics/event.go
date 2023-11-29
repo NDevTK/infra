96f1242d803c38d98bb759eb643c4f26df249e59
Julio Hurtado <juahurta@google.com>
2023-11-29 00:14:26 +0000

    Revert^2 "suite_scheduler: init metrics reporting pipeline"
    This reverts commit 63c2ef66be8ead3dc3ba5fc5ce2db83ebc5f5e98.
    
    Reason for revert: relanding with fix
    
    Original change's description:
    > Revert "suite_scheduler: init metrics reporting pipeline"
    >
    > This reverts commit 51f36ae888ab27ef2186706dac4e5d3888f6ef8a.
    >
    > Reason for revert: broke infra-continuous-bionic-64
    >
    >  infra/cros/cmd/suite_scheduler
    >  # infra/cros/cmd/suite_scheduler
    >  runtime.main_main·f: function main is undeclared in the main package
    >
    > go/src/infra/cros/cmd/suite_scheduler/subcommand.go should have been
    > removed too
    >
    > Original change's description:
    > > suite_scheduler: init metrics reporting pipeline
    > >
    > > BUG=b:309683890
    > > TEST=Local
    > >
    > > Change-Id: Ib187218965f26109f5282972c62391cc83da7543
    > > Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5051362
    > > Auto-Submit: Julio Hurtado <juahurta@google.com>
    > > Commit-Queue: Derek Beckett <dbeckett@chromium.org>
    > > Reviewed-by: Derek Beckett <dbeckett@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#61504}
    >
    > BUG=b:309683890
    >
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I03d3e4573e2f35c9e0eb16fca7b3b60cc9ac79d1
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5068413
    > Commit-Queue: Julio Hurtado <juahurta@google.com>
    > Reviewed-by: Julio Hurtado <juahurta@google.com>
    > Auto-Submit: Josip Sokcevic <sokcevic@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#61505}
    
    BUG=b:309683890
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    
    Change-Id: I28bdfd0fc8be66c1486d68401c79252cf459e0b0
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5068873
    Reviewed-by: Derek Beckett <dbeckett@chromium.org>
    Commit-Queue: Julio Hurtado <juahurta@google.com>
    Cr-Commit-Position: refs/heads/main@{#61527}

---
+++ go/src/infra/cros/cmd/suite_scheduler/metrics/event.go
@@ -0,0 +1,36 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package metrics holds all the schemas and utilities to handle metrics for
+// SuSch v1.5.
+package metrics
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	suschpb "go.chromium.org/chromiumos/infra/proto/go/test_platform/suite_scheduler/v15"
+	infrapb "go.chromium.org/chromiumos/infra/proto/go/testplans"
+)
+
+// GenerateEventMessage builds a metric event.
+func GenerateEventMessage(config *infrapb.SchedulerConfig, runID *suschpb.UID, schedulingDecision *suschpb.SchedulingDecision, bbids []int64) (*suschpb.SchedulingEvent, error) {
+	if runID == nil {
+		return nil, fmt.Errorf("runID cannot be nil")
+	}
+
+	return &suschpb.SchedulingEvent{
+		RunUid: runID,
+		EventUid: &suschpb.UID{
+			Id: uuid.NewString(),
+		},
+		ConfigName: config.Name,
+		SuiteName:  config.Suite,
+		EventTime:  timestamppb.Now(),
+		Decision:   schedulingDecision,
+		Bbids:      bbids,
+	}, nil
+}
