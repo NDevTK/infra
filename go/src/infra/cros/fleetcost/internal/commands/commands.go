e27aeec7365d241631fbfe5de14763432db194ce
Greg NISBET <gregorynisbet@google.com>
2023-12-01 04:12:52 +0000

    [fleetcost] Add a fleet cost ping CLI command
    Add a fleet cost ping CLI command.
    
    BUG=b:311443080
    TEST=make test
    
    Change-Id: I0d3765ac443f4ea88834b991fdfb23818250bd10
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5074949
    Reviewed-by: Garry Wang <xianuowang@chromium.org>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Cr-Commit-Position: refs/heads/main@{#61602}

--- go/src/infra/cros/fleetcost/internal/commands/commands.go
+++ go/src/infra/cros/fleetcost/internal/commands/commands.go
@@ -5,10 +5,19 @@
 package commands
 
 import (
+	"context"
+	"time"
+
 	"github.com/maruel/subcommands"
 
+	"go.chromium.org/luci/auth"
 	"go.chromium.org/luci/auth/client/authcli"
+	"go.chromium.org/luci/common/cli"
+	"go.chromium.org/luci/common/errors"
+	prpc "go.chromium.org/luci/grpc/prpc"
 
+	"infra/cmdsupport/cmdlib"
+	fleetcostpb "infra/cros/fleetcost/api"
 	"infra/cros/fleetcost/internal/site"
 )
 
@@ -30,6 +39,33 @@ type pingCommand struct {
 }
 
 // Run is the main entrypoint to the ping.
-func (c *pingCommand) Run(subcommands.Application, []string, subcommands.Env) int {
-	return 1
+func (c *pingCommand) Run(a subcommands.Application, args []string, env subcommands.Env) int {
+	ctx := cli.GetContext(a, c, env)
+	if err := c.innerRun(ctx, a, args, env); err != nil {
+		cmdlib.PrintError(a, err)
+		return 1
+	}
+	return 0
+}
+
+func (c *pingCommand) innerRun(ctx context.Context, a subcommands.Application, args []string, env subcommands.Env) error {
+	authOptions, err := c.authFlags.Options()
+	if err != nil {
+		return errors.Annotate(err, "ping").Err()
+	}
+	authenticator := auth.NewAuthenticator(ctx, auth.InteractiveLogin, authOptions)
+	httpClient, err := authenticator.Client()
+	if err != nil {
+		return errors.Annotate(err, "ping").Err()
+	}
+	prpcClient := &prpc.Client{
+		C:    httpClient,
+		Host: "127.0.0.1:8800",
+		Options: &prpc.Options{
+			PerRPCTimeout: 30 * time.Second,
+		},
+	}
+	fleetCostClient := fleetcostpb.NewFleetCostPRPCClient(prpcClient)
+	_, err = fleetCostClient.Ping(ctx, &fleetcostpb.PingRequest{})
+	return err
 }
