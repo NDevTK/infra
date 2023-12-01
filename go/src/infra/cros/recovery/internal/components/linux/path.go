bd4c0a17a535b75ed6603b64d22790bd9576cb2e
Jared Bennett <jaredbennett@google.com>
2023-12-01 22:50:23 +0000

    recovery: Add storage utilization report generator util
    Adds a linux utility for generating CSV reports of file storage usage
    in a given directory. This report can give insight into storage
    utilization changes over time, across different repair runs, making
    it easier to point out places where devices need files to be removed
    during repair processes so their drives do not fill up, making them
    unusable.
    
    BUG=b:302225472
    TEST=cd ~/cr/infra/go/src/infra/cros/recovery && \
    go clean && go build && go test ./...
    TEST=cd ~/cr/infra/go/src/infra && golangci-lint run \
    cros/recovery/internal/components/linux/...
    
    Change-Id: I51ae20845889e31073194d55cb33867d80e978dc
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4985756
    Commit-Queue: Jared Bennett <jaredbennett@google.com>
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/main@{#61623}

--- go/src/infra/cros/recovery/internal/components/linux/path.go
+++ go/src/infra/cros/recovery/internal/components/linux/path.go
@@ -1,14 +1,18 @@
-// Copyright 2022 The Chromium Authors. All rights reserved.
+// Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 package linux
 
 import (
+	"bufio"
 	"context"
+	"encoding/csv"
 	"fmt"
 	"math"
+	"path"
 	"path/filepath"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -16,6 +20,7 @@ import (
 	"go.chromium.org/luci/common/errors"
 
 	"infra/cros/recovery/internal/components"
+	"infra/cros/recovery/internal/execs"
 	"infra/cros/recovery/internal/log"
 )
 
@@ -107,4 +112,183 @@ func PathOccupiedSpacePercentage(ctx context.Context, r components.Runner, path 
 	}
 	log.Infof(ctx, "Found %v%% occupied space under %s", occupied, path)
 	return occupied, nil
+}
+
+// StorageUtilizationReportOfFilesInDir generates a CSV report of files present
+// in the provided directory and returns it as a string.
+func StorageUtilizationReportOfFilesInDir(ctx context.Context, runner components.Runner, dirPath string) (string, error) {
+	if !path.IsAbs(dirPath) {
+		return "", errors.Reason("log report of files in dir: dirPath %q is not an absolute path", dirPath).Err()
+	}
+	if err := IsPathExist(ctx, runner, dirPath); err != nil {
+		return "", errors.Annotate(err, "log report of files in dir: dirPath %q does not exist", dirPath).Err()
+	}
+	var csvReport [][]string
+	// Add header row.
+	csvReport = append(csvReport, []string{
+		"filepath",
+		"usage % of partition",
+		"bytes",
+		"KB",
+		"MB",
+	})
+	bytesInKB := math.Pow(2, 10)
+	bytesInMB := math.Pow(2, 20)
+
+	// Add size of partition where folder resides as the first entry.
+	pInfo, err := fetchResidingPartitionSizeInfo(ctx, runner, dirPath)
+	if err != nil {
+		return "", errors.Annotate(err, "log report of files in dir").Err()
+	}
+	csvReport = append(csvReport, []string{
+		fmt.Sprintf("[Mounted Drive %q]", pInfo.mountedOn),
+		fmt.Sprintf("%0.6f", pInfo.usagePercent),
+		fmt.Sprintf("%d of %d", pInfo.usedBytes, pInfo.totalUsableBytes),
+		fmt.Sprintf("%0.6f of %0.6f", float64(pInfo.usedBytes)/bytesInKB, float64(pInfo.totalUsableBytes)/bytesInKB),
+		fmt.Sprintf("%0.6f of %0.6f", float64(pInfo.usedBytes)/bytesInMB, float64(pInfo.totalUsableBytes)/bytesInMB),
+	})
+
+	// Add size of all files, collected using the du command.
+	var fileReportLines [][]string
+	duOutput, err := runner(ctx, time.Minute, "du", "--block-size=1", dirPath)
+	if err != nil {
+		return "", errors.Annotate(err, "log report of files in dir: failed to collect files in dir with du").Err()
+	}
+	linesScanner := bufio.NewScanner(strings.NewReader(duOutput))
+	linesScanner.Split(bufio.ScanLines)
+	for linesScanner.Scan() {
+		// Scan all the words in the data row.
+		wordsScanner := bufio.NewScanner(strings.NewReader(linesScanner.Text()))
+		wordsScanner.Split(bufio.ScanWords)
+		var duDataRow []string
+		for wordsScanner.Scan() {
+			duDataRow = append(duDataRow, wordsScanner.Text())
+		}
+		if len(duDataRow) == 0 {
+			// Skip empty line.
+			continue
+		}
+		if len(duDataRow) != 2 {
+			return "", errors.Annotate(err, "log report of files in dir: failed to parse du output: expected %d data columns, got %d", 2, len(duDataRow)).Err()
+		}
+		fileBytesStr := duDataRow[0]
+		filePath := duDataRow[1]
+		fileBytesInt, err := strconv.Atoi(fileBytesStr)
+		if err != nil {
+			return "", errors.Annotate(err, "log report of files in dir: failed to parse du output: failed to parse bytes %q", fileBytesStr).Err()
+		}
+		// Add to report.
+		fileReportLines = append(fileReportLines, []string{
+			filePath,
+			fmt.Sprintf("%0.6f", (float64(fileBytesInt)/float64(pInfo.totalUsableBytes))*100),
+			fmt.Sprintf("%d", fileBytesInt),
+			fmt.Sprintf("%0.6f", float64(fileBytesInt)/bytesInKB),
+			fmt.Sprintf("%0.6f", float64(fileBytesInt)/bytesInMB),
+		})
+	}
+	// Sort data lines by usage percent (desc) then path (asc) before adding them to the report.
+	sort.SliceStable(fileReportLines, func(i, j int) bool {
+		aUsage, err := strconv.ParseFloat(fileReportLines[i][1], 64)
+		if err != nil {
+			aUsage = 0
+		}
+		bUsage, err := strconv.ParseFloat(fileReportLines[j][1], 64)
+		if err != nil {
+			bUsage = 0
+		}
+		if aUsage == bUsage {
+			return fileReportLines[i][0] < fileReportLines[j][0]
+		}
+		return aUsage > bUsage
+	})
+	csvReport = append(csvReport, fileReportLines...)
+
+	// Log as CSV.
+	marshalledCsvReport, err := marshallCsvRows(csvReport)
+	if err != nil {
+		return "", errors.Annotate(err, "log report of files in dir").Err()
+	}
+	return marshalledCsvReport, nil
+}
+
+func marshallCsvRows(csvRows [][]string) (string, error) {
+	strBuilder := &strings.Builder{}
+	csvWriter := csv.NewWriter(strBuilder)
+	if err := csvWriter.WriteAll(csvRows); err != nil {
+		return "", errors.Annotate(err, "marshall csv rows").Err()
+	}
+	csvWriter.Flush()
+	return strBuilder.String(), nil
+}
+
+type partitionSizeInfo struct {
+	fileSystem           string
+	totalFileSystemBytes int
+	usedBytes            int
+	availableBytes       int
+	usagePercent         float64
+	mountedOn            string
+	totalUsableBytes     int
+}
+
+// fetchResidingPartitionSizeInfo runs "df" with the given path and returns
+// a populated partitionSizeInfo based on its output.
+//
+// Note: partitionSizeInfo.usagePercent is calculated rather than using the
+// rounded value provided by "df" for a more accurate measurement.
+//
+// The partitionSizeInfo.totalUsableBytes value is guaranteed to be non-zero.
+func fetchResidingPartitionSizeInfo(ctx context.Context, runner execs.Runner, dirPath string) (*partitionSizeInfo, error) {
+	// Collect partition size info using df command.
+	dfOutput, err := runner(ctx, time.Minute, "df", "--block-size=1", dirPath)
+	if err != nil {
+		return nil, errors.Annotate(err, "fetch residing partition size: failed to get size of residing partition").Err()
+	}
+	scanner := bufio.NewScanner(strings.NewReader(dfOutput))
+	// Scan to the second line, as the first line is just a header.
+	scanner.Split(bufio.ScanLines)
+	scanner.Scan()
+	if !scanner.Scan() {
+		return nil, errors.Reason("fetch residing partition size: failed to read first two lines of df output").Err()
+	}
+	// Scan the data from this row.
+	scanner = bufio.NewScanner(strings.NewReader(scanner.Text()))
+	scanner.Split(bufio.ScanWords)
+	var dataRow []string
+	for scanner.Scan() {
+		dataRow = append(dataRow, scanner.Text())
+	}
+	if len(dataRow) != 6 {
+		return nil, errors.Reason("fetch residing partition size: expected %d columns in df data row, got %d", 6, len(dataRow)).Err()
+	}
+	// Collect relevant data from output.
+	pInfo := &partitionSizeInfo{
+		fileSystem: dataRow[0],
+		mountedOn:  dataRow[5],
+	}
+	totalFileSystemBytes, err := strconv.Atoi(dataRow[1])
+	if err != nil {
+		return nil, errors.Annotate(err, "fetch residing partition size: failed to parse totalFileSystemBytes from df output from %q", dataRow[1]).Err()
+	}
+	pInfo.totalFileSystemBytes = totalFileSystemBytes
+	usedBytes, err := strconv.Atoi(dataRow[2])
+	if err != nil {
+		return nil, errors.Annotate(err, "fetch residing partition size: failed to parse usedBytes from df output from %q", dataRow[2]).Err()
+	}
+	pInfo.usedBytes = usedBytes
+	availableBytes, err := strconv.Atoi(dataRow[3])
+	if err != nil {
+		return nil, errors.Annotate(err, "fetch residing partition size: failed to parse availableBytes from df output from %q", dataRow[3]).Err()
+	}
+	pInfo.availableBytes = availableBytes
+	// Calculate total usable bytes (will be less than totalFileSystemBytes).
+	pInfo.totalUsableBytes = usedBytes + availableBytes
+	// Calculate usage the same way "df" does.
+	if pInfo.totalUsableBytes == 0 {
+		// Should not ever happen, but through an error here to avoid unexpected
+		// divide by zero from bad df output.
+		return nil, errors.Annotate(err, "fetch residing partition size: zero totalUsableBytes").Err()
+	}
+	pInfo.usagePercent = (float64(pInfo.usedBytes) / float64(pInfo.totalUsableBytes)) * 100.0
+	return pInfo, nil
 }
--- go/src/infra/cros/recovery/internal/components/linux/path_test.go
+++ go/src/infra/cros/recovery/internal/components/linux/path_test.go
@@ -203,3 +203,112 @@ func TestPathOccupiedSpacePercentage(t *testing.T) {
 		})
 	}
 }
+
+func TestStorageUtilizationReportOfFilesInDir(t *testing.T) {
+	const testDirPath = "/tmp/stateful_partition"
+	const sampleDfOutput1 = `Filesystem      1B-blocks      Used  Available Use% Mounted on
+/dev/mmcblk0p1 2053300224 617697280 1312817152  32% /mnt/stateful_partition`
+	const sampleDuOutput1 = `4096	/mnt/stateful_partition/var/lock
+4096	/mnt/stateful_partition/var/db/pkg
+8192	/mnt/stateful_partition/var/db
+4096	/mnt/stateful_partition/var/tmp/portage
+307200	/mnt/stateful_partition/var/tmp
+4096	/mnt/stateful_partition/var/run
+12288	/mnt/stateful_partition/var/lib/dhcpcd
+81920	/mnt/stateful_partition/var/lib/update_engine
+12288	/mnt/stateful_partition/var/lib/trim
+4096	/mnt/stateful_partition/var/lib/chaps
+4096	/mnt/stateful_partition/var/lib/tpm
+`
+	expectedHeaderRow := []string{
+		"filepath",
+		"usage % of partition",
+		"bytes",
+		"KB",
+		"MB",
+	}
+	type mockRunnerOutput struct {
+		dfCmdOutput string
+		duCmdOutput string
+	}
+	tests := []struct {
+		name             string
+		mockRunnerOutput mockRunnerOutput
+		wantCsvDataRows  [][]string
+		wantErr          bool
+	}{
+		{
+			"no output",
+			mockRunnerOutput{
+				"",
+				"",
+			},
+			nil,
+			true,
+		},
+		{
+			"valid available space and no files found",
+			mockRunnerOutput{
+				sampleDfOutput1,
+				"",
+			},
+			[][]string{
+				{"[Mounted Drive \"/mnt/stateful_partition\"]", "31.996512", "617697280 of 1930514432", "603220.000000 of 1885268.000000", "589.082031 of 1841.082031"},
+			},
+			false,
+		},
+		{
+			"valid available space and multiple files found",
+			mockRunnerOutput{
+				sampleDfOutput1,
+				sampleDuOutput1,
+			},
+			[][]string{
+				{"[Mounted Drive \"/mnt/stateful_partition\"]", "31.996512", "617697280 of 1930514432", "603220.000000 of 1885268.000000", "589.082031 of 1841.082031"},
+				{"/mnt/stateful_partition/var/tmp", "0.015913", "307200", "300.000000", "0.292969"},
+				{"/mnt/stateful_partition/var/lib/update_engine", "0.004243", "81920", "80.000000", "0.078125"},
+				{"/mnt/stateful_partition/var/lib/dhcpcd", "0.000637", "12288", "12.000000", "0.011719"},
+				{"/mnt/stateful_partition/var/lib/trim", "0.000637", "12288", "12.000000", "0.011719"},
+				{"/mnt/stateful_partition/var/db", "0.000424", "8192", "8.000000", "0.007812"},
+				{"/mnt/stateful_partition/var/db/pkg", "0.000212", "4096", "4.000000", "0.003906"},
+				{"/mnt/stateful_partition/var/lib/chaps", "0.000212", "4096", "4.000000", "0.003906"},
+				{"/mnt/stateful_partition/var/lib/tpm", "0.000212", "4096", "4.000000", "0.003906"},
+				{"/mnt/stateful_partition/var/lock", "0.000212", "4096", "4.000000", "0.003906"},
+				{"/mnt/stateful_partition/var/run", "0.000212", "4096", "4.000000", "0.003906"},
+				{"/mnt/stateful_partition/var/tmp/portage", "0.000212", "4096", "4.000000", "0.003906"},
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockRunner := func(ctx context.Context, timeout time.Duration, cmd string, args ...string) (string, error) {
+				if strings.HasPrefix(cmd, "test") {
+					return "", nil
+				}
+				if cmd == "df" {
+					return tt.mockRunnerOutput.dfCmdOutput, nil
+				}
+				if cmd == "du" {
+					return tt.mockRunnerOutput.duCmdOutput, nil
+				}
+				return "", errors.Reason("unexpected command run of %q", cmd).Err()
+			}
+			got, err := StorageUtilizationReportOfFilesInDir(context.Background(), mockRunner, testDirPath)
+			if err != nil || tt.wantErr {
+				if tt.wantErr != (err != nil) {
+					t.Errorf("StorageUtilizationReportOfFilesInDir() error = %v, wantErr %v", err, tt.wantErr)
+				}
+				return
+			}
+			allWantedCsvRows := append([][]string{expectedHeaderRow}, tt.wantCsvDataRows...)
+			wantCsv, err := marshallCsvRows(allWantedCsvRows)
+			if err != nil {
+				t.Errorf("test config error: invalid wantCsvDataRows: %v", err)
+			}
+			if got != wantCsv {
+				t.Errorf("StorageUtilizationReportOfFilesInDir()\ngot CSV: = \n%vwant CSV:\n%v", got, wantCsv)
+			}
+		})
+	}
+}
