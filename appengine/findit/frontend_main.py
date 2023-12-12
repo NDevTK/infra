d4468ca9931f9369d1ea9dae05e3d2b92adcde93
Prakhar <pasthana@google.com>
2023-12-12 00:42:48 -0800

    Delete unused url mappings from findit
    There are no usages of /_ah/pipeline urls. Also removing url redirects since all known usages of old url schemes have been removed. Also root url(/) is handling is removed from frontend_main, as it is already handled by default_main. url_redirect handler has been removed too
    
    Bug: 1414535
    Change-Id: Id4311773aea0c7057e2347063eb56d65ed2b0c73

--- appengine/findit/frontend_main.py
+++ appengine/findit/frontend_main.py
@@ -7,30 +7,14 @@ import webapp2
 import gae_ts_mon
 
 from gae_libs import appengine_util
-from gae_libs.pipelines import pipeline_status_ui
 from handlers import config
-from handlers import home
-from handlers import url_redirect
 from handlers.code_coverage import serve_ci_coverage
 from handlers.code_coverage import serve_cq_coverage
 
-# App Engine pipeline status pages.
-pipeline_status_handler_mappings = [
-    ('/_ah/pipeline/rpc/tree', pipeline_status_ui._TreeStatusHandler),
-    ('/_ah/pipeline/rpc/class_paths', pipeline_status_ui._ClassPathListHandler),
-    ('/_ah/pipeline/rpc/list', pipeline_status_ui._RootListHandler),
-    ('/_ah/pipeline(/.+)', pipeline_status_ui._StatusUiHandler),
-]
-pipeline_status_application = webapp2.WSGIApplication(
-    pipeline_status_handler_mappings, debug=False)
-if appengine_util.IsInProductionApp():
-  gae_ts_mon.initialize_prod(pipeline_status_application)
 
 # waterfall frontend.
 waterfall_frontend_web_pages_handler_mappings = [
-    ('/', home.Home),
     ('/waterfall/config', config.Configuration),
-    (r'/.*', url_redirect.URLRedirect),
 ]
 waterfall_frontend_web_application = webapp2.WSGIApplication(
     waterfall_frontend_web_pages_handler_mappings, debug=False)
