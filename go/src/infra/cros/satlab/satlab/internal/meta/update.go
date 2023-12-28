6f1e4d5a5e9bc6ef3e17ade14171ab9244f60217
Eli <elijahtrexler@google.com>
2023-12-28 03:55:49 +0000

    satlab: Fix `satlab update` by updating all pkgs
    `shivas update` and `satlab update` are broken for Satlab as they will
    ignore the ensure file we have and delete all CIPD packages besides
    whichever command we use to update. Took a stab at fixing here:
    https://crrev.com/c/5112362 but based on Robbie/Vadim's comments I think
    we should just pivot `satlab update` to actually call `cipd ensure`.
    
    So this does just that- calls `cipd ensure` with the known CIPD root,
    ensure file.
    
    This does make it so all CIPD packages will be updated (not just
    `satlab`). Notably `shivas` will be updated to prod.
    
    BUG=b/303674138
    Test Plan=manual
    
    Change-Id: I3865d272a1866af939b8fa9b5279ed8deb3b4547
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5115150
    Commit-Queue: Eli Trexler <elijahtrexler@google.com>
    Reviewed-by: Prasad Vuppalapu <prasadv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#62016}

--- go/src/infra/cros/satlab/satlab/internal/meta/update.go
+++ go/src/infra/cros/satlab/satlab/internal/meta/update.go
@@ -6,30 +6,32 @@ package meta
 
 import (
 	"fmt"
+	"os/exec"
 
 	"github.com/maruel/subcommands"
 	"go.chromium.org/luci/auth/client/authcli"
-	"go.chromium.org/luci/common/errors"
 
 	"infra/cros/satlab/common/site"
-	"infra/libs/cipd"
 )
 
-// cipd reference tag for the dev version. Reference tag for prod version is prod.
-var devVersionRefTag string = "latest"
+// CipdRoot is the path to root directory for CIPD.
+var CipdRoot = "/cipd"
+
+// CipdEnsureFile is the path to the ensure file.
+var CipdEnsureFile = "/cipd/spec"
 
 // Update subcommand: Update satlab tool.
 var Update = &subcommands.Command{
 	UsageLine: "upgrade", // we have a separate update cmd under UFS, so changing this to upgrade.
-	ShortDesc: "upgrade satlab",
-	LongDesc: `Upgrade satlab.
+	ShortDesc: "upgrade satlab CIPD packages",
+	LongDesc: `Upgrade satlab CIPD packages.
 
-This is a thin wrapper around CIPD.`,
+This is a thin wrapper around CIPD. Wil`,
 	CommandRun: func() subcommands.CommandRun {
 		c := &updateRun{}
 		c.authFlags.Register(&c.Flags, site.DefaultAuthOptions)
 
-		c.Flags.BoolVar(&c.dev, "dev", false, "Upgrade to dev version")
+		c.Flags.BoolVar(&c.silent, "silent", false, "Whether to run silently.")
 		return c
 	},
 }
@@ -39,13 +41,15 @@ type updateRun struct {
 	subcommands.CommandRunBase
 	authFlags authcli.Flags
 
-	dev bool
+	silent bool
 }
 
 // Run updates satlab and returns an exit status.
 func (c *updateRun) Run(a subcommands.Application, args []string, env subcommands.Env) int {
 	if err := c.innerRun(a, args, env); err != nil {
-		fmt.Fprintf(a.GetErr(), "%s: %s\n", a.GetName(), err)
+		if !c.silent {
+			fmt.Fprintf(a.GetErr(), "%s: %s\n", a.GetName(), err)
+		}
 		return 1
 	}
 	return 0
@@ -53,16 +57,26 @@ func (c *updateRun) Run(a subcommands.Application, args []string, env subcommand
 
 // InnerRun is the implementation of the run command.
 func (c *updateRun) innerRun(a subcommands.Application, args []string, env subcommands.Env) error {
-	if c.dev {
-		if err := cipd.UpdatePackage(site.CipdInstalledPath, devVersionRefTag, a.GetOut(), a.GetErr()); err != nil {
-			return errors.Annotate(err, "update").Err()
-		}
-	} else {
-		if err := cipd.UpdatePackageToProd(site.CipdInstalledPath, a.GetOut(), a.GetErr()); err != nil {
-			return errors.Annotate(err, "update").Err()
-		}
+	if err := c.ensure(a, CipdRoot, CipdEnsureFile); err != nil {
+		return err
 	}
-	fmt.Fprintf(a.GetErr(), "%s: You may need to run satlab login again after the upgrade\n", a.GetName())
-	fmt.Fprintf(a.GetErr(), "%s: Run satlab whoami to check login status\n", a.GetName())
+	if !c.silent {
+		fmt.Fprintf(a.GetErr(), "%s: You may need to run satlab login again after the upgrade\n", a.GetName())
+		fmt.Fprintf(a.GetErr(), "%s: Run satlab whoami to check login status\n", a.GetName())
+	}
+	return nil
+}
+
+// ensure wraps a cipd ensure call.
+func (c *updateRun) ensure(a subcommands.Application, root string, ensureFile string) error {
+	cmd := exec.Command("sudo", "cipd", "ensure", "-root", root, "-ensure-file", ensureFile)
+	if !c.silent {
+		cmd.Stdout = a.GetOut()
+		cmd.Stderr = a.GetErr()
+	}
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
 	return nil
 }
