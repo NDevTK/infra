8d5e9871ba139fc5f595eed7f51456cb9328af77
Brian Ryner <bryner@google.com>
2024-01-10 04:28:28 +0000

    Hardcode the version number for protobuf-cpp-21.
    This release is no longer in the first page of releases returned
    by the github API. Since this series will not get any more updates,
    just hardcode the last v21 release.
    
    Change-Id: I916215ae3624988ee4ab2802f95a9de3c5b2896a
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5183294
    Commit-Queue: Chenlin Fan <fancl@chromium.org>
    Reviewed-by: Chenlin Fan <fancl@chromium.org>
    Auto-Submit: Brian Ryner <bryner@google.com>
    Cr-Commit-Position: refs/heads/main@{#62238}

--- 3pp/protobuf-cpp-21/fetch.py
+++ 3pp/protobuf-cpp-21/fetch.py
@@ -20,64 +20,21 @@ urllib.request.install_opener(
         urllib.request.HTTPSHandler(
             context=ssl.create_default_context(cafile=certifi.where()))))
 
-# https://docs.github.com/en/rest/releases/releases#list-releases
-# Returns a JSON-loadable text response like:
-# [
-#  {
-#   "url": "https://api.github.com/repos/protocolbuffers/protobuf/releases/78625517",
-#   ...
-#   "tag_name": "v21.7",
-#   ...
-#   "assets": [
-#     {
-#       ...,
-#       "browser_download_url": "...",
-#       ...,
-#       "name": "protoc-21.7-win32.zip",
-#       ...,
-#     },
-#     ...
-#   ],
-#   ...
-# }
-#
-# The only thing we use here are the tag names. These are sorted and we
-# select the highest version number as 'latest'. We do this rather than
-# depend on the 'latest' API, which only reports the most recently-created
-# release and as such may include backported security fixes.
-RELEASES = 'https://api.github.com/repos/protocolbuffers/protobuf/releases'
+# Protobuf v21 is no longer being updated, so freeze the release version.
+LATEST = '21.12'
 
 # https://developer.github.com/v3/repos/releases/#get-a-release-by-tag-name
 # Returns a JSON loadable text response like LATEST, but for a specific tag.
 TAGGED_RELEASE = (
     'https://api.github.com/repos/protocolbuffers/protobuf/releases/tags/v%s')
 
-# TODO(crbug.com/1422445): Remove version limit once blocking issues are fixed.
-VERSION_LIMIT = packaging.version.parse("22.0")
-
 
 def do_latest():
-  releases = json.load(urllib.request.urlopen(RELEASES))
-  latest = packaging.version.parse('0')
-  for r in releases:
-    if r['prerelease']:
-      continue
-    v = packaging.version.parse(r['tag_name'][1:])  # Strip leading 'v'
-    if v >= VERSION_LIMIT:
-      continue
-    latest = max(latest, v)
-  print(latest)
+  print(LATEST)
 
 
 def get_download_url(version):
-  # Starting with v21, the language major version is decoupled from the
-  # protocol buffers version.
-  # Starting with v21.11, only zip files are provided. Prefer .tar.gz to
-  # keep a consistent source location for older releases.
-  if version >= '21.11':
-    ext = '.zip'
-  else:
-    ext = '.tar.gz'
+  ext = '.zip'
   name_re = r'protobuf-cpp-([0-9]*\.)?%s' % version + ext
 
   rsp = json.load(urllib.request.urlopen(TAGGED_RELEASE % version))
