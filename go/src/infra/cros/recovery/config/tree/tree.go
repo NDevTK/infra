ed871bb7a6b37b0340c230f9375c33593f5eaa8e
Otabek Kasimov <otabek@google.com>
2024-01-24 11:44:37 -0800

    recovery: Add short version of tree
    Sort the field for better reading of the tree.
    
    BUG=None
    TEST=make test
    
    Change-Id: I1cd873f6f034ca8795a3bb61c586c9826e7c45aa

--- go/src/infra/cros/recovery/config/tree/tree.go
+++ go/src/infra/cros/recovery/config/tree/tree.go
@@ -5,6 +5,7 @@
 package tree
 
 import (
+	"fmt"
 	"infra/cros/recovery/config"
 )
 
@@ -15,7 +16,7 @@ type Configuration struct {
 }
 
 // ConvertConfiguration converts Configuration to a tree representation.
-func ConvertConfiguration(configuration *config.Configuration) *Configuration {
+func ConvertConfiguration(configuration *config.Configuration, shortVersion bool) *Configuration {
 	c := &Configuration{}
 	for _, planName := range configuration.GetPlanNames() {
 		if planName == config.PlanClosing {
@@ -23,10 +24,10 @@ func ConvertConfiguration(configuration *config.Configuration) *Configuration {
 			continue
 		}
 		p := configuration.GetPlans()[planName]
-		c.Plans = append(c.Plans, ConvertPlan(planName, p))
+		c.Plans = append(c.Plans, ConvertPlan(planName, p, shortVersion))
 	}
 	if p, ok := configuration.GetPlans()[config.PlanClosing]; ok {
-		c.Plans = append(c.Plans, ConvertPlan(config.PlanClosing, p))
+		c.Plans = append(c.Plans, ConvertPlan(config.PlanClosing, p, shortVersion))
 	}
 	return c
 }
@@ -47,14 +48,18 @@ type planConverter struct {
 }
 
 // ConvertPlan converts Plan to a tree representation.
-func ConvertPlan(name string, plan *config.Plan) *Plan {
+func ConvertPlan(name string, plan *config.Plan, shortVersion bool) *Plan {
 	p := &Plan{
 		Name:      name,
 		AllowFail: plan.GetAllowFail(),
 	}
+	if shortVersion && p.AllowFail {
+		p.AllowFail = false
+		p.Name = fmt.Sprintf("%s (Allow to fail)", p.Name)
+	}
 	converter := &planConverter{srcPlan: plan}
 	for _, actionName := range plan.GetCriticalActions() {
-		p.CriticalActions = append(p.CriticalActions, converter.convertAcion(actionName, false))
+		p.CriticalActions = append(p.CriticalActions, converter.convertAcion(actionName, false, shortVersion))
 	}
 	return p
 }
@@ -64,20 +69,20 @@ func ConvertPlan(name string, plan *config.Plan) *Plan {
 type Action struct {
 	// Name of the Action.
 	Name string `json:"name,omitempty"`
+	// Name of the exec function to use.
+	ExecName string `json:"exec_name,omitempty"`
 	// Documentation to describe detail of the action.
 	Docs []string `json:"docs,omitempty"`
 	// List of actions to determine if this action is applicable for the resource.
 	Conditions []interface{} `json:"conditions,omitempty"`
 	// List of actions that must pass before executing this action's exec function.
 	Dependencies []interface{} `json:"dependencies,omitempty"`
-	// Name of the exec function to use.
-	ExecName string `json:"exec_name,omitempty"`
+	// List of actions used to recover this action if exec function fails.
+	Recoveries []interface{} `json:"recoveries,omitempty"`
 	// Extra arguments provided to the exec function.
 	ExecArgs []string `json:"exec_args,omitempty"`
 	// Allowed time to execute exec function.
 	Timeout string `json:"timeout,omitempty"`
-	// List of actions used to recover this action if exec function fails.
-	Recoveries []interface{} `json:"recoveries,omitempty"`
 	// If set to true, then the action is treated as if it passed even if it
 	// and all its recovery actions failed.
 	AllowFailAfterRecovery bool `json:"allow_fail_after_recovery,omitempty"`
@@ -85,31 +90,44 @@ type Action struct {
 	RunControl string `json:"run_control,omitempty"`
 }
 
-func (t *planConverter) convertAcion(name string, excludeRecoveries bool) *Action {
+func (t *planConverter) convertAcion(name string, excludeRecoveries, shortVersion bool) *Action {
 	action, ok := t.srcPlan.GetActions()[name]
 	if !ok {
 		return nil
 	}
-	a := &Action{
-		Name:                   name,
-		Docs:                   action.GetDocs(),
-		ExecName:               action.GetExecName(),
-		ExecArgs:               action.GetExecExtraArgs(),
-		AllowFailAfterRecovery: action.GetAllowFailAfterRecovery(),
-		RunControl:             action.GetRunControl().String(),
-	}
-	if action.GetExecTimeout() != nil {
-		a.Timeout = action.GetExecTimeout().AsDuration().String()
+	var a *Action
+	if shortVersion {
+		a = &Action{
+			Name: name,
+		}
+		if action.GetAllowFailAfterRecovery() {
+			a.Name = fmt.Sprintf("%s (Allow to fail)", a.Name)
+		}
+		if name != action.GetExecName() {
+			a.ExecName = action.GetExecName()
+		}
+	} else {
+		a = &Action{
+			Name:                   name,
+			ExecName:               action.GetExecName(),
+			AllowFailAfterRecovery: action.GetAllowFailAfterRecovery(),
+		}
+		a.Docs = action.GetDocs()
+		a.ExecArgs = action.GetExecExtraArgs()
+		a.RunControl = action.GetRunControl().String()
+		if action.GetExecTimeout() != nil {
+			a.Timeout = action.GetExecTimeout().AsDuration().String()
+		}
 	}
 	for _, actionName := range action.GetConditions() {
-		a.Conditions = append(a.Conditions, t.convertAcion(actionName, true))
+		a.Conditions = append(a.Conditions, t.convertAcion(actionName, true, shortVersion))
 	}
 	for _, actionName := range action.GetDependencies() {
-		a.Dependencies = append(a.Dependencies, t.convertAcion(actionName, excludeRecoveries || false))
+		a.Dependencies = append(a.Dependencies, t.convertAcion(actionName, excludeRecoveries || false, shortVersion))
 	}
 	if !excludeRecoveries {
 		for _, actionName := range action.GetRecoveryActions() {
-			a.Recoveries = append(a.Recoveries, t.convertAcion(actionName, true))
+			a.Recoveries = append(a.Recoveries, t.convertAcion(actionName, true, shortVersion))
 		}
 	}
 	return a
