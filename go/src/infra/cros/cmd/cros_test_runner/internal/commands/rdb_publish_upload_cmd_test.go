171368aa489ff4b8a43b140df03794f3b343f712
Allen Xie <zhihuixie@google.com>
2024-01-25 05:11:50 +0000

    cros_publish: add an unit test to verify the data population
    To improve code robustness.
    
    Bug: b:319168950
    Change-Id: I630af0520b8795145566fe0575aad7cc0c4c82a6
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5195099
    Reviewed-by: Jason Kusuma <jkusuma@google.com>
    Reviewed-by: Derek Beckett <dbeckett@chromium.org>
    Commit-Queue: Zhihui Xie <zhihuixie@google.com>
    Cr-Commit-Position: refs/heads/main@{#62558}

--- go/src/infra/cros/cmd/cros_test_runner/internal/commands/rdb_publish_upload_cmd_test.go
+++ go/src/infra/cros/cmd/cros_test_runner/internal/commands/rdb_publish_upload_cmd_test.go
@@ -6,22 +6,38 @@ package commands_test
 
 import (
 	"context"
+	"infra/cros/cmd/common_lib/common"
 	"infra/cros/cmd/common_lib/containers"
 	"infra/cros/cmd/common_lib/tools/crostoolrunner"
 	"infra/cros/cmd/cros_test_runner/data"
 	"infra/cros/cmd/cros_test_runner/internal/commands"
 	"infra/cros/cmd/cros_test_runner/internal/executors"
 	"testing"
+	"time"
 
+	"github.com/golang/protobuf/ptypes/duration"
 	. "github.com/smartystreets/goconvey/convey"
 	_go "go.chromium.org/chromiumos/config/go"
+	configpb "go.chromium.org/chromiumos/config/go"
 	buildapi "go.chromium.org/chromiumos/config/go/build/api"
 	"go.chromium.org/chromiumos/config/go/test/api"
+	testapi "go.chromium.org/chromiumos/config/go/test/api"
 	"go.chromium.org/chromiumos/config/go/test/api/metadata"
 	"go.chromium.org/chromiumos/config/go/test/artifact"
+	artifactpb "go.chromium.org/chromiumos/config/go/test/artifact"
+	labapi "go.chromium.org/chromiumos/config/go/test/lab/api"
 	"go.chromium.org/chromiumos/infra/proto/go/test_platform/skylab_test_runner"
+	bbpb "go.chromium.org/luci/buildbucket/proto"
+	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
 	. "go.chromium.org/luci/common/testing/assertions"
+	"go.chromium.org/luci/luciexe/build"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
+
+func parseTime(s string) time.Time {
+	t, _ := time.Parse("2006-01-02T15:04:05.99Z", s)
+	return t
+}
 
 func TestRdbPublishPublishCmd_UnsupportedSK(t *testing.T) {
 	t.Parallel()
@@ -156,6 +172,229 @@ func TestRdbPublishPublishCmd_ExtractSources(t *testing.T) {
 func TestRdbPublishPublishCmd_ExtractDepsSuccess(t *testing.T) {
 	t.Parallel()
 
+	// Common setup
+	ctrCipd := crostoolrunner.CtrCipdInfo{Version: "prod"}
+	ctr := &crostoolrunner.CrosToolRunner{CtrCipdInfo: ctrCipd}
+	cont := containers.NewCrosPublishTemplatedContainer(
+		containers.CrosRdbPublishTemplatedContainerType,
+		"container/image/path",
+		ctr)
+	exec := executors.NewCrosPublishExecutor(
+		cont,
+		executors.CrosRdbPublishExecutorType)
+	cmd := commands.NewRdbPublishUploadCmd(exec)
+
+	Convey("Populate TestResultForRdb with full info", t, func() {
+		ctx := context.Background()
+		createTime := timestamppb.New(parseTime("2022-09-07T18:53:33.983328614Z"))
+		startedTime := timestamppb.New(parseTime("2022-09-07T20:53:33.983328614Z"))
+		duration := &duration.Duration{Seconds: 60}
+		dut := &labapi.Dut{
+			Id: &labapi.Dut_Id{Value: "0wgtfqin2033834d-ecghcra"},
+			DutType: &labapi.Dut_Chromeos{
+				Chromeos: &labapi.Dut_ChromeOS{
+					Name: "0wgtfqin2033834d-ecghcra",
+					DutModel: &labapi.DutModel{
+						ModelName: "nipperkin",
+					},
+				},
+			},
+		}
+		wantTestResult := &artifactpb.TestResult{
+			TestInvocation: &artifactpb.TestInvocation{
+				PrimaryExecutionInfo: &artifactpb.ExecutionInfo{
+					BuildInfo: &artifactpb.BuildInfo{
+						Name:        "hatch-cq/R106-15048.0.0",
+						Board:       "hatch",
+						BuildTarget: "hatch",
+						BuildMetadata: &artifactpb.BuildMetadata{
+							Sku: &artifactpb.BuildMetadata_Sku{
+								HwidSku: "CRAASK-HULX D4B-F4E-F3F-B2K-L3I-Q6I",
+							},
+							Chipset: &artifactpb.BuildMetadata_Chipset{
+								WifiChip: "INTEL_GFP2_AX211",
+							},
+							Cellular: &artifactpb.BuildMetadata_Cellular{
+								Carrier: "CARRIER_ESIM",
+							},
+							Firmware: &artifactpb.BuildMetadata_Firmware{},
+							Kernel:   &artifactpb.BuildMetadata_Kernel{},
+							Lacros:   &artifactpb.BuildMetadata_Lacros{},
+						},
+					},
+					DutInfo: &artifactpb.DutInfo{
+						Dut: dut,
+						ProvisionState: &testapi.ProvisionState{
+							SystemImage: &testapi.ProvisionState_SystemImage{
+								SystemImagePath: &_go.StoragePath{
+									HostType: _go.StoragePath_GS,
+									Path:     "gs://some-bucket/builder/build-12345",
+								},
+							},
+						},
+					},
+					EnvInfo: &artifactpb.ExecutionInfo_SkylabInfo{
+						SkylabInfo: &artifactpb.SkylabInfo{
+							DroneInfo: &artifactpb.DroneInfo{
+								Drone:       "skylab-drone-deployment-prod-6dc79d4f9-czjlj",
+								DroneServer: "chromeos4-row4-rack1-drone8",
+							},
+							BuildbucketInfo: &artifactpb.BuildbucketInfo{
+								Id:          100,
+								AncestorIds: []int64{98, 99},
+								Builder: &artifactpb.BuilderID{
+									Project: "chromeos",
+									Bucket:  "test_runner",
+									Builder: "test_runner-dev",
+								},
+							},
+							SwarmingInfo: &artifactpb.SwarmingInfo{
+								TaskId:      "taskId0",
+								SuiteTaskId: "parentId0",
+								TaskName:    "bb-100-chromeos/test_runner/test_runner-dev",
+								Pool:        "ChromeOSSkylab",
+								LabelPool:   "DUT_POOL_QUOTA",
+							},
+						},
+					},
+				},
+				DutTopology: &labapi.DutTopology{
+					Id:   &labapi.DutTopology_Id{Value: "0wgtfqin2033834d-ecghcra"},
+					Duts: []*labapi.Dut{dut},
+				},
+			},
+			TestRuns: []*artifactpb.TestRun{
+				{
+					TestCaseInfo: &artifactpb.TestCaseInfo{
+						TestCaseResult: &testapi.TestCaseResult{
+							TestHarness: &testapi.TestHarness{
+								TestHarnessType: &testapi.TestHarness_Tast_{
+									Tast: &testapi.TestHarness_Tast{},
+								},
+							},
+							TestCaseId: &testapi.TestCase_Id{
+								Value: "tast.rlz_CheckPing",
+							},
+							Verdict:   &testapi.TestCaseResult_Pass_{},
+							StartTime: startedTime,
+							Duration:  duration,
+						},
+						DisplayName:     "hatch-cq/R102-14632.0.0-62834-8818718496810023809/wificell-cq/tast.wificell-cq",
+						Suite:           "arc-cts-vm",
+						Branch:          "main",
+						MainBuilderName: "main-release",
+					},
+					LogsInfo: []*configpb.StoragePath{
+						{
+							HostType: configpb.StoragePath_GS,
+							Path:     "gs://some-bucket/builder/build-12345",
+						},
+					},
+					TestHarness: &testapi.TestHarness{
+						TestHarnessType: &testapi.TestHarness_Tast_{
+							Tast: &testapi.TestHarness_Tast{},
+						},
+					},
+					TimeInfo: &artifactpb.TimingInfo{
+						QueuedTime:  createTime,
+						StartedTime: startedTime,
+						Duration:    duration,
+					},
+				},
+			},
+		}
+
+		// Sets up the build info.
+		buildPb := &bbpb.Build{
+			Id:     100,
+			Status: bbpb.Status_SUCCESS,
+			Builder: &bbpb.BuilderID{
+				Project: "chromeos",
+				Bucket:  "test_runner",
+				Builder: "test_runner-dev",
+			},
+			AncestorIds: []int64{98, 99},
+			Tags:        []*buildbucketpb.StringPair{{Key: "display_name", Value: "hatch-cq/R102-14632.0.0-62834-8818718496810023809/wificell-cq/tast.wificell-cq"}},
+			CreateTime:  createTime,
+			Infra: &bbpb.BuildInfra{Swarming: &bbpb.BuildInfra_Swarming{
+				TaskId:      "taskId1",
+				ParentRunId: "parentId1",
+				BotDimensions: []*buildbucketpb.StringPair{
+					{Key: "label-wifi_chip", Value: "INTEL_GFP2_AX211"},
+					{Key: "label-hwid_sku", Value: "CRAASK-HULX D4B-F4E-F3F-B2K-L3I-Q6I"},
+					{Key: "label-carrier", Value: "CARRIER_ESIM"},
+					{Key: "drone", Value: "skylab-drone-deployment-prod-6dc79d4f9-czjlj"},
+					{Key: "drone_server", Value: "chromeos4-row4-rack1-drone8"},
+					{Key: "pool", Value: "ChromeOSSkylab"},
+					{Key: "label-pool", Value: "DUT_POOL_QUOTA"},
+				},
+			}},
+		}
+		buildState, ctx, err := build.Start(ctx, buildPb)
+		defer func() { buildState.End(err) }()
+
+		sk := &data.HwTestStateKeeper{
+			CurrentInvocationId: "Inv-1234",
+			TesthausUrl:         "www.testhaus.com",
+			CftTestRequest: &skylab_test_runner.CFTTestRequest{
+				PrimaryDut: &skylab_test_runner.CFTTestRequest_Device{
+					DutModel: &labapi.DutModel{
+						BuildTarget: "hatch",
+					},
+					ProvisionState: &api.ProvisionState{
+						SystemImage: &api.ProvisionState_SystemImage{
+							SystemImagePath: &_go.StoragePath{
+								HostType: _go.StoragePath_GS,
+								Path:     "gs://some-bucket/builder/build-12345",
+							},
+						},
+					},
+				},
+				AutotestKeyvals: map[string]string{
+					"build_target":        "hatch",
+					"build":               "hatch-cq/R106-15048.0.0",
+					"suite":               "arc-cts-vm",
+					"branch":              "main",
+					"master_build_config": "main-release",
+				},
+			},
+			Devices: map[string]*testapi.CrosTestRequest_Device{
+				common.PrimaryDevice: {
+					Dut: dut,
+				},
+			},
+			GcsUrl:     "gs://some-bucket/builder/build-12345",
+			BuildState: buildState,
+			DutTopology: &labapi.DutTopology{
+				Id:   &labapi.DutTopology_Id{Value: "0wgtfqin2033834d-ecghcra"},
+				Duts: []*labapi.Dut{dut},
+			},
+			TestResponses: &testapi.CrosTestResponse{
+				TestCaseResults: []*testapi.TestCaseResult{
+					{
+						TestHarness: &testapi.TestHarness{
+							TestHarnessType: &testapi.TestHarness_Tast_{
+								Tast: &testapi.TestHarness_Tast{},
+							},
+						},
+						TestCaseId: &testapi.TestCase_Id{
+							Value: "tast.rlz_CheckPing",
+						},
+						Verdict:   &testapi.TestCaseResult_Pass_{},
+						StartTime: startedTime,
+						Duration:  duration,
+					},
+				},
+			},
+		}
+
+		// Extract deps first
+		err = cmd.ExtractDependencies(ctx, sk)
+		So(err, ShouldBeNil)
+		So(sk.TestResultForRdb, ShouldResembleProto, wantTestResult)
+
+	})
+
 	Convey("ProvisionStartCmd extract deps with TestResultForRdb", t, func() {
 		ctx := context.Background()
 		wantInvId := "Inv-1234"
@@ -177,16 +416,6 @@ func TestRdbPublishPublishCmd_ExtractDepsSuccess(t *testing.T) {
 			},
 			TestResultForRdb: &artifact.TestResult{Version: 1234},
 		}
-		ctrCipd := crostoolrunner.CtrCipdInfo{Version: "prod"}
-		ctr := &crostoolrunner.CrosToolRunner{CtrCipdInfo: ctrCipd}
-		cont := containers.NewCrosPublishTemplatedContainer(
-			containers.CrosRdbPublishTemplatedContainerType,
-			"container/image/path",
-			ctr)
-		exec := executors.NewCrosPublishExecutor(
-			cont,
-			executors.CrosRdbPublishExecutorType)
-		cmd := commands.NewRdbPublishUploadCmd(exec)
 
 		// Extract deps first
 		err := cmd.ExtractDependencies(ctx, sk)
@@ -212,17 +441,6 @@ func TestRdbPublishPublishCmd_ExtractDepsSuccess(t *testing.T) {
 				},
 			},
 		}
-
-		ctrCipd := crostoolrunner.CtrCipdInfo{Version: "prod"}
-		ctr := &crostoolrunner.CrosToolRunner{CtrCipdInfo: ctrCipd}
-		cont := containers.NewCrosPublishTemplatedContainer(
-			containers.CrosRdbPublishTemplatedContainerType,
-			"container/image/path",
-			ctr)
-		exec := executors.NewCrosPublishExecutor(
-			cont,
-			executors.CrosRdbPublishExecutorType)
-		cmd := commands.NewRdbPublishUploadCmd(exec)
 
 		// Extract deps first
 		err := cmd.ExtractDependencies(ctx, sk)
