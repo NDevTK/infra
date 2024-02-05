515890cefc7cef83dd63bdcd4f9efd68039f3f23
Derek Beckett <dbeckett@chromium.org>
2024-02-05 10:39:12 -0800

    ctrv2 mount creds/service_accounts
    BUG=b:322703881
    TEST=build,staging, led
    
    Change-Id: I313102f0e70886ca5cd3f1dd71ad9b315618ecb7

--- go/src/infra/cros/cmd/cros-tool-runner/internal/v2/templates/template_cros_testexec_test.go
+++ go/src/infra/cros/cmd/cros-tool-runner/internal/v2/templates/template_cros_testexec_test.go
@@ -24,7 +24,7 @@ func TestCrosTestPopulate(t *testing.T) {
 	check(t, convertedRequest.ContainerImage, request.ContainerImage)
 	check(t, convertedRequest.AdditionalOptions.Network, "mynet")
 	check(t, convertedRequest.AdditionalOptions.Expose[0], "8001")
-	check(t, len(convertedRequest.AdditionalOptions.Volume), 2)
+	check(t, len(convertedRequest.AdditionalOptions.Volume), 3)
 	if !strings.Contains(strings.Join(convertedRequest.StartCommand, " "), "cros-test") {
 		t.Fatalf("cros-test is not part of start command")
 	}
@@ -46,7 +46,7 @@ func TestCrosTestPopulate_hostNetwork(t *testing.T) {
 	check(t, convertedRequest.ContainerImage, request.ContainerImage)
 	check(t, convertedRequest.AdditionalOptions.Network, "host")
 	check(t, len(convertedRequest.AdditionalOptions.Expose), 0)
-	check(t, len(convertedRequest.AdditionalOptions.Volume), 2)
+	check(t, len(convertedRequest.AdditionalOptions.Volume), 3)
 	if !strings.Contains(strings.Join(convertedRequest.StartCommand, " "), "-port 0") {
 		t.Fatalf("-port 0 is not part of start command")
 	}
