e6390d52944200edc5096740ce2b70e6aaf7949c
Brian Ryner <bryner@google.com>
2024-02-07 23:44:46 +0000

    Run py3 tests from the infra recipes.
    Bug: 1413668
    Change-Id: Ia6a349f77cf3cec9a69399dfc0379c7a8452040e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5273327
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Brian Ryner <bryner@google.com>
    Cr-Commit-Position: refs/heads/main@{#62966}

--- recipes/recipes/infra_repo_trybot.py
+++ recipes/recipes/infra_repo_trybot.py
@@ -83,9 +83,12 @@ def RunSteps(api, go_version_variant, run_lint, skip_python_tests):
     deferred = []
     if api.platform.arch != 'arm':
       with api.context(cwd=co.path.join(patch_root)):
-        deferred.append(
+        deferred.extend([
             api.defer(api.step, 'python tests',
-                      ['vpython3', 'test.py', 'test', '--verbose']))
+                      ['vpython3', 'test.py', 'test', '--verbose']),
+            api.defer(api.step, 'python tests (py3)',
+                      ['vpython3', 'test.py', '--py3', 'test', '--verbose'])
+        ])
 
       if internal and (api.platform.is_linux or api.platform.is_mac) and any(
           f.startswith('appengine/chromiumdash') for f in files):
