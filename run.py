a0439d2a4ba04312aa0ff65748270f700b932bc5
Brian Ryner <bryner@google.com>
2024-02-15 00:56:17 +0000

    Switch infra/run.py to run scripts using vpython3.
    All of the tools that can be run via this script have already been
    made py3-compatible.
    
    Bug: 1413668
    Change-Id: I2bae804f8b22dc480d3344b2671fc7e15ed419c7
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5298474
    Commit-Queue: Brian Ryner <bryner@google.com>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#63146}

--- run.py
+++ run.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env vpython
+#!/usr/bin/env vpython3
 # Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# [VPYTHON:BEGIN]
-# python_version: "2.7"
-# [VPYTHON:END]
-"""Wrapper for `python -m` to make running tools simpler.
+"""Wrapper for `vpython3 -m` to make running tools simpler.
 
 A tool is defined as a python module with a __main__.py file. This latter file
 is run by the present script.
@@ -17,18 +14,17 @@ infra.
 
 assert __name__ == '__main__'
 
-import imp
+import importlib.util
 import os
 import sys
 
 RUNPY_PATH = os.path.abspath(__file__)
 ROOT_PATH = os.path.dirname(RUNPY_PATH)
-ENV_PATH = os.path.join(ROOT_PATH, 'ENV')
 
 # Do not want to mess with sys.path, load the module directly.
-run_helper = imp.load_source(
+spec = importlib.util.spec_from_file_location(
     'run_helper', os.path.join(ROOT_PATH, 'bootstrap', 'run_helper.py'))
+run_helper = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(run_helper)
 
-sys.exit(
-    run_helper.run_py_main_with_venv(sys.argv[1:], RUNPY_PATH, ENV_PATH,
-                                     'infra'))
+sys.exit(run_helper.run_py_main(sys.argv[1:], RUNPY_PATH, 'infra'))
