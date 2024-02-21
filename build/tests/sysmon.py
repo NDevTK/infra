1f2408fc9b159ef818e4267b8700634bb83a318f
Brian Ryner <bryner@google.com>
2024-02-21 19:41:38 +0000

    Add a basic test for sysmon.
    Bug: 325840969
    Change-Id: I0f41189fd88eb0f0dcd3698afcbd2a00232d8193
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5300086
    Auto-Submit: Brian Ryner <bryner@google.com>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#63276}

---
+++ build/tests/sysmon.py
@@ -0,0 +1,22 @@
+# Copyright 2024 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Test sysmon.cipd package by running it with tsmon disabled"""
+
+import os
+import subprocess
+import sys
+
+# .exe on Windows.
+EXE_SUFFIX = '.exe' if sys.platform == 'win32' else ''
+
+
+def main():
+  sysmon = os.path.join(os.getcwd(), 'sysmon' + EXE_SUFFIX)
+  return subprocess.call(
+      [sysmon, '-log-level', 'debug', '-ts-mon-flush', 'manual'],
+      executable=sysmon)
+
+
+if __name__ == '__main__':
+  sys.exit(main())
