b052b960db5f427321cebdcca5f89c14236757b9
Shamira Banu Khader Khan <shamirabanu@google.com>
2024-02-22 19:44:56 +0000

    stable version validator: Check for the existence of gs firmware_from_source.tar.bz2
    BUG = b:286114085
    TEST=go test ./...
    
    Change-Id: Ie162624188c843ef80c2ef7de17258225d28ec67

---
+++ go/src/infra/cmd/stable_version2/internal/cmd/validateconfig/querygs/validate_faft_test.go
@@ -0,0 +1,48 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package querygs
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+var validateFaftUseCases = []struct {
+	name        string
+	in          string
+	expectError bool
+	want        string
+}{
+	{
+		"happy path",
+		"a-firmware/R10-11.12.13-aaaaaa",
+		false,
+		"gs://chromeos-image-archive/a-firmware/R10-11.12.13-aaaaaa/firmware_from_source.tar.bz2",
+	},
+	{
+		"has gs in the path",
+		"gs://chromeos-image-archive/a-firmware/R10-11.12.13-aaaaaa",
+		true,
+		"",
+	},
+}
+
+func TestValidateFaft(t *testing.T) {
+	t.Parallel()
+	for _, uc := range validateFaftUseCases {
+		uc := uc
+		var r Reader
+		r.dld = fakeDownloader
+		r.exst = fakeExistenceChecker
+		got, err := r.validateFaft(uc.in)
+		if uc.expectError && err == nil {
+			t.Errorf("Expected error but got nil")
+		}
+		if diff := cmp.Diff(uc.want, got); diff != "" {
+			t.Errorf("unexpected diff (-want +got): %s", diff)
+		}
+	}
+}
