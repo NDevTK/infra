a0de4cb6958e1b35945f6991be3fef19f2aa0078
Shamira Banu Khader Khan <shamirabanu@google.com>
2024-02-24 00:34:57 +0000

    stable version validator: Check for the existence of gs firmware_from_source.tar.bz2
    BUG = b:286114085
    TEST=go test ./...
    
    Change-Id: Ie162624188c843ef80c2ef7de17258225d28ec67
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5312007
    Reviewed-by: Garry Wang <xianuowang@chromium.org>
    Commit-Queue: Shamira Banu Khader Khan <shamirabanu@google.com>
    Cr-Commit-Position: refs/heads/main@{#63346}

--- go/src/infra/cmd/stable_version2/internal/cmd/validateconfig/querygs/validate_faft.go
+++ go/src/infra/cmd/stable_version2/internal/cmd/validateconfig/querygs/validate_faft.go
@@ -1,14 +1,16 @@
-// Copyright 2022 The ChromiumOS Authors. All rights reserved.
+// Copyright 2022 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 package querygs
 
 import (
+	"context"
 	"fmt"
 	"strings"
 
 	"go.chromium.org/luci/common/gcloud/gs"
+	"go.chromium.org/luci/common/logging"
 )
 
 // validateFaft validates the remote FAFT entry to make sure
@@ -16,29 +18,17 @@ import (
 //
 // See b:241150358 for an example of this issue happening.
 // See b:241155320 for more information about this change itself.
-func (r *Reader) validateFaft(faftVersion string) (string, error) {
+// See b:286114085 for detail information
+func (r *Reader) validateFaft(ctx context.Context, faftVersion string) (string, error) {
 	if strings.HasPrefix(faftVersion, "gs://") {
-		return "", fmt.Errorf("faft version is not expected to be complete gs path %q", faftVersion)
+		return "", fmt.Errorf("validate faft: path is not expected to have gs:// %q", faftVersion)
 	}
 
-	paths := []string{
-		// Empirically, most firmware versions contain a 'metadata.json' file.
-		fmt.Sprintf("gs://chromeos-image-archive/%s/metadata.json", faftVersion),
-		// However, some firmware versions do not contain a metadata.json file and a JSON proto instead.
-		fmt.Sprintf("gs://chromeos-image-archive/%s/firmware_metadata.jsonpb", faftVersion),
+	path := fmt.Sprintf("gs://chromeos-image-archive/%s/firmware_from_source.tar.bz2", faftVersion)
+	err := r.exst(gs.Path(path))
+	if err == nil {
+		return path, nil
 	}
-
-	err := fmt.Errorf("validate faft: internal error no paths selected")
-	for _, path := range paths {
-		// If we encounter an error here, do NOT log it. It leads to confusing log messages that look
-		// like errors that are entirely routine.
-		err = (r.exst)(gs.Path(path))
-		if err == nil {
-			return path, nil
-		}
-	}
-
-	// Heuristically guess that the first path is what we should report as the "real" path if we did not
-	// successfully find any of the paths.
-	return paths[0], err
+	logging.Errorf(ctx, "The firmware file is not present on the path %q and failed with %v\n", path, err)
+	return "", fmt.Errorf("validate faft: %w", err)
 }
---
+++ go/src/infra/cmd/stable_version2/internal/cmd/validateconfig/querygs/validate_faft_test.go
@@ -0,0 +1,50 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package querygs
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+var validateFaftUseCases = []struct {
+	name        string
+	in          string
+	expectError bool
+	want        string
+}{
+	{
+		"happy path",
+		"a-firmware/R10-11.12.13-aaaaaa",
+		false,
+		"gs://chromeos-image-archive/a-firmware/R10-11.12.13-aaaaaa/firmware_from_source.tar.bz2",
+	},
+	{
+		"has gs in the path",
+		"gs://chromeos-image-archive/a-firmware/R10-11.12.13-aaaaaa",
+		true,
+		"",
+	},
+}
+
+func TestValidateFaft(t *testing.T) {
+	t.Parallel()
+	for _, uc := range validateFaftUseCases {
+		ctx := context.Background()
+		uc := uc
+		var r Reader
+		r.dld = fakeDownloader
+		r.exst = fakeExistenceChecker
+		got, err := r.validateFaft(ctx, uc.in)
+		if uc.expectError && err == nil {
+			t.Errorf("Expected error but got nil")
+		}
+		if diff := cmp.Diff(uc.want, got); diff != "" {
+			t.Errorf("unexpected diff (-want +got): %s", diff)
+		}
+	}
+}
