c2d3f43b6f26e004c2728278b59108e8ec9463b2
Derek Beckett <dbeckett@chromium.org>
2024-03-05 03:19:30 +0000

    ctpv2: uprev 3d container
    minor fixes, pulling in updated datasets
    
    Bug: None
    Change-Id: I18e95262677fb3b02ffb843cafa0f27862672ac2
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5342915
    Auto-Submit: Derek Beckett <dbeckett@chromium.org>
    Commit-Queue: Azizur Rahman <azrahman@google.com>
    Reviewed-by: Azizur Rahman <azrahman@google.com>
    Cr-Commit-Position: refs/heads/main@{#63538}

--- go/src/infra/cros/cmd/common_lib/common/filters.go
+++ go/src/infra/cros/cmd/common_lib/common/filters.go
@@ -33,7 +33,7 @@ var (
 
 	// Default shas for backwards compatibility
 	defaultLegacyHWSha  = "a40e76e81f20dc54e26acea67bb9a00ebd0a64552276d4d3931715668798fffd"
-	defaultTTCPSha      = "0ac820d0c425156e8b4b5be6e383b38fb9d78e1519939e7e83fabd50e6b8700a"
+	defaultTTCPSha      = "1eea3c70abe1ec35450755f7c27ac0c93b92e20321464e1535a7dbbf21877e65"
 	defaultProvisionSha = "79878b6cdcde1edbe5abb64c7a7286148b4da0959510e4334b89691edbd1a422"
 
 	prodShas = map[string]string{
