c01d9c7680a027af27af3b2262770823eb5da9c1
Brian Ryner <bryner@google.com>
2024-03-07 06:48:31 +0000

    Explicitly set the CPU target when building openblas.
    This is to prevent depending on a newer model CPU than the
    resulting binary will be deployed on.
    
    Bug: 40942322
    Change-Id: I6eabb214fddf900a40750e32a08ddaaf32ff204a
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5347823
    Auto-Submit: Brian Ryner <bryner@google.com>
    Reviewed-by: Chenlin Fan <fancl@chromium.org>
    Commit-Queue: Chenlin Fan <fancl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#63624}

--- 3pp/openblas/install.sh
+++ 3pp/openblas/install.sh
@@ -9,5 +9,21 @@ set -o pipefail
 
 PREFIX="$1"
 
-make NO_LAPACKE=1 NO_SHARED=1
-make NO_LAPACKE=1 NO_SHARED=1 install PREFIX="${PREFIX}"
+# Make sure we target a base model CPU since we have a wide range of
+# CPUs in the fleet.
+case $_3PP_PLATFORM in
+  mac-amd64)
+    TARGET=CORE2
+    ;;
+  mac-arm64)
+    TARGET=ARMV8
+    ;;
+  *)
+    echo Please configure a CPU target for ${_3PP_PLATFORM}
+    exit 1
+    ;;
+esac
+
+MAKE_FLAGS="NO_LAPACKE=1 NO_SHARED=1 TARGET=${TARGET}"
+make ${MAKE_FLAGS}
+make ${MAKE_FLAGS} install PREFIX="${PREFIX}"
