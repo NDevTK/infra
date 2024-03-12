ab13e038bb0df0030b54ce88745e37fed0e400cd
Shamira Banu Khader Khan <shamirabanu@google.com>
2024-03-12 00:48:09 +0000

    stable version validator: Check for the existence of fw image path
    Check for the existence of fw image path if not add default file firmware_from_source.tar.bz2 as tail.
    
    BUG = b:328679282
    TEST=make test
    
    Change-Id: I0e8f4d526b1a0859d395b475304c17655723423b
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5356525
    Commit-Queue: Otabek Kasimov <otabek@google.com>
    Reviewed-by: Congbin Guo <guocb@chromium.org>
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Cr-Commit-Position: refs/heads/main@{#63706}

--- go/src/infra/cmd/stable_version2/internal/cmd/validateconfig/querygs/validate_faft.go
+++ go/src/infra/cmd/stable_version2/internal/cmd/validateconfig/querygs/validate_faft.go
@@ -19,16 +19,20 @@ import (
 // See b:241150358 for an example of this issue happening.
 // See b:241155320 for more information about this change itself.
 // See b:286114085 for detail information
-func (r *Reader) validateFaft(ctx context.Context, faftVersion string) (string, error) {
-	if strings.HasPrefix(faftVersion, "gs://") {
-		return "", fmt.Errorf("validate faft: path is not expected to have gs:// %q", faftVersion)
+func (r *Reader) validateFaft(ctx context.Context, faftPath string) (string, error) {
+	if strings.HasPrefix(faftPath, "gs://") {
+		return "", fmt.Errorf("validate faft: path is not expected to have gs:// %q", faftPath)
 	}
 
-	path := fmt.Sprintf("gs://chromeos-image-archive/%s/firmware_from_source.tar.bz2", faftVersion)
-	err := r.exst(gs.Path(path))
-	if err == nil {
-		return path, nil
+	//check if the fw image path has .tar.bz2 extension
+	if !strings.HasSuffix(faftPath, ".tar.bz2") {
+		faftPath += "/firmware_from_source.tar.bz2"
 	}
-	logging.Errorf(ctx, "The firmware file is not present on the path %q and failed with %v\n", path, err)
-	return "", fmt.Errorf("validate faft: %w", err)
+
+	gsPath := fmt.Sprintf("gs://chromeos-image-archive/%s", faftPath)
+	if err := r.exst(gs.Path(gsPath)); err != nil {
+		logging.Errorf(ctx, "The firmware file is not present on the path %q and failed with %v\n", gsPath, err)
+		return "", fmt.Errorf("validate faft: %w", err)
+	}
+	return gsPath, nil
 }
--- go/src/infra/cmd/stable_version2/internal/cmd/validateconfig/querygs/validate_faft_test.go
+++ go/src/infra/cmd/stable_version2/internal/cmd/validateconfig/querygs/validate_faft_test.go
@@ -18,14 +18,38 @@ var validateFaftUseCases = []struct {
 	want        string
 }{
 	{
-		"happy path",
+		"happy path - path without specifying the filename",
 		"a-firmware/R10-11.12.13-aaaaaa",
 		false,
 		"gs://chromeos-image-archive/a-firmware/R10-11.12.13-aaaaaa/firmware_from_source.tar.bz2",
 	},
 	{
-		"has gs in the path",
-		"gs://chromeos-image-archive/a-firmware/R10-11.12.13-aaaaaa",
+		"happy path - path with specified filename",
+		"firmware-b-12345.B-branch-firmware/R10-11.12.13-aaaaaa/b/test2.tar.bz2",
+		false,
+		"gs://chromeos-image-archive/firmware-b-12345.B-branch-firmware/R10-11.12.13-aaaaaa/b/test2.tar.bz2",
+	},
+	{
+		"happy path - branch path without specifying the filename ",
+		"firmware-c-12345.B-branch-firmware/R10-11.12.13-aaaaaa",
+		false,
+		"gs://chromeos-image-archive/firmware-c-12345.B-branch-firmware/R10-11.12.13-aaaaaa/firmware_from_source.tar.bz2",
+	},
+	{
+		"has gs in the path - type 1 path",
+		"gs://chromeos-image-archive/a-firmware/R10-11.12.13-aaaaaa/firmware_from_source.tar.bz2",
+		true,
+		"",
+	},
+	{
+		"has gs in the path - type 2 path",
+		"gs://chromeos-image-archive/firmware-b-12345.B-branch-firmware/R10-11.12.13-aaaaaa/b/firmware_from_source.tar.bz2",
+		true,
+		"",
+	},
+	{
+		"has gs in the path - type 3 path",
+		"gs://chromeos-image-archive/firmware-c-12345.B-branch-firmware/R10-11.12.13-aaaaaa/firmware_from_source.tar.bz2",
 		true,
 		"",
 	},
@@ -41,10 +65,10 @@ func TestValidateFaft(t *testing.T) {
 		r.exst = fakeExistenceChecker
 		got, err := r.validateFaft(ctx, uc.in)
 		if uc.expectError && err == nil {
-			t.Errorf("Expected error but got nil")
+			t.Errorf("case %q: Expected error but got nil", uc.name)
 		}
 		if diff := cmp.Diff(uc.want, got); diff != "" {
-			t.Errorf("unexpected diff (-want +got): %s", diff)
+			t.Errorf("case %q: unexpected diff: %s \n(-want: %q \n+got: %q)", uc.name, diff, uc.want, got)
 		}
 	}
 }
