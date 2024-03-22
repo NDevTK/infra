cfd9eaf35304695b0b0e1b20256b6f5f34471215
Jonathan Pittman <jmpittman@google.com>
2024-03-22 14:36:03 +0000

    3pp packages for openssh and hiba
    Change-Id: Ic12ea8399c00bd4d13e576223e9adde9964824c3

---
+++ 3pp/hiba/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright 2024 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e # Exit immediately if a command exits with a non-zero status.
+set -x # Print commands and their arguments as they are executed.
+set -o pipefail
+
+PREFIX="$1"
+DEPS_PREFIX="$2"
+
+./autogen.sh
+
+LD_LIBRARY_PATH="${DEPS_PREFIX}/usr/lib" ./configure \
+  --with-opensshdir="${DEPS_PREFIX}" \
+  --with-openssldir="${DEPS_PREFIX}/usr/include" \
+  --disable-shared \
+  --prefix="${PREFIX}" \
+  --host="${CROSS_TRIPLE}" \
+  || cat config.log
+
+make install
