6eb9ae07c4da6ce9ff8b167154438f8f148526ea
Jonathan Pittman <jmpittman@google.com>
2024-03-23 01:58:51 +0000

    3pp packages for openssh and hiba
    Change-Id: Ic12ea8399c00bd4d13e576223e9adde9964824c3

---
+++ 3pp/hiba/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Copyright 2024 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e # Exit immediately if a command exits with a non-zero status.
+set -x # Print commands and their arguments as they are executed.
+set -o pipefail
+
+PREFIX="$1"
+DEPS_PREFIX="$2"
+
+ls -l "${DEPS_PREFIX}"
+ls -l "${DEPS_PREFIX}/lib"
+ls -l "${DEPS_PREFIX}/include"
+ls -l "${DEPS_PREFIX}/include/openssh"
+ls -l "${DEPS_PREFIX}/include/openssl"
+
+./autogen.sh
+
+export LIBRARY_PATH="${DEPS_PREFIX}/lib"
+
+./configure \
+  --disable-shared \
+  --with-opensshdir="${DEPS_PREFIX}/include/openssh" \
+  --with-openssldir="${DEPS_PREFIX}/include/" \
+  --prefix="${PREFIX}" \
+  --host="${CROSS_TRIPLE}" \
+  CFLAGS="-Wno-unused-command-line-argument" \
+  || cat config.log
+
+make V=1
+make install
---
+++ 3pp/openssh/install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Copyright 2024 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e # Exit immediately if a command exits with a non-zero status.
+set -x # Print commands and their arguments as they are executed.
+set -o pipefail
+
+PREFIX="$1"
+DEPS_PREFIX="$2"
+
+autoreconf
+
+./configure \
+  --with-ssl-dir="${DEPS_PREFIX}" \
+  --prefix="${PREFIX}" \
+  --host="${CROSS_TRIPLE}" \
+  || cat config.log
+
+make install
+
+# OpenSSH does not export *.a and *.h files. So we have to copy them instead.
+
+mkdir -p "${PREFIX}/lib"
+cp libssh.a "${PREFIX}/lib/"
+cp openbsd-compat/libopenbsd-compat.a "${PREFIX}/lib/"
+
+mkdir -p "${PREFIX}/include/openssh/openbsd-compat"
+cp -r *.h "${PREFIX}/include/openssh/"
+cp -r openbsd-compat/*.h "${PREFIX}/include/openssh/openbsd-compat/"
+
