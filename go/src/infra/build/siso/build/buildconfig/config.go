0fde4b8da242bdcc3751470e7b5b2b1600ab50fa
Fumitoshi Ukai <ukai@google.com>
2024-03-28 11:43:03 +0900

    siso: record handler stacktrace in siso_output too
    e.g.
    FAILED[handle]: ebab8f46-85a6-4143-9ac7-8ca69969b0b7 ACTION //components/privacy_sandbox/privacy_sandbox_attestations/proto:proto_gen(//build/toolchain/linux:clang_x64)
    err: failed to run protoc_wrapper: readfile missing.proto: read file /usr/local/google/home/ukai/src/chromium/src/missing.proto: lstat /usr/local/google/home/ukai/src/chromium/src/missing.proto: no such file or directory
    stacktrace:Traceback (most recent call last):
      proto_linux.star:21:37: in __protoc_wrapper
      protoc_wrapper.star:107:40: in __scandeps
      protoc_wrapper.star:26:16: in __protoc_scandeps
      <builtin>: in read
    
    siso_rule:proto/protoc_wrapper
    "__components_privacy_sandbox_privacy_sandbox_attestations_proto_proto_gen___build_toolchain_linux_clang_x64__rule" "./gen/components/privacy_sandbox/privacy_sandbox_attestations/proto/privacy_sandbox_attestations.pb.h"
    python3 ../../tools/protoc_wrapper/protoc_wrapper.py privacy_sandbox_attestations.proto --protoc ./protoc --proto-in-dir ../../components/privacy_sandbox/privacy_sandbox_attestations/proto --cc-out-dir gen/components/privacy_sandbox/privacy_sandbox_attestations/proto --cc-options lite=true: --py-out-dir pyproto/components/privacy_sandbox/privacy_sandbox_attestations/proto
    
    
    Bug: b/331633786
    Change-Id: Icdad6ba6909ccf442dc6814ffb4e6ea96a4fd988

--- go/src/infra/build/siso/build/buildconfig/config.go
+++ go/src/infra/build/siso/build/buildconfig/config.go
@@ -105,6 +105,28 @@ func New(ctx context.Context, fname string, flags map[string]string, repos map[s
 	}, nil
 }
 
+// HandlerError is error of handler.
+type HandlerError struct {
+	entry string
+	err   *starlark.EvalError
+}
+
+func (e HandlerError) Error() string {
+	return fmt.Sprintf("failed to run %s: %v", e.entry, e.err)
+}
+
+func (e HandlerError) Backtrace() string {
+	stack := e.err.CallStack
+	if last := len(stack) - 1; last >= 0 && stack[last].Pos.Filename() == "<builtin>" {
+		stack = stack[:last]
+	}
+	return stack.String()
+}
+
+func (e HandlerError) Unwrap() error {
+	return e.err
+}
+
 // Init initializes config by running `init`.
 func (cfg *Config) Init(ctx context.Context, hashFS *hashfs.HashFS, buildPath *build.Path) (string, error) {
 	fun, ok := cfg.globals[configEntryPoint]
@@ -135,6 +157,7 @@ func (cfg *Config) Init(ctx context.Context, hashFS *hashfs.HashFS, buildPath *b
 		var eerr *starlark.EvalError
 		if errors.As(err, &eerr) {
 			clog.Warningf(ctx, "stacktrace:\n%s", eerr.Backtrace())
+			return "", HandlerError{entry: configEntryPoint, err: eerr}
 		}
 		return "", fmt.Errorf("failed to run %s: %w", configEntryPoint, err)
 	}
@@ -232,6 +255,8 @@ func (cfg *Config) Handle(ctx context.Context, handler string, bpath *build.Path
 		var eerr *starlark.EvalError
 		if errors.As(err, &eerr) {
 			clog.Warningf(ctx, "stacktrace:\n%s", eerr.Backtrace())
+			return HandlerError{entry: handler, err: eerr}
+
 		}
 		return fmt.Errorf("failed to run %s: %w", handler, err)
 	}
