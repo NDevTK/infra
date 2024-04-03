df9accec5f3fc34c238d328416af10a3e0a9a8ef
Gregory NISBET <gregorynisbet@google.com>
2024-04-03 20:08:36 +0000

    [fleetcost] Set -use-id-token always.
    Always set the auth options in a way equivalent to passing in -use-id-token.
    
    BUG=b:331291398
    TEST=make test AND ./fleetcost get-ci -dev
    
    Change-Id: I44a8b270b2014f52bfbe93b651f1fb1317098d7a
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5421569
    Reviewed-by: Anushruth Sasivehalli <anushruth@chromium.org>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Cr-Commit-Position: refs/heads/main@{#64237}

--- go/src/infra/cros/fleetcost/internal/commands/common.go
+++ go/src/infra/cros/fleetcost/internal/commands/common.go
@@ -14,12 +14,20 @@ import (
 )
 
 // getSecureClient gets a secure http.Client pointed at a specific host.
+//
+// TODO(gregorynisbet): Remove this function as well as the dependency on authcli.Flags.
+//
+//	We should be able to manually construct an auth.Options object with the settings that we want.
+//	However, I know for a fact that using authFlags to produce an authFlags.Options() object
+//	produces a usable client. Sometime in the future, I will opportunistically replace this
+//	function with something more reasonable.
 func getSecureClient(ctx context.Context, host string, authFlags authcli.Flags) (*http.Client, error) {
 	authOptions, err := authFlags.Options()
 	if err != nil {
 		return nil, errors.Annotate(err, "creating secure client").Err()
 	}
-	if authOptions.UseIDTokens && authOptions.Audience == "" {
+	authOptions.UseIDTokens = true
+	if authOptions.Audience == "" {
 		authOptions.Audience = "https://" + host
 	}
 	authenticator := auth.NewAuthenticator(ctx, auth.InteractiveLogin, authOptions)
