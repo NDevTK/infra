46057894dd37ba1fa05b78ae377aa84fc8b1d29a
Gregory NISBET <gregorynisbet@google.com>
2024-04-10 09:46:20 -0700

    [fleetcost] Add validation to create-ci on server side.
    Add server-side validation for the CreateCostIndicator RPC.
    
    BUG=331291398
    TEST=make test
    
    Change-Id: I92e487a099f1444799ef853da3108c6a96a68dd9

--- go/src/infra/cros/fleetcost/internal/costserver/cost_indicator_test.go
+++ go/src/infra/cros/fleetcost/internal/costserver/cost_indicator_test.go
@@ -8,6 +8,9 @@ import (
 	"context"
 	"testing"
 
+	"google.golang.org/genproto/googleapis/type/money"
+
+	models "infra/cros/fleetcost/api/models"
 	fleetcostAPI "infra/cros/fleetcost/api/rpc"
 	testsupport "infra/cros/fleetcost/internal/costserver/testsupport"
 )
@@ -16,7 +19,16 @@ func TestCreateCostIndicator(t *testing.T) {
 	t.Parallel()
 	tf := testsupport.NewFixture(context.Background(), t)
 
-	_, err := tf.Frontend.CreateCostIndicator(tf.Ctx, &fleetcostAPI.CreateCostIndicatorRequest{})
+	_, err := tf.Frontend.CreateCostIndicator(tf.Ctx, &fleetcostAPI.CreateCostIndicatorRequest{
+		CostIndicator: &models.CostIndicator{
+			Board: "board",
+			Model: "model",
+			Cost: &money.Money{
+				CurrencyCode: "USD",
+				Units:        12,
+			},
+		},
+	})
 	if err != nil {
 		t.Errorf("unexpected error: %s", err)
 	}
---
+++ go/src/infra/cros/fleetcost/internal/validation/validate-ci.go
@@ -0,0 +1,38 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package validation contains validation for requests.
+package validation
+
+import (
+	"strings"
+
+	"go.chromium.org/luci/common/errors"
+
+	fleetcostAPI "infra/cros/fleetcost/api/rpc"
+)
+
+// ValidateCreateCostIndicatorRequest performs a shallow validation of cost indicator request fields.
+func ValidateCreateCostIndicatorRequest(request *fleetcostAPI.CreateCostIndicatorRequest) error {
+	var errs []error
+	indicator := request.GetCostIndicator()
+	if indicator.GetName() != "" {
+		errs = append(errs, errors.New("indicator name must be empty"))
+	}
+	if indicator.GetBoard() == "" {
+		errs = append(errs, errors.New("board cannot be empty"))
+	}
+	if !IsServoBoard(indicator.GetBoard()) && indicator.GetModel() == "" {
+		errs = append(errs, errors.New("model cannot be empty"))
+	}
+	if indicator.GetCost() == nil {
+		errs = append(errs, errors.New("cost must be provided"))
+	}
+	return errors.Append(errs...)
+}
+
+// IsServoBoard checks if the board is a servo board.
+func IsServoBoard(board string) bool {
+	return strings.Contains(board, "servo")
+}
