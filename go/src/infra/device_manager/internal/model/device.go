1817e670d0968c7a8be7ca61aa41ff140706a139
Justin Suen <justinsuen@google.com>
2024-04-11 21:33:17 +0000

    device-manager: add status columns to queries
    Models and controllers should be able to query last_update_time and
    is_active statuses from the DB.
    
    BUG=b/326504353
    TEST=make test; make dev
    
    Change-Id: I9255de1a28986cc38f0d637407b0135f92605228

--- go/src/infra/device_manager/internal/model/device.go
+++ go/src/infra/device_manager/internal/model/device.go
@@ -7,38 +7,53 @@ package model
 import (
 	"context"
 	"database/sql"
+	"time"
 
 	"go.chromium.org/luci/common/logging"
 )
 
 // Device contains a single row from the Devices table in the database.
 type Device struct {
-	ID            string
-	DeviceAddress string
-	DeviceType    string
-	DeviceState   string
+	ID              string
+	DeviceAddress   string
+	DeviceType      string
+	DeviceState     string
+	LastUpdatedTime time.Time
+	IsActive        bool
 }
 
 // GetDeviceByName gets a Device from the database by name.
 func GetDeviceByName(ctx context.Context, db *sql.DB, deviceName string) (Device, error) {
-	var device Device
+	var (
+		device          Device
+		lastUpdatedTime sql.NullTime
+	)
 	err := db.QueryRowContext(ctx, `
 		SELECT
 			id,
 			device_address,
 			device_type,
-			device_state
+			device_state,
+			last_update_time,
+			is_active
 		FROM "Devices"
 		WHERE id=$1;`, deviceName).Scan(
 		&device.ID,
 		&device.DeviceAddress,
 		&device.DeviceType,
 		&device.DeviceState,
+		&lastUpdatedTime,
+		&device.IsActive,
 	)
 	// TODO (b/328662436): Collect metrics on results
 	if err != nil {
 		logging.Errorf(ctx, "GetDeviceByName: failed to get Device %s: %s", deviceName, err)
 		return device, err
+	}
+
+	// Handle possible null times
+	if lastUpdatedTime.Valid {
+		device.LastUpdatedTime = lastUpdatedTime.Time
 	}
 
 	return device, nil
@@ -56,13 +71,17 @@ func UpdateDevice(ctx context.Context, tx *sql.Tx, updatedDevice Device) error {
 		SET
 			device_address=COALESCE($2, device_address),
 			device_type=COALESCE($3, device_type),
-			device_state=COALESCE($4, device_state)
+			device_state=COALESCE($4, device_state),
+			last_updated_time=COALESCE($5, last_updated_time),
+			is_active=COALESCE($6, is_active)
 		WHERE
 			id=$1;`,
 		updatedDevice.ID,
 		updatedDevice.DeviceAddress,
 		updatedDevice.DeviceType,
 		updatedDevice.DeviceState,
+		updatedDevice.LastUpdatedTime,
+		updatedDevice.IsActive,
 	)
 	if err != nil {
 		logging.Errorf(ctx, "UpdateDevice: failed to update Device %s: %s", updatedDevice.ID, err)
