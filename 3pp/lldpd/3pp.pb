5eaf5138fd10959aa30efef97e41a6a541135005
Jonathan Pittman <jmpittman@google.com>
2024-04-11 04:02:39 +0000

    Build lldpd via 3pp
    Change-Id: I6e070bc67ff9d5f4ed4c56329f1243148795deb7

---
+++ 3pp/lldpd/3pp.pb
@@ -0,0 +1,22 @@
+# Copyright 2024 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+create {
+  platform_re: "linux-.*|mac-.*"
+    source {
+    url {
+      download_url: "https://media.luffy.cx/files/lldpd/lldpd-1.0.18.tar.gz"
+      version: "1.0.18"
+    }
+    unpack_archive: true
+  }
+
+  build {
+    tool: "tools/autoconf"
+    tool: "tools/automake"
+    tool: "tools/libtool"
+  }
+}
+
+upload { pkg_prefix: "tools" }
---
+++ 3pp/lldpd/install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Copyright 2024 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e # Exit immediately if a command exits with a non-zero status.
+set -x # Print commands and their arguments as they are executed.
+set -o pipefail
+
+PREFIX="$1"
+DEPS_PREFIX="$2"
+
+_CONFIG_ARGS=(
+  "CFLAGS=-O2"
+  "--disable-shared"
+  "--enable-static"
+  "--with-launchddaemonsdir=${PREFIX}/Library/LaunchDaemons"
+  "--with-systemdsystemunitdir=${PREFIX}/usr/lib/systemd/system"
+  "--with-sysusersdir=${PREFIX}/usr/lib/sysusers.d"
+  "--prefix=${PREFIX}"
+)
+
+if [[ -n "${CROSS_TRIPLE}" ]]; then
+  _CONFIG_ARGS=( "${_CONFIG_ARGS[@]}" "--host=${CROSS_TRIPLE}" )
+fi
+
+echo $PATH
+
+ls -l
+
+# autogen.sh does `LIBTOOLIZE=${LIBTOOLIZE:-glibtoolize}` when uname reports
+# Darwin, but libtool/libtoolize brought in as a dependency in 3pp.pb is already
+# the GNU one. For Linux, this is effectively a no-op since autogen.sh sets it
+# to libtoolize when any other OS is detected.
+# export LIBTOOLIZE=libtoolize
+# ./autogen.sh
+
+mkdir build
+pushd build
+
+../configure --help
+
+
+../configure "${_CONFIG_ARGS[@]}" || cat config.log
+make -j $(nproc) install
+
+ls -lR ${PREFIX}
