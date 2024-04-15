a276cc12e964d035943bd4ef68869f05b25d0add
Justin Suen <justinsuen@google.com>
2024-04-15 23:20:43 +0000

    device-manager: add status columns to Devices table
    - Add last_updated_time column to track when the Device information was
      last updated. This is needed to let downstream users determine
      freshness of data.
    - Add is_active column to track if a Device is still in commission. We
      will keep decommissioned devices in the database but downstream users
      should be able to see commission status.
    
    TEST=alembic upgrade head
    BUG=b/326504353
    
    Change-Id: Id825d4c65835270e7f00659a8e279837a779b6b7
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5443897
    Commit-Queue: Justin Suen <justinsuen@google.com>
    Reviewed-by: Garry Wang <xianuowang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#64518}

---
+++ go/src/infra/device_manager/database/versions/20240410_2305-469ac7ced828_add_status_columns_to_devices_table.py
@@ -0,0 +1,30 @@
+# Copyright 2024 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""add status columns to Devices table
+
+Revision ID: 469ac7ced828
+Revises: 095e5749b6e6
+Create Date: 2024-04-10 23:05:23.341893
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision: str = '469ac7ced828'
+down_revision: Union[str, None] = '095e5749b6e6'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+  op.add_column("Devices", sa.Column("last_updated_time", sa.DateTime()))
+  op.add_column("Devices", sa.Column("is_active", sa.Boolean))
+
+
+def downgrade() -> None:
+  op.drop_column("Devices", "last_updated_time")
+  op.drop_column("Devices", "is_active")
