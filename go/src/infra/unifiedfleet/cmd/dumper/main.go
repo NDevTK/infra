64f610f79ee3aa4829104105b0d5b06f18a29fc2
Gregory NISBET <gregorynisbet@google.com>
2024-04-15 18:47:51 +0000

    [ufs] [cloud-profiler] Enable cloud profiler for dumper too.
    This CL is similar to https://chromium-review.googlesource.com/c/infra/infra/+/5455171, but
    for the dumper not the main service binary.
    
    The dumper and the main service binary will drain to different-but-related service names.
    
    BUG=334177235
    TEST=run the dumper locally as the dev project and observe the lack of panic.
    
    Change-Id: Ia04ff0fb8ba34cd681cb6b3163146b848e913494
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5455212
    Reviewed-by: Anushruth Sasivehalli <anushruth@chromium.org>
    Reviewed-by: Yang Yang <echoyang@chromium.org>
    Commit-Queue: Anushruth Sasivehalli <anushruth@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#64505}

--- go/src/infra/unifiedfleet/cmd/dumper/main.go
+++ go/src/infra/unifiedfleet/cmd/dumper/main.go
@@ -8,7 +8,10 @@ import (
 	"flag"
 
 	"cloud.google.com/go/bigquery"
+	"cloud.google.com/go/profiler"
 
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/logging"
 	"go.chromium.org/luci/server"
 	"go.chromium.org/luci/server/gaeemulation"
 	"go.chromium.org/luci/server/limiter"
@@ -32,6 +35,25 @@ func main() {
 	cfgLoader.RegisterFlags(flag.CommandLine)
 
 	server.Main(nil, modules, func(srv *server.Server) error {
+		// We closely follow the profiler-enabling documentation available at the following URL:
+		// https://cloud.google.com/profiler/docs/profiling-go#enabling-profiler-api
+		cfg := profiler.Config{
+			Service: "ufs-dumper",
+			// TODO(gregorynisbet): replace with commit hash or some other smarter way of getting
+			//                      the UFS version.
+			ServiceVersion:    "1.0.0",
+			EnableOCTelemetry: false,
+			ProjectID:         srv.Options.CloudProject,
+		}
+
+		profilerStartErr := profiler.Start(cfg)
+		// TODO(gregorynisbet): Upgrade this to a panic once enabling the profiler is reliable enough in prod.
+		if profilerStartErr == nil {
+			logging.Infof(srv.Context, "profiler started successfully: de3e33ed-b5c9-40c6-b0ea-52a3ca5d6138")
+		} else {
+			logging.Errorf(srv.Context, "%s\n", errors.Annotate(profilerStartErr, "error encountered when setting up profiler during startup").Err())
+		}
+
 		// Load service config form a local file (deployed via GKE),
 		// periodically reread it to pick up changes without full restart.
 		if _, err := cfgLoader.Load(srv.Context); err != nil {
