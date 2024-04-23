71f1e4c6ddcd183eaae435a325b98fadcc3bab51
Justin Suen <justinsuen@google.com>
2024-04-23 06:46:29 +0000

    fleet-libs: delete unused function
    This was moved to the attacheddevice submodule and thus is not needed.
    
    BUG=b/326504353
    TEST=go test ./...; go build; ./shivas internal-print-bot-info manual
    testing
    
    Change-Id: I17a580ae5a465988b86e9313245591da461b07a6
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5469143
    Reviewed-by: Garry Wang <xianuowang@chromium.org>
    Commit-Queue: Justin Suen <justinsuen@google.com>
    Cr-Commit-Position: refs/heads/main@{#64721}

--- go/src/infra/libs/fleet/device/dut/dut_dimensions.go
+++ go/src/infra/libs/fleet/device/dut/dut_dimensions.go
@@ -10,9 +10,7 @@ import (
 
 	"infra/cros/dutstate"
 	"infra/libs/skylab/inventory/swarming"
-	"infra/libs/skylab/inventory/swarming/attacheddevice"
 	ufspb "infra/unifiedfleet/api/v1/models"
-	ufsAPI "infra/unifiedfleet/api/v1/rpc"
 )
 
 // GetDUTBotDims gets all Swarming dimensions of a DUT bot.
@@ -39,20 +37,6 @@ func GetDUTBotDims(ctx context.Context, r swarming.ReportFunc, ds dutstate.Info,
 	if deviceData.GetLabConfig().GetLogicalZone() != ufspb.LogicalZone_LOGICAL_ZONE_UNSPECIFIED {
 		dims["logical_zone"] = []string{deviceData.GetLabConfig().GetLogicalZone().String()}
 	}
-	swarming.Sanitize(dims, r)
-	return dims
-}
-
-// GetAttachedDeviceBotDims gets all Swarming dimensions of an AttachedDevice bot.
-func GetAttachedDeviceBotDims(ctx context.Context, r swarming.ReportFunc, ds dutstate.Info, deviceData *ufsAPI.AttachedDeviceData) swarming.Dimensions {
-	dims := attacheddevice.Convert(deviceData)
-	if v := deviceData.GetMachine().GetLocation(); v != nil {
-		location := fmt.Sprintf("%s-aisle%s-row%s-rack%s-racknumber%s-shelf%s-position%s",
-			v.GetAisle(), v.GetZone(), v.GetRow(), v.GetRack(), v.GetRackNumber(), v.GetShelf(), v.GetPosition())
-		dims["location"] = []string{location}
-	}
-	dims["dut_state"] = []string{string(ds.State)}
-	dims["ufs_zone"] = []string{deviceData.GetLabConfig().GetZone()}
 	swarming.Sanitize(dims, r)
 	return dims
 }
