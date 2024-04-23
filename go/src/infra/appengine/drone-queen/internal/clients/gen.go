f707d3605cd71c8e91fbc0f18c6dfec4807c0da4
Bryan Ni <bniche@google.com>
2024-04-23 15:49:19 +0000

    [cloudbots] [drone-queen] Add swarming client to drone-queen
    BUG=b:333924617
    TEST=go test
    
    Change-Id: I8e17b843498e193e2e815827aff99b743bf6e1d7
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5467312
    Reviewed-by: JB Tamas-Leloup <jtamasleloup@google.com>
    Commit-Queue: Bryan Ni <bniche@google.com>
    Cr-Commit-Position: refs/heads/main@{#64732}

---
+++ go/src/infra/appengine/drone-queen/internal/clients/gen.go
@@ -0,0 +1,7 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package clients
+
+//go:generate mockgen -source swarming.go -destination swarming.mock.go -package clients -write_package_comment=false -copyright_file=copyright
---
+++ go/src/infra/appengine/drone-queen/internal/clients/swarming.go
@@ -0,0 +1,61 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package clients defines the PRPC clients
+package clients
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"go.chromium.org/luci/grpc/prpc"
+	"go.chromium.org/luci/server/auth"
+	apipb "go.chromium.org/luci/swarming/proto/api_v2"
+)
+
+// SwarmingClient is a Swarming API wrapper for drone-queen specific usage.
+//
+// In prod, a SwarmingClient for interacting with the Swarming service will be
+// used. Tests should use a fake implementation.
+type SwarmingClient interface {
+	ListBots(ctx context.Context, in *apipb.BotsRequest) (*apipb.BotInfoListResponse, error)
+}
+
+// swarmingServiceImpl for use in real production envs.
+type swarmingServiceImpl struct {
+	BotsClient apipb.BotsClient
+}
+
+// Ensure swarmingClientImpl implements SwarmingClient.
+var _ SwarmingClient = &swarmingServiceImpl{}
+
+var MockSwarmingClientKey = "used in tests only for setting the mock SwarmingClient"
+
+// NewSwarmingClient returns a new SwarmingClient to interact with Swarming APIs.
+func NewSwarmingClient(ctx context.Context, host string, project string) (SwarmingClient, error) {
+	if mockClient, ok := ctx.Value(&MockSwarmingClientKey).(*MockSwarmingClient); ok {
+		return mockClient, nil
+	}
+
+	t, err := auth.GetRPCTransport(ctx, auth.AsProject, auth.WithProject(project))
+	if err != nil {
+		return nil, err
+	}
+
+	prpcClient := prpc.Client{
+		C:    &http.Client{Transport: t},
+		Host: host,
+	}
+	return &swarmingServiceImpl{
+		BotsClient: apipb.NewBotsClient(&prpcClient),
+	}, nil
+}
+
+// ListBots calls `apipb.BotsClient.ListBots` to get the bots of a task via a task id.
+func (s *swarmingServiceImpl) ListBots(ctx context.Context, req *apipb.BotsRequest) (*apipb.BotInfoListResponse, error) {
+	subCtx, cancel := context.WithTimeout(ctx, 60*time.Second)
+	defer cancel()
+	return s.BotsClient.ListBots(subCtx, req)
+}
