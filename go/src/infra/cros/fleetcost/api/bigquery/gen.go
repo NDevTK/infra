d91240a7811e2bd5243b01872b421a656ceb0324
Gregory NISBET <gregorynisbet@google.com>
2024-04-24 18:54:09 +0000

    [fleetcost] Create the BigQuery schema for saved fleet cost results.
    Results are by DUT, so we report the name.
    
    The name is the hostname or the device id as appropriate.
    
    Known issues:
    1) we don't have an explicit time field.
    
    I want this table to be partitioned by ingestion time. I would ideally
    prefer this ingestion time field to be present in the proto schema for
    explicitness, but I don't know how to make that work with
    BQSchemaUpdater yet.
    
    2) Just in general, I want more information on what the dashboards
    that will be consuming this table are likely to look like.
    
    BUG=321105751
    TEST=make gen
    
    Change-Id: Id07a79128aea481998c76880f06d43abbf4494b9
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5477881
    Reviewed-by: Vineet Joshi <vkjoshi@google.com>
    Reviewed-by: Garry Wang <xianuowang@chromium.org>
    Commit-Queue: Gregory Nisbet <gregorynisbet@google.com>
    Cr-Commit-Position: refs/heads/main@{#64768}

---
+++ go/src/infra/cros/fleetcost/api/bigquery/gen.go
@@ -0,0 +1,8 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package bqpb is the generated bigquery protos for the fleet cost service.
+package bqpb
+
+//go:generate cproto
