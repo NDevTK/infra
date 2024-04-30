1e7fa42d41f10706f8c9852dbe15d7fab1500805
Jared Bennett <jaredbennett@google.com>
2024-04-30 00:38:48 +0000

    config: Add btpeer imaging to repair
    Updates the btpeer repair config with actions that will install
    the expected btpeer OS image release as defined by the new btpeer
    image release process.
    
    In this initial rollout phase, only a small subset of testbeds
    will have their btpeers updated with the image-based release process,
    with the majority using the current chameleond-based release process.
    This is managed by an action condition on the two processes' base
    actions.
    
    The OS provisioning actions have been copied over from the btpeer
    provision config, functionally unchanged. Redundant action deps have
    been removed, and the download image action no longer has the
    image_url exec arg specified as it is dynamically determined by the
    release process.
    
    BUG=b:336878507
    TEST=cd ~/cr/infra/go/src/infra/cros/recovery && \
    make build && make trees
    TEST=tested repair locally on testbed needing image-based release from
    a legacy image state => successful AB partition and image install,
    repeated repair runs on same btpeer correctly identified state as
    expected and repair did not make changes to btpeer
    TEST=tested repair locally on testbed needing chameleond-based
    release from a legacy image state => successfully skipped the
    image-based release actions and ran the chameleond-based release
    actions as normal
    TEST=tested repair locally on testbed needing image-based release
    on btpeer previously imaged via repair after releasing a new image
    via config change => AB partitioning skipped as expected, new image
    release installed successfully
    
    Change-Id: Ifb1ef82b948ca052045cf03bebd153fb69a753b9
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5492888
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Jared Bennett <jaredbennett@google.com>
    Reviewed-by: Jason Stanko <jstanko@google.com>
    Cr-Commit-Position: refs/heads/main@{#64896}

--- go/src/infra/cros/recovery/config/config_btpeer.go
+++ go/src/infra/cros/recovery/config/config_btpeer.go
@@ -16,6 +16,8 @@ func btpeerRepairPlan() *Plan {
 			"Set state: BROKEN",
 			"Device is pingable",
 			"Device is SSHable",
+			"Fetch btpeer image release config from GCS",
+			"OS image is up to date",
 			"Chameleond version is up to date",
 			"Chameleond service is running",
 			"Device has been rebooted recently",
@@ -76,9 +78,25 @@ func btpeerRepairPlan() *Plan {
 				ExecTimeout: durationpb.New(5 * time.Minute),
 				RunControl:  RunControl_ALWAYS_RUN,
 			},
+			"Chameleond service is running": {
+				Docs: []string{
+					"Checks the status of the chameleond service on the device to see if it is running.",
+					"Fails if the service is not running and attempts recovery by rebooting.",
+				},
+				ExecName: "btpeer_assert_chameleond_service_is_running",
+				RecoveryActions: []string{
+					"Reboot device",
+				},
+				RunControl: RunControl_ALWAYS_RUN,
+			},
+
+			// Chameleond release process actions.
 			"Chameleond version is up to date": {
 				Docs: []string{
 					"Checks if the chameleond version on the btpeer is outdated and updates it if it is.",
+				},
+				Conditions: []string{
+					"Btpeer release process should be chameleond-based",
 				},
 				Dependencies: []string{
 					"Fetch btpeer chameleond release config from GCS",
@@ -147,14 +165,240 @@ func btpeerRepairPlan() *Plan {
 					"Reboot device",
 				},
 			},
-			"Chameleond service is running": {
+
+			// OS image release process actions.
+			"Fetch btpeer image release config from GCS": {
 				Docs: []string{
-					"Checks the status of the chameleond service on the device to see if it is running.",
-					"Fails if the service is not running and attempts recovery by rebooting.",
+					"Retrieves the production btpeer image config from GCS and stores it in the exec state for later reference.",
 				},
-				ExecName: "btpeer_assert_chameleond_service_is_running",
+				ExecName:   "btpeer_image_fetch_release_config",
+				RunControl: RunControl_RUN_ONCE,
+			},
+			"Btpeer release process should be image-based": {
+				Docs: []string{
+					"Passes if the release process this specific btpeer use is image-based.",
+					"The btpeer will be chosen to use the image-based process if it currently has a custom image installed (i.e. existence of an image UUID in scope) or the hostname of the primary dut in this testbed is present in the image release config's NextImageVerificationDutPool.",
+				},
+				ExecName: "btpeer_assert_release_process_matches",
+				ExecExtraArgs: []string{
+					"expected_release_process:image",
+				},
+				RunControl: RunControl_RUN_ONCE,
+			},
+			"Btpeer release process should be chameleond-based": {
+				Docs: []string{
+					"Passes if the release process this specific btpeer use is image-based.",
+					"The btpeer will be chosen to use the chameleond-based process if it is not chosen to use the new image-based release process.",
+				},
+				ExecName: "btpeer_assert_release_process_matches",
+				ExecExtraArgs: []string{
+					"expected_release_process:chameleond",
+				},
+				RunControl: RunControl_RUN_ONCE,
+			},
+			"OS image is up to date": {
+				Docs: []string{
+					"Ensures the OS image installed on the btpeer is the expected OS image release.",
+					"Checks the btpeer scope state for the expected image UUID and installed image UUID and fails if they differ.",
+					"Recovers by installing the expected image release.",
+				},
+				Conditions: []string{
+					"Btpeer release process should be image-based",
+				},
+				Dependencies: []string{
+					"Identify expected image release",
+					"Identify installed image release",
+				},
+				ExecName: "btpeer_image_assert_expected_installed",
 				RecoveryActions: []string{
-					"Reboot device",
+					"Provision OS to expected image release",
+				},
+			},
+			"Identify expected image release": {
+				Docs: []string{
+					"Identifies which btpeer image this specific btpeer should have installed based on the release config and the primary dut's hostname, and then stores it in the scope.",
+				},
+				ExecName:   "btpeer_image_identify_expected",
+				RunControl: RunControl_RUN_ONCE,
+			},
+			"Identify installed image release": {
+				Docs: []string{
+					"Reads the image UUID from the image build info file present on all ChromeOS Raspberry Pi btpeer OS image installations and stores it in the scope.",
+					"Still passes if the image build info file is not present (legacy image assumed), leaving the image UUID unset so that 'Btpeer has expected image release installed' fails and installs the expected image.",
+				},
+				ExecName: "btpeer_image_fetch_installed_uuid",
+				ExecExtraArgs: []string{
+					"allow_legacy_image:true",
+				},
+				RunControl: RunControl_ALWAYS_RUN,
+			},
+			"Provision OS to expected image release": {
+				Docs: []string{
+					"Installs the expected OS image release onto the btpeer.",
+					"This process can take about 30 minutes and includes many device reboots.",
+					"If this fails, a manual recovery of flashing the SD card with a known good image is likely necessary.",
+				},
+				Dependencies: []string{
+					"AB Partition device",
+					"Temp Boot into A partition",
+					"Set permanent boot partition as A",
+					"Download expected OS image release to device",
+					"Flash B partitions with downloaded OS image",
+					"Temp Boot into B partition",
+					"Set permanent boot partition as B",
+					"Verify newly installed image has an image UUID",
+				},
+				ExecName:   "sample_pass",
+				RunControl: RunControl_ALWAYS_RUN,
+			},
+			"AB Partition device": {
+				Docs: []string{
+					"Takes a device with the default partitioning scheme and re-partitions it.",
+					"This action is skipped if the device is already AB partitioned.",
+				},
+				Conditions: []string{
+					"Device is not AB partitioned",
+				},
+				Dependencies: []string{
+					"Verify device has space for new B partitions",
+					"Verify device has standard partition scheme",
+					"Enable initrd",
+					"Shrink rootfs",
+					"Disable initrd",
+					"Create new AB Partitions",
+				},
+				ExecName:   "sample_pass",
+				RunControl: RunControl_ALWAYS_RUN,
+			},
+			"Device is not AB partitioned": {
+				Docs: []string{
+					"Checks that the raspberry pi is not AB partitioned",
+				},
+				ExecName: "btpeer_has_partitions_with_labels",
+				ExecExtraArgs: []string{
+					"labels:BOOT_A,ROOT_A,BOOT_B,ROOT_B",
+					"expect_match:false",
+				},
+				RunControl: RunControl_ALWAYS_RUN,
+			},
+			"Verify device has space for new B partitions": {
+				Docs: []string{
+					"Verifies that the device has space to add the new",
+					"BOOT_B/ROOT_B partitions",
+				},
+				ExecName:   "btpeer_has_partition_room",
+				RunControl: RunControl_ALWAYS_RUN,
+			},
+			"Verify device has standard partition scheme": {
+				Docs: []string{
+					"Verifies that the device has the default raspberry PI partitioning scheme",
+					"consisting of 1 FAT32 boot partition and 1 EXT4 rootfs partition.",
+				},
+				ExecName:   "btpeer_device_has_standard_partitions",
+				RunControl: RunControl_ALWAYS_RUN,
+			},
+			"Enable initrd": {
+				Docs: []string{
+					"Enable initrd on the btpeers so we can add a pre-mount hook for rootfs.",
+				},
+				ExecName:    "btpeer_enable_initrd",
+				RunControl:  RunControl_ALWAYS_RUN,
+				ExecTimeout: &durationpb.Duration{Seconds: 600},
+			},
+			"Shrink rootfs": {
+				Docs: []string{
+					"Shrinks the device rootfs so it can be partitioned.",
+				},
+				ExecName:    "btpeer_shrink_rootfs",
+				RunControl:  RunControl_ALWAYS_RUN,
+				ExecTimeout: &durationpb.Duration{Seconds: 1000},
+			},
+			"Disable initrd": {
+				Docs: []string{
+					"Disables use of initrd on the btpeer.",
+				},
+				ExecName:    "btpeer_disable_initrd",
+				RunControl:  RunControl_ALWAYS_RUN,
+				ExecTimeout: &durationpb.Duration{Seconds: 600},
+			},
+			"Create new AB Partitions": {
+				Docs: []string{
+					"Takes a normal-partitioned raspberry PI partitions it for AB booting.",
+				},
+				ExecName:    "btpeer_partition_device",
+				ExecTimeout: &durationpb.Duration{Seconds: 1000},
+				RunControl:  RunControl_ALWAYS_RUN,
+			},
+			"Temp Boot into A partition": {
+				Docs: []string{
+					"Temporarily boots into the A partition on the device so we can ",
+					"verify it's ok before setting it as the permanent boot device.",
+				},
+				ExecName: "btpeer_temp_boot_into_partition",
+				ExecExtraArgs: []string{
+					"boot_partition_label:BOOT_A",
+				},
+				RunControl:  RunControl_ALWAYS_RUN,
+				ExecTimeout: &durationpb.Duration{Seconds: 600},
+			},
+			"Set permanent boot partition as A": {
+				Docs: []string{
+					"Sets boot partition A as the default boot partition to use on reboot.",
+				},
+				ExecName: "btpeer_set_permanent_boot_partition",
+				ExecExtraArgs: []string{
+					"boot_partition_label:BOOT_A",
+				},
+				RunControl:  RunControl_ALWAYS_RUN,
+				ExecTimeout: &durationpb.Duration{Seconds: 600},
+			},
+			"Download expected OS image release to device": {
+				Docs: []string{
+					"Downloads and decompresses the expected OS image release to the device",
+				},
+				ExecName:    "btpeer_download_image",
+				RunControl:  RunControl_ALWAYS_RUN,
+				ExecTimeout: &durationpb.Duration{Seconds: 960},
+			},
+			"Flash B partitions with downloaded OS image": {
+				Docs: []string{
+					"Flashes a new OS onto the BOOT/ROOT_B partitions",
+				},
+				ExecName:    "btpeer_provision_device",
+				RunControl:  RunControl_ALWAYS_RUN,
+				ExecTimeout: &durationpb.Duration{Seconds: 2400},
+			},
+			"Temp Boot into B partition": {
+				Docs: []string{
+					"Temporarily boots into the B partition on the device so we can ",
+					"verify it's ok before setting it as the permanent boot device.",
+				},
+				ExecName: "btpeer_temp_boot_into_partition",
+				ExecExtraArgs: []string{
+					"boot_partition_label:BOOT_B",
+				},
+				RunControl:  RunControl_ALWAYS_RUN,
+				ExecTimeout: &durationpb.Duration{Seconds: 600},
+			},
+			"Set permanent boot partition as B": {
+				Docs: []string{
+					"Sets boot partition B as the default boot partition to use on reboot.",
+				},
+				ExecName: "btpeer_set_permanent_boot_partition",
+				ExecExtraArgs: []string{
+					"boot_partition_label:BOOT_B",
+				},
+				RunControl:  RunControl_ALWAYS_RUN,
+				ExecTimeout: &durationpb.Duration{Seconds: 600},
+			},
+			"Verify newly installed image has an image UUID": {
+				Docs: []string{
+					"Reads the image UUID from the image build info file present on all ChromeOS Raspberry Pi btpeer OS image installations and stores it in the scope.",
+					"Fails if unable to get the image UUID from the newly installed image release, as all image releases are expected to have UUIDs.",
+				},
+				ExecName: "btpeer_image_fetch_installed_uuid",
+				ExecExtraArgs: []string{
+					"allow_legacy_image:false",
 				},
 				RunControl: RunControl_ALWAYS_RUN,
 			},
