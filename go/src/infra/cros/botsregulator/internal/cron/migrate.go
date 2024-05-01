51f7f4ebfd7a879e222ee56bc888a9c52b91956b
JB Tamas-Leloup <jtamasleloup@google.com>
2024-05-01 19:27:50 +0000

    [botsregulator] Add CloudBots migration cron flow
    TEST=go test ./...
    BUG=b:334890818
    
    Change-Id: I0bddbc19b9613c9bf7f476cd9425afadb00e2f4d

---
+++ go/src/infra/cros/botsregulator/internal/cron/migrate.go
@@ -0,0 +1,23 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package cron
+
+import (
+	"context"
+
+	"go.chromium.org/luci/common/logging"
+
+	"infra/cros/botsregulator/internal/migrator"
+	"infra/cros/botsregulator/internal/regulator"
+)
+
+func Migrate(ctx context.Context, r *regulator.RegulatorOptions) error {
+	m, err := migrator.NewMigrator(ctx, r)
+	if err != nil {
+		return err
+	}
+	logging.Infof(ctx, "migrator: %v", m)
+	return nil
+}
