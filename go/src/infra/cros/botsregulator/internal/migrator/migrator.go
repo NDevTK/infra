561e7dc7dee71a3ff25af6ab8f29ae4376dec7c7
JB Tamas-Leloup <jtamasleloup@google.com>
2024-05-06 22:34:13 +0000

    [botsregulator] Fetch migration.cfg from luci-config
    BUG=b:334890819
    
    Change-Id: Ic9f4aba4f560c5bca6ef8aa926a2353a96fd4ce9

--- go/src/infra/cros/botsregulator/internal/migrator/migrator.go
+++ go/src/infra/cros/botsregulator/internal/migrator/migrator.go
@@ -8,11 +8,17 @@ package migrator
 import (
 	"context"
 
+	"go.chromium.org/luci/common/errors"
 	"go.chromium.org/luci/common/logging"
+	"go.chromium.org/luci/config/cfgclient"
 
 	"infra/cros/botsregulator/internal/clients"
 	"infra/cros/botsregulator/internal/regulator"
+	"infra/cros/botsregulator/protos"
 )
+
+// migrationFile is the the name of the CloudBots migration file.
+const migrationFile = "migration.cfg"
 
 type migrator struct {
 	ufsClient clients.UFSClient
@@ -27,4 +33,15 @@ func NewMigrator(ctx context.Context, r *regulator.RegulatorOptions) (*migrator,
 	return &migrator{
 		ufsClient: uc,
 	}, nil
+}
+
+// GetMigrationConfig fetches CloudBots migration file from luci-config.
+func (m *migrator) GetMigrationConfig(ctx context.Context) (*protos.Migration, error) {
+	logging.Infof(ctx, "fetching migration file: %s \n", migrationFile)
+	out := &protos.Migration{}
+	err := cfgclient.Get(ctx, "services/${appid}", migrationFile, cfgclient.ProtoText(out), nil)
+	if err != nil {
+		return nil, errors.Annotate(err, "could not fetch migration file").Err()
+	}
+	return out, nil
 }
