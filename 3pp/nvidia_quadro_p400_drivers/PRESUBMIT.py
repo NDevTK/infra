6364d9c8d9f0c06ef637838f6817c1f8420f831a
Sam Dockery <samdockery@google.com>
2024-05-09 14:41:04 +0000

    [3pp] Add Nvidia Quadro P400 GPU Driver
    Bug: b/339232683
    Change-Id: Ifce2157ffa2a0ea940f53901890fc13f34aa8755
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5523353
    Reviewed-by: Brian Ryner <bryner@google.com>
    Commit-Queue: Sam Dockery <samdockery@google.com>
    Cr-Commit-Position: refs/heads/main@{#65268}

---
+++ 3pp/nvidia_quadro_p400_drivers/PRESUBMIT.py
@@ -0,0 +1,27 @@
+# Copyright 2024 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+PRESUBMIT_VERSION = '2.0.0'
+
+
+def _GetPCNDEnv(input_api):
+  """Gets the common environment for running PCND tests."""
+  PCND_env = dict(input_api.environ)
+  current_path = input_api.PresubmitLocalPath()
+  PCND_env.update({
+      'PYTHONPATH': current_path,
+      'PYTHONDONTWRITEBYTECODE': '1',
+  })
+  return PCND_env
+
+
+def CheckScriptsUnittests(input_api, output_api):
+  """Runs the unittests in the scripts/ directory."""
+  return input_api.canned_checks.RunUnitTestsInDirectory(
+      input_api,
+      output_api,
+      input_api.PresubmitLocalPath(),
+      [r'^.+_unittest\.py$'],
+      env=_GetPCNDEnv(input_api),
+  )
